dbbbd157b841313cc00d1330d0243ddcdd540304
vpolets <vpolets@gmail.com>
2019-06-12 17:29:35 +0300

    updated tests

--- src/rentalCarCost.test.js
+++ src/rentalCarCost.test.js
@@ -7,12 +7,17 @@ test(`Do not add any discount for less than 3 days`, () => {
     .toBe(80);
 });
 
-test('Should add the discount for 3 and more days of rent', () => {
-  expect(total(4))
-    .toBe(140);
+test('Should add the basic discount from 3 to 6 days of rent', () => {
+  expect(total(3))
+    .toBe(100);
+});
+
+test('Should add the basic discount from 3 to 6 days of rent', () => {
+  expect(total(6))
+    .toBe(220);
 });
 
 test('Should add an additional discount for 7 and more days of rent', () => {
-  expect(total(10))
-    .toBe(350);
+  expect(total(7))
+    .toBe(230);
 });
