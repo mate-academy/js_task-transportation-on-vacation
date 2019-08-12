63f42433a4706cc5a17bdeeb527b2f3baf1a33b5
Illia Bezkorovainyi <illia.bezkorovainyi@heathmont.net>
2019-08-12 19:33:59 +0300

    changes after review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -23,10 +23,11 @@ function calculateRentalCost(days) {
   const rentOrdinary = 40;
   const threeDaysOff = 20;
   const sevenDaysOff = 50;
+
   if (days >= 7) {
     return (rentOrdinary * days) - sevenDaysOff;
   } else if (days >= 3 && days < 7) {
-    return (rentOrdinary * days) - threeDaysOff;
+    return rentOrdinary * days - threeDaysOff;
   }
   return days * rentOrdinary;
 }
