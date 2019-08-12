ccf19dd91d9851304ca1ee736442fc719302dba2
Viktoriia Tsykun <vikatsikun@gmail.com>
2019-08-12 15:05:04 +0300

    Comments fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -23,6 +23,7 @@ function calculateRentalCost(days) {
   const dayRent = 40;
   const discount7Days = 50;
   const discount3Days = 20;
+
   if (days >= 7) {
     return dayRent * days - discount7Days;
   } else if (days >= 3 && days < 7) {
