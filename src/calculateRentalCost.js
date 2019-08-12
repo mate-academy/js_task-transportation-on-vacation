03f8cff00376357d228254a5d5b8db785af3120d
KseniiaMazan <ksusamazan@gmail.com>
2019-08-12 14:59:47 +0300

    code style is fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -23,10 +23,13 @@ function calculateRentalCost(days) {
   const discountFor3Days = 20;
   const discoutnFor7Days = 50;
   const carRentalPrice = 40;
+
   if (days >= 3 && days < 7) {
     return days * carRentalPrice - discountFor3Days;
   } else if (days >= 7) {
     return days * carRentalPrice - discoutnFor7Days;
-  } return days * carRentalPrice;
+  }
+
+  return days * carRentalPrice;
 }
 module.exports = calculateRentalCost;
