13a498b688e5237d43a82eac85b030b2bd3eaaa3
vladyslav223 <vladyslav223@gmail.com>
2019-08-13 00:00:09 +0300

    conditions

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -25,7 +25,13 @@ function calculateRentalCost(days) {
   const totalPrice = oneDayPrice * days;
   const days3discount = 20;
   const days7discount = 50;
-  return totalPrice - (days < 3 ? 0 : days < 7 ? days3discount : days7discount);
+  if (days < 3) {
+    return totalPrice;
+  } else if (days < 7) {
+    return totalPrice - days3discount;
+  } else {
+    return totalPrice - days7discount;
+  }
 }
 
 module.exports = calculateRentalCost;
