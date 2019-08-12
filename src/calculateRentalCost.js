2f653f8865410fd48b8628757a3887e0a4e966c5
Miroslava <777shadowpriest777@gmail.com>
2019-08-12 21:02:18 +0300

    fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -21,11 +21,15 @@
  */
 function calculateRentalCost(days) {
   let totalCost = days * 40;
+  const weekDiscount = 50;
+  const twoDaysDiscount = 20;
+
   if (days >= 7) {
-    totalCost -= 50;
+    totalCost -= weekDiscount;
   } else if (days > 2) {
-    totalCost -= 20;
+    totalCost -= twoDaysDiscount;
   }
+
   return totalCost;
 }
 
