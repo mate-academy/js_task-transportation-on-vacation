a93f0df5bc62dd6dd2de61edde6e65a8202b4842
Rafler <amuronyk@gmail.com>
2019-08-13 10:22:02 +0300

    correcting mistakes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,12 +20,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const cost = 40;
+  const bigOff = 50;
+  const smallOff = 20;
   if (days >= 3 && days < 7) {
-    return days * 40 - 20;
+    return days * cost - smallOff;
   } else if (days >= 7) {
-    return days * 40 - 50;
+    return days * cost - bigOff;
   }
-  return days * 40;
+
+  return days * cost;
 }
 
 module.exports = calculateRentalCost;
