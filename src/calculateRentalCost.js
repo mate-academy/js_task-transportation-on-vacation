d002e92e7fc4c4a5dbbab32ed78ecca69382e022
Volodsher <volodsher@gmail.com>
2019-08-13 12:25:18 +0300

    after correction

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -22,13 +22,19 @@
 function calculateRentalCost(days) {
   // write code here
   let totalCost = 0;
+  const daylyPrice = 40;
+  const price = days * daylyPrice;
+  const discount3Days = 20;
+  const discount7Days = 50;
+
   if (days >= 3 && days < 7) {
-    totalCost = days * 40 - 20;
+    totalCost = price - discount3Days;
   } else if (days >= 7) {
-    totalCost = days * 40 - 50;
+    totalCost = price - discount7Days;
   } else {
-    totalCost = days * 40;
+    totalCost = price;
   }
+
   return totalCost;
 }
 
