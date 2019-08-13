023fe9a116ecae69df8caef59bea4a112ccdc3b4
Khrystyna Styslo <khrystyna.styslo@gmail.com>
2019-08-13 10:42:11 +0300

    fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,11 +20,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const costPerDay = 40;
+  const discountThree = 20;
+  const discountSeven = 50;
   if (days >= 3 && days < 7) {
-    return days * 40 - 20;
+    return days * costPerDay - discountThree;
   } else if (days >= 7) {
-    return days * 40 - 50;
-  } return days * 40;
+    return days * costPerDay - discountSeven;
+  }
+  return days * costPerDay;
 }
 
 module.exports = calculateRentalCost;
