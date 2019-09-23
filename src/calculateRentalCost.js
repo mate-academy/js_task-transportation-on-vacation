d017690e457e5e18a8a034dbde6248604993c3b6
Shurkodr <sashaglazunov2@gmail.com>
2019-09-23 15:13:13 +0300

    passed local tests

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -21,6 +21,15 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let price;
+  if (days >= 7) {
+    price = days * 40 - 50;
+  } else if (days >= 3) {
+    price = days * 40 - 20;
+  } else {
+    price = days * 40;
+  }
+  return price;
 }
 
 module.exports = calculateRentalCost;
