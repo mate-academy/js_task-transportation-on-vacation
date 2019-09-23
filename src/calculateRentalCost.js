44741bad9b5f3f78c3520d570394349744e85a3a
lubremets <lubremets@gmail.com>
2019-09-23 12:56:59 +0300

    Solution Transportation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,7 +20,13 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  if (days < 3) {
+    return days * 40;
+  }
+  if (days < 7) {
+    return days * 40 - 20;
+  }
+  return days * 40 - 50;
 }
 
 module.exports = calculateRentalCost;
