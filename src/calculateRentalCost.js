c785bb5a314992fe345ec7f48284cc5953e15466
nikakarelets <v.karelets@gmail.com>
2019-09-23 15:44:40 +0300

    Fix vars names

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -21,12 +21,14 @@
  */
 function calculateRentalCost(days) {
   const totalCost = days * 40;
+  const sevenMoreOff = 50;
+  const threeMoreOff = 20;
   if (days < 3) {
     return totalCost;
   } else if (days >= 7) {
-    return (totalCost) - 50;
+    return totalCost - sevenMoreOff;
   } else if (days >= 3) {
-    return (totalCost) - 20;
+    return totalCost - threeMoreOff;
   }
 }
 
