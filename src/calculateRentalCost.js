eedabeb4269511fb82f91db485901a602c3adab0
VitaliyStarodub <starodub.vital@gmail.com>
2019-10-01 22:49:25 +0300

    fs_on_aug19_vitalii

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -21,9 +21,17 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  const rentalPerDay = days * 40;
-  return days >= 7 ? rentalPerDay - 50
-    : days >= 3 ? rentalPerDay - 20 : rentalPerDay;
+  const DAY_RENT = 40;
+  const DISCOUNT_FOR_3_DAYS_RENT = 20;
+  const DISCOUNT_FOR_7_DAYS_RENT = 50;
+  const rentalPerDay = days * DAY_RENT;
+  if (days >= 7) {
+    return rentalPerDay - DISCOUNT_FOR_7_DAYS_RENT;
+  } else if (days >= 3) {
+    return rentalPerDay - DISCOUNT_FOR_3_DAYS_RENT;
+  } else {
+    return rentalPerDay;
+  }
 }
 
 module.exports = calculateRentalCost;
