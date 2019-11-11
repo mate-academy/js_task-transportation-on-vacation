abee9df7aeef30e2ddc7811d26e1ee21fcf17496
ksukhaldun <kseniya.khaldun@gmail.com>
2019-11-11 11:56:32 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,13 +20,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const cost = 40;
+  const discount50 = 50;
+  const discount20 = 20;
+  const total = days * cost;
   if (days >= 7) {
-    return days * 40 - 50;
+    return total - discount50;
   } else if (days >= 3) {
-    return days * 40 - 20;
-  } else {
-    return days * 40;
-  }
+    return total - discount20;
+  } return total;
 }
 
 module.exports = calculateRentalCost;
