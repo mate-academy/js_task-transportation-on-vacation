5039ce12f4188b9f84b014830d3f358cfb0f85bf
Liliya Taran <liliyataran2017@gmail.com>
2019-11-11 11:45:44 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -21,13 +21,16 @@
  */
 function calculateRentalCost(days) {
   const rent = 40;
+  const days7Discount = 50;
+  const days3Discount = 20;
+  const totalPrice = rent * days;
 
   if (days >= 7) {
-    return rent * days - 50;
+    return rent * days - days7Discount;
   } else if (days >= 3) {
-    return rent * days - 20;
+    return rent * days - days3Discount;
   } else {
-    return rent * days;
+    return totalPrice;
   }
 }
 
