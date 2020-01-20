12f3babfe795978f14c4415598dd63953f31b8bd
Vova Pyvovar <ditinapivo@gmail.com>
2020-01-20 18:14:10 +0200

    add two const

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,13 +20,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const cost = 40;
-  let total = cost * days;
+  const dayCost = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+  let total = dayCost * days;
 
-  if (days >= 3 && days <= 6) {
-    total = total - 20;
+  if (days >= 3 && days < 7) {
+    total = total - smallDiscount;
   } else if (days >= 7) {
-    total = total - 50;
+    total = total - bigDiscount;
   }
 
   return total;
