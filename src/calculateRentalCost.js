46ecebb0853d739629e6f12014a36fbf7cbc0a57
Roma <romakoder@gmail.com>
2020-01-20 16:28:56 +0200

    fixed variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -27,9 +27,9 @@ function calculateRentalCost(days) {
   if (days < 3) {
     return days * price;
   } else if (days >= 3 && days <= 6) {
-    return days * 40 - discountSmall;
+    return days * price - discountSmall;
   } else if (days >= 7) {
-    return days * 40 - discountBig;
+    return days * price - discountBig;
   }
 }
 
