f95a0c79238dee12385a66d75f155bc22d003e7d
Посіпака <minion@MacBook-Air-Posipaka.local>
2020-01-20 13:59:12 +0200

    add variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -21,10 +21,14 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const costOneDay = 40;
+  const discountMoreThreeDays = 20;
+  const discountMoreSevenDays = 50;
+
   if (days >= 7) {
-    return (days * 40 - 50);
+    return (days * costOneDay - discountMoreSevenDays);
   } else if (days >= 3) {
-    return (days * 40 - 20);
+    return (days * costOneDay - discountMoreThreeDays);
   } else {
     return (days * 40);
   }
