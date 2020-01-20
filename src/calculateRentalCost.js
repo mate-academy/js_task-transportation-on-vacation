9ecfa06cc0e55d8fbc03622529a9011f5f2b01d6
ronyshchenko <roma.onyshchenko@gmail.com>
2020-01-20 16:08:29 +0200

    two

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -21,14 +21,16 @@
  */
 function calculateRentalCost(days) {
   const rentInDay = 40;
+  const discount = 20;
+  const maxDiscount = 50;
   const rentPriceBase = days * rentInDay;
 
   if (days < 3) {
     return rentPriceBase;
   } else if (days < 7) {
-    return rentPriceBase - 20;
+    return rentPriceBase - discount;
   } else {
-    return rentPriceBase - 50;
+    return rentPriceBase - maxDiscount;
   }
 }
 
