f21c67135b9674f97222428b11a3c38a2dfee46f
Actireon <eheieiei37@gmail.com>
2020-01-20 16:31:48 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,7 +20,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentCarPriсe = 40;
+  const threeMoreDays = 20;
+  const sevenMoreDays = 50;
+
+  if (days >= 7) {
+    return days * rentCarPriсe - sevenMoreDays;
+  } else if (days >= 3) {
+    return days * rentCarPriсe - threeMoreDays;
+  } else {
+    return days * rentCarPriсe;
+  }
 }
 
 module.exports = calculateRentalCost;
