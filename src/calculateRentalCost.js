35628081660564e2c91c1f8667ebc31357f892e1
Roma <romakoder@gmail.com>
2020-01-20 15:42:21 +0200

    add variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,12 +20,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const price = 40;
+  const discountSmall = 20;
+  const discountBig = 50;
+
   if (days < 3) {
-    return days * 40;
+    return days * price;
   } else if (days >= 3 && days <= 6) {
-    return days * 40 - 20;
+    return days * 40 - discountSmall;
   } else if (days >= 7) {
-    return days * 40 - 50;
+    return days * 40 - discountBig;
   }
 }
 
