93c7c436a9bf457e38342a3956c8781d41b8e83b
Viacheslav <slavic.lavrenyuk@gmail.com>
2020-01-20 16:03:01 +0200

    Add Const

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,12 +20,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const dayRent = 40;
+  const offSevenDays = 50;
+  const offThreeDays = 20;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * dayRent - offSevenDays;
   } else if (days >= 3) {
-    return days * 40 - 20;
+    return days * dayRent - offThreeDays;
   } else {
-    return days * 40;
+    return days * dayRent;
   }
 }
 
