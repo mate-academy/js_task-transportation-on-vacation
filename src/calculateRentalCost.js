20a287b0f2dbc4e443f9a8c6ef408be5b906b754
Yaroslav Magrelo <slava.magrelo@gmail.com>
2020-01-20 18:40:42 +0200

    add var for discount

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -22,15 +22,17 @@
 function calculateRentalCost(days) {
   const rentalCostPerDay = 40;
   const rentalCostPerPeriod = days * rentalCostPerDay;
+  const basicDiscount = 20;
+  const premiumDiscount = 50;
 
   if (days === 0) {
     return 0;
   } else if (days < 3) {
     return rentalCostPerPeriod;
   } else if (days > 2 && days < 7) {
-    return rentalCostPerPeriod - 20;
+    return rentalCostPerPeriod - basicDiscount;
   } else {
-    return rentalCostPerPeriod - 50;
+    return rentalCostPerPeriod - premiumDiscount;
   }
 }
 
