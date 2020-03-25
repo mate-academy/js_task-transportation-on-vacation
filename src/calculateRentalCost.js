7aec9a89d629f36087aababad229534254299031
Rustam Khananov <antmine2017@gmail.com>
2020-03-25 14:08:06 +0200

    fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,12 +20,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const disc3 = 20;
+  const disc7 = 50;
+  const price = 40;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * price - disc7;
   } else if (days >= 3) {
-    return days * 40 - 20;
+    return days * price - disc3;
   } else {
-    return days * 40;
+    return days * price;
   };
 }
 
