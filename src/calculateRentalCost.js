58d1c6c2032017861c604ab23e625a7d1ae04d97
Yarik1993 <makarov1603@icloud.com>
2020-03-25 15:34:30 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,12 +20,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const rentCosts = 40;
+  const smallDiscount = 20;
+  const discount = 50;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * rentCosts - discount;
   } else if (days >= 3) {
-    return days * 40 - 20;
+    return days * rentCosts - smallDiscount;
   } else {
-    return days * 40;
+    return days * rentCosts;
   }
 }
 
