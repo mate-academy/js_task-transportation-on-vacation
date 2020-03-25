fe215601b79d794be3da32eedf353d7bc38d202a
Yevhenii <DrozhzhynY>
2020-03-25 12:39:44 +0200

    add names for nums

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,20 +20,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const dayCarCosts = 40;
+  const threeDaysMoreDiscount = 20;
+  const sevenDaysMoreDiscount = 50;
+
   if (days <= 0) {
     return 'Введите число бльше 0';
   }
 
   if (days <= 2) {
-    return days * 40;
+    return days * dayCarCosts;
   }
 
   if (days >= 3 && days <= 6) {
-    return days * 40 - 20;
+    return days * dayCarCosts - threeDaysMoreDiscount;
   }
 
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * dayCarCosts - sevenDaysMoreDiscount;
   }
 }
 
