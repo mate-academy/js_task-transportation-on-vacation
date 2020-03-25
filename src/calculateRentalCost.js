d210d3c43261c5e5ec2862a3ff9dff5d0cba8a3c
Oleg <fasfas7971@gmail.com>
2020-03-25 11:59:14 +0200

    Fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -23,17 +23,15 @@ function calculateRentalCost(days) {
   const countRent = 40;
   let allPrice = 0;
 
-  if (days > 1) {
-    if (days >= 7) {
-      allPrice = countRent * days - 50;
-    } else if (days >= 3 && days <= 7) {
-      allPrice = countRent * days - 20;
-    } else {
-      allPrice = countRent * days;
-    }
-
-    return allPrice;
+  if (days >= 7) {
+    allPrice = countRent * days - 50;
+  } else if (days >= 3) {
+    allPrice = countRent * days - 20;
+  } else {
+    allPrice = countRent * days;
   }
+
+  return allPrice;
 }
 
 module.exports = calculateRentalCost;
