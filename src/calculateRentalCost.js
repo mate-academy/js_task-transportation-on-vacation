83271814fc903963aaf64303457b2cf6e943f085
Vladimir-Beliy <vovaazick@gmail.com>
2020-03-25 13:05:53 +0200

    fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -20,15 +20,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const costFor1Day = 40;
+  const costForAllDays = costFor1Day * days;
+  const discountFor7Days = 50;
+  const discountFor3Days = 20;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return costForAllDays - discountFor7Days;
+  } else if (days >= 3) {
+    return costForAllDays - discountFor3Days;
   }
 
-  if (days >= 3) {
-    return days * 40 - 20;
-  }
-
-  return days * 40;
+  return costForAllDays;
 }
 
 module.exports = calculateRentalCost;
