975ea9644884fc46b04583db019b8ebc3fcad905
nazmeln <nazik100378@gmail.com>
2020-05-20 21:26:43 +0300

    solved

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,20 +1,19 @@
 'use strict';
 
 /**
- * After a hard quarter in the office you decide to get some rest on a vacation.
-* So you will book a flight for you
-  and your family and try to leave all the mess behind you.
+ * After a hard quarter in the office you decide to get some rest
+ * on a vacation. So you will book a flight for you and your girlfriend
+ * and try to leave all the mess behind you.
  *
- * You will need a rental car in order for you to get around in your vacation.
- * The manager of the car rental makes you some good offers.
+ * You will need a rental car in order for you to get around in your
+ * vacation. The manager of the car rental makes you some good offers.
  *
- * Every day you rent the car costs $40.
- * If you rent the car for 7 or more days, you get $50 off your total.
- * Alternatively, if you rent the car for 3 or more days,
- * you get $20 off your total.
+ * Every day you rent the car costs $40. If you rent the car for 7 or
+ * more days, you get $50 off your total. Alternatively, if you rent
+ * the car for 3 or more days, you get $20 off your total.
  *
- * Implement calculateRentalCost function
- * which returns the total amount for different count of days.
+ * Implement calculateRentalCost function which returns the total
+ * amount for different count of days.
  *
  * Examples:
  *  - calculateRentalCost(1) === 40
@@ -27,13 +26,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const priceOneDay = 40;
+  const rentalCost = days * priceOneDay;
+  const saleFor3Days = 20;
+  const saleFor7Days = 50;
+
   if (days < 3) {
     return days * 40;
-  } else if (days >= 3 && days <= 6) {
-    return days * 40 - 20;
-  } else if (days <= 7) {
-    return days * 40 - 50;
+  } else if (days < 7) {
+    return rentalCost - saleFor3Days;
+  } else {
+    return rentalCost - saleFor7Days;
   }
 }
 
