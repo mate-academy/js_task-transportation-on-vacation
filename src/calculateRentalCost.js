7a4c34d04e7b1767f407a611cbeda550d274125b
Виктор Зинченко <zenia1@i.ua>
2020-05-21 22:27:06 +0300

    newSolution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,18 +1,18 @@
 'use strict';
 
 function calculateRentalCost(days) {
-  const priceForDay = 40;
-  const smalldiscount = 20;
-  const BigDiscount = 50;
+  const pricePerDay = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
   if (days < 3) {
-    return days * priceForDay;
-  } ;
-
-  if (days >= 3 && days < 7) {
-    return days * priceForDay - smalldiscount;
+    return days * pricePerDay;
   };
 
-  return days * priceForDay - BigDiscount;
+  if (days >= 3 && days < 7) {
+    return days * pricePerDay - smallDiscount;
+  };
+
+  return days * pricePerDay - bigDiscount;
 }
 module.exports = calculateRentalCost;
