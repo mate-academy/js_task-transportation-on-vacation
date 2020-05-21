eb041e6de063dc9cc705aa13c0c9ccdd13678fff
Stanislav <stasusss07@gmail.com>
2020-05-21 12:04:35 +0300

    fix vars

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,12 +1,18 @@
 'use strict';
 
 function calculateRentalCost(days) {
-  if (days < 3) {
-    return days * 40;
-  } else if (days >= 3 && days < 7) {
-    return days * 40 - 20;
-  } else if (days >= 7) {
-    return days * 40 - 50;
+  const dayCost = 40;
+  const daysForSmallDiscount = 3;
+  const daysForBigDiscount = 7;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
+  if (days < daysForSmallDiscount) {
+    return days * dayCost;
+  } else if (days >= daysForSmallDiscount && days < daysForBigDiscount) {
+    return days * dayCost - smallDiscount;
+  } else if (days >= daysForBigDiscount) {
+    return days * dayCost - bigDiscount;
   }
 }
 
