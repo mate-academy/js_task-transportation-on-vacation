623af02e8eeebb5de9ff79bce2a13996fc9236e0
Denys <gryshko_denys@gmail.com>
2020-05-21 11:55:24 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -2,16 +2,20 @@
 
 /**
  * After a hard quarter in the office you decide to get some rest on a vacation.
- * So you will book a flight for you and your family and try to leave all the mess behind you.
- * 
+ * So you will book a flight for you and your family
+ *  and try to leave all the mess behind you.
+ *
  * You will need a rental car in order for you to get around in your vacation.
  * The manager of the car rental makes you some good offers.
- * 
- * Every day you rent the car costs $40. If you rent the car for 7 or more days, you get $50 off your total.
- * Alternatively, if you rent the car for 3 or more days, you get $20 off your total.
  *
- * Implement calculateRentalCost function which returns the total amount for different count of days.
- * 
+ * Every day you rent the car costs $40.
+ *  If you rent the car for 7 or more days, you get $50 off your total.
+ * Alternatively, if you rent the car for 3 or more days,
+ *  you get $20 off your total.
+ *
+ * Implement calculateRentalCost function
+ *  which returns the total amount for different count of days.
+ *
  * Examples:
  *  - calculateRentalCost(1) === 40
  *  - calculateRentalCost(3) === 100
@@ -23,7 +27,20 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCost = 40;
+  const threeDaysBonus = 20;
+  const sevenDaysBonus = 50;
+  const firstBonusStart = 3;
+  const secondBonusStart = 7;
+  let rentPrice = days * rentalCost;
+
+  if (days >= firstBonusStart && days < secondBonusStart) {
+    rentPrice -= threeDaysBonus;
+  } else if (days >= secondBonusStart) {
+    rentPrice -= sevenDaysBonus;
+  }
+
+  return rentPrice;
 }
 
 module.exports = calculateRentalCost;
