0855d78ac02365f92d06ea4f555d97d7a7d83df9
Bohdan Polienko <mignatov756@gmail.com>
2020-05-21 20:46:11 +0300

    Fixes after code review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,7 +7,24 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let result = 0;
+  const costPerDay = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
+  if (days < 3) {
+    result += days * costPerDay;
+  }
+
+  if (days >= 3 && days < 7) {
+    result += days * costPerDay - smallDiscount;
+  }
+
+  if (days >= 7) {
+    result += days * costPerDay - bigDiscount;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
