f1f71ad0c3164d9ca4132ee91351f25d934ddec9
aleksey-10 <alexcare1337@gmail.com>
2020-05-21 11:50:40 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,10 +4,12 @@ function calculateRentalCost(days) {
   let cost = 40 * days;
   const discount1 = 20;
   const discount2 = 50;
+  const discountDays1 = 3;
+  const discountDays2 = 7;
 
-  if (days > 2 && days < 7) {
+  if (days >= discountDays1 && days < discountDays2) {
     cost -= discount1;
-  } else if (days > 6) {
+  } else if (days >= discountDays2) {
     cost -= discount2;
   }
 
