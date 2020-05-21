be775e20cd33e8f5780035a0d3c3a26656e3027f
Glib Rulev <velzepooz@gmail.com>
2020-05-21 11:08:18 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,7 +7,15 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentPrice = 40;
+
+  if (days >= 7) {
+    return (days * rentPrice) - 50;
+  } else if (days >= 3) {
+    return (days * rentPrice) - 20;
+  } else {
+    return days * rentPrice;
+  }
 }
 
 module.exports = calculateRentalCost;
