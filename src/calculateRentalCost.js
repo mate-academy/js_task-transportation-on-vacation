3a84b851ea6bea2fd5307709b36f3e80c5ad8e67
VlaDinter <vladyslavdinter@gmail.com>
2020-05-22 10:53:32 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,11 +3,15 @@
 function calculateRentalCost(days) {
   const dayRent = 40;
   let discount = 0;
+  const minDiscount = 20;
+  const maxDiscount = 20;
+  const minDaysDiscount = 20;
+  const maxDaysDiscount = 20;
 
-  if (days >= 7) {
-    discount = 50;
-  } else if (days >= 3) {
-    discount = 20;
+  if (days >= maxDaysDiscount) {
+    discount = maxDiscount;
+  } else if (days >= minDaysDiscount) {
+    discount = minDiscount;
   }
 
   return dayRent * days - discount;
