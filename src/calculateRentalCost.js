f9ec629561cd3e655bffd53cd860a04f801f02da
movchko <lemberg.my.city@gmail.com>
2020-08-03 18:22:51 +0300

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -26,9 +26,18 @@
  *
  * @return {number}
  */
+let result = 0;
 
 function calculateRentalCost(days) {
-  // write code here
+  result = days * 40;
+
+  if (days > 2 && days < 7) {
+    result -= 20;
+  } else if (days > 6) {
+    result -= 50;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
