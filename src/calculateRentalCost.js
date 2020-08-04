02105178a13cbb5afe820e4c91b7d910f05c8414
Alisa <cheshyr.alisa@gmail.com>
2020-08-04 18:15:27 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -32,14 +32,10 @@ function calculateRentalCost(days) {
 
   if (days < 3) {
     return days * 40;
-  }
-
-  if (days >= 3 && days < 7) {
-    return days * 40 - 20;
-  }
-
-  if (days >= 7) {
-    return days * 40 - 50;
+  } else if (days < 7) {
+    return (days * 40) - 20;
+  } else {
+    return (days * 40) - 50;
   }
 }
 
