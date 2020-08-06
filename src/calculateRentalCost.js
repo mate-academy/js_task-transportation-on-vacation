6e2157d7a0bfb45477930e0cf2042a205e99dd9b
Oleksandr Rohatnov <mr.rogatnov@gmail.com>
2020-08-06 08:55:58 +0300

    fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,15 @@
  */
 
 function calculateRentalCost(days) {
+  const priceDay = days * 40;
+
   if (days < 3) {
-    return (days * 40);
+    return priceDay;
   } else if (days >= 3 && days < 7) {
-    return (days * 40) - 20;
-  } else {
-    return (days * 40) - 50;
+    return priceDay - 20;
   }
+
+  return priceDay - 50;
 }
 
 module.exports = calculateRentalCost;
