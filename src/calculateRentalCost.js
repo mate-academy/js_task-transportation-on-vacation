edf8d3b34a09bf3bb83b530bf739b646bc32d78e
Tania Omelko <tanyaomelko39@gmail.com>
2020-09-21 20:48:17 +0300

    Ovva :(

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,11 +28,11 @@
  */
 
 function calculateRentalCost(days) {
-  if (days >= 1 && days < 3) {
+  if (days < 3) {
     return days * 40;
   }
 
-  if (days >= 3 && days < 7) {
+  if (days < 7) {
     return days * 40 - 20;
   }
 
