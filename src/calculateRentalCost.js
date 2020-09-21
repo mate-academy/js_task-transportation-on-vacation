e50b2445a7a1b431b49fd53f71653b28c243dc21
KhrystynaYelyseyeva <play1409@gmail.com>
2020-09-21 19:57:27 +0300

    fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -32,13 +32,11 @@ function calculateRentalCost(days) {
     return days * 40;
   }
 
-  if (days >= 3 && days < 7) {
+  if (days < 7) {
     return days * 40 - 20;
   }
 
-  if (days >= 7) {
-    return days * 40 - 50;
-  }
+  return days * 40 - 50;
 }
 
 module.exports = calculateRentalCost;
