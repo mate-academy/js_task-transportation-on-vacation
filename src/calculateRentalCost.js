c4ae8f857486b3970f2d42732d18bab61f57b260
Alona <aionovaionova@gmail.com>
2020-09-21 11:16:08 +0300

    changed without ternany

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,13 @@
  */
 
 function calculateRentalCost(days) {
-  return days < 3 ? days * 40 : days < 7 ? (days * 40) - 20 : (days * 40) - 50;
+  if (days < 3) {
+    return days * 40;
+  } else if (days < 7) {
+    return (days * 40) - 20;
+  } else {
+    return (days * 40) - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
