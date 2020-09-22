5f7eb997824f6b32a5be5562067b9d94e5814740
Olexiy Bashmakov <olexiybashmakov@gmail.com>
2020-09-22 14:00:06 +0300

    added variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,20 @@
  */
 
 function calculateRentalCost(days) {
+  const discountForWeek = 50;
+  const discountFor3Days = 20;
+  const dayCost = 40;
+
   if (days < 3) {
-    return days * 40;
+    return days * dayCost;
   }
 
   if (days >= 3 && days < 7) {
-    return (days * 40) - 20;
+    return (days * dayCost) - discountFor3Days;
   }
 
   if (days >= 7) {
-    return (days * 40) - 50;
+    return (days * dayCost) - discountForWeek;
   }
 }
 
