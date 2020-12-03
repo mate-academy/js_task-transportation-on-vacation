f5c3270ff216fddcaf7ad9579fb98700727f8bc7
gorzer <gorzer.rostyslav@gmail.com>
2020-12-03 13:30:02 +0200

    New Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,14 @@
 
 function calculateRentalCost(days) {
   // write code here
-  const carCoasts = 40;
+  const rentalPrice = 40;
 
   if (days >= 7) {
-    return (days * carCoasts) - 50;
+    return (days * rentalPrice) - 50;
   } else if (days >= 3) {
-    return (days * carCoasts) - 20;
+    return (days * rentalPrice) - 20;
   } else {
-    return carCoasts * days;
+    return rentalPrice * days;
   }
 }
 
