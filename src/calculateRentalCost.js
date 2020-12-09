84a93ab7e1f267da5ba6f85945380e20950e0f84
DarMatt <daryamatviienko@gmail.com>
2020-12-09 17:21:32 +0200

    delete else

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,9 +30,13 @@
 function calculateRentalCost(days) {
   if (days < 3) {
     return days * 40;
-  } else if (days >= 3 && days < 7) {
+  }
+
+  if (days >= 3 && days < 7) {
     return days * 40 - 20;
-  } else if (days >= 7) {
+  }
+
+  if (days >= 7) {
     return days * 40 - 50;
   }
 }
