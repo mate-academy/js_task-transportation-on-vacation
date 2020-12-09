5c25427fb5c6f27169a581dbf68277511f70a17f
5Mountains <V.Piatyhor@gmail.com>
2020-12-09 18:23:27 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,19 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const price = 40;
+  const discount = 20;
+  const maxDiscount = 50;
+
+  if (days < 3) {
+    return (price * days);
+  }
+
+  if (days >= 3 && days < 7) {
+    return (days * price - discount);
+  }
+
+  return (days * price - maxDiscount);
 }
 
 module.exports = calculateRentalCost;
