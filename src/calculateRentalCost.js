2b477297fb41440d8ce34bfc75295c614af6a41e
Ihor Drul <druleger@gmail.com>
2021-01-24 16:27:50 +0200

    My solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,15 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const cost = days * 40;
+
+  if (days < 3) {
+    return cost;
+  } else if (days >= 3 && days < 7) {
+    return cost - 20;
+  } else {
+    return cost - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
