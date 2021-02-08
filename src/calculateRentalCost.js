a83dc2a849a2e3a249441eca4cea8cb8469532c3
Yevhenii Yashnyi <y.yashnyi@gmail.com>
2021-02-08 13:23:43 +0200

    added variables2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,19 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  const oneDayPrice = 40;
+  const oneDayCost = 40;
   const smallDiscount = 20;
-  const bigDiscount = 40;
+  const bigDiscount = 50;
   const daysForSmallDiscount = 3;
   const daysForBigDiscount = 7;
 
   if (days >= daysForBigDiscount) {
-    return days * oneDayPrice - bigDiscount;
+    return days * oneDayCost - bigDiscount;
   } else if (days >= daysForSmallDiscount) {
-    return days * oneDayPrice - smallDiscount;
-  } else {
-    return days * oneDayPrice;
+    return days * oneDayCost - smallDiscount;
   }
+
+  return days * oneDayCost;
 }
 
 module.exports = calculateRentalCost;
