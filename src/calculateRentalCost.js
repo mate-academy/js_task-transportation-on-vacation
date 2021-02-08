552076941f59fef4523194e92e4045a5cf962884
morozoleh <olegmoroz93@gmail.com>
2021-02-08 12:45:57 +0200

    it has been changed after student reviewing

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,8 +30,7 @@
 function calculateRentalCost(days) {
   if (days < 3) {
     return days * 40;
-  } else if (
-    days < 7) {
+  } else if (days < 7) {
     return days * 40 - 20;
   } else {
     return days * 40 - 50;
