5177804fda52a6f0b1fc67cf19568adf58c76369
Sofiia Asieieva <sofiia.asieieva@gmail.com>
2021-02-09 12:00:46 +0200

    created function calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,20 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const costPerDay = 40;
+  const costWithoutDiscount = costPerDay * days;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
+  if (days >= 7) {
+    return costWithoutDiscount - bigDiscount;
+  }
+
+  if (days >= 3) {
+    return costWithoutDiscount - smallDiscount;
+  }
+
+  return costWithoutDiscount;
 }
 
 module.exports = calculateRentalCost;
