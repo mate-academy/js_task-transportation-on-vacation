f54be94dab8b7652aa607e172e9c64e5b02c1363
Alex Fedorchenko <mralex.fed@gmail.com>
2021-03-11 14:32:45 +0200

    Fixes after code review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,5 +1,4 @@
 'use strict';
-
 /**
  * After a hard quarter in the office you decide
  * to get some rest on a vacation.
@@ -9,18 +8,17 @@
  * You will need a rental car in order for you to get around in your vacation.
  * The manager of the car rental makes you some good offers.
  *
- * Every day you rent the car costs $40.
- * If you rent the car for 7 or more days, you get $50 off your total.
- * Alternatively, if you rent the car for 3 or more days,
- * you get $20 off your total.
+ * Every day you rent the car costs $dailyCost.
+ * Alternatively, if you rent the car for bigWeekend or more days,
+ * you get $minSale off your total.
  *
  * Implement calculateRentalCost function
  * which returns the total amount for different count of days.
  *
  * Examples:
- *  - calculateRentalCost(1) === 40
- *  - calculateRentalCost(3) === 100
- *  - calculateRentalCost(7) === 230
+ *  - calculateRentalCost(1) === dailyCost
+ *  - calculateRentalCost(bigWeekend) === 100
+ *  - calculateRentalCost(weekend) === 2bigWeekend0
  *
  * @param {number} days
  *
@@ -28,11 +26,24 @@
  */
 
 function calculateRentalCost(days) {
-  const result = (days >= 3 && days < 7)
-    ? days * 40 - 20 : (days >= 7)
-      ? days * 40 - 50 : (days < 3 && days > 0)
-        ? days * 40 : 0;
+  const dailyCost = 40;
+  const minSale = 20;
+  const maxSale = 50;
+  const bigWeekend = 3;
+  const weekend = 7;
 
-  return result;
+  if (days >= bigWeekend && days < weekend) {
+    return days * dailyCost - minSale;
+  }
+
+  if (days >= weekend) {
+    return days * dailyCost - maxSale;
+  }
+
+  if (days < bigWeekend && days > 0) {
+    return days * dailyCost;
+  } else {
+    return 0;
+  }
 }
 module.exports = calculateRentalCost;
