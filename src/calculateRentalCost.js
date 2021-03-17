668c0fd405ae1936785b140b0edd1a188234bf26
Vlad Starzhitskiy <study.html.css.java@gmail.com>
2021-03-17 22:36:53 +0200

    lint fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,13 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  if (days < 3){
+    return (days * 40);
+  } if (7 > days && days >=3 ){
+    return (days * 40 - 20);
+  } if (days >= 7){
+    return (days * 40 - 50);
+  }
 }
 
 module.exports = calculateRentalCost;
