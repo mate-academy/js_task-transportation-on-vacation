a163c5ddd62802da3d4bd26ba9f988ff42155b63
Bogdan Kotsupey <mnenugnoidti@gmail.com>
2021-03-19 17:41:00 +0200

    RentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let total = 0;
+
+  if (days < 3) {
+    total = 40 * days;
+  }
+
+  if (days >= 3 && days <= 6) {
+    total = (days * 40) - 20;
+  }
+
+  if (days >= 7) {
+    total = days * 40 - 50;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
