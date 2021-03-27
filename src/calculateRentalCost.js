4e050cfe0b67e7dfc9444c9ad21931648da13315
romandubovskyi <rdubowskij@gmail.com>
2021-03-27 15:14:38 +0200

    implemented rental price calculation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentalDayPrice = 40;
+  const weekDiscount = 50;
+  const threeDaysDiscount = 20;
+
+  if (days >= 7) {
+    return days * rentalDayPrice - weekDiscount;
+  }
+
+  if (days >= 3) {
+    return days * rentalDayPrice - threeDaysDiscount;
+  }
+
+  return days * rentalDayPrice;
 }
 
 module.exports = calculateRentalCost;
