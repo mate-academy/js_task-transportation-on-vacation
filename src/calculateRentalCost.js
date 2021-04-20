7fc8361ed45e6d09a48a164284c2e1cd19cadbe3
Leonid Vegera <leonid.vegera@gmail.com>
2021-04-20 22:28:20 +0300

    Fixes after code review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,11 +30,13 @@
 function calculateRentalCost(days) {
   if (days >= 7) {
     return (days * 40) - 50;
-  } else if (days >= 3) {
-    return (days * 40) - 20;
-  } else {
-    return (days * 40);
   }
+
+  if (days >= 3) {
+    return (days * 40) - 20;
+  }
+
+  return (days * 40);
 }
 
 module.exports = calculateRentalCost;
