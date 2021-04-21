f39fcf7ddc1b7880299bbee3a1bb405c9373dcd7
Igor Lisogor <illisogori@gmail.com>
2021-04-21 15:05:22 +0300

    Changes after review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,24 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  let rentalCost = 0;
   const priceDay = 40;
   const discountThreeDays = 20;
   const discountSevenDays = 50;
 
   if (days > 2 && days < 7) {
-    rentalCost = days * priceDay - discountThreeDays;
+    return days * priceDay - discountThreeDays;
   }
 
   if (days >= 7) {
-    rentalCost = days * priceDay - discountSevenDays;
+    return days * priceDay - discountSevenDays;
   }
 
-  if (rentalCost === 0) {
-    rentalCost = days * priceDay;
-  }
-
-  return rentalCost;
+  return days * priceDay;
 }
 
 module.exports = calculateRentalCost;
