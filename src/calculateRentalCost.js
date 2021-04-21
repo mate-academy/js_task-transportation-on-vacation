b6bada4f78eb7d09cf615140bedca10d55b3b6d4
Pavel Kuzmenko <pashokkuzmenko@gmail.com>
2021-04-21 12:59:57 +0300

    v0.2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -34,7 +34,9 @@ function calculateRentalCost(days) {
 
   if (days >= 7) {
     return days * costRentDay - discountMax;
-  } else if (days >= 3) {
+  }
+
+  if (days >= 3) {
     return days * costRentDay - discountMin;
   }
 
