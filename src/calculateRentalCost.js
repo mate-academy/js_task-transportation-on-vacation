8ab0474854324e733c17527672b82fb179d185b3
Ihor Karpyn <igor82200@gmail.com>
2021-04-21 12:14:55 +0300

    test

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const cost = days * 40;
+
+  if (days < 3) {
+    return cost;
+  } else if (days < 7) {
+    return cost - 20;
+  } else {
+    return cost - 40;
+  }
 }
 
 module.exports = calculateRentalCost;
