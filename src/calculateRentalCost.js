ba03ae1d65bf5b1b372d8b0fe40afd0e5fb58fec
Dmytro_Kuznietsov <kdvgym@gmail.com>
2021-06-25 18:14:45 +0300

    Fixes after code review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -11,7 +11,7 @@
  *
  * Every day you rent the car costs $40.
  * If you rent the car for 7 or more days, you get $50 off your total.
- * Alternatively, if you rent the car for 3 or more days,
+ * Alternatively if you rent the car for 3 or more days,
  * you get $20 off your total.
  *
  * Implement calculateRentalCost function
