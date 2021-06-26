e083de0565316ea009b22c8f5cd92971718db422
coyotious <coyotious@gmail.com>
2021-06-26 17:55:19 +0300

    added function calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,16 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const costPerDay = 40;
+  let discount = 0;
+
+  if (days >= 7) {
+    discount = 50;
+  } else if (days >= 3) {
+    discount = 20;
+  }
+
+  return costPerDay * days - discount;
 }
 
 module.exports = calculateRentalCost;
