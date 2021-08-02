69976441d817ac4054989db751a5df79dce724c0
Oleksandr Zhitenko <geetonio28@gmail.com>
2021-08-02 20:56:59 +0300

    task  is done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rent = 40;
+
+  if (days >= 7) {
+    return days * rent - 50;
+  } else if (days >= 3) {
+    return days * rent - 20;
+  } else {
+    return days * rent;
+  }
 }
 
 module.exports = calculateRentalCost;
