9569238e9a0d9e2bc53252fc3da9158158462754
Oleksandr Strilchuk <virass.m@gmail.com>
2021-08-03 18:06:22 +0300

    Done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const totalCost = days * 40;
+
+  if (days >= 7) {
+    return totalCost - 50;
+  } else if (days >= 3) {
+    return totalCost - 20;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
