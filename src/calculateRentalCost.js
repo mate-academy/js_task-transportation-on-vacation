0c04ee9633f6f6833bc4c7be4dd99744d31d9682
dmytro_mykoliv <amigo.del.ua@gmail.com>
2021-08-03 12:13:32 +0200

    simplified

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -33,9 +33,15 @@ function calculateRentalCost(days) {
   const discountFor3days = 20;
   const discountFor7days = 50;
 
-  return days >= 7 ? totalRentCost - discountFor7days
-    : days >= 3 ? totalRentCost - discountFor3days
-      : totalRentCost;
+  if (days >= 7) {
+    return totalRentCost - discountFor7days;
+  }
+
+  if (days >= 3) {
+    return totalRentCost - discountFor3days;
+  } else {
+    return totalRentCost;
+  }
 }
 
 module.exports = calculateRentalCost;
