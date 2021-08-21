45cfa5b5cbbac115e41b6791d6f88a358d31d291
andrewSuprun <86914377+andrewSuprun@users.noreply.github.com>
2021-08-21 16:11:44 +0200

    Update src/calculateRentalCost.js
    Co-authored-by: Kryvytskyi Volodymyr <vovakrivickiy34@gmail.com>

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,11 +30,13 @@
 function calculateRentalCost(days) {
   if (days < 3) {
     return days * 40;
-  } else if (days >= 3 && days < 7) {
-    return (days * 40) - 20;
-  } else {
-    return (days * 40) - 50;
   }
+  
+  if (days < 7) {
+    return (days * 40) - 20;
+  }
+  
+  return (days * 40) - 50;
 }
 
 module.exports = calculateRentalCost;
