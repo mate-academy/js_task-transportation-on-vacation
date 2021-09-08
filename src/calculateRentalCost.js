65dd15a6d3c9198c34cfba17c1ba20f9f922dbeb
Anastasiia Afanasieva <anastasiiaafanasieva@Anastasiias-MacBook-Air.local>
2021-09-08 13:48:33 +0300

    solution js_task-transportation-on-vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,18 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let result = 0;
+  const everydayCost = 40;
+
+  if (days < 3) {
+    result = days * everydayCost;
+  } else if (days < 7) {
+    result = (days * everydayCost) - 20;
+  } else {
+    result = (days * everydayCost) - 50;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
