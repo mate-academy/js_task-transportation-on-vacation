2f5239efc83e1f6dd25da2f1d8f3a13474bc98e0
Victor Shevchuk <callmevitok@gmail.com>
2021-09-14 20:48:17 +0300

    solution2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,12 +28,14 @@
  */
 
 function calculateRentalCost(days) {
+  const fullPrice = days * 40;
+
   if (days < 3) {
     return days * 40;
   } else if (days < 7) {
-    return (days * 40) - 20;
+    return fullPrice - 20;
   } else {
-    return (days * 40) - 50;
+    return fullPrice - 50;
   }
 }
 
