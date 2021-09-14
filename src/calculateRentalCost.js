14f78c5d69700af891e90bf64310c8ae5c2bd5ab
Maksym Vikolash <vikolashmaxim@gmail.com>
2021-09-14 16:46:46 +0300

    code refactoring

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,17 @@
  *
  */
 function calculateRentalCost(days) {
+  const fullPrice = days * 40;
+
   if (days < 3) {
-    return days * 40;
+    return fullPrice;
   }
 
   if (days < 7) {
-    return days * 40 - 20;
-  } else {
-    return days * 40 - 50;
+    return fullPrice - 20;
   }
+
+  return fullPrice - 50;
 }
 
 module.exports = calculateRentalCost;
