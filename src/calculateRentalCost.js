32008b32226cea894d15cd8669cbc5103d0fa684
Oleh-Tsvietkov <tsvietkov-oleh@gmail.com>
2021-10-10 20:28:44 +0300

    Solution_21-10-10

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,17 @@
 
 function calculateRentalCost(days) {
   // write code here
+  let discountTotal = 0;
+
+  //
+  if ((days >= 3) && (days < 7)) {
+    discountTotal = 20;
+  } else
+  if (days >= 7) {
+    discountTotal = 50;
+  }
+
+  return days * 40 - discountTotal;
 }
 
 module.exports = calculateRentalCost;
