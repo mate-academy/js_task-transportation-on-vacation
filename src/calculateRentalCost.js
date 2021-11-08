5ee0d110bec6ccceb406f8cf7ee849c5c0e1d3a3
TatianaMorozova04 <morozovatatiana04@gmail.com>
2021-11-08 17:10:56 +0200

    calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let sum = 0;
+
+  if (days >= 7) {
+    sum = days * 40 - 50;
+  } else if (days >= 3 && days < 7) {
+    sum = days * 40 - 20;
+  } else {
+    sum = days * 40;
+  }
+
+  return sum;
 }
 
 module.exports = calculateRentalCost;
