a02dff8a6a8bdc8692a8f960f7ca6a27e68dd849
Dariia Romanova <dariia.romanova@gmail.com>
2021-11-08 17:45:40 +0200

    Fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  const rentalCost = 40;
+  const rentalCost = days * 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
   if (days >= 7) {
-    return days * rentalCost - 50;
+    return rentalCost - bigDiscount;
   } else if (days >= 3) {
-    return days * rentalCost - 20;
+    return rentalCost - smallDiscount;
   }
 
-  return days * rentalCost;
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
