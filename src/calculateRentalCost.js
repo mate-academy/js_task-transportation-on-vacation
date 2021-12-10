c993af586a83c7dab5cf422479660a75594d40fd
Iren Gorak <iren.horak@gmail.com>
2021-12-10 18:31:38 +0200

    Task 1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  if (days < 3) {
+    return (days * 40);
+  }
+
+  if (days < 7) {
+    return (days * 40 - 0.2 * 100);
+  }
+
+  if (days >= 7) {
+    return ((days * 40) - 50);
+  }
 }
 
 module.exports = calculateRentalCost;
