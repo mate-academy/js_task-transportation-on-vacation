08f04f1f53f30ab37947a271e9fb64d95a029f32
Mikedacheka <mikedacheka@gmail.com>
2021-12-14 00:27:50 +0200

    sm fixes,maybe last

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,13 +29,15 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const totalCost = days * 40;
+
   if (days >= 7) {
-    return (days * 40 - 50);
+    return (totalCost - 50);
   } else if (days >= 3) {
-    return (days * 40 - 20);
+    return (totalCost - 20);
   }
 
-  return days * 40;
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
