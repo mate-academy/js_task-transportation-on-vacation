d4a8bdfc382fa340df670cfdd3b5898e1df4f1cc
Roman Moroz <65686226+morozit@users.noreply.github.com>
2022-02-01 09:57:23 +0200

    Moroz

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,16 @@
 
 function calculateRentalCost(days) {
   // write code here
+  switch (true) {
+    case days < 3:
+      return days * 40;
+    case days >= 3 && days < 7:
+      return days * 40 - 20;
+    case days >= 7:
+      return days * 40 - 50;
+    default:
+      break;
+  }
 }
 
 module.exports = calculateRentalCost;
