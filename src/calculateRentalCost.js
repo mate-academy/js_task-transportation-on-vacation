c62043a847fccfe9904f094063212874a59c84eb
Yevhen Veselov <veselovyevhen@gmail.com>
2022-02-02 21:24:10 +0200

    1st commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentCost = 40;
+
+  if (days < 3) {
+    return rentCost * days;
+  }
+
+  if (days >= 3 && days < 7) {
+    return (rentCost * days) - 20;
+  }
+
+  if (days >= 7) {
+    return (rentCost * days) - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
