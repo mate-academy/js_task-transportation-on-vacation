60e04af63799ab871dbba56c7254fbadff179fe8
Alexxs <irreligious86@gmail.com>
2022-02-09 18:14:53 +0200

    fix suggestions

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -2,14 +2,16 @@
 
 function calculateRentalCost(days) {
   const price = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
   const totalSum = days * price;
 
-  if (days / 7 >= 1) {
-    return totalSum - 50;
+  if (days >= 7) {
+    return totalSum - bigDiscount;
   }
 
-  if (days / 3 >= 1) {
-    return totalSum - 20;
+  if (days >= 3) {
+    return totalSum - smallDiscount;
   }
 
   return totalSum;
