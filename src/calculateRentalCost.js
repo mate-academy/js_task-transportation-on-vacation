a2d1e8b4084fabf3ad54ac133aef011c592ab80b
Vladyslava Buzova <dev.vladyslava@gmail.com>
2022-03-08 23:48:07 +0800

    some message

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentPrice = 40;
+
   if (days >= 7) {
-    return (days * 40) - 50;
+    return (days * rentPrice) - 50;
   }
 
   if (days >= 3) {
-    return (days * 40) - 20;
+    return (days * rentPrice) - 20;
   }
 
-  return (days * 40);
+  return (days * rentPrice);
 }
 
 module.exports = calculateRentalCost;
