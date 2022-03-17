ee1b3d4d508b3a6557e2c402abc9e4e95c1f6cfd
Ruslan Hubanov <ruslangubanov88@gmail.com>
2022-03-17 19:14:41 +0200

    fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,18 @@
  */
 
 function calculateRentalCost(days) {
+  const forty = 40;
+  const fifty = 50;
+  const twenty = 20;
+  const sum = days * forty;
+
   if (days <= 2) {
     return days * 40;
   } else if (days >= 3 && days < 7) {
-    return (days * 40) - 20;
-  } else {
-    return (days * 40) - 50;
+    return (sum) - twenty;
   }
+
+  return sum - fifty;
 }
 
 module.exports = calculateRentalCost;
