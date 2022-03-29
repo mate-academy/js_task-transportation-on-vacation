45a073a612a2d1ee3fb85581fd4b32b998f88963
Eugenia <eugeniasaf@gmail.com>
2022-03-29 03:25:34 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let cost = 0;
+
+  if (days < 3) {
+    cost = 40 * days;
+  }
+
+  if (days >= 3 && days < 7) {
+    cost = 40 * days - 20;
+  }
+
+  if (days >= 7) {
+    cost = 40 * days - 50;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
