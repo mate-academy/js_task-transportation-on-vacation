ea5121b299d6dcf419573eefb37d00554156147a
Ruslana Tomnyuk <rusia.vasia1@gmail.com>
2022-03-30 15:15:34 +0300

    changes after reject

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,19 @@
  */
 
 function calculateRentalCost(days) {
+  const price = 40;
+  const minDiscount = 20;
+  const maxDiscount = 50;
+
   if (days < 3) {
-    return days * 40;
+    return days * price;
   }
 
   if (days >= 7) {
-    return (days * 40) - 50;
+    return (days * price) - maxDiscount;
   }
 
-  return (days * 40) - 20;
+  return (days * price) - minDiscount;
 }
 
 module.exports = calculateRentalCost;
