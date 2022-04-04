22b098b4cd2c9fce2428ff426dc34a73649e7953
Liuda Chuzdiuk <liuda.chuzdiuk@gmail.com>
2022-04-04 10:41:07 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -27,8 +27,17 @@
  * @return {number}
  */
 
-function calculateRentalCost(days) {
-  // write code here
+ function calculateRentalCost(days) {
+  const totalCost = days * 40;
+  let discount = 0;
+
+  if (days >= 7) {
+    discount = 50;
+  } else if (days >= 3) {
+    discount = 20;
+  }
+
+  return totalCost - discount;
 }
 
 module.exports = calculateRentalCost;
