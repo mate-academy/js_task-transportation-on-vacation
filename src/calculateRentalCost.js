d12b91ce998cc473c63a644976e23e0259be481b
Bogdan Prokopenko <pb.dvlpr@gmail.com>
2022-04-07 16:37:51 +0300

    Firs Run Task1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let count = 0;
+
+  if (days < 3) {
+    count = days * 40;
+  } else if (days < 7) {
+    count = days * 40 - 20;
+  } else {
+    count = days * 40 - 50;
+  }
+
+  return count;
 }
 
 module.exports = calculateRentalCost;
