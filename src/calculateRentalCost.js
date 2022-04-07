c25b0801998b37b4a6e5dd5f2d08a9aea9cdfd41
Roman Stas <romastas1996@gmail.com>
2022-04-07 16:02:03 +0300

    Sulution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,15 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const rentDay = 40;
+
+  if (days >= 7) {
+    return (days * rentDay) - 50
+  } else if(days >= 3) {
+    return (days * rentDay) - 20
+  } else if (days > 0) {
+    return days * rentDay
+  };
 }
 
 module.exports = calculateRentalCost;
