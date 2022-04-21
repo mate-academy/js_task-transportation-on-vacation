47f4b4c1e2924fe298b5bced83d1f2a244767bae
Lebogang Mogane <mogane420@gmail.com>
2022-04-21 15:30:31 +0200

    created a variable

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,17 @@
  */
 
 function calculateRentalCost(days) {
+  const cost = days * 40;
+
   if (days >= 7) {
-    return (days * 40) - 50;
+    return cost - 50;
   }
 
   if (days >= 3) {
-    return (days * 40) - 20;
+    return cost - 20;
   }
 
-  return days * 40;
+  return cost;
 }
 
 module.exports = calculateRentalCost;
