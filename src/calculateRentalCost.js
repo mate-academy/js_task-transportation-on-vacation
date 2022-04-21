077d624ae0c5ab23092de1a03650d0b9b992e57e
Amit Patil <amitp0156@gmail.com>
2022-04-21 16:11:57 +0530

    removed if else statements and made code more readable

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,19 @@
  */
 
 function calculateRentalCost(days) {
+  const costPerDay = 40;
+  const disountForSevenDays = 50;
+  const disountForThreeDays = 20;
+
   if (days > 6) {
-    return (days * 40) - 50;
-  } else if (days > 2) {
-    return (days * 40) - 20;
-  } else {
-    return days * 40;
+    return (days * costPerDay) - disountForSevenDays;
   }
+
+  if (days > 2) {
+    return (days * costPerDay) - disountForThreeDays;
+  }
+
+  return days * costPerDay;
 }
 
 module.exports = calculateRentalCost;
