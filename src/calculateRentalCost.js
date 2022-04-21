1372522e4d2c307fc04f3866fd3f4968e725fd02
Andrei Sucman <67225216+andreisucman@users.noreply.github.com>
2022-04-21 14:22:43 +0300

    Removed else statements
    Yes, I also find that else statements are more confusing. But I thought it's just me who thinks so.

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,12 +28,14 @@
  */
 
 function calculateRentalCost(days) {
-const dailyPrice = 40;
+  const dailyPrice = 40;
   let discount = 0;
 
   if (days >= 3 && days < 7) {
     discount = 20;
-  } else if (days >= 7) {
+  }
+
+  if (days >= 7) {
     discount = 50;
   }
 
