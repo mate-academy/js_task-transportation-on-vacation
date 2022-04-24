862a536533aaab32c5131989b387cd623b992aec
Zubov Illia <contactduodiamonds@gmail.com>
2022-04-24 23:49:37 +0300

    added variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,19 @@
  */
 
 function calculateRentalCost(days) {
+  const price = 40;
+  const discount = 20;
+  const bigDiscount = 50;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * price - bigDiscount;
   }
 
   if (days >= 3) {
-    return days * 40 - 20;
+    return days * price - discount;
   }
 
-  return days * 40;
+  return days * price;
 }
 
 module.exports = calculateRentalCost;
