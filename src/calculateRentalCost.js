ed1fba451eb406b6f75862feb30c26d6b68a3a4d
Vlad Demchuk <vladyslavdemchuk1999@gmail.com>
2022-04-26 15:29:31 +0300

    fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,19 @@
  */
 
 function calculateRentalCost(days) {
+  const price = days * 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return price - bigDiscount;
   }
 
   if (days >= 3) {
-    return days * 40 - 20;
+    return price - smallDiscount;
   }
 
-  return days * 40;
+  return price;
 }
 
 module.exports = calculateRentalCost;
