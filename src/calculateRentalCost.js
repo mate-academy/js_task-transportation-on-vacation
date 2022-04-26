7e9e9a1f43404d0f4cc2f35c420612cad834aaa5
elkhayate <melkhayate61@gmail.com>
2022-04-26 17:14:01 +0000

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,11 +30,13 @@
 function calculateRentalCost(days) {
   // write code here
   let result = 0;
+  const bigDiscount = 50;
+  const smallDiscount = 20;
 
   if (days >= 3 && days < 7) {
-    result -= 20;
+    result -= smallDiscount;
   } else if (days >= 7) {
-    result -= 50;
+    result -= bigDiscount;
   }
 
   result += days * 40;
