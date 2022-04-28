ae591e7aa2a80188f966ec02d719392d61eb1f56
Ihor Pavlenko <pavlenkoigor3@gmail.com>
2022-04-28 22:36:42 +0300

    fixed commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,19 @@
  */
 
 function calculateRentalCost(days) {
+  const daysCost = days * 40;
+  const daysDiscount = 20;
+  const weekDiscount = 50;
+
   if (days > 6) {
-    return days * 40 - 50;
+    return daysCost - weekDiscount;
   }
 
   if (days > 2) {
-    return days * 40 - 20;
+    return daysCost - daysDiscount;
   }
 
-  return days * 40;
+  return daysCost;
 }
 
 module.exports = calculateRentalCost;
