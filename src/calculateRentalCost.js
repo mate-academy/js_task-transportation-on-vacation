c2b00fcad3d3ac2cd4b7c478455c9320da09b852
Dmytro Lebedchenko <103206316+dmytro-lebedchenko@users.noreply.github.com>
2022-05-17 11:20:47 +0300

    Update calculateRentalCost.js
    Function calculateRentalCost was updated.

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -31,9 +31,11 @@ function calculateRentalCost(days) {
   const dayPay = 40;
   let discount = 0;
 
-  if ((days >= 3) && (days <= 6)) {
+  if (days >= 3) {
     discount = 20;
-  } else if (days >= 7) {
+  }
+
+  if (days >= 7) {
     discount = 50;
   }
 
