c5caf6f325ac59566577160e84255e2c85e36a91
Yevgeniyaa Yevdokimova <jane.evdokimova277@gmail.com>
2022-05-23 17:55:14 +0300

    Fixes after code review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,18 @@
  */
 
 function calculateRentalCost(days) {
+  const price = days * 40;
+
   if (days < 3) {
-    return days * 40;
+    return price;
   }
 
   if (days >= 3 && days < 7) {
-    return days * 40 - 20;
+    return price - 20;
   }
 
   if (days >= 7) {
-    return days * 40 - 50;
+    return price - 50;
   }
 }
 
