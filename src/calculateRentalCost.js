7eb9219f8f6e9559fff8ddc1fdcd15f7155bc88c
Stanislav Topikha <stanislav.topikha@gmail.com>
2022-06-06 13:01:38 +0300

    Task completed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let discount = 0;
+
+  if (days >= 3) {
+    discount = 20;
+
+    if (days >= 7) {
+      discount = 50;
+    }
+  }
+
+  return (days * 40) - discount;
 }
 
 module.exports = calculateRentalCost;
