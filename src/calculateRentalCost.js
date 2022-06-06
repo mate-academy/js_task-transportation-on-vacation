2d6e2fd3493f72f4dded2ccb05f20475882a8b5a
Yana Ostapenko <yana.ostapenko@gmail.com>
2022-06-06 12:50:21 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentWithoutDiscount = days * 40;
+
+  if (days >= 7) {
+    return rentWithoutDiscount - 50;
+  }
+
+  if (days >= 3) {
+    return rentWithoutDiscount - 20;
+  }
+
+  return rentWithoutDiscount;
 }
 
 module.exports = calculateRentalCost;
