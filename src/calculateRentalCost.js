47d9216b3c3d0314ded330b7ef6bd7a7eb9e3eb5
Ostap Salo <jlumon211@gmail.com>
2022-06-06 16:38:53 +0300

    Update code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,19 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
-  let count = 0;
-  const rent = 40;
-
   if (days < 3) {
-    count = days * rent;
-  } else if (days >= 3 && days < 7) {
-    count = days * rent - 20;
-  } else {
-    count = days * rent - 50;
+    return days * 40;
   }
 
-  return count;
+  if (days >= 3 && days < 7) {
+    return days * 40 - 20;
+  }
+
+  return days * 40 - 50;
 }
 
 module.exports = calculateRentalCost;
