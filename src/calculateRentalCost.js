969130f5d36cfa305300102856d481d9ae0d7b81
olekskov <oleksandr@kovalenko.cc>
2022-06-12 10:57:44 +0300

    Initial Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,16 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const dayPrice = 40;
+  const total = days * dayPrice;
+  const firstDiscountPeriod = 3;
+  const firstDiscountAmount = 20;
+  const secondDiscountPeriod = 7;
+  const secondDiscountAmount = 50;
+
+  return days < firstDiscountPeriod ? total
+    : days < secondDiscountPeriod ? total - firstDiscountAmount
+      : total - secondDiscountAmount;
 }
 
 module.exports = calculateRentalCost;
