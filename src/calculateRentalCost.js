e94e8b50c36acc643ec1439c4d1343f83ce76aae
Ihor Novakovskyi <igorplan3@gmail.com>
2022-06-16 14:33:57 +0300

    students fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,11 +30,13 @@
 function calculateRentalCost(days) {
   if (days >= 3 && days < 7) {
     return days * 40 - 20;
-  } else if (days >= 7) {
-    return days * 40 - 50;
-  } else {
-    return days * 40;
   }
+
+  if (days >= 7) {
+    return days * 40 - 50;
+  }
+
+  return days * 40;
 }
 
 module.exports = calculateRentalCost;
