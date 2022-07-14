4aeb9b2435edad316adc4f58939df16fb4fa5962
Taras-1 <taras.kovalcuk93@gmail.com>
2022-07-14 22:14:31 +0300

    Solution

--- readme.md
+++ readme.md
@@ -1,3 +1,3 @@
 # Boilerplate for JS tasks
 
-**Read [the guideline](https://github.com/mate-academy/js_task-guideline/blob/master/README.md) before start**
+**Read [the guideline](https://github.com/mate-academy/js_task-guideline/btlob/master/READMnpm E.md) before start**
--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  if (days >= 7) {
+    return ((days * 40) - 50);
+  }
+
+  if (days < 3) {
+    return days * 40;
+  }
+
+  if (days >= 3 && days < 7) {
+    return ((days * 40) - 20);
+  }
 }
 
 module.exports = calculateRentalCost;
