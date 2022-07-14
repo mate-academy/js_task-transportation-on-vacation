1c8be6c04f0ce47dd38c7f61abf48e15e26d673f
Tetiana Melnyk <uasmiykaua@gmail.com>
2022-07-14 13:55:12 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,12 @@
  */
 
 function calculateRentalCost(days) {
-  function calculateRentalCost(days) {
-    if (days < 3) {
-
-      return days * 40;
-    } else if (days < 7) {
-
-      return days * 40 - 20;
-    } else {
-
-      return days * 40 - 50;
-    }
+  if (days < 3) {
+    return days * 40;
+  } else if (days < 7) {
+    return days * 40 - 20;
+  } else {
+    return days * 40 - 50;
   }
 }
 
