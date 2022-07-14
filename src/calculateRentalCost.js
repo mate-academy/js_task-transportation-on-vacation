d75bb7e59f0862aba8fc8543f8c8cb2843b484b7
Taras-1 <taras.kovalcuk93@gmail.com>
2022-07-14 22:40:05 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,17 @@
  */
 
 function calculateRentalCost(days) {
+
+  if (days < 3) {
+    return days * 40;
+  }
+
   if (days >= 7) {
     return ((days * 40) - 50);
   }
 
   if (days >= 3 && days < 7) {
     return ((days * 40) - 20);
-  }
-
-  if (days < 3) {
-    return days * 40;
   }
 }
 
