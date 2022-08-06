82c873fca9a2878432e709bf828f1c3c37374813
Kolia Plemennyk <plemianniknikolay@gmail.com>
2022-08-06 16:03:37 +0300

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,12 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  return days < 1
+    ? 0 : days < 3
+      ? days * 40
+      : days < 7
+        ? days * 40 - 20
+        : days * 40 - 50;
 }
 
 module.exports = calculateRentalCost;
