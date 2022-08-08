c431408a26521a02e777916ec9b34571dfbf3b44
Erik Karaman <erikkaraman7@gmail.com>
2022-08-08 15:48:42 +0300

    add edits

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,21 @@
  */
 
 function calculateRentalCost(days) {
+  const price = 40;
+  const smallDiscount = 20;
+  const largeDiscount = 50;
+
   if (days < 3) {
-    return 40 * days;
-  } else if (days < 7) {
-    return 40 * days - 20;
-  } else {
-    return 40 * days - 50;
-  }
-}
+    return price * days;
+  };
+
+  if (days < 7) {
+    return price * days - smallDiscount;
+  };
+
+  if (days >= 7) {
+    return price * days - largeDiscount;
+  };
+};
 
 module.exports = calculateRentalCost;
