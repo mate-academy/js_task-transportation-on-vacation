44da1cb296a064ff3c2e626498fadcabab38d855
Volodymir Tymtsias <vovanuch2007@gmail.com>
2022-08-09 13:56:40 +0300

    moved calculations to variable

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,17 @@
  */
 
 function calculateRentalCost(days) {
+  let rentalCost = 0;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    rentalCost = days * 40 - 50;
   } else if (days >= 3) {
-    return days * 40 - 20;
+    rentalCost = days * 40 - 20;
   } else {
-    return days * 40;
+    rentalCost = days * 40;
   }
+
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
