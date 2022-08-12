f1c9fdcf310770950fbfd6668a1e2ada21034aba
Maryna Kucher <marina.kucher8@gmail.com>
2022-08-12 12:12:03 +0300

    Solution was fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,12 @@
 
 function calculateRentalCost(days) {
   const allCount = days * 40;
-  let result = 0;
+  let result = allCount - 50;
 
   if (days < 3) {
     result = allCount;
   } else if (days >= 3 && days <= 6) {
     result = allCount - 20;
-  } else {
-    result = allCount - 50;
   }
 
   return result;
