eabac126cbe6cafc2ab804aa64326ac62073b333
Olha Moroziuk <olha.pm.lviv@gmail.com>
2022-08-16 15:45:47 +0300

    Fixes after 1st code review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,18 @@
  */
 
 function calculateRentalCost(days) {
+  const sum = 40 * days;
+
   if (days >= 7) {
-    return (40 * days - 50);
+    return (sum - 50);
   }
 
   if (days >= 3) {
-    return (40 * days - 20);
+    return (sum - 20);
   }
 
   if (days < 3) {
-    return (40 * days);
+    return sum;
   }
 }
 
