7f606acedf447fbd41c2b7e02741271c5d860fcd
Vitaliy Beketov <beketov2403@gmail.com>
2022-09-02 18:07:01 +0300

    1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const costFoDay = 40;
+  const offSeven = 50;
+  const offThree = 20;
+
+  if (days >= 3 && days < 7) {
+    return (days * costFoDay - offThree);
+  } else if (days >= 7) {
+    return (days * costFoDay - offSeven);
+  } else {
+    return (days * costFoDay);
+  }
 }
 
 module.exports = calculateRentalCost;
