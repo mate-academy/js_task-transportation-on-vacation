e1c93a1201e7c7d6fb3bef5737088161033e0977
Ivan Nevmerzhytskyi <nevmerzhytskyi.ivan@gmail.com>
2022-09-05 21:27:43 +0300

    extra fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,16 @@
  */
 
 function calculateRentalCost(days) {
-  const bigDiscount = 50;
-  const smallDiscount = 20;
+  const discountForSevenDays = 50;
+  const discountForThreeDays = 20;
   const rentWithoutDiscount = days * 40;
 
   if (days >= 7) {
-    return rentWithoutDiscount - bigDiscount;
+    return rentWithoutDiscount - discountForSevenDays;
   }
 
-  if (days >= 3 && days < 7) {
-    return rentWithoutDiscount - smallDiscount;
+  if (days >= 3) {
+    return rentWithoutDiscount - discountForThreeDays;
   }
 
   return rentWithoutDiscount;
