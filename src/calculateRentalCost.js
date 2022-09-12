ff8c10209527d1d2f316d609a87a33806001b9c8
Denys Kleimenov <denyskleimenovit@gmail.com>
2022-09-12 13:44:53 +0300

    Task was solved

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let price = 0;
+
+  for (let i = 0; i < days; i++) {
+    price += 40;
+  }
+
+  if (days >= 7) {
+    price = price - 50;
+  } else if (days >= 3 && days < 7) {
+    price = price - 20;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
