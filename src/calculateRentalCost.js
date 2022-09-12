b3afbb60454b25f5859347dba6800acceb577b46
Andrii Zhabynets <a.zhabynets03@gmail.com>
2022-09-12 15:57:18 +0300

    Result

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const oneDayPrice = 40;
+
+  if (days >= 3 && days < 7) {
+    return (days * oneDayPrice) - 20;
+  }
+
+  if (days >= 7) {
+    return (days * oneDayPrice) - 50;
+  }
+
+  return days * oneDayPrice;
 }
 
 module.exports = calculateRentalCost;
