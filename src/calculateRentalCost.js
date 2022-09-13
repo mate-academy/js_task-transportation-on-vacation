fbbd21e925f664cd0025fb445daab548638f9b6c
Oleh Bazyla <olegbazila@gmail.com>
2022-09-13 21:48:05 +0300

    Fixed after review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,18 @@
  */
 
 function calculateRentalCost(days) {
+  const dayCarCost = 40;
   const sevenDaysDiscount = 50;
   const threeDaysDiscount = 20;
-  const rentCosts = days * 40;
+  const rentCosts = days * dayCarCost;
 
   if (days >= 7) {
     return rentCosts - sevenDaysDiscount;
   } else if (days >= 3) {
     return rentCosts - threeDaysDiscount;
-  } else {
-    return rentCosts;
   }
+
+  return rentCosts;
 }
 
 module.exports = calculateRentalCost;
