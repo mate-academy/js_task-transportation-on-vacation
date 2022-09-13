2d288b4cc0ec6bca79a7fde5bdcb9a02850ea8a0
Serhii Hluhovskyi <glugovsky@proton.me>
2022-09-13 17:00:04 +0300

    Added variables for magic numbers

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,12 +29,14 @@
 
 function calculateRentalCost(days) {
   const cost = 40;
+  const discountAfterThreeDays = 20;
+  const discountAfterSevenDays = 50;
   let discount = 0;
 
   if (days >= 7) {
-    discount = -50;
+    discount -= discountAfterSevenDays;
   } else if (days >= 3) {
-    discount = -20;
+    discount -= discountAfterThreeDays;
   }
 
   return days * cost + discount;
