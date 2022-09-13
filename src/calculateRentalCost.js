31e6bc6f9d72df491af4a926dddb8483c56d60bd
Yelisieiev Kim <rbv271@outlook.com>
2022-09-13 21:37:44 +0300

    edited according checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,16 +29,16 @@
 
 function calculateRentalCost(days) {
   const dayRent = 40;
-  const threeDayDiscount = 20;
-  const sevenDayDiscount = 50;
+  const generalDiscount = 20;
+  const wideDiscount = 50;
 
   if (days < 3) {
     return days * dayRent;
   } else if (days < 7) {
-    return days * dayRent - threeDayDiscount;
-  } else if (days >= 7) {
-    return days * dayRent - sevenDayDiscount;
+    return days * dayRent - generalDiscount;
   }
+
+  return days * dayRent - wideDiscount;
 }
 
 module.exports = calculateRentalCost;
