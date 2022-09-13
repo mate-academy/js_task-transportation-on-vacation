88e829f79e88e592ddd7d1f798458b99c9c6e0a4
M.Ali Yücesoylu <myucesoylu@gmail.com>
2022-09-13 14:29:21 +0300

    solution 1.0.2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,12 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  let sum = days * 40;
+  const rentCost = 40;
+  const sum = days * rentCost;
+  const bigDiscount = 50;
+  const smallDiscount = 20;
 
   if ((days > 2) && (days < 7)) {
-    sum -= 20;
+    return sum - smallDiscount;
   } else if (days > 6) {
-    sum -= 50;
+    return sum - bigDiscount;
   }
 
   return sum;
