76e3ad6c34e5a369bbfbc0d3ff393b17622ca286
Andrew Skoryk <andrewtimelessone@gmail.com>
2022-09-13 14:54:09 +0300

    fix: added more variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,14 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  const cost = days * 40;
+  const dayCost = 40;
+  const cost = days * dayCost;
+  const basicDiscount = 20;
+  const additionalDiscount = 30;
 
   switch (true) {
     case (days >= 7):
-      return cost - 50;
+      return cost - basicDiscount - additionalDiscount;
 
     case (days >= 3):
-      return cost - 20;
+      return cost - basicDiscount;
 
     default:
       return cost;
