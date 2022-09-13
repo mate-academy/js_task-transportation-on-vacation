f4d3888fc68f608c72563be4561891d8293867b3
Vlad Malinin <vladmalinin1999@gmail.com>
2022-09-13 15:24:29 +0300

    Added variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,20 @@
  */
 
 function calculateRentalCost(days) {
+  const rentPerDay = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+  const fullPrice = rentPerDay * days;
+
   if (days < 3) {
-    return days * 40;
+    return fullPrice;
   }
 
   if (days >= 3 && days < 7) {
-    return days * 40 - 20;
+    return fullPrice - smallDiscount;
   }
 
-  return days * 40 - 50;
+  return fullPrice - bigDiscount;
 }
 
 module.exports = calculateRentalCost;
