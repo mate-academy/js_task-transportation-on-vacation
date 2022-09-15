1176234e3d7363e4035751d9f3148a370c862a3a
RomaVarkov <varkovroma@gmail.com>
2022-09-15 10:46:24 +0300

    changed structure

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,20 @@
  */
 
 function calculateRentalCost(days) {
-  if (days < 3) {
-    return days * 40;
-  } else if (days >= 3 && days < 7) {
-    return days * 40 - 20;
-  } else if (days >= 7) {
-    return days * 40 - 50;
+  const dayPrice = 40;
+  const total = days * dayPrice;
+  const discountBig = 50;
+  const discountSmall = 20;
+
+  if (days >= 7) {
+    return total - discountBig;
   }
+
+  if (days >= 3) {
+    return total - discountSmall;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
