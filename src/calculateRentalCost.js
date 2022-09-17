0d05015502f4e697fa031b5e99433ea5ee27804a
Maks Manalatii <manmaks122@gmail.com>
2022-09-17 14:41:48 +0300

    Fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  const sum = 40 * days;
+  const rentPrice = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
-  switch (true) {
-    case days >= 7:
-      return sum - 50;
-    case days >= 3 && days < 7:
-      return sum - 20;
-    default:
-      return sum;
+  if (days < 3) {
+    return days * rentPrice;
   }
+
+  if (days >= 3 && days < 7) {
+    return (days * rentPrice) - smallDiscount;
+  }
+
+  return (days * rentPrice) - bigDiscount;
 }
 
 module.exports = calculateRentalCost;
