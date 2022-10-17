4e139afd27680270ba286dcc4ed0dde553e11f85
Kukharuk <its.up.to.you.dk@gmail.com>
2022-10-17 09:46:57 +0300

    firstJS_Git

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let amount = 0;
+
+  if (days < 3) {
+    amount = days * 40;
+
+    return amount;
+  }
+
+  if (days < 7) {
+    amount = (days * 40) - 20;
+
+    return amount;
+  }
+
+  return (days * 40) - 50;
 }
 
 module.exports = calculateRentalCost;
