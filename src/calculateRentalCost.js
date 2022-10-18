cdc189ed3a6f0f6dd80ec23aee14eb9c66cc95fa
Artem66 <melodpi@gmail.com>
2022-10-18 18:51:40 +0100

    implemented calculateRentalCost function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const amount = days * 40;
+
+  if (days >= 7) {
+    return amount - 50;
+  } else if (days >= 3) {
+    return amount - 20;
+  }
+
+  return amount;
 }
 
 module.exports = calculateRentalCost;
