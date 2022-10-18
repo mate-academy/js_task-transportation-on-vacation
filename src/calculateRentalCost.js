9055fc42fb6a7240839e1bb5b502f48b5e9cd922
Max Skorokhod <mrzandras@gmail.com>
2022-10-18 17:25:58 +0300

    checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,18 +29,18 @@
 
 function calculateRentalCost(days) {
   const fixedSum = 40 * days;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
   if (days < 3) {
     return fixedSum;
   }
 
   if (days < 7) {
-    return fixedSum - 20;
+    return fixedSum - smallDiscount;
   }
 
-  if (days >= 7) {
-    return fixedSum - 50;
-  }
+  return fixedSum - bigDiscount;
 }
 
 module.exports = calculateRentalCost;
