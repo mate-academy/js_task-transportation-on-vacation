8dee493245ca7cd1ad2f3c098e863fc2f572f7e4
Karyna Salnkova <karynasalnykova@gmail.com>
2022-10-19 14:18:30 +0300

    Solution was fixed by checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,20 @@
  */
 
 function calculateRentalCost(days) {
+  const costForOneDay = 40;
+  const discountForThreeDays = 20;
+  const discountForSevenDays = 50;
+
   if (days < 3) {
-    return 40 * days;
+    return costForOneDay * days;
   }
 
   if (days >= 3 && days < 7) {
-    return 40 * days - 20;
+    return costForOneDay * days - discountForThreeDays;
   }
 
   if (days >= 7) {
-    return 40 * days - 50;
+    return costForOneDay * days - discountForSevenDays;
   }
 }
 
