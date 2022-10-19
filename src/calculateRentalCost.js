1aba9fc12385f473ec0759fa3db6fe75a02c0930
Viktoriia Ovcharenko <viktoriiaovcharenko1415@gmail.com>
2022-10-19 17:16:18 +0300

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  let result = 0;
+  let totalPrice = days * 40;
 
   if (days >= 7) {
-    result = days * 40 - 50;
+    totalPrice = days * 40 - 50;
   } else if (days >= 3) {
-    result = days * 40 - 20;
-  } else {
-    result = days * 40;
+    totalPrice = days * 40 - 20;
   }
 
-  return result;
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
