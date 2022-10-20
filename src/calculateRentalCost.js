813886f31c5d29b58aee50c0228f75d3010a319c
AndreyChernysh <achernysh744@gmail.com>
2022-10-20 08:04:14 +0300

    fix issue

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,17 +29,29 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const carRent = 40;
+  const minDiscount = 20;
+  const maxDiscount = 50;
+  const bill = days * carRent;
 
-  switch (true) {
-    case (days >= 3 && days < 7):
-      return days * 40 - 20;
-
-    case (days >= 7):
-      return days * 40 - 50;
-
-    default:
-      return days * 40;
+  if (days < 3) {
+    return bill;
+  } else if (days < 7) {
+    return bill - minDiscount;
   }
+
+  return bill - maxDiscount;
+
+  // switch (true) {
+  //   case (days >= 3 && days < 7):
+  //     return days * carRent - minDiscount;
+
+  //   case (days >= 7):
+  //     return days * carRent - maxDiscount;
+
+  //   default:
+  //     return days * carRent;
+  // }
 }
 
 module.exports = calculateRentalCost;
