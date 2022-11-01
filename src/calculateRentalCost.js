d6bc13172652f31e0fc1a54e976adbe3035c9bc2
vadiimvooo <ASD4S.Profile@gmail.com>
2022-11-01 11:29:37 +0200

    Made some changes according to checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,9 +28,20 @@
  */
 
 function calculateRentalCost(days) {
-  return days < 3 ? days * 40
-    : days >= 3 && days < 7 ? days * 40 - 20
-      : days * 40 - 50;
+  const costPerDay = 40;
+  const totalAmount = days * costPerDay;
+  const basicDiscount = 20;
+  const extendedDiscount = 50;
+
+  if (days < 3) {
+    return totalAmount;
+  }
+
+  if (days >= 3 && days < 7) {
+    return totalAmount - basicDiscount;
+  }
+
+  return totalAmount - extendedDiscount;
 }
 
 module.exports = calculateRentalCost;
