a3afb9cb5e51fc712cc08becd6b9d4a4121ada3f
zbyn3k <coding707@mailbox.org>
2022-11-04 09:03:39 +0100

    Added rentPerDay variable to store daily cost of renting a car.

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,20 @@
  */
 
 function calculateRentalCost(days) {
+  const rentPerDay = 40;
   const basicDiscount = 20;
   const greatDiscount = 50;
-  let rentCost = days * 40;
+  let rentCost = days * rentPerDay;
 
   if (days >= 7) {
     rentCost = rentCost - greatDiscount;
+
     return rentCost;
   }
 
   if (days >= 3) {
     rentCost = rentCost - basicDiscount;
+
     return rentCost;
   }
 
