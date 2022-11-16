9c4ced8964d383d4b7576a3ccbc788caf1565e12
VladimirEdelmann <VladimirEdelmann@gmail.com>
2022-11-16 14:54:26 +0200

    Fix on the checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,18 @@
 
 function calculateRentalCost(days) {
   const cost = days * 40;
+  const smallDiscount = 20;
+  const basicDiscount = 50;
 
   if (days >= 7) {
-    return cost - 50;
-  } else if (days >= 3) {
-    return cost - 20;
-  } else {
-    return cost;
+    return cost - basicDiscount;
   }
+
+  if (days >= 3) {
+    return cost - smallDiscount;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
