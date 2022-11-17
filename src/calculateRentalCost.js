48d625d5f96f928e05bd2f3c495aa1413be8dec4
Ninel Levchenko <blackmaplearts@gmail.com>
2022-11-17 14:10:37 +0200

    fixed 2.0

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,14 @@
 
 function calculateRentalCost(days) {
   const dayPrice = 40;
-  let totalPrice = days * dayPrice;
+  const totalPrice = days * dayPrice;
   const discountBasic = 20;
   const discountForWeek = 50;
 
   if (days >= 7) {
-    totalPrice -= discountForWeek;
+    return totalPrice - discountForWeek;
   } else if (days >= 3) {
-    totalPrice -= discountBasic;
+    return totalPrice - discountBasic;
   }
 
   return totalPrice;
