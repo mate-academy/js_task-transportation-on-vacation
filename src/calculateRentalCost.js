4df6c82adf1a0d42c07e0e8e54ce626987879fb3
mr-valentos <nerpavlad@gmail.com>
2022-11-17 11:15:49 +0200

    rental discount

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,17 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const cost = 40 * days;
+
+  if (days >= 3 && days < 7) {
+    return cost - 20;
+  }
+
+  if (days >= 7) {
+    return cost - 50;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
