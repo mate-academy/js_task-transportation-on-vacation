972373f191fcbaac1741d9eedb29ee04ace5ec14
Nurdan Okcu Tanich <nurokcudan@gmail.com>
2022-11-19 13:46:45 +0100

    if else improved

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -32,11 +32,13 @@ function calculateRentalCost(days) {
 
   if (days >= 7) {
     return cost - 50;
-  } else if (days >= 3) {
-    return cost - 20;
-  } else {
-    return cost;
   }
+
+  if (days >= 3) {
+    return cost - 20;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
