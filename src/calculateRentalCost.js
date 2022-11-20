194feb262eaeaae2b215e11716be6e9b4c919f06
Veronika Yasinska <yasinskaa.veronika@gmail.com>
2022-11-20 23:17:29 +0200

    edited checklist mistake

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -34,7 +34,9 @@ function calculateRentalCost(days) {
 
   if (days >= 7) {
     return days * carRent - advancedDiscount;
-  } else if (days >= 3) {
+  }
+
+  if (days >= 3) {
     return days * carRent - basicDiscount;
   }
 
