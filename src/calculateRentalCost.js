6c7842a1832e214e1c5a531c9c5b5d1383e90ddc
Anastasiia <anastasianauk44@gmail.com>
2022-11-21 12:07:21 +0200

    return in if was added

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,14 +28,14 @@
  */
 
 function calculateRentalCost(days) {
-  let amount = 40 * days;
+  const amount = 40 * days;
   const minDiscount = 20;
   const maxDiscount = 50;
 
   if (days >= 3 && days < 7) {
-    amount -= minDiscount;
+    return amount - minDiscount;
   } else if (days >= 7) {
-    amount -= maxDiscount;
+    return amount - maxDiscount;
   }
 
   return amount;
