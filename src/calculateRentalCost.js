fa858f1a3d8cc2493fcea369541667c6e1b91fbd
Olha Mamai <olgga.ju@gmail.com>
2022-11-21 14:40:10 +0200

    additional variables were created

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -31,12 +31,14 @@ function calculateRentalCost(days) {
   const dailyRent = 40;
   const shortTermDiscount = 20;
   const longTermDiscount = 50;
+  const longTerm = 7;
+  const shortTerm = 3;
 
-  if (days >= 7) {
+  if (days >= longTerm) {
     return dailyRent * days - longTermDiscount;
   }
 
-  if (days >= 3) {
+  if (days >= shortTerm) {
     return dailyRent * days - shortTermDiscount;
   }
 
