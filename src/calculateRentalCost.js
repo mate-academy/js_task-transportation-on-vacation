c068bfc48478b34ebea0aebcd8918ede73401a07
Alireza Mohammadhossein <mh.alireza1993@gmail.com>
2022-11-22 13:36:40 +0100

    fixed comments

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  if (days < 3) {
-    return days * 40;
-  }
-
-  if (days >= 3 && days < 7) {
-    return days * 40 - 20;
-  }
+  const cost = days * 40;
 
   if (days >= 7) {
-    return days * 40 - 50;
+    return cost - 50;
   }
+
+  if (days >= 3) {
+    return cost - 20;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
