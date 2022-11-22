96f9b9454d2188e2e0a93e2cd236d9ba760d739b
Levente Krotos <krotos.levi@gmail.com>
2022-11-22 10:54:52 +0100

    fixed code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,16 +29,18 @@
 
 function calculateRentalCost(days) {
   const total = 40 * days;
+  const normalDiscount = 20;
+  const extendedDiscount = 50;
 
   if (days >= 3 && days < 7) {
-    return total - 20;
+    return total - normalDiscount;
   }
 
   if (days >= 7) {
-    return total - 50;
-  } else {
-    return total;
+    return total - extendedDiscount;
   }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
