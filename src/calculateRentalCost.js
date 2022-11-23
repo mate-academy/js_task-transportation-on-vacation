937b3284178be87b77b2c239457bb52d51d84ba2
Oleksandr Dehtiarov <retabo.kainrou@gmail.com>
2022-11-24 00:04:56 +0200

    Solution js task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let costRental = 0;
+
+  for (let i = 0; i < days; i++) {
+    costRental += 40;
+  }
+
+  if (days >= 3 && days < 7) {
+    costRental -= 20;
+  } else if (days >= 7) {
+    costRental -= 50;
+  }
+
+  return costRental;
 }
 
 module.exports = calculateRentalCost;
