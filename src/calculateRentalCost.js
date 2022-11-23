e1a175ec926361858502212f54e518ca75414771
Daryna Zinchenko <zinchenko.daryna@gmail.com>
2022-11-23 16:00:17 +0100

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,20 @@
  */
 
 function calculateRentalCost(days) {
+  const basicDiscount = 20;
+  const extraDiscount = 50;
+  const oneDayCost = 40;
+
   if (days < 3) {
     return days * 40;
   }
 
   if (days < 7) {
-    return days * 40 - 20;
+    return days * oneDayCost - basicDiscount;
   }
 
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * oneDayCost - extraDiscount;
   }
 }
 
