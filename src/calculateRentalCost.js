118003c6488e488a60c07a245491593978755aa2
Ch0p3r84 <marin.iliev@yahoo.com>
2022-12-12 19:16:51 +0100

    fixed as per requested from review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,14 +28,16 @@
  */
 
 function calculateRentalCost(days) {
-  let rentalCost = days * 40;
-
-  if (days >= 3) {
-    rentalCost = (days * 40) - 20;
-  }
+  const rentalCost = days * 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
   if (days >= 7) {
-    rentalCost = (days * 40) - 50;
+    return rentalCost - bigDiscount;
+  }
+
+  if (days >= 3) {
+    return rentalCost - smallDiscount;
   }
 
   return rentalCost;
