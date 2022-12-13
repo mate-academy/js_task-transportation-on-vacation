75a03259b0ff196e20d8ffbfc93eba22cd90bb10
svarugg <youtrouble1@gmail.com>
2022-12-13 14:41:26 +0200

    not a commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,7 @@
  */
 
 function calculateRentalCost(days) {
-  if (days >= 7) {
-    return days * 40 - 50;
-  } else if (days >= 3) {
-    return days * 40 - 20;
-  } else {
-    return days * 40;
-  }
+  return days;
 }
 
 module.exports = calculateRentalCost;
