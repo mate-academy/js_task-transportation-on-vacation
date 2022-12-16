3f6d22b1c56a730c002a7dc2dfa56491a03ef02a
Karadzha Mykola <nicholaskaradzha@gmail.com>
2022-12-16 17:40:15 +0000

    variables added, else if construction removed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,11 +29,15 @@
 
 function calculateRentalCost(days) {
   const baseValue = 40;
+  const bigDiscount = 50;
+  const smallDiscount = 20;
 
   if (days >= 7) {
-    return days * baseValue - 50;
-  } else if (days >= 3) {
-    return days * baseValue - 20;
+    return days * baseValue - bigDiscount;
+  }
+
+  if (days >= 3) {
+    return days * baseValue - smallDiscount;
   }
 
   return days * baseValue;
