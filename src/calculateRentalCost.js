9913583e755a922975c987195b8151e435abc001
Igor Galkin <igor_galkin@ukr.net>
2022-12-23 15:11:13 +0200

    fixed remarks

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,11 +30,13 @@
 function calculateRentalCost(days) {
   const dayCost = 40;
   const totalCost = days * dayCost;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
   if (days >= 3 && days < 7) {
-    return totalCost - 20;
+    return totalCost - smallDiscount;
   } else if (days >= 7) {
-    return totalCost - 50;
+    return totalCost - bigDiscount;
   };
 
   return totalCost;
