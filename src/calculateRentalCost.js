2683cc80b5280ba416ac06fb67f93ca87423a076
Anastasiia-Khudych <anastasiiakhudych@gmail.com>
2022-12-24 19:19:17 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const totalSum = days * 40;
+
+  if (days < 3) {
+    return totalSum;
+  }
+
+  if (days >= 3 && days < 7) {
+    return totalSum - 20;
+  }
+
+  if (days >= 7) {
+    return totalSum - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
