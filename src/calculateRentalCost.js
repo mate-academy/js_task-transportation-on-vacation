f5f4a5848cb3a5326b2ae195bd70a4a7f6e26eb2
Andrey Berezyanskiy <andrey.berezyanskiy@gmail.com>
2022-12-27 18:23:54 +0200

    was added variables for values

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,16 @@
 
 function calculateRentalCost(days) {
   const rentCost = 40;
+  const lowSale = 20;
+  const highSale = 50;
   const totalRent = days * rentCost;
 
   if (days >= 7) {
-    return totalRent - 50;
+    return totalRent - highSale;
   }
 
   if (days >= 3) {
-    return totalRent - 20;
+    return totalRent - lowSale;
   }
 
   return totalRent;
