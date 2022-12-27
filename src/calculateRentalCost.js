8d6d30180e998e2faa2139d4f4fdf88ea16f6cf1
Iryna Pysanko <malva37@gmail.com>
2022-12-27 17:25:49 +0200

    remove extra validation, added variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  if (days < 3) {
-    return days * 40;
-  }
+  const rentCost = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
   if (days >= 3 && days < 7) {
-    return days * 40 - 20;
+    return rentCost * days - smallDiscount;
   }
 
   if (days >= 7) {
-    return days * 40 - 50;
+    return rentCost * days - bigDiscount;
   }
+
+  return days * rentCost;
 }
 
 module.exports = calculateRentalCost;
