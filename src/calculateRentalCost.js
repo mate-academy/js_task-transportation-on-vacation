963a309d85ecc017a2c165ae031fdcdb4121db74
Igor Kashchenko <blackangryrabbit@gmail.com>
2022-12-27 17:09:36 +0200

    optimized solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,13 +30,15 @@
 function calculateRentalCost(days) {
   const dayRent = 40;
   const calculation = days * dayRent;
-
-  if (days >= 3 && days < 7) {
-    return calculation - 20;
-  }
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
   if (days >= 7) {
-    return calculation - 50;
+    return calculation - bigDiscount;
+  }
+
+  if (days >= 3) {
+    return calculation - smallDiscount;
   }
 
   return calculation;
