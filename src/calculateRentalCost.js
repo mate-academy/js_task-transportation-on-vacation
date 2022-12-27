a325cff6df1c5afbdd72428dc21e57b37f1ee076
Oleh Chermak <ochermak2002@gmail.com>
2022-12-27 16:08:41 +0200

    Updated

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,15 @@
  */
 
 function calculateRentalCost(days) {
+  const price = days * 40;
+
   if (days > 6) {
-    return days * 40 - 50;
+    return price - 50;
   } else if (days > 2) {
-    return days * 40 - 20;
+    return price - 20;
   }
 
-  return days * 40;
+  return price;
 }
 
 module.exports = calculateRentalCost;
