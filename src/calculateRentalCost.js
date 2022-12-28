6d1758481ecb8c57e295bbc45ce30e07657d016b
Bohdan Kutasevich <obvinmusic@gmail.com>
2022-12-28 13:59:51 +0200

    added variables for repeat code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,16 +29,19 @@
 
 function calculateRentalCost(days) {
   const perDay = 40;
+  const amountWithoutDiscont = days * perDay;
+  const discountForSeven = 50;
+  const discountForThree = 20;
 
   if (days >= 7) {
-    return days * perDay - 50;
+    return amountWithoutDiscont - discountForSeven;
   }
 
   if (days >= 3) {
-    return days * perDay - 20;
+    return amountWithoutDiscont - discountForThree;
   }
 
-  return days * perDay;
+  return amountWithoutDiscont;
 }
 
 module.exports = calculateRentalCost;
