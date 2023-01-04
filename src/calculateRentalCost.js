e6109975cf1df9ddc6526871508bcc63f790e202
Nataliia Batiukova <batiukova.nataliia@gmail.com>
2023-01-04 09:49:47 +0000

    variable total is removed and early return is applied

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,13 @@
  */
 
 function calculateRentalCost(days) {
-  let total = 0;
-
   if (days < 3) {
-    total = days * 40;
+    return days * 40;
   } else if (days >= 3 && days <= 6) {
-    total = days * 40 - 20;
+    return days * 40 - 20;
   } else {
-    total = days * 40 - 50;
+    return days * 40 - 50;
   }
-
-  return total;
 }
 
 module.exports = calculateRentalCost;
