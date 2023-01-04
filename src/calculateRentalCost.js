9b30adbe6d332e7c624e6e092fe44ee0bcc119bd
Oleksandra-Test <121618578+Oleksandra-Test@users.noreply.github.com>
2023-01-04 11:34:40 +0200

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const oneDayCost = 40;
+
+  if (days < 3) {
+    return oneDayCost * days;
+  }
+
+  if (days >= 3 & days < 7) {
+    return oneDayCost * days - 20;
+  }
+
+  if (days >= 7) {
+    return oneDayCost * days - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
