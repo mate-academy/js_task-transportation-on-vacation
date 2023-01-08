28b535805cc8e7b3288cfe48f8aaebc2e130fb98
Ivan <90928916+vanyachyzh@users.noreply.github.com>
2023-01-08 11:31:59 +0200

    task is fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,19 @@
  */
 
 function calculateRentalCost(days) {
+  const regularPrice = days * 40;
+  const bigDiscount = 50;
+  const smallDiscount = 20;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return regularPrice - bigDiscount;
   }
 
   if (days >= 3) {
-    return days * 40 - 20;
+    return regularPrice - smallDiscount;
   }
 
-  return days * 40;
+  return regularPrice;
 }
 
 module.exports = calculateRentalCost;
