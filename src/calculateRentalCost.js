d20b9adec0ecafa93f05fb6d1345cf1791712535
Mykola-Borodavka <k_borodavka@ukr.net>
2023-01-16 00:02:29 +0200

    First commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,18 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const cost = 40;
+  let price = cost * days;
+
+  if (days >= 3 && days < 7) {
+    price -= 20;
+  }
+
+  if (days >= 7) {
+    price -= 50;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
