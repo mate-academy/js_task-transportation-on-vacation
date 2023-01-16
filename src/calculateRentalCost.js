990fe0171bb6162bcf80b220a07dc06df9e714c8
Nikolay Kapshuk <nikolajkapsuk22@gmail.com>
2023-01-16 18:58:09 +0200

    second commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
-}
+  let money = 0;
+
+  money = days * 40;
+
+  if (days > 6) {
+    money = days * 40 - 50;
+  } else if (days > 2) {
+    money = days * 40 - 20;
+  }
+
+  return money;
+};
 
 module.exports = calculateRentalCost;
