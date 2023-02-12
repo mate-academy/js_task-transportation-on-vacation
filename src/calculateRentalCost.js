1250e91bde1679314290a0c67e25d14be4d429b6
Anna <annaliman87@gmail.com>
2023-02-12 22:44:28 +0100

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,14 +28,16 @@
  */
 
 function calculateRentalCost(days) {
+  const bigDiscount = 50;
+  const smallDiscount = 20;
   let result = days * 40;
 
   if (days >= 7) {
-    result = result - 50;
+    result = result - bigDiscount;
   }
 
   if (days >= 3 && days < 7) {
-    result = result - 20;
+    result = result - smallDiscount;
   }
 
   return result;
