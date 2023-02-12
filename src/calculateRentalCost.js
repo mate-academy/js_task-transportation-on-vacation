5eb4565d3da0c77e967b93b5d588b2a84f4667e9
Ultramarin <000100010111ultramrin@gmail.com>
2023-02-12 23:11:37 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,18 @@
 
 function calculateRentalCost(days) {
   // write code here
+  let totalSum = days * 40;
+
+  // eslint-disable-next-line no-constant-condition
+  if ((days >= 3) && (days < 7)) {
+    totalSum -= 20;
+  };
+
+  if (days >= 7) {
+    totalSum -= 50;
+  };
+
+  return totalSum;
 }
 
 module.exports = calculateRentalCost;
