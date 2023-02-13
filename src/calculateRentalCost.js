998a75ae9a8b55eb760b8b8b3f976573e37ca5b7
Yaroslav <yaroslav.dzyadiv@gmail.com>
2023-02-13 13:23:02 +0200

    Soleved

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentPerDay = 40;
+
+  if (days < 3) {
+    return days * rentPerDay;
+  }
+
+  if (days < 7) {
+    return days * rentPerDay - 20;
+  }
+
+  if (days >= 7) {
+    return days * rentPerDay - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
