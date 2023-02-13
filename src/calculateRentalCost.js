d5a7027bb90a7a3d96405ca64058ed995713da69
Dmytro <kuharchuk.dmytro1337@gmail.com>
2023-02-13 15:35:12 +0200

    variables for price and discounts were created

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,19 @@
  */
 
 function calculateRentalCost(days) {
+  const rentPrice = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
   if (days < 3) {
-    return days * 40;
+    return days * rentPrice;
   }
 
   if (days < 7) {
-    return days * 40 - 20;
+    return days * rentPrice - smallDiscount;
   }
 
-  return days * 40 - 50;
+  return days * rentPrice - bigDiscount;
 }
 
 module.exports = calculateRentalCost;
