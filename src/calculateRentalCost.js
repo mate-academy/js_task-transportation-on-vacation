c1d95261fee31884cf5dd4c5b47be97130e6eb91
milenadiamonds1 <milenadubova16@gmail.com>
2023-02-15 14:29:35 +0200

    fixed all

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,13 +29,17 @@
 
 function calculateRentalCost(days) {
   const price = 40;
-
   const calculate = price * days;
 
+  const discountOnSeven = 50;
+  const discountOnThree = 20;
+
   if (days >= 7) {
-    return calculate - 50;
-  } else if (days >= 3) {
-    return calculate - 20;
+    return calculate - discountOnSeven;
+  }
+
+  if (days >= 3) {
+    return calculate - discountOnThree;
   }
 
   return calculate;
