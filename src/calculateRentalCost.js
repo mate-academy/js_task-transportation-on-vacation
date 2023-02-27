5969accd24c2885a78287775cf261092965f3830
anton_chornobai <antonchornobajj@gmail.com>
2023-02-27 18:41:22 +0100

    small and big discounts were added

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,12 +8,15 @@
 
 function calculateRentalCost(days) {
   const pricePerDay = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
   let sum = days * pricePerDay;
 
   if (days >= 7) {
-    sum -= 50;
+    sum -= bigDiscount;
   } else if (days >= 3) {
-    sum -= 20;
+    sum -= smallDiscount;
   }
 
   return sum;
