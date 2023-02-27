fbb8ea24094279df0f1e6fba6085302dcdfc96b3
Yurii Hlushenko <yauglushenko@gmail.com>
2023-02-27 17:21:41 +0200

    magic numbers fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -31,17 +31,19 @@ function calculateRentalCost(days) {
   const dayRent = 40;
   const discount = 20;
   const weekDiscount = 50;
+  const threeDays = 3;
+  const week = 7;
 
-  if (days < 3) {
+
+  if (days < threeDays) {
     return days * dayRent;
   }
 
-  if (days < 7) {
+  if (days < week) {
     return (days * dayRent) - discount;
   }
 
   return (days * dayRent) - weekDiscount;
-
 }
 
 module.exports = calculateRentalCost;
