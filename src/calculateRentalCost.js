c46f76447ab04fe08a7fa5cd935a6c636d9c5e18
yandapanda19 <i.am.kitiket@gmail.com>
2023-03-02 15:59:45 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const total = days * 40;
+
+  if (days >= 3 && days < 7) {
+    return total - 20;
+  } else if (days >= 7) {
+    return total - 50;
+  } else {
+    return total;
+  }
 }
 
 module.exports = calculateRentalCost;
