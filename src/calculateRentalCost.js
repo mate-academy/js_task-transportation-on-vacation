9ebb4e5e8190d0cc83ccbc1ee4ebed605287b137
Vasyl Zhyliakov <snak871@gmail.com>
2023-03-12 13:41:43 +0200

    Rental cost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let totalAmount = 0;
+
+  if (days >= 7) {
+    totalAmount = days * 40 - 50;
+  }
+
+  if (days < 7 && days >= 3) {
+    totalAmount = days * 40 - 20;
+  }
+
+  if (days < 3) {
+    totalAmount = days * 40;
+  }
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
