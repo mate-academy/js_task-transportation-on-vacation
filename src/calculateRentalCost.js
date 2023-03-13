49d960b65af3558f53919b121359e1fb6c53a398
Vyacheslav Stolyarchuk <haunt.beta@gmail.com>
2023-03-13 12:47:40 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,11 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  switch (true) {
+    case days < 3: return days * 40;
+    case days < 7: return days * 40 - 20;
+    case days >= 7: return days * 40 - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
