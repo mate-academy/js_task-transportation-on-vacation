b484fe4b57421d80282c91e86b2217821356e658
kukla1989 <kukla.1989@ukr.net>
2023-03-13 17:47:31 +0200

    solve task calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const priceWithoutDiscount = days * 40;
+
+  if (days < 3) {
+    return priceWithoutDiscount;
+  } else if (days < 7) {
+    return priceWithoutDiscount - 20;
+  }
+
+  return priceWithoutDiscount - 50;
 }
 
 module.exports = calculateRentalCost;
