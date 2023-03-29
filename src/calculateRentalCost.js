0b83fa45cc2add0bd9e1e8c1d4960f25a0b178f9
Paweł Smoliński <ski1993@o2.pl>
2023-03-29 11:20:01 +0200

    fixed solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,19 +29,20 @@
 
 function calculateRentalCost(days) {
   // write code here
-  const costPerDay = 40;
+  const costPerDay = days * 40;
   const daysForBigDiscout = 7;
   const daysForSmallDiscout = 3;
   const smallDiscount = 20;
   const bigDiscount = 50;
 
+
   if (days >= daysForBigDiscout) {
-    return (days * costPerDay) - bigDiscount;
+    return costPerDay - bigDiscount;
   } else if (days >= daysForSmallDiscout) {
-    return (days * costPerDay) - smallDiscount;
+    return costPerDay - smallDiscount;
   }
 
-  return days * costPerDay;
+  return costPerDay;
 }
 
 module.exports = calculateRentalCost;
