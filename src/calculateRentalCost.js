2583097f022d44d62850878621be77f2e814f8fc
catgode <105614630+catgode@users.noreply.github.com>
2023-03-30 01:42:02 +0200

    checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,20 @@
  */
 
 function calculateRentalCost(days) {
-  let cost = days * 40;
+  const basicCost = days * 40;
+  const smallDiscount = 20;
+  const largeDiscount = 50;
+  const shortDiscountBreakpoint = 3;
+  const longDiscountBreakpoint = 7;
+  let finalCost = basicCost;
 
-  if (days >= 7) {
-    cost = cost - 50;
-  } else if (days >= 3) {
-    cost = cost - 20;
+  if (days >= longDiscountBreakpoint) {
+    finalCost -= largeDiscount;
+  } else if (days >= shortDiscountBreakpoint) {
+    finalCost -= smallDiscount;
   }
 
-  return cost;
+  return finalCost;
 }
 
 module.exports = calculateRentalCost;
