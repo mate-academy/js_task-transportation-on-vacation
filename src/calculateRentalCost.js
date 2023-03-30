c7b0f6fe846ca4b1900865b6cf12696ddfcecb00
kq333 <afugowski@yandex.com>
2023-03-30 11:04:03 +0200

    fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -31,17 +31,18 @@ function calculateRentalCost(days) {
   const mdDiscount = 20;
   const lgDiscount = 50;
   const price = 40;
+  const priceBeforDiscount = days * price;
 
   if (days <= 2) {
-    return days * price;
+    return priceBeforDiscount;
   }
 
   if (days >= 3 && days <= 6) {
-    return (days * price) - mdDiscount;
+    return priceBeforDiscount - mdDiscount;
   }
 
   if (days >= 7) {
-    return (days * price) - lgDiscount;
+    return priceBeforDiscount - lgDiscount;
   }
 }
 
