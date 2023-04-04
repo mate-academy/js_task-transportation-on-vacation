cece003ecef5c3aad643ce1c1b996c0972258104
Yevhen Bilenkiy <evgenbilenkiy@gmail.com>
2023-04-04 22:45:22 +0300

    Fixes after code rewiew

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -31,14 +31,16 @@
 function calculateRentalCost(days) {
   // write code here
   const costPerDay = 40;
+  const costPerDayForThree = 20;
+  const costPerDayForSeven = 50;
   const totalCost = costPerDay * days;
 
   if (days >= 7) {
-    return totalCost - 50;
+    return totalCost - costPerDayForSeven;
   }
 
   if (days >= 3) {
-    return totalCost - 20;
+    return totalCost - costPerDayForThree;
   }
 
   return totalCost;
