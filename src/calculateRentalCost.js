02685ce543353e1021bf561764928b8410260825
Atlas Werecik <mw.greybeard72@gmail.com>
2023-04-04 23:04:55 +0200

    comments fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,13 +30,18 @@
 function calculateRentalCost(days) {
   // write code here
   const cost = 40;
-  const total = (cost * days);
+  const total = cost * days;
   const largeDiscount = 50;
   const smallDiscount = 20;
+  const noDiscount = 2;
   const largeDiscountReq = 7;
   const smallDiscountReq = 3;
 
-  if (days < largeDiscountReq && days >= smallDiscountReq) {
+  if (days <= noDiscount) {
+    return total;
+  }
+
+  if (days < largeDiscountReq) {
     return total - smallDiscount;
   }
 
