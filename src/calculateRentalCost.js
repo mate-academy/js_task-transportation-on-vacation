72c3587cd8f2c550429580a5b6fb2786453ae2b4
Mateusz Michalowski <mateusz.michalowski000@gmail.com>
2023-04-05 15:31:43 +0200

    update

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,26 +30,19 @@
 function calculateRentalCost(days) {
   // write code here
   const pricePerDay = 40;
-  const sevenDayDiscount = 50;
-  const threeDayDiscount = 20;
+  const smallDiscount = 50;
+  const bigDiscount = 20;
   const totalCost = days * pricePerDay;
 
   if (days >= 7) {
-    return totalCost - sevenDayDiscount;
-  } else if (days >= 3) {
-    return totalCost - threeDayDiscount;
-  } else {
-    return totalCost;
+    return totalCost - smallDiscount;
   }
 
-  // switch (true) {
-  //   case days >= 7:
-  //     return (days * 40) - 50;
-  //   case days >= 3:
-  //     return (days * 40) - 20;
-  //   default:
-  //     return (days * 40);
-  // }
+  if (days >= 3) {
+    return totalCost - bigDiscount;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
