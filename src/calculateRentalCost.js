ab23b7c1417b7fc0a1fcc1547c5aaae663fb2862
AnastassiaH <nastyagurska@gmail.com>
2023-04-07 18:36:36 +0300

    add const numbres

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,17 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const price = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
   let total;
 
   if (days >= 7) {
-    total = days * 40 - 50;
+    total = days * price - bigDiscount;
   } else if (days >= 3) {
-    total = days * 40 - 20;
+    total = days * price - smallDiscount;
   } else {
-    total = days * 40;
+    total = days * price;
   }
 
   return total;
