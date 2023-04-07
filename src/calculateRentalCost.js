86569fc2edbb7f946ff86a137ba8854313e170e6
Leonid Krainik <rialleons@gmail.com>
2023-04-07 11:22:37 +0300

    Calculate the cost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,18 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentCost = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
+  switch (true) {
+    case days < 3:
+      return days * rentCost;
+    case days < 7:
+      return (days * rentCost) - smallDiscount;
+    default:
+      return (days * rentCost) - bigDiscount;
+  }
 }
 
 module.exports = calculateRentalCost;
