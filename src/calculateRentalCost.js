1201982d67b0815866a2fa6280ac22b43c439690
Nikita211112 <nikita211112@icloud.com>
2023-04-12 10:18:43 +0200

    Code was changed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,14 +28,16 @@
  */
 
 function calculateRentalCost(days) {
-  let sum = 0;
+  const bigDiscount = 50;
+  const smallDiscount = 20;
+  const sum = days * 40;
 
   if (days >= 7) {
-    sum = (days * 40) - 50;
-  } else if (days >= 3 && days < 7) {
-    sum = (days * 40) - 20;
-  } else {
-    sum = days * 40;
+    return sum - bigDiscount;
+  }
+
+  if (days >= 3 && days < 7) {
+    return sum - smallDiscount;
   }
 
   return sum;
