48c8ee36d8d8098d659010cad706cb0bd130391b
Oleksandr Burovetskyi <oleksandr.burovetskyi.knm.2020@lpnu.ua>
2023-04-14 12:38:30 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const dayCost = 40;
+
+  let discount = 0;
+
+  if (days >= 3) {
+    discount += 20;
+  }
+
+  if (days >= 7) {
+    discount += 30;
+  }
+
+  return dayCost * days - discount;
 }
 
 module.exports = calculateRentalCost;
