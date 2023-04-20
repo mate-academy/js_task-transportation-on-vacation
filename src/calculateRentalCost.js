7310092058eafb89c851ba57cf35a3ac11a8401a
Oleksii Lavka <lavka.alexey37gmail.com>
2023-04-20 19:38:46 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,10 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const carPrice = 40;
+  const discount = days >= 3 && days < 7 ? 20 : days >= 7 ? 50 : 0;
+
+  return carPrice * days - discount;
 }
 
 module.exports = calculateRentalCost;
