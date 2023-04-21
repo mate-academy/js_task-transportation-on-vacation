b902c4d7b2bbf90c6b3391bd5c3ec3a9d414b16a
Oleksandr_Hundertailo <oleksandrhundertailo@gmail.com>
2023-04-21 15:29:48 +0200

    added changes according to mentor comments

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,15 +29,17 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const dailyCost = 40;
+
   if (days >= 7) {
-    return ((days * 40) - 50);
+    return ((days * dailyCost) - 50);
   }
 
   if (days >= 3) {
-    return ((days * 40) - 20);
+    return ((days * dailyCost) - 20);
   }
 
-  return (days * 40);
+  return (days * dailyCost);
 }
 
 module.exports = calculateRentalCost;
