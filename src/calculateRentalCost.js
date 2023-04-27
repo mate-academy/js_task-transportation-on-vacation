6eb16002e8161d90c672e87e4dd0615e23e63ff0
Oleksii Hrytsenko <oleksii.hrytsenko@icloud.com>
2023-04-27 16:47:21 +0300

    Fixes to code review;

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,15 +30,15 @@
 function calculateRentalCost(days) {
   const sumPerDay = 40;
   const totalSum = sumPerDay * days;
-  const minGetOff = 20;
-  const maxGetOff = 50;
+  const smallDiscount = 20;
+  const largeDiscount = 50;
 
   if (days >= 7) {
-    return totalSum - maxGetOff;
+    return totalSum - largeDiscount;
   }
 
   if (days >= 3) {
-    return totalSum - minGetOff;
+    return totalSum - smallDiscount;
   }
 
   return totalSum;
