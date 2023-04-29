9d8ae40b3127e6850669629ef9686e24e894486e
Your Name <you@example.com>
2023-04-29 16:04:37 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let rentalCost = 0;
+
+  rentalCost = days * 40;
+
+  if (days >= 3) {
+    rentalCost = (days * 40) - 20;
+  }
+
+  if (days >= 7) {
+    rentalCost = (days * 40) - 50;
+  }
+
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
