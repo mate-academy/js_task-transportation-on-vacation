e712eb8eb5c0cb541745473e9806f00f42f18449
Evhen Holub <golub0208@gmail.com>
2023-05-03 17:42:33 +0300

    task done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const price = 40;
+  const calculate = days * price;
+  const smallTotal = 20;
+  const bigTotal = 50;
+
+  let total = 0;
+
+  if (days < 3) {
+    total = calculate;
+  } else if (days < 7 && days >= 3) {
+    total = calculate - smallTotal;
+  } else if (days >= 7) {
+    total = calculate - bigTotal;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
