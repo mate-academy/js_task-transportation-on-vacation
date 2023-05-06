d389b125399f210888111805534cb5faec316815
Stoianova Ivanna <ctoyanova13@gmail.com>
2023-05-06 11:27:39 +0300

    fix after review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,18 @@
  */
 
 function calculateRentalCost(days) {
+  const costs = 40;
+
   if (days <= 0) {
-    return 'error';
+    return 'error text';
   }
 
   if (days < 3) {
-    return days * 40;
+    return days * costs;
   } else if (days >= 3 && days < 7) {
-    return days * 40 - 20;
+    return days * costs - 20;
   } else if (days >= 7) {
-    return days * 40 - 50;
+    return days * costs - 50;
   }
 }
 
