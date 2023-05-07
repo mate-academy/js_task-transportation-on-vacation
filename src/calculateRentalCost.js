3a24bdc3e6f0dfebe737674fabaf6fdff83d3dba
Mykola Lopushenko <21kolya05lopushenko@gmail.com>
2023-05-07 15:38:36 -0700

    vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentCost = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
+  if (days >= 3) {
+    return rentCost * days - smallDiscount;
+  }
+
+  if (days >= 7) {
+    return rentCost * days - bigDiscount;
+  }
+
+  return rentCost * days;
 }
 
 module.exports = calculateRentalCost;
