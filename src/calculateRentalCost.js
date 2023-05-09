537aaa8518e4b976d4b5b64d229949d28d4dce1d
Maryan Mats <cholgyni@gmail.com>
2023-05-09 15:01:04 +0300

    fix every mistakes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,28 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const costDay = 40;
+  const weekDiscount = 50;
+  const threeDayDiscount = 20;
+  const week = 7;
+  const threeDays = 3;
+  let total = 0;
+
+  if (days >= week) {
+    total = costDay * days - weekDiscount;
+
+    return total;
+  }
+
+  if (days >= threeDays) {
+    total = days * costDay - threeDayDiscount;
+
+    return total;
+  }
+
+  total = days * costDay;
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
