8da115b6d8b70490b5107a8f61b11750edb21c6c
Bogdan Parubchenko <bogdanparubchenko@gmail.com>
2023-05-09 19:51:09 +0300

    Solution2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -31,12 +31,14 @@ function calculateRentalCost(days) {
   const total = days * 40;
   const smallDiscount = 20;
   const bigDiscount = 50;
+  const week = 7;
+  const severalDays = 3;
 
-  if (days >= 7) {
+  if (days >= week) {
     return total - bigDiscount;
   }
 
-  if (days >= 3) {
+  if (days >= severalDays) {
     return total - smallDiscount;
   }
 
