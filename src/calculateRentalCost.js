9682590da070bbab2175b513af9d7330af541ce7
Lidia Pintos Medina <lidia_pm@msn.com>
2023-05-11 00:33:57 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,16 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const basePrice = 40;
+  const pricePerDays = days * basePrice;
+
+  if (days >= 7) {
+    return pricePerDays - 50;
+  } else if (days >= 3 && days <= 6) {
+    return pricePerDays - 20;
+  }
+
+  return pricePerDays;
 }
 
 module.exports = calculateRentalCost;
