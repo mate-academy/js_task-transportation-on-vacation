7b6668db4e7af7a18d8ce27307675e4f3411a766
Anna Holovko <anutkagolovko1472@gmail.com>
2023-05-11 15:01:03 +0300

    fix mistakes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -18,9 +18,9 @@
  * which returns the total amount for different count of days.
  *
  * Examples:
- *  - calculateRentalCost(1) === 40
- *  - calculateRentalCost(3) === 100
- *  - calculateRentalCost(7) === 230
+ *  - calculateRentalCost(1) === 40;
+ *  - calculateRentalCost(3) === 100;
+ *  - calculateRentalCost(7) === 230;
  *
  * @param {number} days
  *
