8a7e63b7a2ae011b2b15342c86c577700f9445a4
Alona Shelikhova <ashelyaa17@gmail.com>
2023-05-11 12:12:09 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -30,20 +30,26 @@
 function calculateRentalCost(days) {
   const value = 40;
   let sum = 0;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
 
-  if (days >= 1) {
-    sum = days * value;
+  if (days >= 7) {
+    sum = days * value - bigDiscount;
+
+    return sum;
   }
 
   if (days >= 3) {
-    sum = days * value - 20;
+    sum = days * value - smallDiscount;
+
+    return sum;
   }
 
-  if (days >= 7) {
-    sum = days * value - 50;
-  }
+  if (days >= 1) {
+    sum = days * value;
 
-  return sum;
+    return sum;
+  }
 }
 
 module.exports = calculateRentalCost;
