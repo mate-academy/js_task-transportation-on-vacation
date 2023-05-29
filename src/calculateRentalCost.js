1643daf082c97cb7627c230584e5a3143191cb7a
Alisa Skrypnyk <topt18@gmail.com>
2023-05-29 15:43:08 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,13 +29,15 @@
 
 function calculateRentalCost(days) {
   const dayPrice = 40;
+  const basicDiscount = 50;
+  const minimalDiscount = 20;
 
   if (days >= 7) {
-    return (days * dayPrice) - 50;
+    return (days * dayPrice) - basicDiscount;
   }
 
   if (days >= 3) {
-    return (days * dayPrice) - 20;
+    return (days * dayPrice) - minimalDiscount;
   }
 
   if (days < 3) {
