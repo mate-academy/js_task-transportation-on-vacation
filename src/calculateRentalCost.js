fcfaa41ecff98c24e7461281d3eef5bc5bcb5f3e
denys2 <brownexpert@gmail.com>
2023-05-31 22:23:13 +0300

    Solution Create variables for 40, 20 and 50

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  const totalCost = days * 40;
+  const carPriceDay = 40;
+  const discountSevenDays = 50;
+  const discountTwoDays = 20;
 
   if (days < 3) {
-    return totalCost;
-  } else if (days < 7) {
-    return totalCost - 20;
+    return days * carPriceDay;
   }
 
-  return totalCost - 50;
+  if (days <= 6) {
+    return (days * carPriceDay) - discountTwoDays;
+  }
+
+  return (days * carPriceDay) - discountSevenDays;
 }
 
 module.exports = calculateRentalCost;
