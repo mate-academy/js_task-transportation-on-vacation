3eacafaaee1b21686e165a32c11fb42124d8825c
IlonaBorkovska <borkovska.io@gmail.com>
2023-05-31 16:58:55 +0300

    fix mistakes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,18 @@
 
 function calculateRentalCost(days) {
   const totalCost = days * 40;
+  const shortTermDiscount = 20;
+  const longTermDiscount = 50;
 
   if (days < 3) {
     return totalCost;
-  } else if (days < 7) {
-    return totalCost - 20;
   }
 
-  return totalCost - 50;
+  if (days < 7) {
+    return totalCost - shortTermDiscount;
+  }
+
+  return totalCost - longTermDiscount;
 }
 
 module.exports = calculateRentalCost;
