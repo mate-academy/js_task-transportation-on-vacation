350e90562d43736ba37078f2e83c8529da8d43ff
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-06-01 19:36:36 +0300

    add const

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,9 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  return days < 3 ? days * 40
-    : days < 7 ? (days * 40) - 20
-      : (days * 40) - 50;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  return days < SHORT_TERM ? days * PRICE_PER_DAY
+    : days < LONG_TERM ? (days * PRICE_PER_DAY) - SHORT_TERM_DISCOUNT
+      : (days * PRICE_PER_DAY) - LONG_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
