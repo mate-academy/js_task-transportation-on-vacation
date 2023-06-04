5971f5313511b2f996a7ceb6c84aaba7e6b1f6b6
Dima Astapenko <bonum_taurus@ukr.net>
2023-06-04 14:38:54 +0300

    fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,21 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
+  const PRICE_PER_DAY = 40 * days;
   const LONG_PERIOD = 7;
   const LONG_PERIOD_DISCOUNT = 50;
   const SHORT_PERIOD = 3;
   const SHORT_PERIOD_DISCOUNT = 20;
 
   if (days >= LONG_PERIOD) {
-    return days * PRICE_PER_DAY - LONG_PERIOD_DISCOUNT;
+    return PRICE_PER_DAY - LONG_PERIOD_DISCOUNT;
   }
 
   if (days >= SHORT_PERIOD) {
-    return days * PRICE_PER_DAY - SHORT_PERIOD_DISCOUNT;
+    return PRICE_PER_DAY - SHORT_PERIOD_DISCOUNT;
   }
 
-  return days * PRICE_PER_DAY;
+  return PRICE_PER_DAY;
 }
 
 module.exports = calculateRentalCost;
