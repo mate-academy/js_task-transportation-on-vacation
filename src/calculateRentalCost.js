8f1479af0034cc1fc413ecb0d7dc36ee116436f5
Oleh Kandaskalov <olegkandaskalov@gmail.com>
2023-06-07 13:25:18 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -34,11 +34,17 @@ function calculateRentalCost(days) {
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  return days < SHORT_TERM
-    ? days * PRICE_PER_DAY
-    : days < LONG_TERM
-      ? (days * PRICE_PER_DAY) - SHORT_TERM_DISCOUNT
-      : (days * PRICE_PER_DAY) - LONG_TERM_DISCOUNT;
+  if (days < SHORT_TERM) {
+    return days * PRICE_PER_DAY;
+  }
+
+  if (days < LONG_TERM) {
+    return (days * PRICE_PER_DAY) - SHORT_TERM_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    return (days * PRICE_PER_DAY) - LONG_TERM_DISCOUNT;
+  }
 }
 
 module.exports = calculateRentalCost;
