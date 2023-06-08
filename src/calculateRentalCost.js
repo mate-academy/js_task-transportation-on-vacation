6f4899f3533fb21d1cccbb161ec00b8586ae473b
ihorhladkov <ihor.hladkov@gmail.com>
2023-06-08 14:16:02 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const SHORT_PERIOD = 3;
+  const SHORT_DISCOUNT = 20;
+  const LONG_PERIOD = 7;
+  const LONG_DISCOUNT = 50;
+
+  const totalSum = PRICE_PER_DAY * days;
+
+  if (days >= LONG_PERIOD) {
+    return totalSum - LONG_DISCOUNT;
+  }
+
+  if (days >= SHORT_PERIOD) {
+    return totalSum - SHORT_DISCOUNT;
+  }
+
+  return totalSum;
 }
 
 module.exports = calculateRentalCost;
