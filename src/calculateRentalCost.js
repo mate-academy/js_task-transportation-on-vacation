d34acfeb1388d92b80db7147eae2cd59c535e689
Andrii Melnyk <andrii.melnyk1994@icloud.com>
2023-06-09 14:47:31 +0300

    fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,22 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
-  const LONG_TERM = 7;
-  const LONG_TEMM_DISCOUNT = 50;
+  const COST_PER_DAY = 40;
   const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
-  const basePrice = days * PRICE_PER_DAY;
+  const LONG_TERM = 7;
 
-  if (days < SHORT_TERM) {
-    return basePrice;
+  let discount = 0;
+
+  if (days >= SHORT_TERM) {
+    discount = 20;
   }
 
-  if (days < LONG_TERM) {
-    return basePrice - SHORT_TERM_DISCOUNT;
+  if (days >= LONG_TERM) {
+    discount = 50;
   }
 
-  return basePrice - LONG_TEMM_DISCOUNT;
+  return (COST_PER_DAY * days) - discount;
 }
 
 module.exports = calculateRentalCost;
