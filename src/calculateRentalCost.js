f9e99f2bbacc50155dd9a06690a45c772174e2d8
Valeriy Lopatko <vlopatko1999@gmail.com>
2023-06-10 00:04:02 +0300

    added task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const rentLongTime = 7;
+  const rentSmallTime = 3;
+  const rentDefaultCost = 40;
+  const saleLongTime = 50;
+  const saleSmallTime = 20;
+
+  if (days >= rentSmallTime) {
+    return days >= rentLongTime
+      ? days * rentDefaultCost - saleLongTime
+      : days * rentDefaultCost - saleSmallTime;
+  };
+
+  return days * rentDefaultCost;
 }
 
 module.exports = calculateRentalCost;
