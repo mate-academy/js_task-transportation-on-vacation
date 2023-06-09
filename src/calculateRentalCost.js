517a5b59fdcf18e810a087d7bcae809422033a45
Yevhen <ko4etov.evgeniy@gmail.com>
2023-06-09 17:39:24 +0300

    create function rental costs

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const COSTS_RENT_CAR_PER_DAY = 40;
+  const DISCOUNT_LONG_TERM = 50;
+  const DISCOUNT_SHORT_TERM = 20;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+
+  const basePrice = COSTS_RENT_CAR_PER_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return basePrice - DISCOUNT_LONG_TERM;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - DISCOUNT_SHORT_TERM;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
