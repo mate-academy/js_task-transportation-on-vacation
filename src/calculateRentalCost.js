a46cafb48a501464e15eb072d3ed689b5fa86443
Andrii Melnyk <andrii.melnyk1994@icloud.com>
2023-06-09 14:26:05 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TEMM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days < SHORT_TERM) {
+    return basePrice;
+  }
+
+  if (days < LONG_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice - LONG_TEMM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
