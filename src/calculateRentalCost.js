fb0885e17a30148affe23729fc916283f98206bd
Oleh Khalanskyi <okhalanskyi@gmail.com>
2023-06-10 20:34:23 +0300

    fix: fixed by checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -34,13 +34,17 @@ function calculateRentalCost(days) {
   const LONG_TERM_DISCOUNT = 50;
   const DAY_RENT_COST = 40;
 
-  if (days >= SHORT_TERM && days < LONG_TERM) {
-    return DAY_RENT_COST * days - SHORT_TERM_DISCOUNT;
-  } else if (days >= LONG_TERM) {
-    return DAY_RENT_COST * days - LONG_TERM_DISCOUNT;
+  const basePrice = days * DAY_RENT_COST;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  return DAY_RENT_COST * days;
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
