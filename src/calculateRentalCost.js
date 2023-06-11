96868d61ec6850b3bd8d46922a504f658c4539a5
Zoriana Melnychuk <zoryashka3002@gmail.com>
2023-06-11 14:37:51 +0200

    added the required variable

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -34,16 +34,17 @@ function calculateRentalCost(days) {
   const BIG_DISCOUNT = 50;
   const LONG_TERM = 7;
   const SHORT_TERM = 3;
+  const basePrice = COST_PER_DAY * days;
 
   if (days >= LONG_TERM) {
-    return days * COST_PER_DAY - BIG_DISCOUNT;
+    return basePrice - BIG_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return days * COST_PER_DAY - SMALL_DISCOUNT;
+    return basePrice - SMALL_DISCOUNT;
   }
 
-  return days * COST_PER_DAY;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
