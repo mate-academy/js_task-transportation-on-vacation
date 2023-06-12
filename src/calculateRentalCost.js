01f4cc710489c9b49d75a52b09907439596f0f73
Vladyslav <morphey138@gmail.com>
2023-06-12 10:14:11 +0300

    minor cod fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -31,12 +31,14 @@ function calculateRentalCost(days) {
   const rentCost = days * 40;
   const midDiscount = 20;
   const largeDiscount = 50;
+  const longTerm = 7;
+  const midTerm = 3;
 
-  if (days >= 7) {
+  if (days >= longTerm) {
     return rentCost - largeDiscount;
   }
 
-  if (days >= 3) {
+  if (days >= midTerm) {
     return rentCost - midDiscount;
   }
 
