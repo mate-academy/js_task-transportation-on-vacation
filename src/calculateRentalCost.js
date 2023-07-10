7806b2a37d20c611d19262439188b010a6e1c204
Horizon-git <apbreloaded947@gmail.com>
2023-07-10 23:15:55 +0300

    completed task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const COST_PER_DAY = 40;
+
+  const rentalCost = days * COST_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return rentalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return rentalCost - SHORT_TERM_DISCOUNT;
+  }
+
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
