b763b9ec0389ad6e74118577dc9275d8d1d63776
Joanna Konopacka <joanna.konopacka@hotmail.com>
2023-07-21 13:21:33 +0200

    CalculatedRentalCost-solved

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,7 @@ const SmallDiscount = 20;
 const BigDiscount = 50;
 
 function calculateRentalCost(days) {
-  let rentalCost = DayPrice * days; //Calculate the total cost without any discounts
+  let rentalCost = DayPrice * days; //The total cost without any discounts
   if (days >= 7) {
     rentalCost -= BigDiscount; //The big discount if renting 7 or more days
   } else if (days >= 3) {
