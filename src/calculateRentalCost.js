fc19fa3aee49c92f70490a80a1a0e22b85c1e2eb
Dolnys <thedolnys@gmail.com>
2023-07-21 15:08:40 +0200

    corrected variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -26,21 +26,21 @@
  *
  * @return {number}
  */
-const dailyRate = 40;
-const longTerm = 7;
-const longTermDiscount = 50;
-const shortTerm = 3;
-const shortTermDiscount = 20;
+const DAILY_RATE = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
 
 function calculateRentalCost(days) {
-  const totalCost = days * dailyRate;
+  const totalCost = days * DAILY_RATE;
 
-  if (days >= longTerm) {
-    return totalCost - longTermDiscount;
+  if (days >= LONG_TERM) {
+    return totalCost - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= shortTerm) {
-    return totalCost - shortTermDiscount;
+  if (days >= SHORT_TERM) {
+    return totalCost - SHORT_TERM_DISCOUNT;
   }
 
   return totalCost;
