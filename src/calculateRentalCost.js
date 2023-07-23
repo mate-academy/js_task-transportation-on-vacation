21d7885ffe1eccb45303d7b8291e389bee630e2e
Aleksandr <kartanovaleks@gmail.com>
2023-07-24 00:10:11 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  return days >= 7 ? days * 40 - 50 : days >= 3 ? days * 40 - 20 : days * 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MEDIUM_TERM = 3;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  const PRICE = 40;
+
+  return days >= LONG_TERM
+    ? days * PRICE - LONG_TERM_DISCOUNT
+    : days >= MEDIUM_TERM
+      ? days * PRICE - MEDIUM_TERM_DISCOUNT
+      : days * PRICE;
 }
 
 module.exports = calculateRentalCost;
