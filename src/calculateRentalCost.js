408b2749c94380d673fb212391a862c1efa3685a
Hubert Wiktor <hubertwiktor6@gmail.com>
2023-07-25 10:17:02 +0200

    sorry

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -26,21 +26,23 @@
  *
  * @return {number}
  */
-const longTerm = 7;
-const longTermDiscount = 50;
-const shortTerm = 3;
-const shortTermDiscount = 20;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
 
 function calculateRentalCost(days) {
-  const basePrice = days * 40;
+  const BASE_PRICE = days * 40;
 
-  if (days >= longTerm) {
-    return basePrice - longTermDiscount;
-  } else if (days >= shortTerm) {
-    return basePrice - shortTermDiscount;
+  if (days >= LONG_TERM) {
+    return BASE_PRICE - LONG_TERM_DISCOUNT;
   }
 
-  return basePrice;
+  if (days >= SHORT_TERM) {
+    return BASE_PRICE - SHORT_TERM_DISCOUNT;
+  }
+
+  return BASE_PRICE;
 }
 
 module.exports = calculateRentalCost;
