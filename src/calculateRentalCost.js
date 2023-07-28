0cf89b7a897461bbefc95a0ae9929089a3a6d120
Ivanna Romanovych <ivankaromanovych12@gmail.com>
2023-07-28 11:56:43 +0300

    add more descriptive names

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -27,24 +27,24 @@
  * @return {number}
  */
 
-const CONT_TERM = 3;
-const CONT_TERM_DISCOUNT = 20;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
 const LONG_TERM = 7;
 const LONG_TERM_DISCOUNT = 50;
 const DAY_RENTAL_COST = 40;
 
 function calculateRentalCost(days) {
-  const sum = days * DAY_RENTAL_COST;
+  const baseCost = days * DAY_RENTAL_COST;
 
   if (days >= LONG_TERM) {
-    return sum - LONG_TERM_DISCOUNT;
+    return baseCost - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= CONT_TERM) {
-    return sum - CONT_TERM_DISCOUNT;
+  if (days >= SHORT_TERM) {
+    return baseCost - SHORT_TERM_DISCOUNT;
   }
 
-  return sum;
+  return baseCost;
 }
 
 module.exports = calculateRentalCost;
