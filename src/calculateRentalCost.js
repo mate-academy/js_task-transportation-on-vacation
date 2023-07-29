c5b83131d7c37f2c1df0c152df4d47950eca4271
Bohdan Antipenko <dr.antipenko2016@gmail.com>
2023-07-29 17:21:34 +0300

    2.0

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,14 +28,15 @@
  */
 
 function calculateRentalCost(days) {
-  let sumOfRent = 40;
+  let sumOfRent = 0;
+  const DAY_RENT_PRICE = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  for (let i = 1; i < days; i++) {
-    sumOfRent += 40;
+  for (let i = 0; i < days; i++) {
+    sumOfRent += DAY_RENT_PRICE;
   }
 
   if (days >= SHORT_TERM && days < LONG_TERM) {
