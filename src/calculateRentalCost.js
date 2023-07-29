fc22bc70b01bad1d9ed194048d09d8fe419e2a01
Max <gcrazy007@gmail.com>
2023-07-29 11:17:52 +0300

    1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,25 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const MIDDLE_TERM = 3;
+
+  const MIDDLE_TERM_DISCOUNT = 20;
+
+  const LONG_TERM = 7;
+
+  const LONG_TERM_DISCOUNT = 50;
+
+  const basePrice = 40 * days;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MIDDLE_TERM) {
+    return basePrice - MIDDLE_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
