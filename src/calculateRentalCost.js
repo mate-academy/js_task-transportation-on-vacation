c5fc3cd449be60348bc0998e821e39aa4f8db976
vanom <envinenvin@gmail.com>
2023-07-31 15:59:44 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -34,15 +34,17 @@ function calculateRentalCost(days) {
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
 
+  const BASE_PRICE = PRICE_PER_DAY * days;
+
   if (days >= LONG_TERM) {
-    return PRICE_PER_DAY * days - LONG_TERM_DISCOUNT;
+    return BASE_PRICE - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return PRICE_PER_DAY * days - SHORT_TERM_DISCOUNT;
+    return BASE_PRICE - SHORT_TERM_DISCOUNT;
   }
 
-  return PRICE_PER_DAY * days;
+  return BASE_PRICE;
 }
 
 module.exports = calculateRentalCost;
