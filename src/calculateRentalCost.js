e5f663c7f5f3e0f036dbd0b695546f36131df09d
Andrii Nakonechnyi <andreynakonechnuy11@mail.com>
2023-07-31 15:53:20 +0300

    transportation-on vacation-2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,11 +29,11 @@
 
 function calculateRentalCost(days) {
   const price = 40;
+  const totalPrice = days * price;
   const LONG_TERMS = 7;
   const LONG_TERMS_DISCONT = 50;
   const SHORT_TERMS = 3;
   const SHORT_TERMS_DISCONT = 20;
-  const totalPrice = days * price;
 
   if (days >= LONG_TERMS) {
     return totalPrice - LONG_TERMS_DISCONT;
