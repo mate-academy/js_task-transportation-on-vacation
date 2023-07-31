27e6a2c9b1c879970ac007ba5f4adc8d547e6d13
irinagos <irynagos47@gmail.com>
2023-07-31 18:47:43 +0300

    fixed code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,22 +29,22 @@
 
 function calculateRentalCost(days) {
   const PRICE_PER_DAY = 40;
-  const SHORT_TERM = 3;
-  const LONG_TERM = 7;
+  const SHORT_TERM_DAYS = 3;
+  const LONG_TERM_DAYS = 7;
   const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM_DISCOUNT = 50;
 
-  const rentalCosts = days * PRICE_PER_DAY;
+  const basePrice = days * PRICE_PER_DAY;
 
-  if (days < SHORT_TERM) {
-    return rentalCosts;
+  if (days >= LONG_TERM_DAYS) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days < LONG_TERM && days >= SHORT_TERM) {
-    return rentalCosts - SHORT_TERM_DISCOUNT;
+  if (days >= SHORT_TERM_DAYS) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
-  return rentalCosts - LONG_TERM_DISCOUNT;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
