a1952667ce1b4aa2a074f2b93e977c395c17b1aa
SergS <my@example.com>
2023-08-03 16:36:52 +0300

    the task was implemented

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_PER_DAY = 40;
+  const LONG_RENT = 7;
+  const LONG_RENT_DISCOUNT = 50;
+  const SHORT_RENT = 3;
+  const SHORT_RENT_DISCOUNT = 20;
+
+  if (days >= LONG_RENT) {
+    return days * RENT_PER_DAY - LONG_RENT_DISCOUNT;
+  }
+
+  if (days >= SHORT_RENT) {
+    return days * RENT_PER_DAY - SHORT_RENT_DISCOUNT;
+  }
+
+  return days * RENT_PER_DAY;
 }
 
 module.exports = calculateRentalCost;
