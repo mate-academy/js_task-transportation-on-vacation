14897f090ca7ac47d2f45f576378eb511646e5fa
Yevhenii Voronin <voronine@ukr.net>
2023-08-04 12:56:14 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  const mult = days * 40;
-  let result = mult;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const CAR_COST = 40;
+  const COST_FOR_DAYS = days * CAR_COST;
 
-  if (days >= 7) {
-    result = mult - 50;
-  } else if (days >= 3) {
-    result = mult - 20;
+  if (days >= LONG_TERM) {
+    return COST_FOR_DAYS - LONG_TERM_DISCOUNT;
   }
 
-  return result;
+  if (days >= SHORT_TERM) {
+    return COST_FOR_DAYS - SHORT_TERM_DISCOUNT;
+  }
+
+  return COST_FOR_DAYS;
 }
 
 module.exports = calculateRentalCost;
