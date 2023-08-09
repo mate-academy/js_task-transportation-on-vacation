296a9f48741fae7da72807a8e7a9877503f356c0
Tetyana Shatrova <shatrova.tetyana@gmail.com>
2023-08-09 16:49:16 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,19 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let amount = 0;
+
+  amount = days * 40;
+
+  if (days >= 3 && days < 7) {
+    amount = days * 40 - 20;
+  }
+
+  if (days >= 7) {
+    amount = days * 40 - 50;
+  }
+
+  return amount;
 }
 
 module.exports = calculateRentalCost;
