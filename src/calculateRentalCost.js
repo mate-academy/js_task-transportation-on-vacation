70f4b5114c903e0fbe92e03b5f41d7ea224f6466
DmytroPopovych <0686385865d@gmail.com>
2023-08-15 23:16:16 +0300

    Update calculateRentalCost.js
    task completed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,25 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  let basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    const basePrice = days * PRICE_PER_DAY;
+
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
