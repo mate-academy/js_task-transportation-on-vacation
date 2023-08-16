072912382c4c50c5579f12070a2caed15340a034
Nataliia Kukushkina <n.kukushkina@gmail.com>
2023-08-16 12:05:58 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,25 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const PRICE_PER_DAY = 40;
+
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const baseAmount = PRICE_PER_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return baseAmount - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return baseAmount - SHORT_TERM_DISCOUNT;
+  }
+
+  return baseAmount;
 }
 
 module.exports = calculateRentalCost;
