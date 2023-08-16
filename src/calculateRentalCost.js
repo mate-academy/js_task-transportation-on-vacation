285143a2d985c1437a59706ae6d1a05feeb5e22e
Hryhorii Starykov <hr.starykov@gmail.com>
2023-08-16 23:41:21 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,18 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const THREE_DAY_DISCOUNT = 20;
+  const SEVEN_DAY_DISCOUNT = 50;
+
+  switch (true) {
+    case days < 3 :
+      return PRICE_PER_DAY * days;
+    case days >= 3 && days < 7:
+      return PRICE_PER_DAY * days - THREE_DAY_DISCOUNT;
+    case days >= 7 :
+      return PRICE_PER_DAY * days - SEVEN_DAY_DISCOUNT;
+  }
 }
 
 module.exports = calculateRentalCost;
