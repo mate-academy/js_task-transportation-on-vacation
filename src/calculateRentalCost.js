01579db133fa4160302f1c06acd125efa4cd62ef
avakiel <79792776+avakiel@users.noreply.github.com>
2023-09-02 19:34:24 +0300

    add1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -36,11 +36,11 @@ function calculateRentalCost(days) {
   const BASE_PRICE = PRICE_PER_DAY * days;
 
   if (days >= LONG_TERM) {
-    return (BASE_PRICE) - LONG_TERM_DISCOUNT;
+    return BASE_PRICE - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return (BASE_PRICE) - SHORT_TERM_DISCOUNT;
+    return BASE_PRICE - SHORT_TERM_DISCOUNT;
   }
 
   return BASE_PRICE;
