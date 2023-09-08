2783c8bc3450b3ba91b68dfdf746dd5e9dc0645d
Wita-Shchurko <vitaliya.mememe@gmail.com>
2023-09-08 10:37:39 +0300

    add changes according  to comments

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -27,22 +27,22 @@
  * @return {number}
  */
 
-function calculateRentalCost(days) {
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const BASE_PRICE = 40;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const BASE_PRICE = 40;
 
-  if (days < SHORT_TERM) {
-    return days * BASE_PRICE;
+function calculateRentalCost(days) {
+  if (days >= LONG_TERM) {
+    return (days * BASE_PRICE) - LONG_TERM_DISCOUNT;
   }
 
-  if (days < LONG_TERM) {
+  if (days >= SHORT_TERM) {
     return (days * BASE_PRICE) - SHORT_TERM_DISCOUNT;
   }
 
-  return (days * BASE_PRICE) - LONG_TERM_DISCOUNT;
+  return days * BASE_PRICE;
 }
 
 module.exports = calculateRentalCost;
