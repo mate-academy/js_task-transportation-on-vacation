1e443d65097a2c7c934adff83ade12b567dfb44f
Artur Yaroshenko <free.shok2@gmail.com>
2023-09-10 14:10:49 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,26 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const WRONG_TERM = 0;
+  const STANDART_COST = days * 40;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  if (days > WRONG_TERM) {
+    if (days < SHORT_TERM) {
+      return STANDART_COST;
+    }
+
+    if (days >= SHORT_TERM && days < LONG_TERM) {
+      return STANDART_COST - SHORT_TERM_DISCOUNT;
+    }
+
+    return STANDART_COST - LONG_TERM_DISCOUNT;
+  }
+
+  return 'Days cannot be negative or equal to zero!';
 }
 
 module.exports = calculateRentalCost;
