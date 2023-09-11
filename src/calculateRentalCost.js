5f210103a7fd814c8aa37920241f3e3c4336c92a
andrew gidrovich <mr.androvich55555@gmail.com>
2023-09-11 19:06:42 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -27,8 +27,24 @@
  * @return {number}
  */
 
+const BASE_RENTAL_PRICE_PER_DAY = 40;
+const LONG_TERM_THRESHOLD = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM_THRESHOLD = 3;
+const SHORT_TERM_DISCOUNT = 20;
+
 function calculateRentalCost(days) {
-  // write code here
+  const basePrice = days * BASE_RENTAL_PRICE_PER_DAY;
+
+  if (days >= LONG_TERM_THRESHOLD) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM_THRESHOLD) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
