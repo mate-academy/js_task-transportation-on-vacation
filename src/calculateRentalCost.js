f13f02684177d9e8834193843f78e984d1cef196
Sviatoslav Kondur <sviatikkondur5@gmail.com>
2023-09-14 15:09:32 +0200

    transportation on vacation - solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const dayRate = 40;
+
+  const total = days * dayRate;
+
+  if (days >= LONG_TERM) {
+    return total - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return total - SHORT_TERM_DISCOUNT;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
