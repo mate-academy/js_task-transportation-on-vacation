da5c98c8c8654d64a9a39553bb67abb1a9d60e74
koros <korosov.j@gmail.com>
2023-09-14 12:22:13 +0300

    added task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const DAY_COST = 40;
+
+  if (days < SHORT_TERM) {
+    return days * DAY_COST;
+  };
+
+  if (days < LONG_TERM) {
+    return days * DAY_COST - SHORT_TERM_DISCOUNT;
+  };
+
+  return days * DAY_COST - LONG_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
