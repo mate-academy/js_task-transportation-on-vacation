f72d3b78ad0071595c6f8a8e39370932fc1f3eb1
Denis Shyshenko <denisbmw2.84@gmail.com>
2023-09-25 12:16:41 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,18 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
-}
+  const SHORT_TIME = 3;
+  const LONG_TIME = 7;
+  const RENT = 40;
+  const DISCOUNT = 20;
+  const MAX_DISCOUNT = 50;
 
+  if (days >= LONG_TIME) {
+    return days * RENT - MAX_DISCOUNT;
+  } else if (days >= SHORT_TIME) {
+    return days * RENT - DISCOUNT;
+  }
+
+  return days * RENT;
+}
 module.exports = calculateRentalCost;
