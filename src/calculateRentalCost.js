f536c64edec9df599070d7a3fdb5658336e4198c
Volodymyr Tkach <vip.vova.tkach.2002@gmail.com>
2023-10-01 22:37:14 +0300

    do function calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_PER_DAY = 40;
+  const SHORT_TERM = 3;
+  const LONG_TERM = 7;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+
+  if (days >= LONG_TERM) {
+    return days * RENT_PER_DAY - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return days * RENT_PER_DAY - SHORT_TERM_DISCOUNT;
+  }
+
+  return days * RENT_PER_DAY;
 }
 
 module.exports = calculateRentalCost;
