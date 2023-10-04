a027163f5f729bfbca3780e1fe5ac98a617d6fba
Alex Zavodov <96802636+lexzane@users.noreply.github.com>
2023-10-05 00:44:53 +0300

    feat [js-adv-1]: implement car rental cost calculator

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const fullPrice = PRICE_PER_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return fullPrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return fullPrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return fullPrice;
 }
 
 module.exports = calculateRentalCost;
