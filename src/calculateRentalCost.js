ecf72721c3cad0194b3ea74836ea9178c3b09c79
Dmitro Kornienko <fbshark12>
2023-10-10 13:05:53 +0300

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,20 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const priceOfDay = 40;
+  const MID_TERM = 3;
+  const LONG_TERM = 7;
+  const MID_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+  let total = days * priceOfDay;
+
+  if (days >= LONG_TERM) {
+    total -= LONG_TERM_DISCOUNT;
+  } else if (days >= MID_TERM) {
+    total -= MID_TERM_DISCOUNT;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
