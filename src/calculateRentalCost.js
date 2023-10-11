bc9e3dae984ab046611f22bc7e78fdf621707026
Rostyslav Sobchyshyn <rosta2604@icloud.com>
2023-10-11 13:17:28 +0100

    added const 7

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -32,12 +32,13 @@ function calculateRentalCost(days) {
   const basicDiscount = 20;
   const maxDiscount = 50;
   const basicPrice = price * days;
+  const week = 7;
 
-  if (days >= 3 && days < 7) {
+  if (days >= 3 && days < week) {
     return basicPrice - basicDiscount;
   }
 
-  if (days >= 7) {
+  if (days >= week) {
     return basicPrice - maxDiscount;
   }
 
