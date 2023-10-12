996434c8c036d2e05297a6028c03219f23e275a2
Svyatoslav <svyatoslavkolomoets@gmail.com>
2023-10-12 15:47:05 +0300

    add best solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,11 +28,12 @@
  */
 
 function calculateRentalCost(days) {
+  const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
-  const sum = days * 40;
+  const sum = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
     return sum - LONG_TERM_DISCOUNT;
