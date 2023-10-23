4d2ce7932d5ade5a1a856a8f64f2e5dab6a492b2
Shadi Nasser <shadiknasser@gmail.com>
2023-10-23 21:17:16 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,6 @@
  */
 
 function calculateRentalCost(days) {
-  let totalCost = 0;
   const DAY_PRICE = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
@@ -37,21 +36,15 @@ function calculateRentalCost(days) {
   const basePrice = days * DAY_PRICE;
 
   if (days >= LONG_TERM) {
-    totalCost = basePrice - LONG_TERM_DISCOUNT;
-
-    return totalCost;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    totalCost = basePrice - SHORT_TERM_DISCOUNT;
-
-    return totalCost;
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   if (days < 3) {
-    totalCost = basePrice;
-
-    return totalCost;
+    return basePrice;
   }
 }
 
