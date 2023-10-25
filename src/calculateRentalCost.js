8a44659db12fdec16edda13a98a0b321f7014011
Denys Halytskyi <azeldfarm@gmail.com>
2023-10-25 21:13:25 +0200

    add task solution №2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,25 +29,18 @@
 
 function calculateRentalCost(days) {
   const PRICE_ONE_DAY = 40;
-
   const LONG_TERM = 7;
   const BIG_DISCOUNT = 50;
-
   const SMALL_TERM = 3;
   const LITTLE_DISCOUNT = 20;
-
-  let TOTAL_PRICE = days * PRICE_ONE_DAY;
+  const TOTAL_PRICE = days * PRICE_ONE_DAY;
 
   if (days >= LONG_TERM) {
-    TOTAL_PRICE -= BIG_DISCOUNT;
-
-    return TOTAL_PRICE;
+    return TOTAL_PRICE - BIG_DISCOUNT;
   }
 
   if (days >= SMALL_TERM) {
-    TOTAL_PRICE -= LITTLE_DISCOUNT;
-
-    return TOTAL_PRICE;
+    return TOTAL_PRICE - LITTLE_DISCOUNT;
   }
 
   return TOTAL_PRICE;
