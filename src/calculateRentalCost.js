e3e3cb724dede8584166c0964f1ab969c4ae5367
Mateusz Jarosz <mati5033onet@gmail.com>
2023-10-25 12:29:48 +0200

    Solutuin

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  if (days < 3) {
+    return days * 40;
+  }
+
+  if (days > 2 && days < 7) {
+    return days * 40 - 20;
+  }
+
+  if (days > 6) {
+    return days * 40 - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
