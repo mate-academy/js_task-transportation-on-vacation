767e58a39966fd8ccd21cf113539228cfc83435e
Wojciech Sęk <62895274+kruladmiral@users.noreply.github.com>
2023-10-26 18:14:01 +0200

    Revert "GitHub js exercise"

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,18 +28,7 @@
  */
 
 function calculateRentalCost(days) {
-  let totalCost = '0';
-  const oneDayCost = 40;
-
-  if (days < 3) {
-    totalCost = days * oneDayCost;
-  } else if (days >= 3 && days < 7) {
-    totalCost = days * oneDayCost - 20;
-  } else {
-    totalCost = days * oneDayCost - 50;
-  }
-
-  return totalCost;
+  // write code here
 }
 
 module.exports = calculateRentalCost;
