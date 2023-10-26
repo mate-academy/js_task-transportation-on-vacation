2ad657d84d600730188191863ba05019440060f7
Wojciech Sęk <wojtasskate@wp.pl>
2023-10-26 20:37:02 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,20 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  function calculateRentalCost(days) {
+    let totalCost = '0';
+    const oneDayCost = 40;
+  
+    if (days < 3) {
+      totalCost = days * oneDayCost;
+    } else if (days >= 3 && days < 7) {
+      totalCost = days * oneDayCost - 20;
+    } else {
+      totalCost = days * oneDayCost - 50;
+    }
+  
+    return totalCost;
+  }
 }
 
 module.exports = calculateRentalCost;
