b193255bffbf75655db59007cb2405466c714f33
Stepan Rad <stepanrad@MacBook-Air-Stepan.local>
2023-10-26 19:28:40 +0300

    added basic_cost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,20 +29,21 @@
 
 function calculateRentalCost(days) {
   const COST_PER_DAY = 40;
+  const BASIC_COST = days * COST_PER_DAY;
   const LONG_TERM_VACATION = 7;
   const LONG_TERM_VACATION_DISCOUNT = 50;
   const SHORT_TERM_VACATION = 3;
   const SHORT_TERM_VACATION_DISCOUNT = 20;
 
   if (days >= LONG_TERM_VACATION) {
-    return days * COST_PER_DAY - LONG_TERM_VACATION_DISCOUNT;
+    return BASIC_COST - LONG_TERM_VACATION_DISCOUNT;
   }
 
   if (days >= SHORT_TERM_VACATION) {
-    return days * COST_PER_DAY - SHORT_TERM_VACATION_DISCOUNT;
+    return BASIC_COST - SHORT_TERM_VACATION_DISCOUNT;
   }
 
-  return days * COST_PER_DAY;
+  return BASIC_COST;
 }
 
 module.exports = calculateRentalCost;
