7c169a15209cc5f2ceb1b5ee6b4b9dbb5c86e55b
Svitlana <svitlana.ponomarchuk@gmail.com>
2023-10-27 17:31:07 +0300

    fixed function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -27,13 +27,13 @@
  * @return {number}
  */
 
-function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
+const PRICE_PER_DAY = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
 
+function calculateRentalCost(days) {
   if (days >= LONG_TERM) {
     return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
   }
