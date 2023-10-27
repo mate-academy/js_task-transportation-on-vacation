a14097e81afee9a9d34b2b05801d578e8e59854a
Nazar Boblyk <147517227+BoblykNazar@users.noreply.github.com>
2023-10-27 12:21:10 +0300

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -33,7 +33,7 @@ function calculateRentalCost(days) {
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
   const PRICE_PER_DAY = 40;
-  const basePrice =  days * PRICE_PER_DAY;
+  const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
