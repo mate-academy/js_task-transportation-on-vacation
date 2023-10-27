d80221afcb24856b282cdc643aa0e30ec346d290
boytrend0108 <boytrend@gmail.com>
2023-10-27 19:49:33 +0300

    fix code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  if (days > 6) {
-    return (days * 40) - 50;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const PRICE_PER_DAY = 40;
+
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days > 2) {
-    return (days * 40) - 20;
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
-  return days * 40;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
