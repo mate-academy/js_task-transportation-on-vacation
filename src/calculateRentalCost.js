adba54bb0c5fcaa57d4e34b990eff208b4057479
Egor <yehorheneralov@gmail.com>
2023-10-30 15:43:41 +0200

    fixed mentors requirements

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -26,22 +26,21 @@
  *
  * @return {number}
  */
+const PRICE_PER_DAY = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
 
 function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
-
-  let basePrice = days * PRICE_PER_DAY;
+  const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    return PRICE_PER_DAY * days - LONG_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return PRICE_PER_DAY * days - SHORT_TERM_DISCOUNT;
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   return basePrice;
