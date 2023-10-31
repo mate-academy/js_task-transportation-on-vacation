b4414d6973ce83bb9093a422d2ddb422a24feaac
Olena Pavlova <pawlowakiew@gmail.com>
2023-10-31 13:58:57 +0100

    add function calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 30;
+  const baseTotal = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return baseTotal - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return baseTotal - SHORT_TERM_DISCOUNT;
+  }
+
+  return baseTotal;
 }
 
 module.exports = calculateRentalCost;
