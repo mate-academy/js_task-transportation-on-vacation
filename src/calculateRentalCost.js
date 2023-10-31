9b5de663603d7f1a18594e57330e64bbc14893c2
Anastasiia Semenova <semenovaanastasiiaa@gmail.com>
2023-10-31 19:47:18 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const price = days * 40;
+  const shortTerm = 3;
+  const longTerm = 7;
+  const shortTermDiscount = 20;
+  const longTermDiscount = 50;
+
+  if (days >= longTerm) {
+    return price - longTermDiscount;
+  }
+
+  if (days >= shortTerm) {
+    return price - shortTermDiscount;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
