ccb54f4d774c4f25b51cd8cffca2a8ffa0ea414c
Misha Martynov <martunmtm@gmail.com>
2023-11-02 17:17:31 +0200

    add_js_task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_ONE_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const totalcost = days * PRICE_ONE_DAY;
+
+  if (days >= LONG_TERM) {
+    return totalcost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return totalcost - SHORT_TERM_DISCOUNT;
+  }
+
+  return totalcost;
 }
 
 module.exports = calculateRentalCost;
