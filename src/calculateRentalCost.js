86b9eb44b21fda19c893a683dd00367a6da9d7d4
Paweł Nowicki <pawel.nowicki87@tlen.pl>
2023-11-03 21:58:38 +0100

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -32,9 +32,9 @@ function calculateRentalCost(days) {
     return (days * 40);
   } else if (days <= 6) {
     return (days * 40) - 20;
-  } else {
-    return (days * 40) - 50;
   }
+
+  return (days * 40) - 50;
 }
 
 module.exports = calculateRentalCost;
