2204b9e7c9e92091950d796e56677eaaef9317ca
Michał Reszka <miszareszka@gmail.com>
2023-11-07 17:07:52 +0100

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,18 @@
  */
 
 function calculateRentalCost(days) {
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
+  const totalCost = days * 40;
 
-  if (days < 1) {
-    return 0;
-  } else if (days >= LONG_TERM) {
-    return days * 40 - LONG_TERM_DISCOUNT;
-  } else if (days >= 3) {
-    return days * 40 - 20;
+  if (days >= LONG_TERM) {
+    return totalCost - LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    return totalCost - SHORT_TERM_DISCOUNT;
   }
 
-  return days * 40;
+  return totalCost;
 }
 module.exports = calculateRentalCost;
