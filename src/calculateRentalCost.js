38891e6fbda2352b59bd71c48e851af4c4223266
Gennadiy Boreyko <gennadiyboreyko0@gmail.com>
2023-11-11 15:39:13 +0200

    add the rent car solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const COST_PER_DAY = 40;
+  const SHORT_TERM = 3;
+  const LONG_TERM = 7;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+  const baseRentCost = COST_PER_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return (baseRentCost - LONG_TERM_DISCOUNT);
+  }
+
+  if (days >= SHORT_TERM) {
+    return (baseRentCost - SHORT_TERM_DISCOUNT);
+  }
+
+  return baseRentCost;
 }
 
 module.exports = calculateRentalCost;
