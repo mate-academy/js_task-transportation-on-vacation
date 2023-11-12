60f597329368d7e4c98bbe307d720deabec98e57
MaksymKolesnyk <maksymka@icloud.com>
2023-11-12 10:43:13 -0500

    add task solution by Maksym Kolesnyk

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3
+  const SHORT_TERM_DISCOUNT = 20;
+  const DAY_PRICE = 40;
+  const TOTAL = days * DAY_PRICE;
+
+  if (days >= LONG_TERM) {
+    return TOTAL - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return TOTAL - SHORT_TERM_DISCOUNT;
+  }
+
+  return TOTAL;
 }
 
 module.exports = calculateRentalCost;
