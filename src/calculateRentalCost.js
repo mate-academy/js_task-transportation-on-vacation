36e02aa67dde2cfccc247b743d0d1ed72733c213
Misha Hrynko <misha@mate.academy>
2023-11-13 08:06:37 +0200

    test node version

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let total = 0;
+
+  if (days >= 7) {
+    total = days * 40 - 50;
+  } else if (days >= 3) {
+    total = days * 40 - 20;
+  } else {
+    total = days * 40;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
