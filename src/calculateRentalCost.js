fc2f7fc5698dc50c296ef05caeab06270d4a281d
Natalia-Slavuta <tritoutike@gmail.com>
2023-11-21 18:16:41 +0200

    task done: implementation of calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+
+  if (days >= 7) {
+    return days * pricePerDay - 50;
+  }
+
+  if (days >= 3) {
+    return days * pricePerDay - 20;
+  }
+
+  return days * pricePerDay;
 }
 
 module.exports = calculateRentalCost;
