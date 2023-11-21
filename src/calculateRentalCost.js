a7cce9e9909ccbe654bcfc09a2308fc6d0f09f61
Andrii Veselovsky <andrii.veselovsky@gmail.com>
2023-11-21 23:09:35 +0200

    Solutin

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,22 +28,25 @@
  */
 
 function calculateRentalCost(days) {
-  const total = days * 40;
+  const PRICE_PER_DAY = 40;
+  const basePrice = days * PRICE_PER_DAY;
+  const SHORT_TERM = 3;
+  const LONG_TERM = 7;
   const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM_DISCOUNT = 50;
 
   // No discount
-  if (days < 3) {
-    return total;
+  if (days < SHORT_TERM) {
+    return basePrice;
   }
 
-  // 20% discount
-  if (days < 7) {
-    return total - SHORT_TERM_DISCOUNT;
+  // 20$ discount
+  if (days < LONG_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   // 50$ discount
-  return total - LONG_TERM_DISCOUNT;
+  return basePrice - LONG_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
