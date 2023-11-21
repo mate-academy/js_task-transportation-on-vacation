138d4bd12293240cedf1f4c1ca9eb6122bde51b3
Natalia-Slavuta <tritoutike@gmail.com>
2023-11-21 18:24:52 +0200

    refactoring

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,17 +28,20 @@
  */
 
 function calculateRentalCost(days) {
-  const pricePerDay = 40;
+  const PRICE_PER_DAY = 40;
+  const  LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  let basePrice = days*PRICE_PER_DAY;
 
-  if (days >= 7) {
-    return days * pricePerDay - 50;
+  if (days>=LONG_TERM){
+    return basePrice - LONG_TERM_DISCOUNT;
   }
-
-  if (days >= 3) {
-    return days * pricePerDay - 20;
+  if (days>=SHORT_TERM){
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
-
-  return days * pricePerDay;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
