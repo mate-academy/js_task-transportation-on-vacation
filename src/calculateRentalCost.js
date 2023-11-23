31cd0d03ba35f75f4ecadb84296216c049df1cfb
Vlad Pashko <vladpavloggg@gmail.com>
2023-11-23 18:07:59 +0300

    fixed variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  if (days >= 7) {
-    return (days * 40 - 50);
+  const PRICE_PER_DAY = 40;
+  const LONG_RENT = 7;
+  const LONG_RENT_DISCOUNT = 50;
+  const SHORT_RENT = 3;
+  const SHORT_RENT_DISCOUNT = 20;
+
+  const basicPrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_RENT) {
+    return basicPrice - LONG_RENT_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return (days * 40 - 20);
+  if (days >= SHORT_RENT) {
+    return basicPrice - SHORT_RENT_DISCOUNT;
   }
 
-  return (days * 40);
+  return basicPrice;
 }
 
 module.exports = calculateRentalCost;
