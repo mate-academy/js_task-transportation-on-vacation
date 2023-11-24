a1491efe7be9cab80244c4842aa06f70f7589704
Oleksandra Kuziura <radioday2014@gmail.com>
2023-11-24 14:19:12 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,26 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  let totalPrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    totalPrice = totalPrice - LONG_TERM_DISCOUNT;
+
+    return totalPrice;
+  }
+
+  if (days >= SHORT_TERM) {
+    totalPrice = totalPrice - SHORT_TERM_DISCOUNT;
+
+    return totalPrice;
+  }
+
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
