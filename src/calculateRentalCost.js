a7c61e599081e0b84f18f7370062bd4849747364
Vasyl1991 <vasyi19shych91@gmail.com>
2023-11-28 17:07:05 +0200

    add task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const MIDDLE_TERM = 3;
+  const MIDDLE_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const BASE_PRICE = 40 * days;
+
+  if (days >= LONG_TERM) {
+    return BASE_PRICE - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MIDDLE_TERM) {
+    return BASE_PRICE - MIDDLE_TERM_DISCOUNT;
+  }
+
+  return BASE_PRICE;
 }
 
 module.exports = calculateRentalCost;
