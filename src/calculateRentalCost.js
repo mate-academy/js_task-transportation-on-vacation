09f3be99209a2ce64b718a0dda40883305f839e1
Valentyna Lytvynenko <valentina.vik10@gmail.com>
2023-12-04 23:18:53 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_COSTS = 40;
+  const LONG_TERM = 7;
+  const BIG_OFF = 50;
+  const SHORT_TERM = 3;
+  const SMALL_OFF = 20;
+
+  const value = days * RENT_COSTS;
+
+  if (days >= LONG_TERM) {
+    return value - BIG_OFF;
+  }
+
+  if (days >= SHORT_TERM) {
+    return value - SMALL_OFF;
+  }
+
+  return value;
 }
 
 module.exports = calculateRentalCost;
