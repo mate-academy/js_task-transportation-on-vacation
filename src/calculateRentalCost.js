5c18fd2d025e6c8cbfcad2f2895dde5d94cc623a
Jenya Golovko <u9on1210@gmail.com>
2023-12-05 17:40:37 +0200

    first task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const SHORT_TERM = 2;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const basePrice = 40;
+
+  if (days >= LONG_TERM) {
+    return (basePrice * days) - LONG_TERM_DISCOUNT;
+  }
+
+  if (SHORT_TERM < days) {
+    return (basePrice * days) - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice * days;
 }
 
 module.exports = calculateRentalCost;
