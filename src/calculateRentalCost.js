2dc21eb5cf363638a7e85a202db8493b8b32fb19
Denis Vrublevskii <vrublevskijdenis9@gmail.com>
2023-12-07 11:40:16 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const ONE_DAY_PRICE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const DEFAULT_PRICE = days * ONE_DAY_PRICE;
+
+  if (days >= LONG_TERM) {
+    return DEFAULT_PRICE - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return DEFAULT_PRICE - SHORT_TERM_DISCOUNT;
+  }
+
+  return DEFAULT_PRICE;
 }
 
 module.exports = calculateRentalCost;
