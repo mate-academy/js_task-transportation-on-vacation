cc30431829f9ced93198b626633cfebdfb865da8
Anastasiia <vasilisandreeva00@gmail.com>
2023-12-12 14:54:05 +0200

    add variables to the solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,21 +28,22 @@
  */
 
 function calculateRentalCost(days) {
+  const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const MEDIUM_TERM = 3;
   const MEDIUM_TERM_DISCOUNT = 20;
-  const PRICE_PER_DAY = days * 40;
+  const PRICE = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    return PRICE_PER_DAY - LONG_TERM_DISCOUNT;
+    return PRICE - LONG_TERM_DISCOUNT;
   }
 
   if (days >= MEDIUM_TERM) {
-    return PRICE_PER_DAY - MEDIUM_TERM_DISCOUNT;
+    return PRICE - MEDIUM_TERM_DISCOUNT;
   }
 
-  return PRICE_PER_DAY;
+  return PRICE;
 }
 
 module.exports = calculateRentalCost;
