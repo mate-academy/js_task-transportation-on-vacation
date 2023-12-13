cd0cf8c93f4a9cab9f2681696cbeb7e189bee97d
Nikita Ganzha <geekneek28@gmail.com>
2023-12-13 12:11:36 +0200

    create function calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_PER_DAY = 40;
+  const LONG_TERM_RENTAL = 7;
+  const LONG_TERM_RENTAL_DISCOUNT = 50;
+  const SHORT_TERM_RENTAL = 3;
+  const SHORT_TERM_RENTAL_DISCOUNT = 20;
+  const totalCost = RENT_PER_DAY * days;
+
+  if (days >= LONG_TERM_RENTAL) {
+    return totalCost - LONG_TERM_RENTAL_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM_RENTAL) {
+    return totalCost - SHORT_TERM_RENTAL_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
