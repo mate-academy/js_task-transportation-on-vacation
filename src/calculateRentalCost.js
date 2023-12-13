589f544fd8db96acd8bcea5aaf507ba74264bbec
Alina <aliska.nofear@gmail.com>
2023-12-13 15:59:18 +0200

    fixed code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -34,15 +34,13 @@ function calculateRentalCost(days) {
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  const basePrice = days * PRICE_PER_DAY
-
-  if(days >= SHORT_TERM) {
-    return basePrice - SHORT_TERM_DISCOUNT;
-  } 
+  const basePrice = days * PRICE_PER_DAY;
   
   if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
-  }
+  } else if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }; 
 
   return basePrice;
 }
