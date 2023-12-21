228066a62867012028052c8c4b6ea5fb31e89cfd
Rostuk Dudchak <rostikdudchak15@gmail.com>
2023-12-21 14:24:58 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,23 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const PRICE_PAR_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHOTR_LONG_TERM = 3;
+  const SHOTR_LONG_TERM_DISCOUNT = 20;
+
+  const basePrice = days * PRICE_PAR_DAY;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHOTR_LONG_TERM) {
+    return basePrice - SHOTR_LONG_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
