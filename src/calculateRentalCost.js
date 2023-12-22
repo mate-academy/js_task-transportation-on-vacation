a73c4223248547f3687dbe8e776f738278cf8430
Yuliia Kindruk <yulia1307yk@gmail.com>
2023-12-22 12:16:02 +0200

    first task with corections

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,18 +29,22 @@
 
 function calculateRentalCost(days) {
   const PRICE_PER_DAY = 40;
-  const price = days * PRICE_PER_DAY;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  let basePrice = days * PRICE_PER_DAY;
 
-  if (days >= 7) {
-    return price - 50;
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return price - 20;
+  if (days>= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
-  if (days < 3) {
-    return price;
+  if (days < SHORT_TERM) {
+    return basePrice;
   }
 }
 
