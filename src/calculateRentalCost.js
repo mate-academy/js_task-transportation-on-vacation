121db31022c04cb8bde5122af6d35a22439542fa
Ihor Prodan <prodanigor0@gmail.com>
2024-01-02 14:24:28 +0100

    add task solution calculateRentalCost - fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,16 +28,18 @@
  */
 
 function calculateRentalCost(days) {
+  const LONG_TERM = 7;
   const dayPrice = 40;
-  const discountSevenDays = 50;
-  const discountThreeDays = 20;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const SHORT_TERM = 3;
 
-  if (days >= 7) {
-    return days * dayPrice - discountSevenDays;
+  if (days >= LONG_TERM) {
+    return days * dayPrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return days * dayPrice - discountThreeDays;
+  if (days >= SHORT_TERM) {
+    return days * dayPrice - SHORT_TERM_DISCOUNT;
   }
 
   return days * dayPrice;
