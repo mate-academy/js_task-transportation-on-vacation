33135511ac2d7b29eef69bc6b0ffcc190ec01b8b
Sergio Cuadros <sergiocua2@gmail.com>
2024-01-07 20:37:16 -0300

    solution to rental car problem

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  let totalCost = days * 40;
+
+  if (days >= 7) {
+    totalCost -= 50;
+  }
+
+  if (days >= 3 && days < 7) {
+    totalCost -= 20;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
