79f2adc98bfeddcb5c4a47fb8ad34cdebda2fef0
Oleksandr Poviakalo <oleksandr.poviakalo@gmail.com>
2024-01-15 19:30:28 +0200

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MIDL_TERM = 3;
+  const MIDL_TERM_DISCOUNT = 20;
+  const rentCar = 40;
+
+  if (days >= LONG_TERM) {
+    return days * rentCar - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MIDL_TERM) {
+    return days * rentCar - MIDL_TERM_DISCOUNT;
+  }
+
+  return days * rentCar;
 }
 
 module.exports = calculateRentalCost;
