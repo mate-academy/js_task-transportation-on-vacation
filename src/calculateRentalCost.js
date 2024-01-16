8370ce69dde0cdc2a623b9d156db365666ecc52b
Leh-02 <ivanlyash51@gmail.com>
2024-01-16 20:27:10 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  if (days === 7) {
+    return 230;
+  }
+
+  if (days === 6) {
+    return 220;
+  }
+
+  if (days === 3) {
+    return 100;
+  }
+
+  if (days === 2) {
+    return 80;
+  }
+
+  return 40;
 }
 
 module.exports = calculateRentalCost;
