dd905564852dd024ed92f2719c97bb3bb3749413
Timofii Ustilou <timoshaustilov@gmail.com>
2024-01-22 13:42:57 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  let renalCost = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return renalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return renalCost - SHORT_TERM_DISCOUNT;
+  }
+
+  return renalCost;
 }
 
 module.exports = calculateRentalCost;
