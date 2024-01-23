580da16b3a807f47b988cf5b405bdd64c3413b58
Mykyta Kalyna <kalinanikita001@gmail.com>
2024-01-23 11:22:20 +0300

    resoved tusk

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+  const longTerm = 7;
+  const shortTerm = 3;
+  const longRent = 50;
+  const shortRent = 20;
+
+  const basePrice = days * pricePerDay;
+
+  if (days >= longTerm) {
+    return basePrice - longRent;
+  }
+
+  if (days >= shortTerm && days < longTerm) {
+    return basePrice - shortRent;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
