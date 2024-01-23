73b33cd02473be97f82ca400730796dc3993dfaa
OleksiiNesteruk <nesteruk.oleksiy@gmail.com>
2024-01-23 10:00:44 +0000

    Added task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const RENTAL_COST = 40;
+  const SHORT_PERIOD = 3;
+  const SHORT_PERIOD_DISCOUNT = 20;
+  const LONG_PERIOD = 7;
+  const LONG_PERIOD_DISCOUNT = 50;
+
+  if (days >= LONG_PERIOD) {
+    return days * RENTAL_COST - LONG_PERIOD_DISCOUNT;
+  }
+
+  if (days >= SHORT_PERIOD) {
+    return days * RENTAL_COST - SHORT_PERIOD_DISCOUNT;
+  }
+
+  return days * RENTAL_COST;
 }
 
 module.exports = calculateRentalCost;
