06ce5ae1579600268532014ea7f2d95f75ded5b4
Viktor Zarvanskyi <viktor.zarvanskyi@gmail.com>
2024-01-24 02:11:08 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISC = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISC = 50;
+
+  const basePrice = PRICE_PER_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISC;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISC;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
