6ff717b111cf122b118d9fcf8e07fc76bbc06304
Misha Remeslo <misha.remeslol@gmail.com>
2024-01-27 19:24:31 +0200

    first js task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_CAR_PRICE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days < SHORT_TERM) {
+    return RENT_CAR_PRICE * days;
+  }
+
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    return (RENT_CAR_PRICE * days) - SHORT_TERM_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    return (RENT_CAR_PRICE * days) - LONG_TERM_DISCOUNT;
+  }
 }
 
 module.exports = calculateRentalCost;
