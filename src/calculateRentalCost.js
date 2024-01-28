bbf4ada77b013c48c2cfa73282ecdb4447943917
Dmitry Dukyn <Phoenixka4@gmail.com>
2024-01-28 15:59:34 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const DAY_RENT_COST = 40;
+  const LONG_RENT_DAYS = 7;
+  const LONG_RENT_DISCOUNT = 50;
+  const SHORT_RENT_DAYS = 3;
+  const SHORT_RENT_DISCOUNT = 20;
+
+  const price = days * DAY_RENT_COST;
+
+  if (days >= LONG_RENT_DAYS) {
+    return price - LONG_RENT_DISCOUNT;
+  }
+
+  if (days >= SHORT_RENT_DAYS) {
+    return price - SHORT_RENT_DISCOUNT;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
