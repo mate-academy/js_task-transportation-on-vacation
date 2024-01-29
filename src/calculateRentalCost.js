3dcdcada677206b7f2899b53dcb22859720ed4b6
Natalii0 <natasha.grynchuk@gmail.com>
2024-01-29 22:55:37 +0200

    update src/calculateRentalCost.js 2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  let price = days * 40;
+  let totalCost = days * 40;
 
   if (days >= 7) {
-    price -= 50;
-  } else if (days >= 3) {
-    price -= 20;
+    totalCost -= 50;
   }
 
-  return price;
+  if (days >= 3 && days < 7) {
+    totalCost -= 20;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
