a389abf26be509004ab5c0abbeffdfbc6eba7e6d
DESKTOP-EHDJ2FA\yarik <yarik.zubyk@gmail.com>
2024-02-01 18:00:14 +0200

    added calculate function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,21 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const DAY_RENT = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const LESS_TERM = 3;
+  const LESS_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    return (days * DAY_RENT) - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= LESS_TERM && days <= LONG_TERM) {
+    return (days * DAY_RENT) - LESS_TERM_DISCOUNT;
+  }
+
+  return days * DAY_RENT;
 }
 
 module.exports = calculateRentalCost;
