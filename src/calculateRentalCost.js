35468a2ed06a9920277315e0606f39bbed58a620
Denys209818 <deniskravcuk63@gmail.com>
2024-02-03 17:35:02 +0200

    Load full realization

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,24 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const PRICE_FOR_DAY = 40;
+  const DISCOUNT_FOR_SHORT_TRIP = 20;
+  const DISCOUNT_FOR_LONG_TRIP = 50;
+  const SHORT_TERM_TRIP = 3;
+  const LONG_TERM_TRIP = 7;
+
+  const price = days * PRICE_FOR_DAY;
+
+  if (days >= LONG_TERM_TRIP) {
+    return price - DISCOUNT_FOR_LONG_TRIP;
+  }
+
+  if (days >= SHORT_TERM_TRIP) {
+    return price - DISCOUNT_FOR_SHORT_TRIP;
+    ;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
