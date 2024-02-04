f4403b96bf06e0afe8aada84f5dc4b8815b07cad
Medvid Oleksandr <samaster010604@gmail.com>
2024-02-04 14:35:48 +0200

    rental cost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  let totalCost = 40 * days;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  return totalCost;
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
