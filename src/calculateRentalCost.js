2ba4010791f076881e3fa3be8ca1a4189910a7f0
Serhii_Sharabura <youserhii@gmail.com>
2024-02-05 18:33:11 +0200

    Saved changes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,22 @@
 
 function calculateRentalCost(days) {
   // write code here
-  let price = days * 40;
-  if (days >= 7) {
-    price -= 50;
-  } else if (days >=3) {
-    price -= 20; 
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
-  
-  return price;
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
