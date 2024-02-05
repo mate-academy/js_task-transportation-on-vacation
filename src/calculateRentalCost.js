8e5a5c488081df3a84aeadae7a0234a5b83ad54b
Roman Romanchuk <roma.roman4uk.48@gmail.com>
2024-02-05 20:55:26 +0100

    did the task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,17 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const priceOndeDay = 40;
+
+  if (days >= 7) {
+    return (days * priceOndeDay) - 50;
+  }
+
+  if (days >= 3) {
+    return (days * priceOndeDay) - 20;
+  }
+
+  return days * priceOndeDay;
 }
 
 module.exports = calculateRentalCost;
