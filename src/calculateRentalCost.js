13c2096362d77b81f4fab6638f7f394c24ae570d
Alina Derkach <alinaderkach@MacBook-Air-Alina.local>
2024-02-07 00:47:47 +0200

    Car rent calculation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM_RENT = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_RENT = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  let totalCost = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM_RENT) {
+    return totalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM_RENT) {
+    return totalCost - SHORT_TERM_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
