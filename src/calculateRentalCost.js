4972362779b25091ad19276785c0853ed6c21475
Anastasiia Vasylieva <anastasia.vasylieva97@gmail.com>
2024-02-09 11:34:09 +0100

    comments resolved

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,13 +28,13 @@
  */
 
 function calculateRentalCost(days) {
-  const RENTAL_COST = 40;
+  const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  const basePrice = RENTAL_COST * days;
+  const basePrice = PRICE_PER_DAY * days;
 
   if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
