db2f79cf4dce246f69c1acd0eb7f7586267ac471
Yaroslav Horodynskyi <gorodinskiyyar@gmail.com>
2024-02-09 14:30:53 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,16 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const priceDay = 40;
+  let price = days * priceDay;
+
+  if (days >= 7) {
+    price -= 50;
+  } else if (days >= 3) {
+    price -= 20;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
