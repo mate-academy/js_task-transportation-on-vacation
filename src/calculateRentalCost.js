abd204dddf12d608e8b144549f19277e73dd5b87
Taisiia Solonichenko <ts.max.98@gmail.com>
2024-02-11 21:30:36 +0200

    fix task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,22 +29,24 @@
 
 function calculateRentalCost(days) {
   // write code here
-  const PRICE = 40;
+  const PRICE_PER_DAY = 40;
 
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const MIDDLE_TERM = 3;
   const MIDDLE_TERM_DISCOUNT = 20;
 
+  const basePrice = days * PRICE_PER_DAY;
+
   if (days >= LONG_TERM) {
-    return days * PRICE - LONG_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (days >= MIDDLE_TERM) {
-    return days * PRICE - MIDDLE_TERM_DISCOUNT;
+    return basePrice - MIDDLE_TERM_DISCOUNT;
   }
 
-  return days * PRICE;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
