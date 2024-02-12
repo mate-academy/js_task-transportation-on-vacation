d894ef56ba3b4bfad5c4ef910c1a6503a77ec2b5
Елена Карнаух <elenakarnauh@MacBook-Pro-Elena.local>
2024-02-12 13:49:50 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,21 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const BASIC_PRICE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM = 3;
+  const MID_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    return BASIC_PRICE * days - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MID_TERM) {
+    return BASIC_PRICE * days - MID_TERM_DISCOUNT;
+  }
+
+  return BASIC_PRICE * days;
 }
 
 module.exports = calculateRentalCost;
