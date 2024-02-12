390d5e58337dfce521372f2dc801ed466b07a980
Ruslan Ogerenko <ruslanogerenko@gmail.com>
2024-02-12 13:33:13 +0200

    sloved the task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,6 +28,27 @@
  */
 
 function calculateRentalCost(days) {
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  let totalPrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    totalPrice -= LONG_TERM_DISCOUNT;
+
+    return totalPrice;
+  }
+
+  if (days >= SHORT_TERM) {
+    totalPrice -= SHORT_TERM_DISCOUNT;
+
+    return totalPrice;
+  }
+
+  return totalPrice;
   // write code here
 }
 
