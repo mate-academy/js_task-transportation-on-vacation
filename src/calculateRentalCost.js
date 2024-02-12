f2d849b9bf4b5efebc35cde4fda57bcccc73af6b
Yevhen Vladarskyi <vladarskyy@gmail.com>
2024-02-12 13:37:10 +0200

    small improvement

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -35,12 +35,12 @@ function calculateRentalCost(days) {
   const LONG_TERM_DISCOUNT = 50;
   const basePrice = days * DAY_PRICE;
 
-  if (days >= SHORT_TERM && days < LONG_TERM) {
-    return basePrice - SHORT_TERM_DISCOUNT;
-  }
-
   if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   return basePrice;
