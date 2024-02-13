c4b1cd891b28ee727f395e5083337ec4cb753cd8
Yurii Sokolov <franbow.work@gmail.com>
2024-02-13 14:38:29 +0200

    fix task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,14 +29,16 @@
 
 function calculateRentalCost(days) {
   const BASIC_RENTAL_COST = 40;
+  const SHORT_RENTAL_TERM = 3;
   const SMALL_DISCOUNT = 20;
+  const LONG_RENTAL_TERM = 7;
   const BIG_DISCOUNT = 50;
 
   let totalCost = days * BASIC_RENTAL_COST;
 
-  if (days >= 3 && days < 7) {
+  if (days >= SHORT_RENTAL_TERM && days < LONG_RENTAL_TERM) {
     totalCost = totalCost - SMALL_DISCOUNT;
-  } else if (days >= 7) {
+  } else if (days >= LONG_RENTAL_TERM) {
     totalCost = totalCost - BIG_DISCOUNT;
   }
 
