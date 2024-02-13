0314a6868804d966f5d426dd143b6bb898c25643
Volodymyr Vynnyk <vinnikvvv@gmail.com>
2024-02-13 10:01:14 +0200

    calculate rental cost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,23 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_PERIOD = 7;
+  const SHORT_PERIOD = 3;
+  const LONG_PERIOD_DISCOUNT = 50;
+  const SHORT_PERIOD_DISCOUNT = 20;
+
+  let totalPrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_PERIOD) {
+    return totalPrice - LONG_PERIOD_DISCOUNT;
+  }
+
+  if (days >= SHORT_PERIOD) {
+    return totalPrice - SHORT_PERIOD_DISCOUNT;
+  }
+
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
