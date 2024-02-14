5c0ae79664694ca4afad64fac1e93cd907eb36c2
MalgorzataEljaszukSanchez <malgorzata.sanchez.art@gmail.com>
2024-02-14 12:36:17 +0100

    task update

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,15 +29,19 @@
 function calculateRentalCost(days) {
   const DAILY_COST = 40;
   const LONG_TERM_DISCOUNT = 50;
+  const LONG_TERM_DAYS = 7;
+  const MEDIUM_TERM_DAYS = 3;
   const MEDIUM_TERM_DISCOUNT = 20;
   let rentalCost;
 
-  if (days >= 7) {
-    rentalCost = days * DAILY_COST - LONG_TERM_DISCOUNT;
-  } else if ((days >= 3) && (days < 7)) {
-    rentalCost = days * DAILY_COST - MEDIUM_TERM_DISCOUNT;
-  } else {
-    rentalCost = days * DAILY_COST;
+  rentalCost = days * DAILY_COST;
+
+  if (days >= LONG_TERM_DAYS) {
+    rentalCost = rentalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MEDIUM_TERM_DAYS && days < LONG_TERM_DAYS) {
+    rentalCost = rentalCost - MEDIUM_TERM_DISCOUNT;
   }
 
   return rentalCost;
