4d764489acd6820213aae84e2f683bd5cde4f837
Viktor Lytvynenko <lytvvv@gmail.com>
2024-02-15 00:55:31 +0100

    firstCommit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,6 +29,22 @@
 
 function calculateRentalCost(days) {
   // write code here
+  const PRICE_PER_DAY = 40;
+  const bestPrice = days * PRICE_PER_DAY;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    return bestPrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return bestPrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return bestPrice;
 }
 
 module.exports = calculateRentalCost;
