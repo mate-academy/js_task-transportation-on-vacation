01d73d4e524cd68d69b0047ccb9a081eb8aecf33
Oksana Kril <oksanakril@Oksanas-MacBook-Air.local>
2024-02-15 21:43:38 -0600

    names are fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,21 +28,24 @@
  */
 
 function calculateRentalCost(days) {
+  const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
 
   const MIDDLE_TERM = 3;
   const MIDDLE_TERM_DISCOUNT = 20;
 
+  const totalCost = days * PRICE_PER_DAY;
+
   if (days >= LONG_TERM) {
-    return (days * 40) - LONG_TERM_DISCOUNT;
+    return totalCost - LONG_TERM_DISCOUNT;
   }
 
   if (days >= MIDDLE_TERM) {
-    return (days * 40) - MIDDLE_TERM_DISCOUNT;
+    return totalCost - MIDDLE_TERM_DISCOUNT;
   }
 
-  return days * 40;
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
