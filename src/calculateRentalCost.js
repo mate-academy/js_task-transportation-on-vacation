324a981495d93b2c2845d65615bcc53690f7a43a
Ivan <110199ws@gmail.com>
2024-02-17 15:54:48 +0200

    add

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,15 +28,19 @@
  */
 
 function calculateRentalCost(days) {
+  const price = 40;
+  const minDiscount = 20;
+  const maxDiscount = 50;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * price - maxDiscount;
   }
 
   if (days >= 3) {
-    return days * 40 - 20;
+    return days * price - minDiscount;
   }
 
-  return days * 40;
+  return days * price;
 }
 
 module.exports = calculateRentalCost;
