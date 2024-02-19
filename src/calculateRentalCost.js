448236f85020731fa4ba1f353899d8f85ff2d711
DTMF <yarike451@gmail.com>
2024-02-19 11:53:04 +0200

    ready

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,22 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_PERIOD = 7;
+  const MIDDLE_PERIOD = 3;
+  const MIDDLE_DISCOND = 20;
+  const BIG_DISCOND = 50;
+  const PRICE = PRICE_PER_DAY * days;
+
+  if (days < MIDDLE_PERIOD) {
+    return PRICE;
+  }
+
+  if (days < LONG_PERIOD) {
+    return PRICE - MIDDLE_DISCOND;
+  }
+
+  return PRICE - BIG_DISCOND;
 }
 
 module.exports = calculateRentalCost;
