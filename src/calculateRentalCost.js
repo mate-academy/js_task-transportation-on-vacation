5fcf2a2ad7a39673045bfc71801f27c73e29db04
Serhii Khobotov <hobbitura@gmail.com>
2024-02-19 19:24:29 +0200

    transportation on vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -28,7 +28,24 @@
  */
 
 function calculateRentalCost(days) {
-  // write code here
+
+  const PRICE_ON_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const basePrice = PRICE_ON_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
