637f91144dacb4e491ac4f8419e5c713be35075f
Illia <ironme530@gmail.com>
2024-02-19 09:37:19 +0200

    change variables names

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -29,23 +29,23 @@
 
 function calculateRentalCost(days) {
   // write code here
-  const basePrice = 40;
+  const dayPrice = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  const finalPrice = days * basePrice;
+  const sumPrice = days * dayPrice;
 
   if (days >= LONG_TERM) {
-    return (finalPrice - LONG_TERM_DISCOUNT);
+    return (sumPrice - LONG_TERM_DISCOUNT);
   }
 
   if (days >= SHORT_TERM) {
-    return (finalPrice - SHORT_TERM_DISCOUNT);
+    return (sumPrice - SHORT_TERM_DISCOUNT);
   }
 
-  return finalPrice;
+  return sumPrice;
 }
 
 module.exports = calculateRentalCost;
