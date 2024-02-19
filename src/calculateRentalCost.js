b250d12c121cacde8dd9c38a2c7d4e01a1b216c0
IURII FEDYNA <iurii.fedyna@gmail.com>
2024-02-19 19:17:04 -0330

    update solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -33,16 +33,17 @@ function calculateRentalCost(days) {
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
+  const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return days * PRICE_PER_DAY - SHORT_TERM_DISCOUNT;
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
-  return days * PRICE_PER_DAY;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
