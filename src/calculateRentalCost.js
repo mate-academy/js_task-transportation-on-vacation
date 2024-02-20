5a2465ee86933e1345179a9379cbb4b979b9e444
Bartlomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-02-20 16:05:37 +0100

    Division in loop shortened

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,9 +7,9 @@ function calculateRentalCost(days) {
   let result = days * 40;
 
   if (days >= 7) {
-    result = result - 50;
+    result -= 50;
   } else if (days >= 3) {
-    result = result - 20;
+    result -= 20;
   }
 
   return result;
