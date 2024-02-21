23dd952a85c735cf3fb8883e2398fc717d0c3942
Filip-Mistyuk <mistukfilipp@gmail.com>
2024-02-20 22:19:05 -0800

    Committ

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const Price_per_day = 40;
+  const Long_term = 7;
+  const Long_term_discount = 50;
+  const short_term = 3;
+  const short_term_discount = 20;
+
+  const basePrice = days * Price_per_day;
+
+  if (days >= Long_term) {
+    return basePrice - Long_term_discount;
+  }
+
+  if (days >= short_term) {
+    return basePrice - short_term_discount;
+  }
+
+  return basePrice;
 }
+
 
 module.exports = calculateRentalCost;
