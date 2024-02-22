ce30f1d2c7759248b9dc9b8d972e6c2d968a4b4c
Rafael Wesley <rafaelwesleylima@gmail.com>
2024-02-22 20:10:37 -0300

    add solution according to checklist.md

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,15 +7,21 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  if (days >= 7) {
-    return days * 40 - 50;
+  const SHORT_TERM_DAYS = 3;
+  const LONG_TERM_DAYS = 7;
+  const DISCOUNT_FOR_7_DAYS = 50;
+  const DISCOUNT_FOR_3_DAYS = 20;
+  const totalSpend = days * 40;
+
+  if (days >= LONG_TERM_DAYS) {
+    return totalSpend - DISCOUNT_FOR_7_DAYS;
   }
 
-  if (days >= 3) {
-    return days * 40 - 20;
+  if (days >= SHORT_TERM_DAYS) {
+    return totalSpend - DISCOUNT_FOR_3_DAYS;
   }
 
-  return days * 40;
+  return totalSpend;
 }
 
 module.exports = calculateRentalCost;
