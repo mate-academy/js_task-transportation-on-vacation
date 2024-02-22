27166e5f198fe21e234929fb94b5077d2dd0d4e0
Bartlomiej Karbownik <bartlomiej.karbownik56@gmail.com>
2024-02-22 15:07:27 +0100

    Implemented constants with the uppercase convention

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let result = days * 40;
+  const DAILY_RATE = 40;
+  const WEEK_DISCOUNT = 50;
+  const THREE_DAY_DISCOUNT = 20;
+
+  let result = days * DAILY_RATE;
 
   if (days >= 7) {
-    result -= 50;
+    result -= WEEK_DISCOUNT;
   } else if (days >= 3) {
-    result -= 20;
+    result -= THREE_DAY_DISCOUNT;
   }
 
   return result;
