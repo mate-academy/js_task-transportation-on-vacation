9317b3bbc292754ac7bf195db816f23478676df6
eduvinagre <eduvinagre@gmail.com>
2024-02-26 10:55:49 -0300

    problem solving

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,19 +1,20 @@
+"use strict";
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-    let totalCost = 40 * days;
+  let totalCost = 40 * days;
 
-    if (days >= 7) {
-      totalCost -= 50;
-    } else if (days >= 3) {
-      totalCost -= 20;
-    }
-
-    return totalCost;
+  if (days >= 7) {
+    totalCost -= 50;
+  } else if (days >= 3) {
+    totalCost -= 20;
   }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
