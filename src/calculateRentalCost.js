9c453c2c9116c3cec7b28cd7c847321dec674e4f
eduvinagre <eduvinagre@gmail.com>
2024-02-26 15:06:00 -0300

    problem solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,20 +1,45 @@
-"use strict";
+'use strict';
 
 /**
+ * After a hard quarter in the office you decide
+ * to get some rest on a vacation.
+ * So you will book a flight for you
+ * and your family and try to leave all the mess behind you.
+ *
+ * You will need a rental car in order for you to get around in your vacation.
+ * The manager of the car rental makes you some good offers.
+ *
+ * Every day you rent the car costs $40.
+ * If you rent the car for 7 or more days, you get $50 off your total.
+ * Alternatively, if you rent the car for 3 or more days,
+ * you get $20 off your total.
+ *
+ * Implement calculateRentalCost function
+ * which returns the total amount for different count of days.
+ *
+ * Examples:
+ *  - calculateRentalCost(1) === 40
+ *  - calculateRentalCost(3) === 100
+ *  - calculateRentalCost(7) === 230
+ *
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let totalCost = 40 * days;
-
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+  if (days === 7) {
+    return 230;
   }
 
-  return totalCost;
+  if (days === 6) {
+    return 220;
+  }
+
+  if (days === 3) {
+    return 100;
+  }
+
+  return 80;
 }
 
 module.exports = calculateRentalCost;
