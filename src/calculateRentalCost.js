4a61b00ab4b53b09611097147b3341557343f112
Daniil Fadeev <daniilfadeev2018@gmail.com>
2024-02-26 12:45:58 +0200

    moved all numbers to consts

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,12 +7,14 @@ function calculateRentalCost(days) {
   const ONE_DAY_PRICE = 40;
   const MIDDLE_TERM_SALE = 20;
   const LONG_TERM_SALE = 50;
+  const LONG_TERM = 7;
+  const MIDDLE_TERM = 3;
 
   let total = 0;
 
-  if (days >= 7) {
+  if (days >= LONG_TERM) {
     total = ONE_DAY_PRICE * days - LONG_TERM_SALE;
-  } else if (days >= 3 && days < 7) {
+  } else if (days >= MIDDLE_TERM && days < LONG_TERM) {
     total = ONE_DAY_PRICE * days - MIDDLE_TERM_SALE;
   } else {
     total = ONE_DAY_PRICE * days;
