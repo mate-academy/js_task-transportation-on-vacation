7739f50480dc4436d2d8889135ea09a2c9847fe7
Daniil Fadeev <daniilfadeev2018@gmail.com>
2024-02-26 11:54:34 +0200

    назва коміту

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const ONE_DAY_PRICE = 40;
+  const MIDDLE_TERM_SALE = 20;
+  const LONG_TERM_SALE = 50;
+
+  let total = 0;
+
+  if (days >= 7) {
+    total = ONE_DAY_PRICE * days - LONG_TERM_SALE;
+  } else if (days >= 3 && days < 7) {
+    total = ONE_DAY_PRICE * days - MIDDLE_TERM_SALE;
+  } else {
+    total = ONE_DAY_PRICE * days;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
