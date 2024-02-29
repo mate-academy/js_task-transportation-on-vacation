4fe7e2bcaf695d3797d72236bf08115f6a85d595
Dmytro Boiko <iamboikodmytro@gmail.com>
2024-02-29 20:59:48 +0200

    develop function Rental Cost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const RENTAL_DAY_RATE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    return ((days * RENTAL_DAY_RATE) - LONG_TERM_DISCOUNT);
+  }
+
+  if (days < SHORT_TERM) {
+    return (days * RENTAL_DAY_RATE);
+  }
+
+  return ((days * RENTAL_DAY_RATE) - SHORT_TERM_DISCOUNT);
 }
 
 module.exports = calculateRentalCost;
