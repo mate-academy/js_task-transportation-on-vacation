64d0e330f15fe859c24cdcf1c24adc57de919568
metrakon <metrakon@proton.me>
2024-03-05 22:32:12 +0200

    add calculateRentalCost function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const baseCost = 40;
+  const shortTermDiscount = 20;
+  const longTermDiscount = 50;
+  const shortTermThreshold = 3;
+  const longTermThreshold = 7;
+
+  if (days < shortTermThreshold) {
+    return baseCost * days;
+  }
+
+  if (days >= shortTermThreshold && days < longTermThreshold) {
+    return baseCost * days - shortTermDiscount;
+  }
+
+  if (days >= longTermThreshold) {
+    return baseCost * days - longTermDiscount;
+  }
 }
 
 module.exports = calculateRentalCost;
