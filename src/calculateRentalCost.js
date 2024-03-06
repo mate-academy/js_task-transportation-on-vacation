eace60f2fc60e48134f0e2a4e25a37ebd4adffc2
Olaf <olaf.chuszno@gmail.com>
2024-03-06 19:03:16 +0100

    Extract constants from the function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,13 +3,14 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const MID_TERM = 3;
-  const MID_TERM_DISCOUNT = 20;
-  const PRICE_PER_DAY = 40;
 
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const MID_TERM = 3;
+const MID_TERM_DISCOUNT = 20;
+const PRICE_PER_DAY = 40;
+
+function calculateRentalCost(days) {
   const BASE_PRICE = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
