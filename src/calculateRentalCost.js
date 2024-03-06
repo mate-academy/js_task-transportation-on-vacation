05a655793c96752fa9cb3e579daaf54aceb9d749
Ann <ania.somko@gmail.com>
2024-03-06 21:00:42 +0200

    create variables due to the checklist

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let TOTAL = 0;
+  const MIN_DISCOUNT = 20;
+  const MAX_DISCOUNT = 50;
+  const PRICE = 40;
+  const MIN_DAYS = 3;
+  const MAX_DAYS = 6;
 
-  if (days < 3) {
-    TOTAL = days * 40;
-  } else if (days <= 6) {
-    TOTAL = days * 40 - 20;
-  } else {
-    TOTAL = days * 40 - 50;
+  if (days < MIN_DAYS) {
+    return days * PRICE;
   }
 
-  return TOTAL;
+  if (days <= MAX_DAYS) {
+    return days * PRICE - MIN_DISCOUNT;
+  }
+
+  return days * PRICE - MAX_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
