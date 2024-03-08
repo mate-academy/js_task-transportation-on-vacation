14c4d1a465d34cf2d4f93218af3e35f57994e16e
Levik-24 <lfetman@gmail.com>
2024-03-08 18:41:12 +0200

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const CAR_COST = 40;
+  const LONG_TERM_RENT = 7;
+  const SHORT_TERM_RENT = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const basePrice = days * CAR_COST;
+
+  if (days >= LONG_TERM_RENT) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days < LONG_TERM_RENT && days >= SHORT_TERM_RENT) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
