7b57ff82ce7301ee27833109f7ec79cba1d24492
Julia Churkina <curkinau980@gmail.com>
2024-03-10 01:06:54 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,10 +7,14 @@ function calculateRentalCost(days) {
   const oneDaycost = 40;
   const sevenDaysDiscount = 50;
   const threeDayDiscount = 20;
+  const threeDaysAmount = 3;
+  const sevenDaysAmount = 7;
 
-  if (days < 3) {
+  if (days < threeDaysAmount) {
     return days * oneDaycost;
-  } else if (days >= 3 && days < 7) {
+  }
+
+  if (days >= threeDaysAmount && days < sevenDaysAmount) {
     return days * oneDaycost - threeDayDiscount;
   }
 
