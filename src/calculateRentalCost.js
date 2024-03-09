081669459ba769d4e1d0b4c00bc142cacb59419d
Olha Marchuk <1991mov@gmail.com>
2024-03-09 16:30:29 +0100

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const priceRental = 40;
+  const saleLongPtriod = 50;
+  const saleShortPtriod = 20;
+  const sumRentalStandart = days * priceRental;
+
+  if (days >= 7) {
+    return sumRentalStandart - saleLongPtriod;
+  }
+
+  if (days >= 3) {
+    return sumRentalStandart - saleShortPtriod;
+  }
+
+  return sumRentalStandart;
 }
 
 module.exports = calculateRentalCost;
