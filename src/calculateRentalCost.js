79d4d40fcd5fe31045185a6e949ef19f73007966
Akif Akhmedov <akifakhmedovmail@gmail.com>
2024-03-10 17:38:01 +0100

    correcting misspelled words

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,23 +5,23 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  const RENT_COAST = 40;
+  const RENT_COST = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
 
-  const MIDIUM_TERM = 3;
-  const MIDIUM_TERM_DISCOUNT = 20;
+  const MEDIUM_TERM = 3;
+  const MEDIUM_TERM_DISCOUNT = 20;
 
-  if (days < MIDIUM_TERM) {
-    return days * RENT_COAST;
+  if (days < MEDIUM_TERM) {
+    return days * RENT_COST;
   }
 
-  if (days >= MIDIUM_TERM && days < LONG_TERM) {
-    return days * RENT_COAST - MIDIUM_TERM_DISCOUNT;
+  if (days >= MEDIUM_TERM && days < LONG_TERM) {
+    return days * RENT_COST - MEDIUM_TERM_DISCOUNT;
   }
 
   if (days >= LONG_TERM) {
-    return days * RENT_COAST - LONG_TERM_DISCOUNT;
+    return days * RENT_COST - LONG_TERM_DISCOUNT;
   }
 }
 
