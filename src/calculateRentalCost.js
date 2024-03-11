4f46ab3bc2cdcec98572c93c44628b58112bf8ac
Mariami Borul <mhurhienidze@gmail.com>
2024-03-11 12:32:24 +0200

    added task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,14 +5,16 @@
  */
 function calculateRentalCost(days) {
   const basePrice = 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
+  if (days >= LONG_TERM) {
     return basePrice * days - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
+  if (days >= SHORT_TERM) {
     return basePrice * days - SHORT_TERM_DISCOUNT;
   }
 
