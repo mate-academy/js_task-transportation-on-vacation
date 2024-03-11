07b954c5a889f481d693ff99d60a21b7877c3337
alex_motalex <tokyopac@gmail.com>
2024-03-11 18:11:33 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_RENT = 40;
+  const DAYS_TO_HAVE_LARGE_DISCOUNT = 7;
+  const DAYS_TO_HAVE_LOW_DISCOUNT = 3;
+  const LARGE_DISCOUNT = 50;
+  const LOW_DISCOUNT = 20;
+
+  if (days >= DAYS_TO_HAVE_LARGE_DISCOUNT) {
+    return days * DAILY_RENT - LARGE_DISCOUNT;
+  }
+
+  if (days >= DAYS_TO_HAVE_LOW_DISCOUNT) {
+    return days * DAILY_RENT - LOW_DISCOUNT;
+  }
+
+  return days * DAILY_RENT;
 }
 
 module.exports = calculateRentalCost;
