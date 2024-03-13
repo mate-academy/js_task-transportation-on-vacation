ebfb15712bb2a7beb89981f972aea4eecb2da243
Victor Skits <vityok.nice@gmail.com>
2024-03-13 11:13:20 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,25 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+
+  const smallPeriod = 3;
+  const smallPeriodDiscount = 20;
+
+  const bigPeriod = 7;
+  const bigPeriodDiscount = 50;
+
+  const totalRentalPrice = days * pricePerDay;
+
+  if (days >= bigPeriod) {
+    return totalRentalPrice - bigPeriodDiscount;
+  }
+
+  if (days >= smallPeriod) {
+    return totalRentalPrice - smallPeriodDiscount;
+  }
+
+  return totalRentalPrice;
 }
 
 module.exports = calculateRentalCost;
