6236914ea65fe19b5267f9e3455afc06bc38270f
ihor_romaniuk <iromaniuk@mycubes.nl>
2024-03-17 09:36:59 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,21 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let totalAmount = days * 40;
+
+  switch (true) {
+    case days >= 7: {
+      totalAmount -= 50;
+      break;
+    }
+
+    case days >= 3: {
+      totalAmount -= 20;
+      break;
+    }
+  }
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
