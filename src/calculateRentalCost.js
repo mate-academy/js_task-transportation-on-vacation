2fd132cb948ec15456aa8753c0b7a7f7443ce9de
Vladislav <7evenLays@gmail.com>
2024-03-19 14:36:23 +0200

    all tests good

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DICOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DICOUNT = 20;
+
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DICOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DICOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
