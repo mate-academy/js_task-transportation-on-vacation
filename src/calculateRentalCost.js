9964a9bc52ea6c063acc6b609b854b06902fb4ff
Vnxz <68802852+vinasxz@users.noreply.github.com>
2024-03-19 21:18:51 -0300

    fixing my code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let totalCost = days * 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const BASE_PRICE = 40;
 
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+  if (days >= LONG_TERM) {
+    return days * BASE_PRICE - LONG_TERM_DISCOUNT;
   }
 
-  return totalCost;
+  if (days < LONG_TERM && days >= SHORT_TERM) {
+    return days * BASE_PRICE - SHORT_TERM_DISCOUNT;
+  }
+
+  return days * BASE_PRICE;
 }
 
 module.exports = calculateRentalCost;
