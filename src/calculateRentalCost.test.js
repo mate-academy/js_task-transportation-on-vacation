bb34c2ee70aa16c128d3833dee082b1bd13860df
Oleksandr Tereshchuk <118559210+AlexRomant@users.noreply.github.com>
2024-03-21 20:11:08 +0200

    Update calculateRentalCost.test.js

--- src/calculateRentalCost.test.js
+++ src/calculateRentalCost.test.js
@@ -6,12 +6,10 @@ test('Should add an additional discount for 1 and more days of rent', () => {
   expect(calculateRentalCost(1))
     .toBe(40);
 });
-
 test(`Do not add any discount for less than 3 days`, () => {
   expect(calculateRentalCost(3))
     .toBe(100);
 });
-
 test('Should add the basic discount for 7 days of rent', () => {
   expect(calculateRentalCost(7))
     .toBe(230);
