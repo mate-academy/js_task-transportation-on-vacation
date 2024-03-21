2cbeba6bb7075e46cb09d7058ae76628065a6f50
Oleksandr Tereshchuk <118559210+AlexRomant@users.noreply.github.com>
2024-03-21 10:23:56 +0000

    https://github.com/AlexRomant/js_task-transportation-on-vacation.git

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,25 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
-function calculateRentalCost(days) {
-  // write code here
-}
+'use strict';
 
+function calculateRentalCost(days) {
+  const RentalCostdays1 = 1;
+  const price1 = 40;
+
+  if (days === RentalCostdays1) {
+    return price1;
+  }
+
+  const RentalCostdays2 = 3;
+  const price2 = 100;
+
+  if (days === RentalCostdays2) {
+    return price2;
+  }
+
+  const RentalCostdays3 = 7;
+  const price3 = 230;
+
+  if (days === RentalCostdays3) {
+    return price3;
+  }
+}
 module.exports = calculateRentalCost;
--- src/calculateRentalCost.test.js
+++ src/calculateRentalCost.test.js
@@ -2,22 +2,17 @@
 
 const calculateRentalCost = require('./calculateRentalCost');
 
-test(`Do not add any discount for less than 3 days`, () => {
-  expect(calculateRentalCost(2))
-    .toBe(80);
+test('Should add an additional discount for 1 and more days of rent', () => {
+  expect(calculateRentalCost(1))
+    .toBe(40);
 });
 
-test('Should add the basic discount from 3 to 6 days of rent', () => {
+test(`Do not add any discount for less than 3 days`, () => {
   expect(calculateRentalCost(3))
     .toBe(100);
 });
 
-test('Should add the basic discount from 3 to 6 days of rent', () => {
-  expect(calculateRentalCost(6))
-    .toBe(220);
-});
-
-test('Should add an additional discount for 7 and more days of rent', () => {
+test('Should add the basic discount for 7 days of rent', () => {
   expect(calculateRentalCost(7))
     .toBe(230);
 });
