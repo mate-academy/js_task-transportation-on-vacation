cf91615f59368e4994ae2c496b1995a12a03ca45
Oleksandr Tereshchuk <118559210+AlexRomant@users.noreply.github.com>
2024-03-22 12:22:56 +0200

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -2,19 +2,19 @@
 
 function calculateRentalCost(days) {
   const RentalCostday = 40;
-  const Discount3DaysMore = 20;
-  const Discount7DaysMore = 50;
+  const Discount3DaysOrMore = 20;
+  const Discount7DaysOrMore = 50;
   let totalCost = days * RentalCostday;
 
   if (days ===1) {
     return RentalCostday;
   }
 
-  if (days >= 3 & days <= 7) {
-   totalCost -= Discount3DaysMore;
+  if (days >= 3 && days <= 7) {
+   totalCost -= Discount3DaysOrMore;
   }
   if (days >= 7) {
-    totalCost-= Discount7DaysMore;
+    totalCost-= Discount7DaysOrMore;
   }
   return totalCost;
 }
