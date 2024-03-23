b0b33919977245c205d29a20ab79fc75ede6ffa8
Bohdan Dymydiuk <b.v.dymydiuk@gmail.com>
2024-03-23 09:56:11 +0200

    add task soution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const TOTAL_PRICE = days * PRICE_PER_DAY;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  if (days < SHORT_TERM) {
+    return TOTAL_PRICE;
+  }
+
+  if (days >= LONG_TERM) {
+    return TOTAL_PRICE - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return TOTAL_PRICE - SHORT_TERM_DISCOUNT;
+  }
 }
 
 module.exports = calculateRentalCost;
