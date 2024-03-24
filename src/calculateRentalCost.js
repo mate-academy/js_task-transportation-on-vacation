a567b8f4fd921f761c844a5e5a1faf06a5734348
Ivan Sumskyi <ivensmsk@gmail.com>
2024-03-24 14:32:22 +0200

    move constats

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,12 +3,13 @@
  *
  * @return {number}
  */
+const PRICE_PER_DAY = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
+
 function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
   const totalCost = PRICE_PER_DAY * days;
 
   if (days >= LONG_TERM) {
