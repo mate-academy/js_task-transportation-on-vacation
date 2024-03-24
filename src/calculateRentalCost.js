648b1981bc8fa35cfd3bd83cf6bc099a49e8b30e
Your Name <karpinska660@gmail.com>
2024-03-24 14:02:11 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const REGULAR_COST = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const MIDDLE_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const MIDDLE_TERM = 3;
+  let price = days * REGULAR_COST;
+
+  if (days >= LONG_TERM) {
+    price -= LONG_TERM_DISCOUNT;
+  } else if (days >= MIDDLE_TERM) {
+    price -= MIDDLE_TERM_DISCOUNT;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
