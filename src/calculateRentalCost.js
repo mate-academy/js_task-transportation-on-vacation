4e77969e1f7080be23c752457e96fd2552c74244
Anton Yupko <antonyupkodm@gmail.com>
2024-03-24 14:14:33 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const MID_TERM = 3;
+  const LONG_TERM = 7;
+  const DAILY_RENT = 40;
+  const MID_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+  const BASIC_PRICE = days * DAILY_RENT;
+
+  if (days < MID_TERM) {
+    return BASIC_PRICE;
+  }
+
+  if (days < LONG_TERM) {
+    return BASIC_PRICE - MID_TERM_DISCOUNT;
+  }
+
+  return BASIC_PRICE - LONG_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
