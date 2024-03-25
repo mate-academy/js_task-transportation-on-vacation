3c043e0a3f55057d5bd403d9c2d7796e6cb2d5ce
Yurii Filiushkin <yurii.filiushkin@gmail.com>
2024-03-25 14:00:36 +0200

    Sonic

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_PRICE = 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const BIG_DISCOUNT = 50;
+  const SMALL_DISCAUNT = 20;
+  const totalPrice = DAILY_PRICE * days;
+
+  if (days >= LONG_TERM) {
+    return totalPrice - BIG_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return totalPrice - SMALL_DISCAUNT;
+  }
+
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
