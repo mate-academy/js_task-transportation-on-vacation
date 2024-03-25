dcf9b1569ca0a8e3753f077eb8a56f6419e86018
marchAlbion <marchAlbion@gmail.com>
2024-03-25 14:36:17 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,23 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const LONG_TERM_DISCOUNT = 50;
+  const THREE_DAYS_DISCOUNT = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const SEVEN_DAYS_DISCOUNT = 7;
+  const PRICE_FOR_ONE_DAY = 40;
+
+  let TOTAL_PRICE = PRICE_FOR_ONE_DAY * days;
+
+  if (days >= SEVEN_DAYS_DISCOUNT) {
+    TOTAL_PRICE -= LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= THREE_DAYS_DISCOUNT && days < SEVEN_DAYS_DISCOUNT) {
+    TOTAL_PRICE -= SHORT_TERM_DISCOUNT;
+  }
+
+  return TOTAL_PRICE;
 }
 
 module.exports = calculateRentalCost;
