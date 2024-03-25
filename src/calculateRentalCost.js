7489df13bfd365cd23a6c160e932b029163d1c83
Taras <tarasamchenko@gmail.com>
2024-03-25 18:51:38 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const basePrice = PRICE_PER_DAY * days;
+  const DISCOUNT_FOR_THREE_DAYS = 20;
+  const DISCOUNT_FOR_SEVEN_DAYS = 50;
+
+  if (days > 2 && days < 7) {
+    return basePrice - DISCOUNT_FOR_THREE_DAYS;
+  }
+
+  if (days > 6) {
+    return basePrice - DISCOUNT_FOR_SEVEN_DAYS;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
