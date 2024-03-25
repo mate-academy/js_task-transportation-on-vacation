dae278f6aca5a56284ecde63a968c5efaf620773
Oleh Dynys <dynys.oleh@gmail.com>
2024-03-25 14:44:16 +0200

    solution js first task vs debug

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAY_PRICE = 40;
+  const LONG_TERM = 7;
+  const MIN_TERM = 3;
+  const MIN_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+  const renCost = DAY_PRICE * days;
+
+  if (days >= MIN_TERM && days < LONG_TERM) {
+    return renCost - MIN_TERM_DISCOUNT;
+  }
+
+  if (days >= 7) {
+    return renCost - LONG_TERM_DISCOUNT;
+  }
+
+  return renCost;
 }
 
 module.exports = calculateRentalCost;
