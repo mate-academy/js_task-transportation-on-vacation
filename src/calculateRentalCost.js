207c6a9974b89ddebcabae1afe14610664da84be
Prytula Olexandr <liga5872@gmail.com>
2024-03-25 08:50:03 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,15 +9,15 @@ function calculateRentalCost(days) {
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
-  let price = days * PRICE_PER_DAY;
+  let basicPrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    price -= LONG_TERM_DISCOUNT;
+    basicPrice -= LONG_TERM_DISCOUNT;
   } else if (days >= SHORT_TERM) {
-    price -= SHORT_TERM_DISCOUNT;
+    basicPrice -= SHORT_TERM_DISCOUNT;
   }
 
-  return price;
+  return basicPrice;
 }
 
 module.exports = calculateRentalCost;
