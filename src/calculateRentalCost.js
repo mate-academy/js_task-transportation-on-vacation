881717f129659ce328481a281f848374ab1df7e4
Ilya Solonetskiy <mr.king101@ukr.net>
2024-03-25 16:52:41 +0200

    rentCost Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const EVERY_DAY_PAY = 40;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  if (days >= LONG_TERM) {
+    return days * EVERY_DAY_PAY - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return days * EVERY_DAY_PAY - SHORT_TERM_DISCOUNT;
+  }
+
+  return days * EVERY_DAY_PAY;
 }
 
 module.exports = calculateRentalCost;
