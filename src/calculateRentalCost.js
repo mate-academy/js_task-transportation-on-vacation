6e014162debf0b9be59d42e703c9f6bef005ae95
yarynaburenko <yaryna.burenko@gmail.com>
2024-03-26 14:47:50 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let price = days * 40;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  let price = days * PRICE_PER_DAY;
 
-  if (days >= 7) {
-    price -= 50;
-  } else if (days >= 3) {
-    price -= 20;
+  if (days >= LONG_TERM) {
+    price -= LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    price -= SHORT_TERM_DISCOUNT;
   }
 
   return price;
