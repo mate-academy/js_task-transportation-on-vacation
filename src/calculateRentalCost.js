8672d710cac745b8d03d76748ea6d42008499d9e
DiNMEA <rukiass93@gmail.com>
2024-04-04 12:21:55 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,26 @@
+/* eslint-disable no-const-assign */
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM_7 = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_3 = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
+  let Baseprice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM_7) {
+    return (Baseprice -= LONG_TERM_DISCOUNT);
+  }
+
+  if (days >= SHORT_TERM_3) {
+    return (Baseprice -= SHORT_TERM_DISCOUNT);
+  }
+
+  return days * PRICE_PER_DAY;
+}
 module.exports = calculateRentalCost;
