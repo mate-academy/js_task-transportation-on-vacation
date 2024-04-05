3e8c8f72316c391cbdd1b4adff052c57582a1d54
Seva Podolskiy <s_podolskiy@live.com>
2024-04-05 17:34:23 -0400

    added solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,15 +6,17 @@
 function calculateRentalCost(days) {
   // write code hereadfasdf
 
-  const costPerDay = 40;
+  const COST_PER_DAY = 40;
 
   if (days < 3) {
-    return days * costPerDay;
-  } else if (days < 7) {
-    return days * costPerDay - 20;
-  } else {
-    return days * costPerDay - 50;
+    return days * COST_PER_DAY;
   }
+
+  if (days < 7) {
+    return days * COST_PER_DAY - 20;
+  }
+
+  return days * COST_PER_DAY - 50;
 }
 
 module.exports = calculateRentalCost;
