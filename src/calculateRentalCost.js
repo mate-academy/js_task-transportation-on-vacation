ee05a3f183868a41d707296da78fe11226ad3a6d
Yuliia shevchuk <shevchuuchkaaa@gmail.com>
2024-04-07 16:40:05 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const PRISE_PER_DAY = 40;
+
+  const daysPrice = PRISE_PER_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return daysPrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return daysPrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return daysPrice;
 }
 
 module.exports = calculateRentalCost;
