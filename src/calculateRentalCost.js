fabf5e2c069dc8d6011bd02b2c68972bdf3c4f26
Vadym Hurnik <vad.com.z@gmail.com>
2024-04-09 20:59:17 +0300

    implemented task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCost = 40;
+  const totalCost = rentalCost * days;
+
+  if (days >= 3 && days < 7) {
+    return totalCost - 20;
+  }
+
+  if (days >= 7) {
+    return totalCost - 50;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
