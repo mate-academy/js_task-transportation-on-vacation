ecf1056e55ef8c3952a0cd9edbb2afcee5228c0d
Vadym Hurnik <vad.com.z@gmail.com>
2024-04-09 21:07:26 +0300

    added constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,18 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const rentalCost = 40;
-  const totalCost = rentalCost * days;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
 
-  if (days >= 3 && days < 7) {
-    return totalCost - 20;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const RENTAL_COST_PER_DAY = 40;
+  const TOTAL_COST = RENTAL_COST_PER_DAY * days;
+
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    return TOTAL_COST - SHORT_TERM_DISCOUNT;
   }
 
-  if (days >= 7) {
-    return totalCost - 50;
+  if (days >= LONG_TERM) {
+    return TOTAL_COST - LONG_TERM_DISCOUNT;
   }
 
-  return totalCost;
+  return TOTAL_COST;
 }
 
 module.exports = calculateRentalCost;
