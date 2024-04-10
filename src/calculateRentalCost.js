2d01f521dbe66feb75bc8d5403594b970ca3d55e
under-a <mymail15@i.ua>
2024-04-10 21:04:45 +0300

    Solution added

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyRent = 40;
+  const threeDaysOff = 20;
+  const sevenDaysOff = 50;
+  const totalPayment = days * dailyRent;
+
+  switch (true) {
+    case days >= 7:
+      return totalPayment - sevenDaysOff;
+
+    case days >= 3:
+      return totalPayment - threeDaysOff;
+
+    default:
+      return totalPayment;
+  }
 }
 
 module.exports = calculateRentalCost;
