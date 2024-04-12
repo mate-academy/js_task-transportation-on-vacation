9b5dc088aa9f3375bb605d76739cfe28a05426eb
MarkGodnyi <libertyinprogresslife@gmail.com>
2024-04-12 17:59:29 +0200

    implemented calculate rental cost function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const SMALL_TERM = 3;
+  const DISCOUNT = 20;
+  const BIG_DISCOUNT = 50;
+  const PRICE_PER_DAY = 40;
+  const price = days * PRICE_PER_DAY;
+
+  if (days < SMALL_TERM) {
+    return price;
+  }
+
+  return days < LONG_TERM ? price - DISCOUNT : price - BIG_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
