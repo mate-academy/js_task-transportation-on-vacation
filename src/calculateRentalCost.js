403eef0d1997f35a434afd1aac697420b090a7f1
Ihnat-Zinkevich <ignatzinkevch@gmail.com>
2024-04-15 17:52:08 +0200

    2 commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const price = 40;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const shirtTerm = 3;
+  const shirtTermDiscount = 20;
+  let totalPrice = price * days;
+
+  if (days >= longTerm) {
+    totalPrice -= longTermDiscount;
+  } else if (days >= shirtTerm) {
+    totalPrice -= shirtTermDiscount;
+  }
+
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
