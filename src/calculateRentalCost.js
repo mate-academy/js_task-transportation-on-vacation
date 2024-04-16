1dfee3913a6ea5a8b5b902c1bc9fc9c467bc10ef
Volodymyr Kryvytskyi <v.kryvytskyy@mate.academy>
2024-04-16 16:54:11 +0300

    solution 1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let finalPrice = 0;
+
+  if (days >= 7) {
+    finalPrice = 40 * days - 50;
+  } else if (days >= 3) {
+    finalPrice = 40 * days - 20;
+  } else {
+    finalPrice = 40 * days;
+  }
+
+  return finalPrice;
 }
 
 module.exports = calculateRentalCost;
