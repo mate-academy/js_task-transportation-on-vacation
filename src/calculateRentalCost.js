39fbbf39d50b26bb6f6fcd82380cf171c160785c
Nivon35 <Nivon35@gmail.com>
2024-04-16 20:07:18 +0200

    fixing Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,11 +5,13 @@
  */
 function calculateRentalCost(days) {
   const baseCost = days * 40;
+  const shortDiscount = baseCost - 20;
+  const longDiscount = baseCost - 50;
 
   if (days >= 7) {
-    return baseCost - 50;
+    return longDiscount;
   } else if (days >= 3) {
-    return baseCost - 20;
+    return shortDiscount;
   } else {
     return baseCost;
   }
