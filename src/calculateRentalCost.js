9266e7fe8ab9512cc083c1ab2e663cca6dc50c7e
ElaOz <ozga.elzbieta94@gmail.com>
2024-04-16 22:07:27 +0200

    Code fixes after review - added variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const dayPrice = 40;
+  const discSmall = 20;
+  const discBig = 50;
+
   if (days < 3) {
-    return days * 40;
+    return days * dayPrice;
   } else if (days >= 3 && days < 7) {
-    return days * 40 - 20;
+    return days * dayPrice - discSmall;
   } else if (days >= 7) {
-    return days * 40 - 50;
+    return days * dayPrice - discBig;
   } else {
     return 'please insert number';
   }
