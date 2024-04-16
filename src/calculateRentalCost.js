3f568e6214be4e342d39dee5b808a5cd51d99ea4
ekaterinapopova1382 <145997082+ekaterinapopova1382@users.noreply.github.com>
2024-04-16 20:33:14 +0200

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const dailyRate = 40;
-  let totalCost = days * dailyRate;
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+    const dailyRate = 40;
+    let totalCost = days * dailyRate;
+    let discount7DaysOrMore = 50;
+    let discount3DaysOrMore = 20;
+    if (days >= 7) {
+      totalCost -= discount7DaysOrMore;
+    } else if (days >= 3) {
+      totalCost -= discount3DaysOrMore;
+    }
+    return totalCost;
   }
-  return totalCost;
-}
+
 
 
 module.exports = calculateRentalCost;
