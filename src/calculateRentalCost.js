aa3e0b5c6d6a048bbd81e2b367644d03f5e187a0
Pavel Labenets <labunets.pavel@gmail.com>
2024-04-17 14:08:55 +0300

    ready

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const price = 40;
+  const discount20 = 20;
+  const discount50 = 50;
+  const basePrice = days * price;
+
+  if (days >= 7) {
+    return basePrice - discount50;
+  }
+
+  if (days >= 3) {
+    return basePrice - discount20;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
