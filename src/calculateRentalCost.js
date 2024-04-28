2cdd0dd79cba70bd6e2eddca89568aba913332bc
Viktoriya Riabets <vikamalik30@gmail.com>
2024-04-28 12:21:47 -0400

    fixing errors

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,17 +3,25 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const dailyRate = 40;
-  let discount = 0;
 
-  if (days >= 7) {
-    discount = 50;
-  } else if (days >= 3) {
-    discount = 20;
+const DAILY_RATE = 40;
+const LONG_TERM_DAYS = 7;
+const MID_TERM_DAYS = 3;
+const LONG_TERM_DISCOUNT = 50;
+const MID_TERM_DISCOUNT = 20;
+
+function calculateRentalCost(days) {
+  const totalCost = days * DAILY_RATE;
+
+  if (days >= LONG_TERM_DAYS) {
+    return totalCost - LONG_TERM_DISCOUNT;
   }
 
-  return days * dailyRate - discount;
+  if (days >= MID_TERM_DAYS) {
+    return totalCost - MID_TERM_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
