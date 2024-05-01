200dc95d4d92df5676ecee815b55b80f12148302
Volodymyr <vova.lime@icloud.com>
2024-05-01 17:34:19 +0300

    test

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  if (days <= 2) {
+    return days * 40;
+  }
+
+  if (days <= 6) {
+    return days * 40 - 20;
+  }
+
+  if (days >= 7) {
+    return days * 40 - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
