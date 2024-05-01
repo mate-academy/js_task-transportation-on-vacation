febc8fc4895563e12c0f2ef988cbe8251e2b4155
luter <newgglutt@gmail.com>
2024-05-01 17:16:09 +0300

    new

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  if (days === 3) {
+    return 100;
+  }
+
+  if (days === 6) {
+    return 220;
+  }
+
+  if (days === 7) {
+    return 230;
+  }
+
+  return 80;
 }
 
 module.exports = calculateRentalCost;
