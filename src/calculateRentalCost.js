2324a2531ad7ccb3c368febc4505a7b8585e6654
Khomenko Bohdan <bogdandelakrua@gmail.comgit config --global user.email bogdandelakrua@gmail.com>
2024-05-03 20:32:40 +0300

    restored total cost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,17 +9,17 @@ function calculateRentalCost(days) {
   const MAX_DISCOUNT = 50;
   const MIN_DAYS = 3;
   const MAX_DAYS = 7;
-  const TOTAL_COST = days * COST;
+  let totalCost = days * COST;
 
   if (days >= MAX_DAYS) {
-    return TOTAL_COST - MAX_DISCOUNT;
+    return totalCost - MAX_DISCOUNT;
   }
 
   if (days >= MIN_DAYS) {
-    return TOTAL_COST - MIN_DISCOUNT;
+    return totalCost - MIN_DISCOUNT;
   }
 
-  return TOTAL_COST;
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
