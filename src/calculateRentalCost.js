1dfa25a1eeb7fe754902f1f5aa45c50e6a0c2743
Khomenko Bohdan <bogdandelakrua@gmail.comgit config --global user.email bogdandelakrua@gmail.com>
2024-05-03 20:15:07 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const COST = 40;
+  const MIN_DISCOUNT = 20;
+  const MAX_DISCOUNT = 50;
+  const MIN_DAYS = 3;
+  const MAX_DAYS = 7;
+  const TOTAL_COST = days * COST;
+
+  if (days >= MAX_DAYS) {
+    return TOTAL_COST - MAX_DISCOUNT;
+  }
+
+  if (days >= MIN_DAYS) {
+    return TOTAL_COST - MIN_DISCOUNT;
+  }
+
+  return TOTAL_COST;
 }
 
 module.exports = calculateRentalCost;
