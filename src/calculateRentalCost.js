84853e8f1b1c99d24a1a20d901a53998cdb806b9
Serhiy Nemerko <serhiynemerko@gmail.com>
2024-05-03 22:18:46 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,15 +6,19 @@
 function calculateRentalCost(days) {
   const priseOfDay = 40;
   let total = priseOfDay * days;
+  const middleTermin = 3;
+  const middleTerminDiscount = 20;
+  const longTermin = 7;
+  const longTerminDiscount = 50;
 
-  if (days >= 7) {
-    total -= 50;
+  if (days >= longTermin) {
+    total -= longTerminDiscount;
 
     return total;
   }
 
-  if (days >= 3) {
-    total -= 20;
+  if (days >= middleTermin) {
+    total -= middleTerminDiscount;
   }
 
   return total;
