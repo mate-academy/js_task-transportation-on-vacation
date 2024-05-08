e58e7cee36f38fdef6de875e43ac6e28aa1e985b
Wojciech Goliasz <wojtekgoliasz98@gmail.com>
2024-05-08 22:10:30 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const AMOUNT_FOR_DAY = 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const SMALL_DISCOUNT = 20;
+  const BIG_DISCOUNT = 50;
+
+  const AMOUNT = days * AMOUNT_FOR_DAY;
+
+  if (days >= LONG_TERM) {
+    return AMOUNT - BIG_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return AMOUNT - SMALL_DISCOUNT;
+  }
+
+  return AMOUNT;
 }
 
 module.exports = calculateRentalCost;
