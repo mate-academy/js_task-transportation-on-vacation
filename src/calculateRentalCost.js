f55a8d3b5e44ea3f1d68aae93316dcdde4b60cfc
Liudmyla Pantelieieva <eltechlvwork@gmail.com>
2024-05-10 15:13:36 +0300

    Update solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  if (days >= 7) {
-    return days * 40 - 50;
+  const DAY_COST = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    return days * DAY_COST - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return days * 40 - 20;
+  if (days >= SHORT_TERM) {
+    return days * DAY_COST - SHORT_TERM_DISCOUNT;
   }
 
-  return days * 40;
+  return days * DAY_COST;
 }
 
 module.exports = calculateRentalCost;
