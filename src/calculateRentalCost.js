b96970a92a418908d6124d1fa26352cddea60ba9
oleksandr-saulko <aleksandrsaulko@gmail.com>
2024-05-14 21:18:30 +0300

    solution for actual task version

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,22 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const ONE_DAY_RENT_COST = 40;
-  const STANDART_DISCOUNT = 20;
-  const ADVANSED_DISCOUNT = 50;
-  const SHORT_RENT_PERIOD = 3;
-  const LONG_RENT_PERIOD = 7;
-  const TOTAL_COST_RENT = ONE_DAY_RENT_COST * days;
+  const TERM = 3;
+  const LONG_TERM = 7;
+  const RENT_COST = 40;
+  const TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+  const SUM_RENT = days * RENT_COST;
 
-  if (days >= SHORT_RENT_PERIOD && days < LONG_RENT_PERIOD) {
-    return TOTAL_COST_RENT - STANDART_DISCOUNT;
+  if (days < TERM) {
+    return SUM_RENT;
   }
 
-  if (days >= LONG_RENT_PERIOD) {
-    return TOTAL_COST_RENT - ADVANSED_DISCOUNT;
+  if (days < LONG_TERM) {
+    return SUM_RENT - TERM_DISCOUNT;
   }
 
-  return TOTAL_COST_RENT;
+  return SUM_RENT - LONG_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
