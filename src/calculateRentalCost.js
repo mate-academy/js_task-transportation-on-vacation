8317cf69d262ab3906c3f1e40cbcfa3cc8c63ec5
Humen Vasyl <vasyahumenit@gmail.com>
2024-05-14 13:26:16 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,23 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
   const CONST_ONE_DAY = 40;
   const RENT_FOR_7_DAYS = 7;
   const RENT_FOR_3_DAYS = 3;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
+  const basePrice = CONST_ONE_DAY * days;
 
   if (days >= RENT_FOR_7_DAYS) {
-    return CONST_ONE_DAY * days - LONG_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= RENT_FOR_3_DAYS && days <= 6) {
-    return CONST_ONE_DAY * days - SHORT_TERM_DISCOUNT;
+  if (days >= RENT_FOR_3_DAYS && days < RENT_FOR_7_DAYS) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
-  if (days < 3) {
-    return CONST_ONE_DAY * days;
+  if (days < RENT_FOR_3_DAYS) {
+    return basePrice;
   }
 }
 
