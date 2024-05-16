df6b704d403265f1ca3a72abcab3487edc6b3db7
Kasprukov <kasprukov99@gmail.com>
2024-05-16 18:13:58 +0300

    code fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,13 +7,18 @@ function calculateRentalCost(days) {
   const carCostPerDay = 40;
   const mediumRentTermDiscount = 20;
   const longRentTermDiscount = 50;
+  const price = carCostPerDay * days;
 
   if (days < 3) {
-    return carCostPerDay * days;
-  } else if (days > 2 && days < 7) {
-    return carCostPerDay * days - mediumRentTermDiscount;
-  } else if (days > 6) {
-    return carCostPerDay * days - longRentTermDiscount;
+    return price;
+  }
+
+  if (days >= 3 && days <= 6) {
+    return price - mediumRentTermDiscount;
+  }
+
+  if (days >= 7) {
+    return price - longRentTermDiscount;
   }
 }
 
