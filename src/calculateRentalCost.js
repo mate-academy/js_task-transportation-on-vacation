c2a4f401e6f51595606849bfab5230c3b877df60
Kasprukov <kasprukov99@gmail.com>
2024-05-16 14:05:30 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const carCostPerDay = 40;
+  const mediumRentTermDiscount = 20;
+  const longRentTermDiscount = 50;
+
+  if (days < 3) {
+    return carCostPerDay * days;
+  } else if (days > 2 && days < 7) {
+    return carCostPerDay * days - mediumRentTermDiscount;
+  } else if (days > 6) {
+    return carCostPerDay * days - longRentTermDiscount;
+  }
 }
 
 module.exports = calculateRentalCost;
