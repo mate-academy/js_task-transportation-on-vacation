e3dafde3a59ec3d6ec00c939a3b6595d0f02393f
Ruslan <gnatisin.ruslan1988@gmail.com>
2024-05-18 13:19:25 +0300

    remove comments, added new variable, make the code more beautiful

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,24 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
   const PAY_PER_DAY = 40;
+  const SUM_PER_DAY = days * PAY_PER_DAY;
   const BIG_DISCOUNT = 50;
   const SMALL_DISCOUNT = 20;
   const SHORT_TERM = 3;
   const LONG_TERM = 7;
 
-  if (days < SHORT_TERM) {
-    return days * PAY_PER_DAY;
-  }
-
   if (days >= LONG_TERM) {
-    return days * PAY_PER_DAY - BIG_DISCOUNT;
+    return SUM_PER_DAY - BIG_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return days * PAY_PER_DAY - SMALL_DISCOUNT;
+    return SUM_PER_DAY - SMALL_DISCOUNT;
   }
+
+  return SUM_PER_DAY;
 }
 
 module.exports = calculateRentalCost;
