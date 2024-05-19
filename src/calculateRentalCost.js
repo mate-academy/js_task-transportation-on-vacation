37a5980c625977ffa499576e6e1992c53ef966ed
Illia Kocherovets <illiakocherovets@gmail.com>
2024-05-19 11:46:33 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,19 +6,21 @@
 function calculateRentalCost(days) {
   const RENT_COST_PER_DAY = 40;
   const LONG_TERM_RENT_MIN_DAYS = 7;
-  const LONG_TERM_RENT_DICSOUNT = 50;
+  const LONG_TERM_RENT_DISCOUNT = 50;
   const SHORT_TERM_RENT_MIN_DAYS = 3;
-  const SHORT_TERM_RENT_DICSOUNT = 20;
+  const SHORT_TERM_RENT_DISCOUNT = 20;
+
+  const basePrice = days * RENT_COST_PER_DAY;
 
   if (days >= LONG_TERM_RENT_MIN_DAYS) {
-    return days * RENT_COST_PER_DAY - LONG_TERM_RENT_DICSOUNT;
+    return basePrice - LONG_TERM_RENT_DISCOUNT;
   }
 
   if (days >= SHORT_TERM_RENT_MIN_DAYS) {
-    return days * RENT_COST_PER_DAY - SHORT_TERM_RENT_DICSOUNT;
+    return basePrice - SHORT_TERM_RENT_DISCOUNT;
   }
 
-  return days * RENT_COST_PER_DAY;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
