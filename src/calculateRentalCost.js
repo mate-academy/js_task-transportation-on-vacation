3e0c474169afb6a9d436e7832fa0c2b0685507f4
Nazarii Siryi <aakkrryyttoo99@gmail.com>
2024-05-20 14:38:08 +0300

    fix code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,12 +9,12 @@ function calculateRentalCost(days) {
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
-  let rentCar = days * PRICE_PER_DAY;
+  const rentCar = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    rentCar -= LONG_TERM_DISCOUNT;
+    return rentCar - LONG_TERM_DISCOUNT;
   } else if (days >= SHORT_TERM) {
-    rentCar -= SHORT_TERM_DISCOUNT;
+    return rentCar - SHORT_TERM_DISCOUNT;
   }
 
   return rentCar;
