d5472a662e8375057762e140eba4025b3fed48a6
Yaroslav Leshchenko <yaroslavleshenkoforwork@gmail.com>
2024-05-20 14:16:00 +0300

    more readable code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISC = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISC = 20;
+
+  if (days < SHORT_TERM) {
+    return PRICE_PER_DAY * days;
+  }
+
+  if (days >= LONG_TERM) {
+    return PRICE_PER_DAY * days - LONG_TERM_DISC;
+  }
+
+  if (days >= SHORT_TERM) {
+    return PRICE_PER_DAY * days - SHORT_TERM_DISC;
+  }
 }
 
 module.exports = calculateRentalCost;
