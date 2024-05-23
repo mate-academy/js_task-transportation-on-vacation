473906690535bcea879f22994662e613bdf02721
Khmeletskyy <khmeletsky@gmail.com>
2024-05-23 12:23:31 +0300

    Replace magic numbers with constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,12 +7,14 @@ function calculateRentalCost(days) {
   const COST_PER_DAY = 40;
   const DISCOUNT_FOR_WEEK = 50;
   const DISCOUNT_FOR_THREE_DAYS = 20;
+  const DAYS_FOR_WEEK_DISCOUNT = 7;
+  const DAYS_FOR_THREE_DAYS_DISCOUNT = 3;
 
   let totalCost = days * COST_PER_DAY;
 
-  if (days >= 7) {
+  if (days >= DAYS_FOR_WEEK_DISCOUNT) {
     totalCost -= DISCOUNT_FOR_WEEK;
-  } else if (days >= 3) {
+  } else if (days >= DAYS_FOR_THREE_DAYS_DISCOUNT) {
     totalCost -= DISCOUNT_FOR_THREE_DAYS;
   }
 
