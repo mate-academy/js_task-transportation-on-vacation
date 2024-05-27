ddad7758fb1fca46f69ceab777a3a8eb7f6d2dd3
Oleksii Symonets <alex.simonets@gmail.com>
2024-05-27 20:46:32 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const basePrice = days * PRICE;
+
+  switch (true) {
+    case days >= LONG_TERM:
+      return basePrice - LONG_TERM_DISCOUNT;
+    case days >= SHORT_TERM:
+      return basePrice - SHORT_TERM_DISCOUNT;
+    default:
+      return basePrice;
+  }
 }
 
 module.exports = calculateRentalCost;
