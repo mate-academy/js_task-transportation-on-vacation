afff53a41b6b2ea04d6e3c792f73811e375a626a
malys@LAPTOP-DSQPC4U1 <alenamalyseva422@gmail.com>
2024-05-27 10:43:15 +0300

    task 'Transportation on Vacation' done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const SHORT_PERIOD = 3;
+  const SHORT_PERIOD_DISCOUNT = 20;
+  const LONG_PERIOD = 7;
+  const LONG_PERIOD_DISCOUNT = 50;
+
+  const totalSum = days * PRICE_PER_DAY;
+
+  if (days >= LONG_PERIOD) {
+    return totalSum - LONG_PERIOD_DISCOUNT;
+  }
+
+  if (days >= SHORT_PERIOD) {
+    return totalSum - SHORT_PERIOD_DISCOUNT;
+  }
+
+  return totalSum;
 }
 
 module.exports = calculateRentalCost;
