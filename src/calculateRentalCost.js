cdeed3f0c5c22a5728c86e9b815caa509dd5fd03
Volodymyr Isaiev <isaiev.v.o@gmail.com>
2024-05-27 21:10:27 +0300

    Add vocation cost calculator

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const cost = 40;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+  let vocationCost = 0;
+
+  if (days >= 7) {
+    vocationCost += days * cost - bigDiscount;
+  } else if (days >= 3) {
+    vocationCost += days * cost - smallDiscount;
+  } else {
+    vocationCost += days * cost;
+  }
+
+  return vocationCost;
 }
 
 module.exports = calculateRentalCost;
