12f7892019d6c99eb6d0bba3cfe15c62c17cdb47
Oleksii Symonets <alex.simonets@gmail.com>
2024-05-27 20:59:33 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,8 +7,8 @@ function calculateRentalCost(days) {
   const PRICE = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 7;
-  const SHORT_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
   const basePrice = days * PRICE;
 
   if (days >= LONG_TERM) {
