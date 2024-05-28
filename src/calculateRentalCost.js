2c2f85cf8581b94918b7d18c4cbb857346cdd035
Kryvytskyi Volodymyr <v.kryvytskyy@mate.academy>
2024-05-28 21:52:18 +0300

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let finalPrice = 0;
+  const DAILY_RATE = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_RENTAL = 7;
+  const SHORT_TERM_RENTAL = 3;
 
-  if (days >= 7) {
-    finalPrice = 40 * days - 50;
-  } else if (days >= 3) {
-    finalPrice = 40 * days - 20;
-  } else {
-    finalPrice = 40 * days;
+  if (days >= LONG_TERM_RENTAL) {
+    return (DAILY_RATE * days) - LONG_TERM_DISCOUNT;
   }
 
-  return finalPrice;
+  if (days >= SHORT_TERM_RENTAL) {
+    return (DAILY_RATE * days) - SHORT_TERM_DISCOUNT;
+  }
+
+  return DAILY_RATE * days;
 }
 
 module.exports = calculateRentalCost;
