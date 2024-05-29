48516d4d3e1497dc46117455d5f48db87bfc0854
Roksolana Ponyk <ponykroksolana@gmail.com>
2024-05-29 12:06:58 +0400

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_PER_DAY = 40;
+  const LONG_RENT = 7;
+  const LONG_RENT_DISCOUNT = 50;
+  const SHORT_RENT = 3;
+  const SHORT_RENT_DISCOUNT = 20;
+
+  const PRE_DISCOUNT_PRICE = RENT_PER_DAY * days;
+
+  if (days >= SHORT_RENT) {
+    return PRE_DISCOUNT_PRICE - SHORT_RENT_DISCOUNT;
+  }
+
+  if (days >= LONG_RENT) {
+    return PRE_DISCOUNT_PRICE - LONG_RENT_DISCOUNT;
+  }
+
+  return PRE_DISCOUNT_PRICE;
 }
 
 module.exports = calculateRentalCost;
