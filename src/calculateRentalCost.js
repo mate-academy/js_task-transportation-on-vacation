3312795b805adcaa31fe7df65908f8f0a2474a12
Deniel Frolov <fvgdanil@gmail.com>
2024-05-29 10:29:30 +0300

    add calculateRentalCost function in solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,30 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_CAR_C0ST = 40;
+
+  const LONG_TERM = 7;
+  const LONG_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_DISCOUNT = 20;
+
+  let allResult = 0;
+
+  if (days >= LONG_TERM) {
+    allResult += RENT_CAR_C0ST * days - LONG_DISCOUNT;
+
+    return allResult;
+  }
+
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    allResult += RENT_CAR_C0ST * days - SHORT_DISCOUNT;
+
+    return allResult;
+  }
+
+  return (allResult += RENT_CAR_C0ST * days);
 }
 
 module.exports = calculateRentalCost;
