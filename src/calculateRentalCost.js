9e756f55630f6d29178a3b0a751b29d64c1d4d03
Deniel Frolov <fvgdanil@gmail.com>
2024-05-29 11:58:54 +0300

    add fix 1.1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,28 +5,29 @@
  */
 
 function calculateRentalCost(days) {
-  const RENT_CAR_C0ST = 40;
+  const RENT_CAR_COST = 40;
 
   const LONG_TERM = 7;
   const LONG_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_DISCOUNT = 20;
 
-  let allResult = 0;
+  let totalCost = 0;
 
   if (days >= LONG_TERM) {
-    allResult += RENT_CAR_C0ST * days - LONG_DISCOUNT;
+    totalCost += RENT_CAR_COST * days - LONG_DISCOUNT;
 
-    return allResult;
+    return totalCost;
   }
 
-  if (days >= SHORT_TERM && days < LONG_TERM) {
-    allResult += RENT_CAR_C0ST * days - SHORT_DISCOUNT;
+  if (days >= SHORT_TERM) {
+    totalCost += RENT_CAR_COST * days - SHORT_DISCOUNT;
 
-    return allResult;
+    return totalCost;
   }
+  totalCost += RENT_CAR_COST * days;
 
-  return (allResult += RENT_CAR_C0ST * days);
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
