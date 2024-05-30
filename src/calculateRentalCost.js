f6cb2bac7cb630db86639842661ec1c2f43abd6a
Rusłan Karaniewski <karane88vka@gmail.com>
2024-05-30 18:34:56 +0200

    Fixing after codereview

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,10 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  const rentCost = 40;
-  const rentOff = days < 3 ? 0 : days >= 3 && days >= 7 ? 50 : 20;
+  const RENT_COST = 40;
+  const FINAL_COST = RENT_COST * days;
+  const MEDIUM_TERM = 3;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
 
-  return rentCost * days - rentOff;
+  if (days >= LONG_TERM) {
+    return FINAL_COST - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MEDIUM_TERM) {
+    return FINAL_COST - MEDIUM_TERM_DISCOUNT;
+  }
+
+  return FINAL_COST;
 }
 
 module.exports = calculateRentalCost;
