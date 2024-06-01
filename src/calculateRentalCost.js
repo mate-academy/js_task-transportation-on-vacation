e5d294522af0938b2ee4362660d21febee0cbe24
Lena <elenadobryanskay@gmail.com>
2024-06-01 21:23:23 +0300

    calculate 2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const PRICE_RER_DAY = 40;
+  const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  const basePrice = days * PRICE_RER_DAY;
+  const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
