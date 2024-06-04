3d2e4c94917e76f9f257b1732f7d1a0d75ccd28d
Kovtun-Dmytro <166028084+Kovtun-Dmytro@users.noreply.github.com>
2024-06-04 17:04:22 +0300

    Update calculateRentalCost.js
    changed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,15 +3,23 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  let price = days * 40;
+function calculateRentalCost(numberOfDays) {
+  const dailyRentalRate = 40;
+  const longTermMinDays = 7;
+  const longTermDiscount = 50;
+  const shortTermMinDays = 3;
+  const shortTermDiscount = 20;
 
-  if (days >= 7) {
-    price -= 50;
-  } else if (days >= 3) {
-    price -= 20;
+  let price = numberOfDays * dailyRentalRate;
+
+  if(numberOfDays >= longTermMinDays) {
+    return price - longTermDiscount;
   }
 
+  if(numberOfDays >=  shortTermMinDays) {
+    return price - shortTermDiscount;
+  }
+  
   return price;
 }
 
