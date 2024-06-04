5d75ac865ae5d6ee7616eff4f7cd6de281fa91b8
Arthur Tolkachov <babanjerelo@gmail.com>
2024-06-04 20:35:23 +0300

    [Feature] added rental cost calculation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,29 @@
  *
  * @return {number}
  */
+
+const DAY_COST_VALUE = 40;
+const THREE_DAYS_OFF_VALUE = 20;
+const SEVEN_DAYS_OFF_VALUE = 50;
+
 function calculateRentalCost(days) {
-  // write code here
+  const isInvalidData = typeof days !== 'number';
+
+  if (isInvalidData) {
+    return 0;
+  }
+
+  const totalCost = DAY_COST_VALUE * days;
+
+  if (days >= 7) {
+    return totalCost - SEVEN_DAYS_OFF_VALUE;
+  }
+
+  if (days >= 3) {
+    return totalCost - THREE_DAYS_OFF_VALUE;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
