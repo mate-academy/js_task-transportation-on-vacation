b9dcc01b73952b1b43e2a8896051aa13795c848b
Nikolay <diedaemon@gmail.com>
2024-06-04 14:22:16 +0200

    task complete

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_COST = 40;
+  let discount = 0;
+
+  switch (true) {
+    case days >= 3 && days < 7:
+      discount = 20;
+      break;
+    case days >= 7:
+      discount = 50;
+      break;
+  }
+
+  const TOTAL_COST = days * RENT_COST - discount;
+
+  return TOTAL_COST;
 }
 
 module.exports = calculateRentalCost;
