27f5fe218f0ba0f7c5984025e1a242820e0b40e1
Oleg Usenko <Oleg911Usenko@gmail.com>
2024-06-05 12:22:12 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCost = 40 * days;
+
+  switch (true) {
+    case days >= 3 && days < 7: {
+      return rentalCost - 20;
+    }
+
+    case days >= 7: {
+      return rentalCost - 50;
+    }
+  }
+
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
