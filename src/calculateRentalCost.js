b1dc4cba5f235d2e0e895eee6b7eab263a54b844
Igor <igor.olshanskyi@gmail.com>
2024-06-08 21:59:09 +0300

    solve_magic_numbers

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,14 +7,16 @@ function calculateRentalCost(days) {
   const COST_PER_DAY = 40;
   const DISCOUNT_SHORT_RENT = 20;
   const DISCOUNT_LONG_RENT = 50;
+  const LONG_RENT = 7;
+  const SHORT_RENT = 3;
 
   const price = days * COST_PER_DAY;
 
-  if (days >= 7) {
+  if (days >= LONG_RENT) {
     return price - DISCOUNT_LONG_RENT;
   }
 
-  if (days >= 3) {
+  if (days >= SHORT_RENT) {
     return price - DISCOUNT_SHORT_RENT;
   }
 
