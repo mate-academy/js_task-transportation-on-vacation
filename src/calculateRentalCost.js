568dde487d748732d36cbeb3fe09a5d04451c5df
Kate Hubko <gubkokateryna9592@egmail.com>
2024-06-10 20:20:16 +0300

    Kate Hubko

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,24 +3,25 @@
  *
  * @return {number}
  */
-function calculateRentalCost(numberOfDays) {
+function calculateRentalCost(Dayss) {
   const DAILY_RATE = 40;
   const MEDIUM_TERM = 3;
   const MEDIUM_TERM_DISCOUNT = 20;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
 
-  const baseCost = numberOfDays * DAILY_RATE;
+  const baseCost = Dayss * DAILY_RATE;
 
-  if (numberOfDays >= LONG_TERM) {
+  if (Dayss >= LONG_TERM) {
     return baseCost - LONG_TERM_DISCOUNT;
   }
 
-  if (numberOfDays >= MEDIUM_TERM) {
+  if (Dayss >= MEDIUM_TERM) {
     return baseCost - MEDIUM_TERM_DISCOUNT;
   }
 
   return baseCost;
 }
+
 
 module.exports = calculateRentalCost;
