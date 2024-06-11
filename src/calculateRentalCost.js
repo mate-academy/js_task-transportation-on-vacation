12a94b2a8afc45bbaa9f09e11e06d6ab683cc086
Oleh Voloshyn <ithehaski@gmail.com>
2024-06-11 23:23:38 +0300

    add solution task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TIME_DISC = 50;
+  const MIDL_TIME_DISC = 20;
+  const PRICE = 40;
+  const LONG_TIME = 7;
+  const MIDL_TIME = 3;
+
+  if (days >= LONG_TIME) {
+    return PRICE * days - LONG_TIME_DISC;
+  } else if (days >= MIDL_TIME) {
+    return PRICE * days - MIDL_TIME_DISC;
+  } else {
+    return PRICE * days;
+  }
 }
 
 module.exports = calculateRentalCost;
