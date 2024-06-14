9faf3491046817c2ffb268cf363e37d986d3bce1
Kirill Gasan <dp160190gkj@gmail.com>
2024-06-14 20:19:23 +0300

    update task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,25 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let result = 0;
   const DAY_COST = 40;
-  let discount = 0;
+  let discount;
 
-  for (let n = 1; n <= days; n++) {
-    if (n >= 7) {
-      discount = 50;
-    }
-
-    if (n < 7) {
-      discount = 20;
-    }
-
-    if (n < 3) {
-      discount = 0;
-    }
-
-    result = n * DAY_COST - discount;
+  if (days >= 7) {
+    discount = 50;
   }
+
+  if (days < 7) {
+    discount = 20;
+  }
+
+  if (days < 3) {
+    discount = 0;
+  }
+
+  const result = days * DAY_COST - discount;
 
   return result;
 }
