de1b93c85d575c4c904793a52944ecdb03c24ff9
Kirill Gasan <dp160190gkj@gmail.com>
2024-06-14 20:05:00 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  let result = 0;
+  const DAY_COST = 40;
+  let discount = 0;
 
+  for (let n = 1; n <= days; n++) {
+    if (n >= 7) {
+      discount = 50;
+    }
+
+    if (n < 7) {
+      discount = 20;
+    }
+
+    if (n < 3) {
+      discount = 0;
+    }
+
+    result = n * DAY_COST - discount;
+  }
+
+  return result;
+}
 module.exports = calculateRentalCost;
