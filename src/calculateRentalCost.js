54ee0f85867bdd36d9b1be604cb84d10e0759a53
Oleh Kulynsky <kossup@gmail.com>
2024-06-14 16:33:40 +0300

    init task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let discount = 0;
+  const PRICE = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= 7) {
+    discount = LONG_TERM_DISCOUNT;
+  } else if (days >= 3) {
+    discount = SHORT_TERM_DISCOUNT;
+  }
+
+  return days * PRICE - discount;
 }
 
 module.exports = calculateRentalCost;
