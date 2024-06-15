83be5f177d1a0c38fd7dd0194197171dbe77e167
Ruslana Berkunova <rberknova@gmail.com>
2024-06-15 16:27:17 +0300

    fix solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const dailyRate = 40;
-  let totalCost = days * dailyRate;
+  const DAILY_RATE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
-    totalCost -= 50;
+  const totalCost = days * DAILY_RATE;
+
+  if (days >= LONG_TERM) {
+    return totalCost - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3 && days < 7) {
-    totalCost -= 20;
+  if (days >= SHORT_TERM) {
+    return totalCost - SHORT_TERM_DISCOUNT;
   }
 
   return totalCost;
