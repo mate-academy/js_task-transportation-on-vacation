05847390d1d2c35fbdd171697de94d257c1ffbb0
Dmytro <dima.gusak.01@gmail.com>
2024-06-17 21:04:34 +0200

    Solution corrected

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,22 +8,21 @@ function calculateRentalCost(days) {
   const PRICE_PER_DAY = 40;
   const VERY_SHORT_TERM = 2;
   const LONG_TERM = 7;
-  const LONG_TERM_DISCAUNT = 50;
-  const OVER_SHORT_TERM = 6;
+  const LONG_TERM_DISCOUNT = 50;
   const PRE_SHORT_TERM = 3;
   const SHORT_TERM_DISCAUNT = 20;
   const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
-    return basePrice - LONG_TERM_DISCAUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (days <= VERY_SHORT_TERM) {
     return PRICE_PER_DAY * VERY_SHORT_TERM;
   }
 
-  if (days >= PRE_SHORT_TERM || days <= OVER_SHORT_TERM) {
-    return basePrice - SHORT_TERM_DISCAUNT;
+  if(days >= PRE_SHORT_TERM && days <= LONG_TERM){
+    return basePrice - SHORT_TERM_DISCAUNT
   }
 
   return basePrice;
