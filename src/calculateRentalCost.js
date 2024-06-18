c00667ede053db81972cd3c9a81142a94edf6971
Bohdan98Kuch <bohdan13kuch@gmail.com>
2024-06-18 13:23:27 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const DAILY_FEE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  let sum = days * DAILY_FEE;
+
+  if (days >= LONG_TERM) {
+    return (sum -= LONG_TERM_DISCOUNT);
+  }
+
+  if (days >= SHORT_TERM) {
+    return (sum -= SHORT_TERM_DISCOUNT);
+  }
+
+  return sum;
 }
 
 module.exports = calculateRentalCost;
