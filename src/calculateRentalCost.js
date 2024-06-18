ce3696996df8620e9baf64ad6392f0bccd5cf1f3
Anna Hutnichenko <gutnichenkoo@gmail.com>
2024-06-18 13:52:25 +0300

    add function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCost = days * 40;
+
+  if (days >= 7) {
+    return rentalCost - 50;
+  }
+
+  if (days >= 3) {
+    return rentalCost - 20;
+  }
+
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
