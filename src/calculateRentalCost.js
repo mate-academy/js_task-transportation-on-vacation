cc41c340ee50f79fa0ebc06816e9777489c5986a
DESKTOP-U4CBLI4\ketin <ketinngz@gmail.com>
2024-06-19 00:37:40 +0300

    task completed using switch cases

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,14 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  switch (true) {
+    case days < 3:
+      return days * 40;
+    case days >= 3 && days < 7:
+      return days * 40 - 20;
+    case days >= 7:
+      return days * 40 - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
