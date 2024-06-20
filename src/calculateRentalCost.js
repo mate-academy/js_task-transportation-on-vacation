f511f1f1443e3572f12e3f548a8cc3ca58b8d955
ansnihur <snihur.anastasiia@gmail.com>
2024-06-20 22:33:49 +0300

    checklist checked

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,13 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  if (days >= 1 && days < 3) {
-    return days * 40;
-  } else if (days >= 3 && days <= 6) {
-    return days * 40 - 20;
-  } else if (days >= 7) {
-    return days * 40 - 50;
+  const basePrice = days * 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  } else if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
