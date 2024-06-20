575d142831406e2c337affd9013f09092bf15128
ansnihur <snihur.anastasiia@gmail.com>
2024-06-20 22:43:38 +0300

    improvements added

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const basePrice = days * 40;
+  const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >= SHORT_TERM && days < LONG_TERM) {
-    return basePrice - SHORT_TERM_DISCOUNT;
-  } else if (days >= LONG_TERM) {
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
   return basePrice;
