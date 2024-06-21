cfe4e13ad4a017091e9c4997c9637a6d7b662726
tkach0808 <tkach0808@gmail.com>
2024-06-21 21:55:16 +0300

    fix my fynction #2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,22 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const rentCost = 40;
-  const shortRent = 3;
-  const shortRentDiscount = 20;
-  const longRent = 7;
-  const longRentDiscount = 50;
-  const basePrice = rentCost * days;
+  const RENT_COST = 40;
+  const SHORT_RENT= 3;
+  const SHORT_RENT_DISCOUNT = 20;
+  const LONG_RENT = 7;
+  const LONG_RENT_DISCOUNT = 50;
+  const BASE_PRICE = RENT_COST * days;
 
-  if (days >= shortRent && days < longRent) {
-    return basePrice - shortRentDiscount;
+  if (days >= LONG_RENT) {
+    return BASE_PRICE - LONG_RENT_DISCOUNT;
   }
 
-  if (days >= longRent) {
-    return basePrice - longRentDiscount;
+  if (days >= SHORT_RENT) {
+    return BASE_PRICE - SHORT_RENT_DISCOUNT;
   }
 
-  return basePrice;
+  return BASE_PRICE;
 }
 
 module.exports = calculateRentalCost;
