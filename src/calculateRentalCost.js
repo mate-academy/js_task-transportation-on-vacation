962fa0ccec16321e93253961fc72e44e3c0e815f
SerhiiSemennikov <semennik@hotmail.com>
2024-06-21 15:43:42 +0200

    task-transportation-on-vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const basePrice = 40;
+  const numberOfDays = days;
+  const DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+
+  if (numberOfDays < 3) {
+    return numberOfDays * basePrice;
+  }
+
+  if (numberOfDays >= 7) {
+    return numberOfDays * basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  return numberOfDays * basePrice - DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
