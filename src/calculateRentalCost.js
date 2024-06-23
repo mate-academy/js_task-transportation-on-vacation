d7e7ab59dd2cea605194dd62c7d6f8f538a8be39
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2024-06-23 18:15:06 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const baseRateOneDay = 40;
+  const THREE_DAYS = 3;
+  const SEVEN_DAYS = 7;
+  const DISCOUNT_20 = 20;
+  const DISCOUNT_50 = 50;
+  let totalCost = days * baseRateOneDay;
+
+  if (days >= THREE_DAYS && days < SEVEN_DAYS) {
+    totalCost -= DISCOUNT_20;
+  } else if (days >= SEVEN_DAYS) {
+    totalCost -= DISCOUNT_50;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
