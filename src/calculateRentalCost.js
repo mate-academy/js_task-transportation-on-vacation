87d2d020653f17e76c7f4f452f1fdb3f223a8149
Nataliia Smirnova <ensueno11@gmail.com>
2024-06-23 21:49:29 +0300

    Fead: solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dayCost = 40;
+  let totalCost = days * dayCost;
+
+  if (days >= 3 && days < 7) {
+    totalCost = totalCost - 20;
+  }
+
+  if (days >= 7) {
+    totalCost = totalCost - 50;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
