27f54527485272141cbd57746076abeb6817a207
NataliiaBoiko22 <nata.boyko.kras@gmail.com>
2024-06-24 18:26:10 +0200

    fix comments

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,19 +3,21 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
+function calculateRentalCost(numberOfDays) {
   // write code here
   const baseRateOneDay = 40;
   const THREE_DAYS = 3;
   const SEVEN_DAYS = 7;
   const DISCOUNT_20 = 20;
   const DISCOUNT_50 = 50;
-  let totalCost = days * baseRateOneDay;
+  const totalCost = numberOfDays * baseRateOneDay;
 
-  if (days >= THREE_DAYS && days < SEVEN_DAYS) {
-    totalCost -= DISCOUNT_20;
-  } else if (days >= SEVEN_DAYS) {
-    totalCost -= DISCOUNT_50;
+  if (numberOfDays >= THREE_DAYS && numberOfDays < SEVEN_DAYS) {
+    return totalCost - DISCOUNT_20;
+  }
+
+  if (numberOfDays >= SEVEN_DAYS) {
+    return totalCost - DISCOUNT_50;
   }
 
   return totalCost;
