8945f9182c9969cb498d2181da5b8a02b86d39d2
Ivan Paiareli <ipayareli@gmail.com>
2024-06-25 11:53:46 +0300

    add extra solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,12 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let totalValue = days * 40;
+  const FIRST_LVL_DISCOUNT = 20;
+  const SECOND_LVL_DISCOUNT = 50;
+  const DAILY_PRICE = 40;
+  let totalValue = DAILY_PRICE * days;
 
   if (days >= 3 && days < 7) {
-    totalValue = days * 40 - 20;
+    totalValue = days * DAILY_PRICE - FIRST_LVL_DISCOUNT;
   } else if (days >= 7) {
-    totalValue = days * 40 - 50;
+    totalValue = days * DAILY_PRICE - SECOND_LVL_DISCOUNT;
   }
 
   return totalValue;
