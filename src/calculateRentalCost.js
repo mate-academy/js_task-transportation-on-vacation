8b03aa7e41c1fa9d06cd7abf9a6a6d4648527a9e
Mykhailo Firka <ltcar@online.ua>
2024-06-29 22:52:43 +0300

    task completed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_RATE = 40;
+  const DISCOUNT_MORE_THAN_7_DAYS = 50;
+  const DISCOUNT_MORE_THAN_3_DAYS = 20;
+  const RENTAL_DAYS_MORE_7 = 7;
+  const RENTAL_DAYS_MORE_3 = 3;
+
+  const rentalCost = DAILY_RATE * days;
+
+  if (days >= RENTAL_DAYS_MORE_7) {
+    return rentalCost - DISCOUNT_MORE_THAN_7_DAYS;
+  }
+
+  if (days >= RENTAL_DAYS_MORE_3) {
+    return rentalCost - DISCOUNT_MORE_THAN_3_DAYS;
+  }
+
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
