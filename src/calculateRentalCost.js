099562426f1081c3f7d4f17c64c316117b6d4b83
DDefty <dawid.kogut99@gmail.com>
2024-06-30 16:59:50 +0200

    Function added

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,13 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_COST = 40;
+  if(days<3){
+    return days*RENT_COST;
+  } else if(days < 7){
+    return days*RENT_COST-20;
+  }
+  return days*RENT_COST-40;
 }
 
 module.exports = calculateRentalCost;
