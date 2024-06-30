b0bc1aa18df06d75917a828232b43edd637eeb4a
DDefty <dawid.kogut99@gmail.com>
2024-06-30 17:07:45 +0200

    Fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,12 +7,12 @@ function calculateRentalCost(days) {
   const RENT_COST = 40;
   const RENT_COST_DISCOUNT_SHORT = 20;
   const RENT_COST_DISCOUNT_LONG = 50;
-  const DISCOUNT_SHORT_LENGHT = 3;
-  const DISCOUNT_LONG_LENGHT = 4;
-  if(days<DISCOUNT_SHORT_LENGHT){
+  const RENTAL_DAYS_MORE_7 = 7;
+  const RENTAL_DAYS_MORE_3 = 3;
+  if(days<RENTAL_DAYS_MORE_3){
     return days*RENT_COST;
   }
-  if(days < DISCOUNT_LONG_LENGHT){
+  if(days < RENTAL_DAYS_MORE_7){
     return days*RENT_COST-RENT_COST_DISCOUNT_SHORT;
   }
   return days*RENT_COST-RENT_COST_DISCOUNT_LONG;
