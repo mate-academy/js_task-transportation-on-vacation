0e98fa0068c7d1838742520a72cbc09491443873
DDefty <dawid.kogut99@gmail.com>
2024-06-30 17:05:45 +0200

    Function added

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,13 +5,17 @@
  */
 function calculateRentalCost(days) {
   const RENT_COST = 40;
-  if(days<3){
+  const RENT_COST_DISCOUNT_SHORT = 20;
+  const RENT_COST_DISCOUNT_LONG = 50;
+  const DISCOUNT_SHORT_LENGHT = 3;
+  const DISCOUNT_LONG_LENGHT = 4;
+  if(days<DISCOUNT_SHORT_LENGHT){
     return days*RENT_COST;
   }
-  if(days < 7){
-    return days*RENT_COST-20;
+  if(days < DISCOUNT_LONG_LENGHT){
+    return days*RENT_COST-RENT_COST_DISCOUNT_SHORT;
   }
-  return days*RENT_COST-40;
+  return days*RENT_COST-RENT_COST_DISCOUNT_LONG;
 }
 
 module.exports = calculateRentalCost;
