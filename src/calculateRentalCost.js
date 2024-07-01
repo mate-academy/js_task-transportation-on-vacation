26005d7f18ede32a4687f631722010945dc46433
Olena Horovenko <elena.gorovenko@gmail.com>
2024-07-01 23:40:11 +0200

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,20 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
+'use strict';
+
+//
+//  * @param {number} days
+//
+//  * @return {number}
+//
 function calculateRentalCost(days) {
-  // write code here
+  const regularPrice = 40;
+
+  if (days >= 7) {
+    return days * regularPrice - 50;
+  } else if (days >= 3) {
+    return days * regularPrice - 20;
+  } else {
+    return days * regularPrice;
+  }
 }
 
 module.exports = calculateRentalCost;
