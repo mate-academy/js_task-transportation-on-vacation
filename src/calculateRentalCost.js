7b6acd81268ad372636cf482e267738ec520d342
Yurii <uaremko945@gmail.com>
2024-07-02 21:38:55 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,29 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  let COUNT_MONEY = 0;
+  const PRICE_ONE_DAY = 40;
+  const TOTAL_AMOUNT = PRICE_ONE_DAY * days;
+  const DISCOUNT_THREE_DAYS = 20;
+  const DISCOUNT_SEVEN_DAYS = 50;
+  const THREE_DAY = 3;
+  const SEVEN_DAY = 7;
+
+  if (days < THREE_DAY) {
+    COUNT_MONEY = TOTAL_AMOUNT;
+  }
+
+  if (days >= THREE_DAY) {
+    COUNT_MONEY = TOTAL_AMOUNT - DISCOUNT_THREE_DAYS;
+  }
+
+  if (days >= SEVEN_DAY) {
+    COUNT_MONEY = TOTAL_AMOUNT - DISCOUNT_SEVEN_DAYS;
+  }
+
+  return COUNT_MONEY;
 }
 
 module.exports = calculateRentalCost;
