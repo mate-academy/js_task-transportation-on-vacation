6e3a4b395700d81e1d303fed05e023686fa75f4a
Teollan <serhiikofficial@gmail.com>
2024-07-02 10:40:02 +0300

    initial commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+  const periodDiscounts = { 3: 20, 7: 50 };
+
+  const discount = getDiscount(days, periodDiscounts);
+
+  return days * pricePerDay - discount;
+}
+
+/**
+ * @param {number} days
+ * @param {Object} periodDiscounts
+ *
+ * @return {number}
+ */
+function getDiscount(days, periodDiscounts) {
+  const allPeriods = Object.getOwnPropertyNames(periodDiscounts);
+  const viablePeriods = allPeriods.filter((value) => value <= days);
+  const period = Math.max(...viablePeriods);
+
+  return periodDiscounts[period] ?? 0;
 }
 
 module.exports = calculateRentalCost;
