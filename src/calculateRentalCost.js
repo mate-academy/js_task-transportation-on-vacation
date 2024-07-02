acc20be2e99d563d652207cc08b6b7ca90cab105
Olena Horovenko <elena.gorovenko@gmail.com>
2024-07-02 16:51:45 +0200

    improved out repetitive code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,20 +7,21 @@
 //
 function calculateRentalCost(days) {
   const REGULAR_PRICE = 40;
+  const REGULAR_PRICE_TOTAL = days * REGULAR_PRICE;
+  const MID_TERM = 3;
+  const LONG_TERM = 7;
   const DISCOUNT_MID_TERM = 20;
   const DISCOUNT_LONG_TERM = 50;
-  const LONG_TERM = 7;
-  const MID_TERM = 3;
 
   if (days >= LONG_TERM) {
-    return days * REGULAR_PRICE - DISCOUNT_LONG_TERM;
+    return REGULAR_PRICE_TOTAL - DISCOUNT_LONG_TERM;
   }
 
   if (days >= MID_TERM) {
-    return days * REGULAR_PRICE - DISCOUNT_MID_TERM;
+    return REGULAR_PRICE_TOTAL - DISCOUNT_MID_TERM;
   }
 
-  return days * REGULAR_PRICE;
+  return REGULAR_PRICE_TOTAL;
 }
 
 module.exports = calculateRentalCost;
