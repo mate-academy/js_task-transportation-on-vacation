dd0fd7136512c9094e12d25388521267abcfafb2
Yarik kedrych <yarik.kedrych9@gmail.com>
2024-07-02 19:03:00 +0300

    add task correction

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,20 +5,23 @@
  */
 function calculateRentalCost(days) {
   let total = 0;
+  const LONG_TERM = 7;
+  const SHIRT_TERM = 3;
+  const LONG_TERM_DISKONT = 50;
+  const SHIRT_TERM_DISKONT = 20;
+  const PRICE_RENT = 40;
 
-  if (days < 3) {
-    total += days * 40;
+  if (days < SHIRT_TERM) {
+    return (total += days * PRICE_RENT);
   }
 
-  if (days >= 3 && days < 7) {
-    total += days * 40 - 20;
+  if (days >= SHIRT_TERM && days < LONG_TERM) {
+    return (total += days * PRICE_RENT - SHIRT_TERM_DISKONT);
   }
 
-  if (days >= 7) {
-    total += days * 40 - 50;
+  if (days >= LONG_TERM) {
+    return (total += days * PRICE_RENT - LONG_TERM_DISKONT);
   }
-
-  return total;
 }
 
 module.exports = calculateRentalCost;
