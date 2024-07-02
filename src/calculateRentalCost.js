0d209a0adca10c6216d92c76f2a9bd465c9598f9
Vadym Urbanovych <vadymurbanovych@example.com>
2024-07-02 12:30:34 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,24 @@
  */
 function calculateRentalCost(days) {
   // write code here
+
+  const NORMAL_PRICE = 40;
+  const MID_TERM = 3;
+  const MID_TERM_DISC = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISC = 50;
+
+  let finalPrice = days * NORMAL_PRICE;
+
+  if (days >= LONG_TERM) {
+    return (finalPrice -= LONG_TERM_DISC);
+  }
+
+  if (days >= MID_TERM) {
+    return (finalPrice -= MID_TERM_DISC);
+  }
+
+  return finalPrice;
 }
 
 module.exports = calculateRentalCost;
