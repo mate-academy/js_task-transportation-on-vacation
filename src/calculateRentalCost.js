5ace06e8410b5b688a764a3cdaa3f121d60ed45f
Ivan Opar <vanya.opar@gmail.com>
2024-07-02 09:52:22 +0300

    calcRentCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,26 @@
+'use strict';
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let count = 0;
+
+  for (let d = 1; d <= days; d++) {
+    count += 40;
+  }
+
+  if (days >= 3) {
+    count -= 20;
+  }
+
+  if (days >= 7) {
+    count -= 30;
+  }
+
+  return count;
 }
 
 module.exports = calculateRentalCost;
