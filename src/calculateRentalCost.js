758323bebfc1fdbdda09a87fccc195aec9e47edf
Teut`s <teuts@MacBook-Air.localdomain>
2024-07-02 18:06:09 +0300

    vacationTask

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+
+  if (days < 3) {
+    return pricePerDay * days;
+  } else if (days >= 3 && days <= 6) {
+    return pricePerDay * days - 20;
+  }
+
+  return pricePerDay * days - 50;
 }
 
 module.exports = calculateRentalCost;
