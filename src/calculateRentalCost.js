33712773dc2155c49f737eddb94df03b16efbd77
Katya <katya.stelmakhova@gmail.com>
2024-07-02 21:41:35 +0200

    Solution.Fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,18 +3,21 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const carRentPerDay = 40;
-  const saleForWeekAndMore = 50;
-  const saleForShortTerm = 20;
-  const sevenDaysRent = 7;
-  const threeDaysRent = 3;
-  let totalSum = carRentPerDay * days;
 
-  if (days >= sevenDaysRent) {
-    totalSum = days * carRentPerDay - saleForWeekAndMore;
-  } else if (days >= threeDaysRent) {
-    totalSum = days * carRentPerDay - saleForShortTerm;
+const CAR_RENT_PER_DAY = 40;
+const SALE_FOR_WEEK_AND_MORE = 50;
+const SALE_FOR_SHORT_TERM = 20;
+const SEVEN_DAYS_RENT = 7;
+const THREE_DAYS_RENT = 3;
+
+function calculateRentalCost(days) {
+  const totalSum = CAR_RENT_PER_DAY * days;
+  if (days >= SEVEN_DAYS_RENT) {
+    return totalSum - SALE_FOR_WEEK_AND_MORE;
+  }
+
+  if (days >= THREE_DAYS_RENT) {
+    return totalSum - SALE_FOR_SHORT_TERM;
   }
 
   return totalSum;
