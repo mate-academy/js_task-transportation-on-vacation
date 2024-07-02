ebecf7c6aa32d7d9c1136fb5853c10ecda6f9b30
Roman Prykhodko <r.prykhodko@yahoo.com>
2024-07-02 18:34:31 +0300

    Created the task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -2,9 +2,26 @@
  * @param {number} days
  *
  * @return {number}
+ *
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_RATE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const SHORT_TERM = 3;
+  const TOTAL_COST = days * DAILY_RATE;
+
+  if (days >= LONG_TERM) {
+    return days * DAILY_RATE - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return days * DAILY_RATE - SHORT_TERM_DISCOUNT;
+  }
+
+  return TOTAL_COST;
 }
 
 module.exports = calculateRentalCost;
