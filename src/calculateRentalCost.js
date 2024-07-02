6f984f1d3a079ddd16f9e4473e62a176083f43d9
Olga Brendak <olga.brendak.dg@gmail.com>
2024-07-02 19:30:06 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAY_COST = 40;
+  const LTTL_DAYS = 3;
+  const LTTL_DAYS_DISCOUNT = 20;
+  const MANY_DAYS = 7;
+  const MANY_DAYS_DISCOUNT = 50;
+
+  const sum = DAY_COST * days;
+
+  if (days >= MANY_DAYS) {
+    return sum - MANY_DAYS_DISCOUNT;
+  }
+
+  if (days >= LTTL_DAYS) {
+    return sum - LTTL_DAYS_DISCOUNT;
+  }
+
+  return sum;
+
 }
 
 module.exports = calculateRentalCost;
