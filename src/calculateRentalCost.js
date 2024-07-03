0baea6720e5e79d1c04d09b141c9d4bf990fa1e3
Vladyslava Proskura <vladyslava.proskura@gmail.com>
2024-07-03 19:44:58 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,13 +8,13 @@
 function calculateRentalCost(numberOfDays) {
   const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
-  const LONG_TERM_DiSCOUNT = 50;
+  const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
   const basePrice = numberOfDays * PRICE_PER_DAY;
 
   if (numberOfDays >= LONG_TERM) {
-    return basePrice - LONG_TERM_DiSCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
   if (numberOfDays >= SHORT_TERM) {
