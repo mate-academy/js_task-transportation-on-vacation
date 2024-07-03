155ef7076cfe9f766c8c41dffa2a07f49fd7fe3a
Olena Nikishyna <lyashenko.ol.yur@gmail.com>
2024-07-03 12:48:45 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const payment = 40;
+  let sum = days * payment;
+
+  if (days > 6) {
+    sum -= 50;
+  } else if (days > 2) {
+    sum -= 20;
+  }
+
+  return sum;
 }
 
 module.exports = calculateRentalCost;
