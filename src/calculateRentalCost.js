983f4ea915147f469e3673f8ff863a772397582f
Yurii Kramar <y.kramar15@gmail.com>
2024-07-03 18:25:22 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_FOR_RENT = 40;
+
+  const AMOUN_DAY_FOR_MIN_DISCOUNT = 3;
+  const AMOUN_DAY_FOR_MAX_DISCOUNT = 7;
+
+  const MIN_DISCOUNT = 20;
+  const MAX_DISCOUNT = 50;
+
+  if (days >= AMOUN_DAY_FOR_MIN_DISCOUNT && days < AMOUN_DAY_FOR_MAX_DISCOUNT) {
+    return PRICE_FOR_RENT * days - MIN_DISCOUNT;
+  }
+
+  if (days >= AMOUN_DAY_FOR_MAX_DISCOUNT) {
+    return PRICE_FOR_RENT * days - MAX_DISCOUNT;
+  }
+
+  return PRICE_FOR_RENT * days;
 }
 
 module.exports = calculateRentalCost;
