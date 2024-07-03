a47b475378bc42df8b79b91de53e97a91f97de4f
Roman Prykhodko <r.prykhodko@yahoo.com>
2024-07-03 19:29:58 +0300

    Fixed the task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,23 +5,24 @@
  *
  */
 
+const DAILY_RATE = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM_DISCOUNT = 20;
+const SHORT_TERM = 3;
+
 function calculateRentalCost(days) {
-  const DAILY_RATE = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM_DISCOUNT = 20;
-  const SHORT_TERM = 3;
-  const TOTAL_COST = days * DAILY_RATE;
+  const totalCost = days * DAILY_RATE;
 
   if (days >= LONG_TERM) {
-    return days * DAILY_RATE - LONG_TERM_DISCOUNT;
+    return totalCost - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return days * DAILY_RATE - SHORT_TERM_DISCOUNT;
+    return totalCost - SHORT_TERM_DISCOUNT;
   }
 
-  return TOTAL_COST;
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
