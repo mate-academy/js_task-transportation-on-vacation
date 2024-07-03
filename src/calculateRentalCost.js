3f904c6fdeac7b72d8dd616f5b17f48788da29b7
Artem-Brui <artembryj@gmail.com>
2024-07-03 13:57:46 +0200

    task comleted

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,21 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const costPerDay = 40;
+  const shortTerm = 3;
+  const longTerm = 7;
+  const shortTermDiscount = 20;
+  const longTermDiscount = 50;
+
+  if (days >= longTerm) {
+    return costPerDay * days - longTermDiscount;
+  }
+
+  if (days >= shortTerm) {
+    return costPerDay * days - shortTermDiscount;
+  }
+
+  return costPerDay * days;
 }
 
 module.exports = calculateRentalCost;
