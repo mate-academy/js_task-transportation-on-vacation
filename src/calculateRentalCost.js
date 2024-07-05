62c69ed0ff101ad6f268b816f1c6e00b48f4fe2c
Jonathan da Silva Hora <jonathan.s.hora@gmail.com>
2024-07-04 23:08:29 -0300

    add variables in solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,13 +5,15 @@
  */
 function calculateRentalCost(days) {
   const cost = 40;
+  const longDays = 7;
+  const shortDays = 3;
   const longDiscount = 50;
   const shortDiscount = 20;
   const price = cost * days;
 
-  if (days >= 7) {
+  if (days >= longDays) {
     return price - longDiscount;
-  } else if (days >= 3) {
+  } else if (days >= shortDays) {
     return price - shortDiscount;
   }
 
