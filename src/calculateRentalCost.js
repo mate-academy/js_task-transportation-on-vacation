5295c3a0fce732568cdce5ad94670afe32ee45c1
Jonathan da Silva Hora <jonathan.s.hora@gmail.com>
2024-07-04 23:03:15 -0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const cost = 40;
+  const longDiscount = 50;
+  const shortDiscount = 20;
+  const price = cost * days;
+
+  if (days >= 7) {
+    return price - longDiscount;
+  } else if (days >= 3) {
+    return price - shortDiscount;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
