63a415bc3f18543b72647b3b390fdb53ee14b454
nataSokolnikova <167069028+nataSokolnikova@users.noreply.github.com>
2024-07-08 11:35:40 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,16 +5,20 @@
  */
 const minDaysForDiscount = 3;
 const maxDaysForDiscount = 7;
-const smallDiscountAmount = 20;
+const baseDiscountAmount = 20;
 const largeDiscountAmount = 50;
 const dailyRentalRate = 40;
 
 function calculateRentalCost(days) {
   if (days < minDaysForDiscount) {
     return days * dailyRentalRate;
-  } else if (days >= minDaysForDiscount && days < maxDaysForDiscount) {
-    return days * dailyRentalRate - smallDiscountAmount;
-  } else if (days >= maxDaysForDiscount) {
+  }
+
+  if (days >= minDaysForDiscount && days < maxDaysForDiscount) {
+    return days * dailyRentalRate - baseDiscountAmount;
+  }
+
+  if (days >= maxDaysForDiscount) {
     return days * dailyRentalRate - largeDiscountAmount;
   }
 }
