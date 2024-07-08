fc3649fb269e9f3df051aeae39132f49817dd6ac
nataSokolnikova <167069028+nataSokolnikova@users.noreply.github.com>
2024-07-08 10:46:13 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,19 @@
  *
  * @return {number}
  */
+const discountsDays1 = 3;
+const discountsDays2 = 7;
+const discount1 = 20;
+const discount2 = 50;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days < discountsDays1) {
+    return days * 40;
+  } else if (days >= discountsDays1 && days < discountsDays2) {
+    return days * 40 - discount1;
+  } else if (days >= discountsDays2) {
+    return days * 40 - discount2;
+  }
 }
 
 module.exports = calculateRentalCost;
