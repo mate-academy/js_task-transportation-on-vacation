6226e3a0525ea3c7013de7d63fa6a4372a3c7b56
Iryna Sadova <ira.sadova555@gmail.com>
2024-07-09 13:33:35 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const currentRent = 40;
+  const minDaysOff = 3;
+  const maxDaysOff = 7;
+  const minDiscountRent = 20;
+  const maxDiscountRent = 50;
+
+  if (days >= maxDaysOff) {
+    return currentRent * days - maxDiscountRent;
+  }
+
+  if (days >= minDaysOff) {
+    return currentRent * days - minDiscountRent;
+  }
+
+  return currentRent * days;
 }
 
 module.exports = calculateRentalCost;
