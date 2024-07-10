95f3541c6b982730f36a489b4c9eb697f6ab8ae1
Kayky Rodrigues da Silva <kaykyr.dev@gmail.com>
2024-07-10 03:55:44 -0300

    Calculate Rental Cost Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  if (days < SHORT_TERM) {
+    return days * PRICE_PER_DAY;
+  }
+
+  if (days < LONG_TERM) {
+    return days * PRICE_PER_DAY - SHORT_TERM_DISCOUNT;
+  }
+
+  return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
