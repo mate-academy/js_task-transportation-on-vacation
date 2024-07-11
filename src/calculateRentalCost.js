ce340bd2236e9ce3f37b3099a59e103e1d24d562
Yevhenii Lutsenko <evgenlutscenko@icloud.com>
2024-07-11 10:34:32 +0200

    consts were edited

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 
-const regularPrice = 40;
-const amountOfDaysForFiftyOff = 7;
-const amountOfDaysForTwenty = 3;
+const REGULAR_PRICE = 40;
+const AMOUNT_OF_DAYS_FOR_FIFTY_OFF = 7;
+const AMOUNT_OF_DAYS_FOR_TWENTY_OFF = 3;
 
 function calculateRentalCost(days) {
-  const price = days * regularPrice;
+  const price = days * REGULAR_PRICE;
 
-  if (days >= amountOfDaysForFiftyOff) {
+  if (days >= AMOUNT_OF_DAYS_FOR_FIFTY_OFF) {
     return price - 50;
   }
 
-  if (days >= amountOfDaysForTwenty) {
+  if (days >= AMOUNT_OF_DAYS_FOR_TWENTY_OFF) {
     return price - 20;
   }
 
