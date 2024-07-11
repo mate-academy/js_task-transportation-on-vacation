fc9ee33b609834bf9058e7b42d6607ad50e5339a
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2024-07-11 11:29:29 -0400

    Added task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,15 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let price = days * 40;
+
+  if (days >= 3 && days <= 6) {
+    price = price - 20;
+  } else if (days >= 7) {
+    price = price - 30;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
