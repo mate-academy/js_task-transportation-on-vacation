2c996ed894996151c8ead5c2326c86ae1dece0a3
Sviatoslav Pokhvalenko <pokhvalenkosvyat@gmail.com>
2024-07-11 11:58:16 -0400

    changed again

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,15 +9,17 @@ function calculateRentalCost(days) {
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
-  const BASE_PRICE = days * 40;
+  const basePrice = days * 40;
 
   if (days >= LONG_TERM) {
-    return BASE_PRICE - LONG_TERM_DISCOUNT;
-  } else if (days >= SHORT_TERM) {
-    return BASE_PRICE - SHORT_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  return BASE_PRICE;
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
