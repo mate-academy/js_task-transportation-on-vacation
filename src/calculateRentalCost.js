f258d78f19a4592c73db033b1f43d2256fbecb42
Mariana Krachkovska <mvkrachkovska@gmail.com>
2024-07-13 09:48:53 +0200

    bug fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const EVERY_DAY_COST = 40;
   const DISCOUNT_AFTER_3_DAYS = 20;
   const DISCOUNT_AFTER_7_DAYS = 50;
+  const PRICE = days * 40;
 
-  if (days <= 2) {
-    return days * EVERY_DAY_COST;
+  if (days >= 7) {
+    return PRICE - DISCOUNT_AFTER_7_DAYS;
   }
 
-  if (days <= 6) {
-    return days * EVERY_DAY_COST - DISCOUNT_AFTER_3_DAYS;
-  } else {
-    return days * EVERY_DAY_COST - DISCOUNT_AFTER_7_DAYS;
+  if (days >= 3) {
+    return PRICE - DISCOUNT_AFTER_3_DAYS;
   }
+
+  return PRICE;
 }
 
 module.exports = calculateRentalCost;
