8d1a62030024ca2147c4302d60917435737e8591
OlenaMykhaylenko <lena512000@eyahoo.com>
2024-07-14 11:04:46 +0200

    Solution change

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,9 +1,4 @@
 /**
- * @param {number} days
- *
- * @return {number}
- */
-/**
  * Calculates the rental cost based on the number of days rented,
  * applying different discounts based on the duration.
  *
@@ -11,15 +6,21 @@
  * @return {number} The total rental cost.
  */
 function calculateRentalCost(days) {
-  const dailyRate = 40;
+  const DAILY_RATE = 40;
+  const BASIC_DISCOUNT = 20;
+  const ADDITIONAL_DISCOUNT = 50;
+  const BASIC_DISCOUNT_THRESHOLD = 3;
+  const ADDITIONAL_DISCOUNT_THRESHOLD = 7;
 
-  if (days < 3) {
-    return days * dailyRate;
-  } else if (days >= 3 && days <= 6) {
-    return days * dailyRate - 20;
-  } else {
-    return days * dailyRate - 50;
+  if (days < BASIC_DISCOUNT_THRESHOLD) {
+    return days * DAILY_RATE;
   }
+
+  if (days < ADDITIONAL_DISCOUNT_THRESHOLD) {
+    return days * DAILY_RATE - BASIC_DISCOUNT;
+  }
+
+  return days * DAILY_RATE - ADDITIONAL_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
