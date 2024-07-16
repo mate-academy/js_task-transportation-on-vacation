6098756a033a02ad710b1b098c62608420c1dc4e
Oleksandr Haman <onaleksandr85@gmail.com>
2024-07-17 01:25:19 +0200

    add clearer name

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,21 +4,20 @@
  * @return {number}
  */
 
-const DISCOUNT_THRESHOLD_7_DAYS = 7;
-const DISCOUNT_THRESHOLD_3_DAYS = 3;
-const DISCOUNT_7_DAYS = 50;
+const LONG_TERM = 7;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 50;
 const DISCOUNT_3_DAYS = 20;
+const PRICE = 40;
 
 function calculateRentalCost(days) {
-  const PRICE = 40;
-
   const totalCost = days * PRICE;
 
-  if (days >= DISCOUNT_THRESHOLD_7_DAYS) {
-    return totalCost - DISCOUNT_7_DAYS;
+  if (days >= LONG_TERM) {
+    return totalCost - SHORT_TERM_DISCOUNT;
   }
 
-  if (days >= DISCOUNT_THRESHOLD_3_DAYS) {
+  if (days >= SHORT_TERM) {
     return totalCost - DISCOUNT_3_DAYS;
   }
 
