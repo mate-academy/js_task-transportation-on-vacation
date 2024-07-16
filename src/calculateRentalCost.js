e56163cdade0f1a09d6b8262eb5ec3522a2c0a95
Andrii Larin <gamon.not.dead@gmail.com>
2024-07-16 21:07:59 +0300

    add solutions

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,28 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const DEFAULT_COST = 40;
+  const MEDIUM_TERM = 3;
+  const MEDIUM_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  let rentalPrice = 0;
+
+  if (days >= LONG_TERM) {
+    rentalPrice = DEFAULT_COST * days - LONG_TERM_DISCOUNT;
+
+    return rentalPrice;
+  }
+
+  if (days >= MEDIUM_TERM) {
+    rentalPrice = DEFAULT_COST * days - MEDIUM_TERM_DISCOUNT;
+
+    return rentalPrice;
+  }
+
+  rentalPrice = DEFAULT_COST * days;
+
+  return rentalPrice;
 }
 
 module.exports = calculateRentalCost;
