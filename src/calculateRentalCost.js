dc6574a27c406265efade4c014b8c5153cb7879a
George <george.boujhenitsa@gmail.com>
2024-07-16 17:31:35 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const discountForThreeMoreDays = 20;
+  const discountForSevenMoreDays = 50;
+
   if (days <= 2) {
     return 40 * days;
   }
 
-  if (days > 2 && days <= 6) {
-    return 40 * days - 20;
+  if (days <= 6) {
+    return 40 * days - discountForThreeMoreDays;
   }
 
   if (days >= 7) {
-    return 40 * days - 50;
+    return 40 * days - discountForSevenMoreDays;
   }
 }
 
