d6c1884a6d61db1e61a9d8f989137db73d933e68
Ilona Gogol <ilonka.gogol@gmail.com>
2024-07-16 16:13:49 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,24 +6,26 @@
 function calculateRentalCost(days) {
   // write code here
 
-  const priceForDay = 40;
-  const smallDiscount = 20;
-  const bigDiscount = 50;
-  let count = days * priceForDay;
+  const PRICE_PER_DAY = 40;
+  const SMALL_DISCOUNT = 20;
+  const SHORT_VACATION = 3;
+  const LONG_VACATION = 7;
+  const BIG_DISCOUNT = 50;
+  let TOTAL_COUNT = days * PRICE_PER_DAY;
 
-  if (days >= 7) {
-    count -= bigDiscount;
+  if (days >= LONG_VACATION) {
+    TOTAL_COUNT -= BIG_DISCOUNT;
 
-    return count;
+    return TOTAL_COUNT;
   }
 
-  if (days >= 3) {
-    count -= smallDiscount;
+  if (days >= SHORT_VACATION) {
+    TOTAL_COUNT -= SMALL_DISCOUNT;
 
-    return count;
+    return TOTAL_COUNT;
   }
 
-  return count;
+  return TOTAL_COUNT;
 }
 
 module.exports = calculateRentalCost;
