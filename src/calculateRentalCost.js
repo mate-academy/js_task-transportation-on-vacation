f9a651bc4762952bc90192518bf57b9bb08f8118
Tanya Baletska <t.baletska@gmail.com>
2024-07-17 20:49:11 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+  const BIG_DISCOUNT = 50;
+  const SMALL_DISCOUNT = 20;
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days < SHORT_TERM) {
+    return basePrice;
+  }
+
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    return basePrice - SMALL_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    return basePrice - BIG_DISCOUNT;
+  }
 }
 
 module.exports = calculateRentalCost;
