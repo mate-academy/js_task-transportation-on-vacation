8d24870ed7810e0aa709a36103f5945e8ddbab38
Kostya Vasilenko <kostyantin.vasilenko.03@gmail.com>
2024-07-18 22:34:52 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,18 @@
-/**
+/*
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PriseDay = 40;
+
+  if (days < 3) {
+    return days * PriseDay;
+  } else if (days >= 3 && days < 7) {
+    return days * PriseDay - 20;
+  } else {
+    return days * PriseDay - 50;
+  }
 }
 
 module.exports = calculateRentalCost;
