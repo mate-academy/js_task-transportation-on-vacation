d16fd464369f472107848a14353ea3d9cbe8c418
Mykyta Forostovets <122729163+Galenwood@users.noreply.github.com>
2024-07-18 19:08:30 +0300

    add js solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,23 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
+function calculateRentalCost(numberOfDays) {
   // write code here
+  const basePrice = 40 * numberOfDays;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (numberOfDays >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (numberOfDays >= SHORT_TERM && numberOfDays < LONG_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
