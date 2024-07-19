8ad9ea11ad615aa2086ff7d1c43ef83b67a0d18c
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2024-07-19 13:04:11 +0300

    Implemented calculateReantalCost method

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_PRICE = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const basePrice = DAILY_PRICE * days;
+
+  if (days >= 7) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= 3) {
+    return basePrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
