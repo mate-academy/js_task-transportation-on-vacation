cd55d3c44f514953f0b32f9112c0790548204886
Bohdan Hovorukha <hovorukha.bohdan@gmail.com>
2024-07-19 13:11:17 +0300

    Fixed constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,16 +5,18 @@
  */
 function calculateRentalCost(days) {
   const DAILY_PRICE = 40;
+  const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
   const basePrice = DAILY_PRICE * days;
 
-  if (days >= 7) {
+  if (days >= LONG_TERM) {
     return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
+  if (days >= SHORT_TERM) {
     return basePrice - SHORT_TERM_DISCOUNT;
   }
 
