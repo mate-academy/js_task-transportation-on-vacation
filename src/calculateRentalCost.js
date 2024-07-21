694ff8d7eb32a3414c718c0774bd237aa32f73cf
Yurii <uaremko945@gmail.com>
2024-07-21 18:28:51 +0300

    fixed comments

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,27 +5,83 @@
  */
 
 function calculateRentalCost(days) {
-  let COUNT_MONEY = 0;
-  const PRICE_ONE_DAY = 40;
-  const TOTAL_AMOUNT = PRICE_ONE_DAY * days;
-  const DISCOUNT_THREE_DAYS = 20;
-  const DISCOUNT_SEVEN_DAYS = 50;
-  const THREE_DAY = 3;
-  const SEVEN_DAY = 7;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  if (days < THREE_DAY) {
-    COUNT_MONEY = TOTAL_AMOUNT;
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= THREE_DAY) {
-    COUNT_MONEY = TOTAL_AMOUNT - DISCOUNT_THREE_DAYS;
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
-  if (days >= SEVEN_DAY) {
-    COUNT_MONEY = TOTAL_AMOUNT - DISCOUNT_SEVEN_DAYS;
-  }
-
-  return COUNT_MONEY;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
+
+// function getMin(numbers) {
+//   let min = numbers[0];
+
+//   for (let i = 1; i < numbers.length; i++) {
+//     if (numbers[i] < min) {
+//       min = numbers[i];
+//     }
+//   }
+
+//   return min;
+// }
+
+// function firstAlphabetLetter(word) {
+//   for (let i = 0; i < word.length; i++) {
+//     const char = word[i].toLowerCase();
+
+//     if (/[a-z]/i.test(char)) {
+//       return char;
+//     }
+//   }
+// }
+
+// function getTheBigestDigit(n) {
+//   const bigDigit = 0;
+
+//   for (let ch = 0; ch < n.length; ch++) {
+//     if (ch[0] > ch[1]) {
+//       bigDigit.push(ch[1]);
+//     }
+//   }
+
+//   return bigDigit;
+// }
+
+// function getAverage(numbers) {
+//   if (numbers.length === 0) {
+//     return 0;
+//   }
+
+//   let sum = 0;
+
+//   for (let i = 0; i < numbers.length; i++) {
+//     sum += numbers[i];
+//   }
+
+//   const average = sum / numbers.length;
+
+//   return average;
+// }
+
+// function checkIfAllNumbersAreGreaterThan(numbers, value) {
+//   for (let i = 0; i < numbers.length; i++) {
+//     if (numbers[i] <= value) {
+//       return false;
+//     }
+//   }
+
+//   return true;
+// }
