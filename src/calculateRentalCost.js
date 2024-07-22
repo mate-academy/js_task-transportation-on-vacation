0457a2961d62555c03222d23c0a6a00d063adb94
sinner1993 <sergio1993goga@gmail.com>
2024-07-22 18:32:18 +0200

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  switch (days) {
+    case 7:
+      return 7 * PRICE - LONG_TERM_DISCOUNT;
+    case 6:
+      return 6 * PRICE - SHORT_TERM_DISCOUNT;
+    case 5:
+      return 5 * PRICE - SHORT_TERM_DISCOUNT;
+    case 4:
+      return 4 * PRICE - SHORT_TERM_DISCOUNT;
+    case 3:
+      return 3 * PRICE - SHORT_TERM_DISCOUNT;
+    case 2:
+      return 2 * PRICE;
+    case 1:
+      return 1 * PRICE;
+  }
 }
 
 module.exports = calculateRentalCost;
