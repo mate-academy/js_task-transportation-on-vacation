103c325bba515b33af2c5b42a3f66e527c53361f
Your Name <you@example.com>
2024-07-22 20:50:57 +0700

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const BASE_PRICE = DAYS * 40;
+
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (numberOfDays >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  } else if (numberOfDays >= SHORT_TERM)
+    return (basePrice = SHORT_TERM_DISCOUNT);
+
+  return BASE_PRICE;
 }
 
 module.exports = calculateRentalCost;
