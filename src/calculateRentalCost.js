a17452f81713a924d0ddb48f73839bb49e1e05c0
Viacheslav1982-ai <alenichev.v82@gmail.com>
2024-07-22 20:24:01 +0300

    add task solution1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,27 @@
 /**
  * @param {number} days
  *
+ *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_OF_DAY = 40;
+  const LONG_TERMIN = 7;
+  const LONG_TERMIN_DISCOUNT = 50;
+  const SHORT_TERMIN = 3;
+  const SHORT_TERMIN_DISCOUNT = 20;
+
+  const basePrice = days * PRICE_OF_DAY;
+
+  if (days >= LONG_TERMIN) {
+    return basePrice - LONG_TERMIN_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERMIN) {
+    return basePrice - SHORT_TERMIN_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
