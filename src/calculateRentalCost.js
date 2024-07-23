28cac1b0723ba155761cb1152dce39d2c328ccc2
sinner1993 <sergio1993goga@gmail.com>
2024-07-23 12:11:23 +0200

    fixed literals mistake and else mistake

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,13 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
   const PRICE = 40;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >=7) {
+  if (days >= LONG_TERM) {
     return days * PRICE - LONG_TERM_DISCOUNT;
-  } else if (days >= 3) {
+  }
+
+  if (days >= SHORT_TERM) {
     return days * PRICE - SHORT_TERM_DISCOUNT;
   }
 
