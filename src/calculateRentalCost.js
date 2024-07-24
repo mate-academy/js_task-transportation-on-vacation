c9be028f7be843da0ef84293688f6cedc5250d44
HAIOVIY <gaevoywork@gmail.com>
2024-07-24 14:30:20 +0300

    implement calculateRentalCost method

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PAY_FOR_DAY = 40;
+  const MIN_DAYS_FOR_MAX_OFF = 7;
+  const MAX_OFF = 50;
+  const MIN_DAYS_FOR_FIRST_OFF = 3;
+  const MIN_OFF = 20;
+
+  if (days < MIN_DAYS_FOR_FIRST_OFF) {
+    return days * PAY_FOR_DAY;
+  }
+
+  if (days >= MIN_DAYS_FOR_FIRST_OFF && days < MIN_DAYS_FOR_MAX_OFF) {
+    return days * PAY_FOR_DAY - MIN_OFF;
+  }
+
+  return days * PAY_FOR_DAY - MAX_OFF;
 }
 
 module.exports = calculateRentalCost;
