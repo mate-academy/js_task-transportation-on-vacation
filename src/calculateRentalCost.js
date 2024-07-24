bbcc49f703416f6b0ffd2fa3b594643df20c1b08
Vladyslav Humeniak <vladyslavhumenyak@gmail.com>
2024-07-24 14:26:40 +0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const SHORT_TERM = 3;
+  const LONG_TERM = 7;
+  const RENT_PRICE = 40;
+  const DEFAULT_PRICE = days * RENT_PRICE;
+
+  if (days >= LONG_TERM) {
+    return DEFAULT_PRICE - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return DEFAULT_PRICE - SHORT_TERM_DISCOUNT;
+  }
+
+  return DEFAULT_PRICE;
 }
 
 module.exports = calculateRentalCost;
