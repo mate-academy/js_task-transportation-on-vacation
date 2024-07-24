0d88a1fcd9eef230ff43316ff5a193843cc1ba9e
Artur Misko <arthurmisko@gmail.com>
2024-07-23 22:28:12 -0500

    taskaifix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,17 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let money = days * 40;
+  const cost = days * 40;
 
   if (days >= 7) {
-    money = money - 50;
+    return cost - 50;
   }
 
   if (days >= 3 && days < 7) {
-    money = money - 20;
+    return cost - 20;
   }
 
-  return money;
+  return cost;
 }
 
 module.exports = calculateRentalCost;
