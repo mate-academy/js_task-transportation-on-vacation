6a762ee719954633080247933812fec65c781183
Viacheslav1982-ai <alenichev.v82@gmail.com>
2024-07-27 19:45:35 +0300

    new changes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,23 +5,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const PRASE_OF_DAY = 40;
+  const PRICE_OF_DAY = 40;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
-  let basePrises = days * PRASE_OF_DAY;
+  const basePrice = days * PRICE_OF_DAY;
 
   if (days >= LONG_TERM) {
-    return (basePrises -= LONG_TERM_DISCOUNT);
+    return (basePrice -= LONG_TERM_DISCOUNT);
   }
 
   if (days >= SHORT_TERM) {
-    return (basePrises -= SHORT_TERM_DISCOUNT);
+    return (basePrice -= SHORT_TERM_DISCOUNT);
   }
 
-  return basePrises;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
