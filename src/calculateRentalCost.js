2c32cc5ccbf144e50098996741d2f74214560681
Denys Nikiforov <mega.nds@ukr.net>
2024-08-04 09:31:21 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,25 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const basePrise = 40;
+  const discontSevenDay = 50;
+  const discontThreeDay = 20;
+
+  if (days >= 7) {
+    return basePrise * days - discontSevenDay;
+  }
+
+  if (days === 6) {
+    return basePrise * days - discontThreeDay;
+  }
+
+  if (days >= 3) {
+    return basePrise * days - discontThreeDay;
+  }
+
+  if (days < 3) {
+    return basePrise * days;
+  }
 }
 
 module.exports = calculateRentalCost;
