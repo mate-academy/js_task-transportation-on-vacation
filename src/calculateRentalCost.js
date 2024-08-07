92bb3d05b110d576e87cda4fd042092f33de442d
Tomasz Krzywicki <tomaszkrzywicki4@gmail.com>
2024-08-07 13:52:44 +0200

    Changes after code review

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,16 @@
  */
 function calculateRentalCost(days) {
   const dailyRate = 40;
+  const longTermDays = 7;
+  const longTermDiscount = 50;
+  const shortTermDays = 3;
+  const shortTermDiscount = 20;
   let totalCost = days * dailyRate;
 
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+  if (days >= longTermDays) {
+    totalCost = totalCost - longTermDiscount;
+  } else if (days >= shortTermDays) {
+    totalCost -= shortTermDiscount;
   }
 
   return totalCost;
