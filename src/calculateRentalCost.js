7de8f63fb91eec2255b4b67d117d941820e45223
Sofiia Sharun <sharunsofiia@gmail.com>
2024-08-10 13:06:58 -0500

    Add homework

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+    // write code here
+    const dailyCost = 40;
+    let totalCost = days * dailyCost;
+    const longTermDiscount = 50;
+    const shortTermDiscount = 20;
+
+    if (days >= 7) {
+        return totalCost -= longTermDiscount;
+    }
+    if (days >= 3) {
+        return totalCost -= shortTermDiscount;
+    }
+
+    return totalCost;
 }
 
 module.exports = calculateRentalCost;
