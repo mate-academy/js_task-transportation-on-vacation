1f9a96700d161ed5e175df9c28d23205548d6549
Oleksii Kudriavtsev <Lesha61945@gmail.com>
2024-08-10 12:42:17 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const longTermDiscount = 50;
+  const normalTermDiscount = 20;
+  const rentalCost = 40;
+  const longTerm = 7;
+  const normalTerm = 3;
+  let totalCost = days * rentalCost;
+
+  if (days >= longTerm) {
+    totalCost -= longTermDiscount;
+  }
+
+  if (days >= normalTerm && days < longTerm) {
+    totalCost -= normalTermDiscount;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
