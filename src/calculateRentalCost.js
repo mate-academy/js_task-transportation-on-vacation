e7335eb0b65510e88b08f5954507f916b983d877
Sasha Popovych <sashapopovych98@gmail.com>
2024-08-10 15:49:39 +0200

    Test

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const carRent = 40;
+  let fullCost = days * carRent;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const SHORT_TERM = 3;
+
+  if (days >= LONG_TERM) {
+    fullCost -= LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    fullCost -= SHORT_TERM_DISCOUNT;
+  }
+
+  return fullCost;
 }
 
 module.exports = calculateRentalCost;
