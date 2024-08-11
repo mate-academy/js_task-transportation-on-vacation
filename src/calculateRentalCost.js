6e50454e0e1cf62905908f1b69b53808e7d43fb8
joaoguilherme <joaoguilhermehig@gmail.com>
2024-08-10 22:48:24 -0300

    pronto

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,11 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  return days > 2 && days < 7
+    ? days * 40 - 20
+    : days > 6
+      ? days * 40 - 50
+      : days * 40;
 }
 
 module.exports = calculateRentalCost;
