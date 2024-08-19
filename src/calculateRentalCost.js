af21283147303c8fa204b8e7fbc66a8e3d24c856
Yevhenii Vlasenko <yevhenii.vlasenko.work@gmail.com>
2024-08-19 14:48:33 +0300

    refactor3

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -13,9 +13,9 @@ function calculateRentalCost(days) {
     return defaultCalculation - BIG_DISCOUNT;
   } else if (days >= 3 && days < 7) {
     return defaultCalculation - DISCOUNT;
-  } else {
-    return defaultCalculation;
   }
+
+  return defaultCalculation;
 }
 
 module.exports = calculateRentalCost;
