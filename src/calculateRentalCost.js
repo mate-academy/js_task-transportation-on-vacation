537219f4eeb528b556b5aae12fabc8017e7d44da
Yevhenii Vlasenko <yevhenii.vlasenko.work@gmail.com>
2024-08-19 14:39:21 +0300

    implemented solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dayPrice = 40;
+  let discount = 0;
+
+  switch (true) {
+    case days >= 3 && days < 7:
+      discount = 20;
+      break;
+    case days >= 7:
+      discount = 50;
+      break;
+    default:
+      discount = 0;
+  }
+
+  return dayPrice * days - discount;
 }
 
 module.exports = calculateRentalCost;
