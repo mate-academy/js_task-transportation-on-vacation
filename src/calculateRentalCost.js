84ecef9af07a67aa6340ff1410a2bae1387e5e41
Oleksandr Polishchuk <crazy8diamond@gmail.com>
2024-08-19 12:47:29 +0300

    add variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,12 +7,14 @@ function calculateRentalCost(days) {
   const TOTAL_AMOUNT = days * 40;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
+  const LONG_DAYS = 7;
+  const SHORT_DAYS = 3;
 
-  if (days >= 7) {
+  if (days >= LONG_DAYS) {
     return TOTAL_AMOUNT - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
+  if (days >= SHORT_DAYS) {
     return TOTAL_AMOUNT - SHORT_TERM_DISCOUNT;
   }
 
