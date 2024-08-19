90b84f00c659ae609d0662f8e8b33fdb31140195
Yevhenii Vlasenko <yevhenii.vlasenko.work@gmail.com>
2024-08-19 14:47:52 +0300

    refactor2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,13 +9,12 @@ function calculateRentalCost(days) {
   const baseDayPrice = 40;
   const defaultCalculation = baseDayPrice * days;
 
-  switch (true) {
-    case days >= 3 && days < 7:
-      return defaultCalculation - DISCOUNT;
-    case days >= 7:
-      return defaultCalculation - BIG_DISCOUNT;
-    default:
-      return defaultCalculation;
+  if (days >= 7) {
+    return defaultCalculation - BIG_DISCOUNT;
+  } else if (days >= 3 && days < 7) {
+    return defaultCalculation - DISCOUNT;
+  } else {
+    return defaultCalculation;
   }
 }
 
