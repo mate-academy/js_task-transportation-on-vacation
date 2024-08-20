189b886d924ba1230fa178760cadf2bedc22ffc3
Yevhen Stepanenko <yevhen.s@mate.academy>
2024-08-20 12:47:42 +0300

    second commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  console.log('calculateRentalCost');
+  const dayPrice = 40;
+  const total = days * dayPrice;
+  const smallDiscount = 20;
+  const bigDiscount = 50;
+
+  if (days < 3) {
+    return total;
+  }
+
+  if (days < 7) {
+    return total - smallDiscount;
+  }
+
+  return total - bigDiscount;
 }
 
 module.exports = calculateRentalCost;
