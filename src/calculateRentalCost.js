957b2a4925a24e084e87c400c0b509d9cdfc7f73
Svidruk <mr.svid999@gmail.com>
2024-08-20 13:46:53 +0300

    calculate rental cost solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const carRent = 40;
+  const shortTerm = 3;
+  const longTerm = 7;
+  const threeDaysDiscount = 20;
+  const sevenDaysDiscount = 50;
+
+  let total = days * carRent;
+
+  if (days >= longTerm) {
+    return (total -= sevenDaysDiscount);
+  }
+
+  if (days >= shortTerm) {
+    return (total -= threeDaysDiscount);
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
