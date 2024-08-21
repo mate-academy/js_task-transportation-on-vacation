d79c07619bccc0aad4b63b74102ffd5203cbf544
Your_Name Your_Surname <your@email>
2024-08-21 13:13:25 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const basePrice = 40;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const shortTerm = 3;
+  const shortTermDiscount = 20;
+
+  if (days >= longTerm) {
+    return basePrice * days - longTermDiscount;
+  }
+
+  if (days >= shortTerm) {
+    return basePrice * days - shortTermDiscount;
+  }
+
+  return basePrice * days;
 }
 
 module.exports = calculateRentalCost;
