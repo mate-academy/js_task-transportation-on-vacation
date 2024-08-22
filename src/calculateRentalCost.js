892f48466f8e5dfea628a97c5d3c70e3234a2b11
Andrey Pikalov <andrtan10mail.com@gmail.com>
2024-08-22 23:33:34 +0300

    solved task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  return days >= 7 ? days * 40 - 50 : days >= 3 ? days * 40 - 20 : days * 40;
 }
 
 module.exports = calculateRentalCost;
