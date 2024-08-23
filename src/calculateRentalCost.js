58df1bc9cdde16c628bb158d273e209fc618fff6
Maxym Modrytskyi <maxmodrr@gmail.com>
2024-08-24 00:24:35 +0300

    results

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,21 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const PRICE_FOR_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const result = PRICE_FOR_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return result - LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    return result - SHORT_TERM_DISCOUNT;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
