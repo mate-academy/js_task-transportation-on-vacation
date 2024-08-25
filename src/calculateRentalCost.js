71201132eb5862da2148a87fa9556cbe146c28a2
gordgelin <gordgelin@s69a.su>
2024-08-25 18:52:43 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const COST_PER_DAY = 40;
+
+  const DISCOUNT_LVL1 = 20;
+  const DISCOUNT_LVL2 = 50;
+
+  const DISCOUNT_LVL1_MIN_DAYS = 3;
+  const DISCOUNT_LVL2_MIN_DAYS = 7;
+
+  const sum = days * COST_PER_DAY;
+
+  switch (true) {
+    case days >= DISCOUNT_LVL2_MIN_DAYS:
+      return sum - DISCOUNT_LVL2;
+    case days >= DISCOUNT_LVL1_MIN_DAYS:
+      return sum - DISCOUNT_LVL1;
+    default:
+      return sum;
+  }
 }
 
 module.exports = calculateRentalCost;
