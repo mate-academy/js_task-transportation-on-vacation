ce8eccfbdbbabb5cac475e76d3db326c5845d137
Maksym Fedorchuk <maxeffect.apollo@gmail.com>
2024-08-26 19:55:47 +0300

    cleaning

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,16 +3,18 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const BASE_COST = 40;
+function calculateRentalCost(daysRented) {
+  const DAILY_BASE_COST = 40;
 
-  if (days < 3) {
-    return days * BASE_COST;
-  } else if (days < 7) {
-    return days * BASE_COST - 20;
-  } else {
-    return days * BASE_COST - 50;
+  let totalCost = daysRented * DAILY_BASE_COST;
+
+  if (daysRented >= 7) {
+    totalCost -= 50;
+  } else if (daysRented >= 3) {
+    totalCost -= 20;
   }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
