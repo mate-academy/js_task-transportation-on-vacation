732ad157648901f284d434fec93bde62dbdf2f2b
Eustas <kononenko.dima2001@gmail.com>
2024-08-26 19:03:42 +0300

    solving task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const cost = 40 * days;
+
+  if (days >= 7) {
+    return cost - 50;
+  } else if (days >= 3) {
+    return cost - 20;
+  } else {
+    return cost;
+  }
 }
 
 module.exports = calculateRentalCost;
