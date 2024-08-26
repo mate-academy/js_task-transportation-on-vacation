86626c75d6c033bca98e4a95dd7c5357f49a5444
Rostyslav <rostislavkpi@gmail.com>
2024-08-26 13:12:51 +0300

    Fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,17 +1,17 @@
+const PRICE = 40;
+
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
+
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const PRICE = 40;
-
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
-
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-
   if (days >= SHORT_TERM && days < LONG_TERM) {
     return days * PRICE - SHORT_TERM_DISCOUNT;
   }
