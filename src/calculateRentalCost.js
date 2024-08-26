9820e83dc96f903134771c2e36a73a6a13ad6e7e
Ivan Pavluchuk <java.python10@gmail.com>
2024-08-26 13:48:40 +0300

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,7 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  return days < 3 ? days * 40 : days < 7 ? days * 40 - 20 : days * 40 - 50;
 }
 
 module.exports = calculateRentalCost;
