3d883680ff1d241ddb3a273e19d482130a06b4c5
Ivan Pavluchuk <java.python10@gmail.com>
2024-08-26 21:52:38 +0300

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,7 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
-  return days < 3 ? days * 40 : days < 7 ? days * 40 - 20 : days * 40 - 50;
+  // return days < 3 ? days * 40 : days < 7 ? days * 40 - 20 : days * 40 - 50;
+
+  const price = 40;
+  const longTermDiscount = 50;
+  const longTerm = 7;
+  const shortTermDiscount = 20;
+  const shortTerm = 3;
+  const totalCost = days * price;
+
+  if (days >= longTerm) {
+    return totalCost - longTermDiscount;
+  } else if (days >= shortTerm) {
+    return totalCost - shortTermDiscount;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
