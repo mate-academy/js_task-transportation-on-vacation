d5cc4b6e55561e300130dc072f7eb3f1a4f41f1f
Vitalii <mlineckiyvitaliy@gmail.com>
2024-08-26 18:56:51 +0300

    changed a code for the better one

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,29 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const PRICE = 40;
   const SHORT_TERM = 3;
   const LONG_TERM = 7;
   const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM_DISCOUNT = 50;
-  const PRICE = 40;
-
-  let totalSpent = 0;
+  const DEFAULT_PRICE = days * PRICE;
 
   if (days >= LONG_TERM) {
-    totalSpent = days * PRICE - LONG_TERM_DISCOUNT;
-
-    return totalSpent;
+    return DEFAULT_PRICE - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    totalSpent = days * PRICE - SHORT_TERM_DISCOUNT;
-
-    return totalSpent;
+    return DEFAULT_PRICE - SHORT_TERM_DISCOUNT;
   }
 
-  totalSpent = days * PRICE;
-
-  return totalSpent;
+  return DEFAULT_PRICE;
 }
 
 module.exports = calculateRentalCost;
