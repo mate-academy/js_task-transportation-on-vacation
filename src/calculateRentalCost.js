153f471a3e37bacb2c2e9b7aaf3a93b691a91ad2
Yevhenii Melnyk <rpm.steel@gmail.com>
2024-08-26 11:52:15 +0300

    task has been solved

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const shortTerm = 2;
+  const longTerm = 7;
+  const price = 40;
+  let sale = 0;
+
+  if (days > shortTerm && days < longTerm) {
+    sale = 20;
+  }
+
+  if (days >= longTerm) {
+    sale = 50;
+  }
+
+  return days * price - sale;
 }
 
 module.exports = calculateRentalCost;
