8adec9798003ebf35847d33ede2d7a99c78836d4
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2024-08-26 12:37:01 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const rentalPay = 40;
+  const longTermDiscount = 50;
+  const longTerm = 7;
+  const shortTermDiscount = 20;
+  const shortTerm = 3;
 
+  if (days >= longTerm) {
+    return rentalPay * days - longTermDiscount;
+  } else if (days >= shortTerm) {
+    return rentalPay * days - shortTermDiscount;
+  }
+
+  return rentalPay * days;
+}
 module.exports = calculateRentalCost;
