2101f6e9f518ae6c85a3a9f7b8feb292f0128e60
Bohdan <bogdab09@gmail.com>
2024-08-27 08:24:10 +0300

    2try

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -12,17 +12,18 @@ const PRICE = 40;
 const DAYS_SMALL_DISCOUNT = 3;
 const SMALL_DISCOUNT = 20;
 
-const DAYZ_BIG_DISCOUNT = 7;
+const DAYS_BIG_DISCOUNT = 7;
 const BIG_DISCOUNT = 50;
 
 function calculateRentalCost(days) {
   // write code here
-  if (days >= DAYS_SMALL_DISCOUNT && days < DAYZ_BIG_DISCOUNT) {
-    return days * PRICE - SMALL_DISCOUNT;
+
+  if (days >= DAYS_BIG_DISCOUNT) {
+    return days * PRICE - BIG_DISCOUNT;
   }
 
-  if (days >= DAYZ_BIG_DISCOUNT) {
-    return days * PRICE - BIG_DISCOUNT;
+  if (days >= DAYS_SMALL_DISCOUNT) {
+    return days * PRICE - SMALL_DISCOUNT;
   }
 
   return days * PRICE;
