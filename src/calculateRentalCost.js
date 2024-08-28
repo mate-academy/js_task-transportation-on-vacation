4151f1ec97acf67f13c5967adf655edb0a6f1768
Vasyliazhenko Dmytro Vitaliyovych <demon4225@gmail.com>
2024-08-28 11:45:39 +0300

    code style fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,21 +3,24 @@
  *
  * @return {number}
  */
+
+const RENTAL_PAY = 40;
+const LONG_TERM_DISCOUNT = 50;
+const LONG_TERM = 7;
+const SHORT_TERM_DISCOUNT = 20;
+const SHORT_TERM = 3;
+
 function calculateRentalCost(days) {
-  const RENTAL_PAY = 40;
-  const LONG_TERM_DISCOUNT = 50;
-  const LONG_TERM = 7;
-  const SHORT_TERM_DISCOUNT = 20;
-  const SHORT_TERM = 3;
+  const PAYMENT = RENTAL_PAY * days;
 
   if (days >= LONG_TERM) {
-    return RENTAL_PAY * days - LONG_TERM_DISCOUNT;
+    return PAYMENT - LONG_TERM_DISCOUNT;
   }
 
   if (days >= SHORT_TERM) {
-    return RENTAL_PAY * days - SHORT_TERM_DISCOUNT;
+    return PAYMENT - SHORT_TERM_DISCOUNT;
   }
 
-  return RENTAL_PAY * days;
+  return PAYMENT;
 }
 module.exports = calculateRentalCost;
