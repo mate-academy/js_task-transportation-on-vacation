5b072a90b7d0d6ebcb6df974b8a0969cab75d772
Vlad <vladyslav.vasylevych@gmail.com>
2024-08-28 20:18:45 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,23 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const BASE_COST = 40;
+  const LONG_DISCOUNT = 50;
+  const SHORT_DISCOUNT = 20;
+  const LONG_VACATION = 7;
+  const SHORT_VACATION = 3;
+
+  const totalCost = days * BASE_COST;
+
+  if (days >= LONG_VACATION) {
+    return totalCost - LONG_DISCOUNT;
+  }
+
+  if (days >= SHORT_VACATION) {
+    return totalCost - SHORT_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
