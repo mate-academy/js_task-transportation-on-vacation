e72ad4c5ff83e406fe368479d02b399226762b39
Sholudyvyy <stashohulia@gmail.com>
2024-08-28 17:17:54 +0300

    Move consts outside and make veriable for all rental const

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,13 +3,13 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const RENTAL_COST = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
+const RENTAL_COST = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
 
+function calculateRentalCost(days) {
   if (days >= LONG_TERM) {
     return RENTAL_COST * days - LONG_TERM_DISCOUNT;
   }
@@ -18,7 +18,9 @@ function calculateRentalCost(days) {
     return RENTAL_COST * days - SHORT_TERM_DISCOUNT;
   }
 
-  return RENTAL_COST * days;
+  const allRentalCost = RENTAL_COST * days;
+
+  return allRentalCost;
 }
 
 module.exports = calculateRentalCost;
