e0b706423fcebb55380c11a1193e22c3b6e5dcf2
missk <rostock.vasiljev@gmail.com>
2024-08-29 10:51:38 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const cost = 40;
+  let result = 0;
+
+  if (days < 3) {
+    result = days * cost;
+  }
+
+  if (days >= 3) {
+    result = days * cost - 20;
+  }
+
+  if (days >= 7) {
+    result = days * cost - 50;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
