e72db149f9ed63cedb943052c1521767985ec034
missk <rostock.vasiljev@gmail.com>
2024-08-29 11:18:02 +0300

    fix2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,18 +5,21 @@
  */
 function calculateRentalCost(days) {
   const cost = 40;
+
+  const basecost = days * cost;
+
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
 
   if (days >= LONG_TERM) {
-    return days * cost - LONG_TERM_DISCOUNT;
+    return basecost - LONG_TERM_DISCOUNT;
   } else if (days >= SHORT_TERM) {
-    return days * cost - SHORT_TERM_DISCOUNT;
+    return basecost - SHORT_TERM_DISCOUNT;
   }
 
-  return days * cost;
+  return basecost;
 }
 
 module.exports = calculateRentalCost;
