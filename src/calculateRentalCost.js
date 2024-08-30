a80f51dcf73438d6a7555ef5d3dd4d4a99210fd8
NazarHaida <nazar.haida.knm.2020@lpnu.ua>
2024-08-30 16:05:23 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let res = 0;
+
+  for (let i = 0; i < days; i++) {
+    res += 40;
+  }
+
+  if (days >= 7) {
+    res -= 50;
+  } else if (days >= 3) {
+    res -= 20;
+  }
+
+  return res;
 }
 
 module.exports = calculateRentalCost;
