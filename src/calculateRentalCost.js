7d4022aa63ebacd1d5545909c0f9bed3b0f2e5b2
Mariia Bulchak <bulchakmariiast@gmail.com>
2024-08-31 12:56:48 +0300

    fix from comments

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,13 +3,14 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM = 3;
-  const SHORT_TERM_DISCOUNT = 20;
 
+const PRICE_PER_DAY = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM = 3;
+const SHORT_TERM_DISCOUNT = 20;
+
+function calculateRentalCost(days) {
   const basePrice = days * PRICE_PER_DAY;
 
   if (days >= LONG_TERM) {
