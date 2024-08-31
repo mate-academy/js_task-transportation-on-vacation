88f53e9f4bb9b4f3f2f426f433634c100067f70d
Alina <alina220380@gmail.com>
2024-08-31 17:03:58 +0300

    function implementation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentDay = 40;
+  let fullAmount = 0;
+
+  for (let i = 1; i <= days; i++) {
+    fullAmount += rentDay;
+  }
+
+  if (days >= 7) {
+    fullAmount = fullAmount - 50;
+
+    return fullAmount;
+  }
+
+  if (days >= 3) {
+    fullAmount = fullAmount - 20;
+
+    return fullAmount;
+  }
+
+  return fullAmount;
 }
 
 module.exports = calculateRentalCost;
