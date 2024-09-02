29532ac490407f60194ea91abab073d7df1d0d1f
Misha Hrynko <glukym@gmail.com>
2024-09-02 20:33:48 +0300

    Update test.yml

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,18 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const dayCosts = 40;
+  let totalAmount = days * dayCosts;
+
+  if (days >= 7) {
+    totalAmount -= 50;
+  } else {
+    if (days >= 3) {
+      totalAmount -= 20;
+    }
+  }
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
