8b91dd4adfabbfe4ea75b6c65413c0dee94858f3
Yuriy Bilskiy <yuriy.bilskiy@gmail.com>
2024-09-02 15:34:30 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const discountOfSevenDays = 50;
+  const discountOfThreeOrMoreDays = 20;
+  const total = days * 40;
 
+  if (days >= 7) {
+    return total - discountOfSevenDays;
+  }
+
+  if (days >= 3 || days >= 6) {
+    return total - discountOfThreeOrMoreDays;
+  }
+
+  return 80;
+}
 module.exports = calculateRentalCost;
