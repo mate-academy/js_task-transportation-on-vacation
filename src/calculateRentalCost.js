e94949f50f51a872f817ece412074f987005067a
luckezQA <labchuk2007@gmail.com>
2024-09-03 22:02:29 +0300

    hometask

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,20 @@
+'use strict';
+
 /**
  * @param {number} days
- *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyRate = 40;
+  let totalCost = days * dailyRate;
+
+  if (days >= 7) {
+    totalCost -= 50; // скидка $50, если 7 и более дней
+  } else if (days >= 3) {
+    totalCost -= 20; // скидка $20, если 3 и более дней
+  }
+
+  return totalCost;
 }
 
-module.exports = calculateRentalCost;
+module.exports = calculateRentalCost; // CommonJS синтаксис экспорта
