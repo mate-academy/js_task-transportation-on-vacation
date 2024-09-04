f04d1ab746d572c70f450bd33911cb0b78e26b16
Jose Langoni <zecalangoni@gmail.com>
2024-09-04 18:56:01 +0100

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -11,21 +11,15 @@ function calculateRentalCost(days) {
 
   const totalRentPrice = days * dailyRent;
 
-  if (days === 0) {
-    return 0;
-  }
-
   if (days < 3) {
     return totalRentPrice;
   }
 
-  if (days >= 3 && days < 7) {
+  if (days < 7) {
     return totalRentPrice - shortTermDiscount;
   }
 
-  if (days >= 7) {
-    return totalRentPrice - longTermDiscount;
-  }
+  return totalRentPrice - longTermDiscount;
 }
 
 module.exports = calculateRentalCost;
