dd5b63a7dc9f171665eeb97220d93f2de016fe3b
Maxim Berestovyi <maxl.beresto2019@gmail.com>
2024-09-04 17:48:07 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyRate = 40;
+  let totalCost = days * dailyRate;
+  const minDiscont = 20;
+  const maxDiscont = 50;
+  const minDays = 3;
+  const maxDays = 7;
+
+  if (days >= maxDays) {
+    totalCost -= maxDiscont;
+  } else if (days >= minDays) {
+    totalCost -= minDiscont;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
