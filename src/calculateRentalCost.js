7fa84b1e5f758e7b66e053fc6978832697e56586
Orest <orestgabrus3@gmail.com>
2024-09-04 19:04:26 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rent = 40;
+  const minDiscount = 20;
+  const maxDiscount = 50;
+  const minTerm = 3;
+  const longTerm = 7;
+
+  if (days >= longTerm) {
+    return rent * days - maxDiscount;
+  }
+
+  if (days >= minTerm) {
+    return rent * days - minDiscount;
+  }
+
+  return rent * days;
 }
 
 module.exports = calculateRentalCost;
