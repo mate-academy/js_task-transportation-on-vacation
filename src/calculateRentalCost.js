84f15b6f30f1256691fca1bc9da8dbdbbbb69046
Anastasiia Cherednik <cherednikanastasiia@gmail.com>
2024-09-04 12:52:29 +0300

    Fixes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,13 +7,13 @@ function calculateRentalCost(days) {
   const dailyRate = 40;
   let totalCost = days * dailyRate;
 
-  if (days < 3) {
-    return totalCost;
-  } else if (days >= 3 && days <= 6) {
-    return (totalCost = totalCost - 20);
-  } else if (days >= 7) {
-    return (totalCost = totalCost - 50);
+  if (days >= 7) {
+    totalCost -= 50;
+  } else if (days >= 3) {
+    totalCost -= 20;
   }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
