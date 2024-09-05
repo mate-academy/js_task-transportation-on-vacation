4baf264ab9de80b4314187af2bd78bd2a1b1b672
iryna112 <zlata120707@gmail.com>
2024-09-05 21:31:28 +0300

    Update3

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,13 +5,13 @@
  */
 function calculateRentalCost(days) {
   // write code here
- const PRICE_PER_DAY = 40;
+const PRICE_PER_DAY = 40;
 const LONG_TERM = 7;
 const LONG_TERM_DISCOUNT = 50;
 const SHORT_TERM = 3;
 const SHORT_TERM_ DISCOUNT = 20;
 let basePrice = days * PRICE_PER_DAY;
- if (days >= LONG_TERM {
+ if (days >= LONG_TERM) {
    return basePrice - LONG_TERM_DISCOUNT
  }
   if (days >= SHORT_TERM) {
