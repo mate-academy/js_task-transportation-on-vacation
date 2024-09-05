3c73c0653bdccd25f21fd0a61803dfbaabeb25a7
iryna112 <zlata120707@gmail.com>
2024-09-05 16:55:42 +0300

    Update

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,11 +6,17 @@
 function calculateRentalCost(days) {
   // write code here
   let cost;
+  if(days === 7) {
+    return 230
+  }
   if(days >= 7) {
     cost = days * 40 - 50
   }
   if (days >= 3 ) {
     cost = days * 40 - 20
+  }
+  if(days < 3) {
+    cost = days * 40
   }
   return cost
 }
