effd5445f09bbe9ab0a2d6dd70d9782e5e29a7e3
EqVert <EqVert@gmail.com>
2024-09-06 22:48:12 +0300

    Solutin fix1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,14 +5,19 @@
  */
 function calculateRentalCost(days) {
   let sale = 0;
+  const PRICE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
-    sale = 50;
-  } else if (days >= 3) {
-    sale = 20;
+  if (days >= LONG_TERM) {
+    sale = LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    sale = SHORT_TERM_DISCOUNT;
   }
 
-  return days * 40 - sale;
+  return days * PRICE - sale;
 }
 
 module.exports = calculateRentalCost;
