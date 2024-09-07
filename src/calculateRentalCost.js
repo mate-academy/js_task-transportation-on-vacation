f3be4b6d22232304217bf9b4921d0c7057ecf2e3
Vlad Chudin <itcourses4325@gmail.com>
2024-09-07 12:26:19 +0300

    succsesjava

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+  const calcCount = pricePerDay * days;
+  const sailForThreeMore = 20;
+  const sailForSevenMore = 50;
+  const longPeriod = 7;
+  const shortPeriod = 3;
+
+  if (days >= longPeriod) {
+    return calcCount - sailForSevenMore;
+  }
+
+  if (days >= shortPeriod) {
+    return calcCount - sailForThreeMore;
+  }
+
+  if (days > 0) {
+    return calcCount;
+  }
 }
 
 module.exports = calculateRentalCost;
