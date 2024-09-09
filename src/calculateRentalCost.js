e22797a6a10087dbc4d0049bb43edb4f199681e0
Ivan Vaniuk <ivanvanuk77@gmail.com>
2024-09-09 16:47:18 +0300

    Done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_COST = 40;
+  const DISCOUNT_FOR_THREE_DAYS = 20;
+  const DISCOUNT_FOR_SEVEN_DAYS = 50;
+
+  let totalCost = days * DAILY_COST;
+
+  if (days >= 7) {
+    totalCost -= DISCOUNT_FOR_SEVEN_DAYS;
+  } else if (days >= 3) {
+    totalCost -= DISCOUNT_FOR_THREE_DAYS;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
