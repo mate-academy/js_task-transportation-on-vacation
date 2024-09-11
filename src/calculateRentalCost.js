c382c87c38572ea0a42e5165e741ddb09516186a
Mykola <mykola.stanj@gmail.com>
2024-09-11 16:03:22 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dayCost = 40;
+  const result = days * dayCost;
+  const shortTerm = 3;
+  const shortTermDisc = 20;
+  const longTerm = 7;
+  const longTermDisc = 50;
+
+  if (days >= longTerm) {
+    return result - longTermDisc;
+  }
+
+  if (days >= shortTerm) {
+    return result - shortTermDisc;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
