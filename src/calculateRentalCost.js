4bb710b0ad2da860cd228ad5a9a3b59aa917731d
unknown <yuli4ka.lashch98@gmail.com>
2024-09-12 19:22:09 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const threeDayDiscount = 20;
+  const sevenDayDiscount = 50;
+  const basePrice = 40;
+
+  if (days >= 7) {
+    return days * basePrice - sevenDayDiscount;
+  }
+
+  if (days >= 3) {
+    return days * basePrice - threeDayDiscount;
+  }
+
+  return days * basePrice;
 }
 
 module.exports = calculateRentalCost;
