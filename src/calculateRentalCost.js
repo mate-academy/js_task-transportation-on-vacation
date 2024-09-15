e44fed28b644b70e9e9e47cfed0322cdc7c56e48
Nikita <onishenkonikitos@gmail.com>
2024-09-15 02:55:24 +0200

    fix bags

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,30 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let sum = 0;
   const costCar = 40;
+  let sum = costCar * days;
   const threeDaysDiscount = 20;
   const sevenDaysDiscount = 50;
 
   if (days < 3) {
-    sum = costCar * days;
-
     return sum;
   }
 
-  if (days >= 3 && days <= 6) {
-    sum = costCar * days - threeDaysDiscount;
+  if (days < 7) {
+    sum -= threeDaysDiscount;
 
     return sum;
   }
 
   if (days >= 7) {
-    sum = costCar * days - sevenDaysDiscount;
+    sum -= sevenDaysDiscount;
 
     return sum;
   }
-
-  return sum;
 }
 
 module.exports = calculateRentalCost;
