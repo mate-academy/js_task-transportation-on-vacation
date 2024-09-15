d86bd2af2a79844f038f6401e59f7c36349a8ed6
Maxim Reznichenko <kuplin7@gmail.com>
2024-09-15 15:45:23 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,15 +1,22 @@
 function calculateRentalCost(days) {
-  let totalCost = days * 40;
+  const DAILY_RATE = 40;           // Фиксированная стоимость аренды за день
+  const BIG_DISCOUNT = 50;         // Скидка при аренде 7 и более дней
+  const SMALL_DISCOUNT = 20;       // Скидка при аренде от 3 до 6 дней
+  const LONG_TERM_RENTAL_DAYS = 7; // Дней для большой скидки
+  const MID_TERM_RENTAL_DAYS = 3;  // Дней для средней скидки
 
-  if (days >= 7) {
-    totalCost -= 50;
+  const totalCost = days * DAILY_RATE; // Общая стоимость аренды
+
+  // Применяем скидки и возвращаем стоимость
+  if (days >= LONG_TERM_RENTAL_DAYS) {
+    return totalCost - BIG_DISCOUNT;
   }
 
-  if (days >= 3 && days < 7) {
-    totalCost -= 20;
+  if (days >= MID_TERM_RENTAL_DAYS) {
+    return totalCost - SMALL_DISCOUNT;
   }
 
-  return totalCost;
+  return totalCost; // Если арендовано меньше 3 дней скидка не применяется
 }
 
 module.exports = calculateRentalCost;
