8b93673635b615cc19935d949a340e047b94267b
Nikita Slobodian <nicaly15117@gmail.com>
2024-09-16 15:22:49 +0300

    Solution-3

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,22 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  // write code here
   const oneDayRent = 40;
   const discountMoreSevenDays = 50;
   const discountMoreThreeDays = 20;
 
-  let sumRent = 0;
-
   if (days >= 7) {
-    sumRent = oneDayRent * days - discountMoreSevenDays;
-
-    return sumRent;
+    return oneDayRent * days - discountMoreSevenDays;
   }
 
   if (days >= 3) {
-    sumRent = oneDayRent * days - discountMoreThreeDays;
-
-    return sumRent;
+    return oneDayRent * days - discountMoreThreeDays;
   }
 
   return oneDayRent * days;
