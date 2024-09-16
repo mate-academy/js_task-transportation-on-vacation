6a6998dabb9a9da27774005f34a0c55bc9352c4c
Alona Loik <crazybeelviv@gmail.com>
2024-09-16 10:43:22 -0400

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentPerDay = 40;
+  const advanceDiscount = 50;
+  const regularDiscount = 20;
+  const res = rentPerDay * days;
+
+  if (days > 2) {
+    return days > 6 ? res - advanceDiscount : res - regularDiscount;
+  }
+
+  return res;
 }
 
 module.exports = calculateRentalCost;
