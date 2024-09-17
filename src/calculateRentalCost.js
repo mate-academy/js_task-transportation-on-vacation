7577ecccf28edad46487e39cb7445ff46b255559
Anna Tuhusova <bilachayka@mail.com>
2024-09-17 20:40:31 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,16 +9,17 @@ function calculateRentalCost(days) {
   const SHORT_TERM_DISCOUNT = 20;
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
+  const basePrice = days * dayPrice;
 
   if (days < SHORT_TERM) {
-    return days * dayPrice;
+    return basePrice;
   }
 
   if (days >= LONG_TERM) {
-    return days * dayPrice - LONG_TERM_DISCOUNT;
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  return days * dayPrice - SHORT_TERM_DISCOUNT;
+  return basePrice - SHORT_TERM_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
