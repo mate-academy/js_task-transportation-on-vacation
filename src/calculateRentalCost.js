cab8399f415879043e4f299fe3f958825faf122b
LX-y-ka <alexandravirtualife@gmail.com>
2024-09-17 23:00:54 +0300

    add constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,17 +3,24 @@
  *
  * @return {number}
  */
+
+const RENT_COST = 40;
+const LONG_TERM_RENTAL = 7;
+const LONG_TERM_RENTAL_DISCOUNT = 50;
+const SHORT_TERM_RENTAL = 3;
+const SHORT_TERM_RENTAL_DISCOUNT = 20;
+
 function calculateRentalCost(days) {
   // write code here
-  if (days >= 7) {
-    return days * 40 - 50;
+  if (days >= LONG_TERM_RENTAL) {
+    return days * RENT_COST - LONG_TERM_RENTAL_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return days * 40 - 20;
+  if (days >= SHORT_TERM_RENTAL) {
+    return days * RENT_COST - SHORT_TERM_RENTAL_DISCOUNT;
   }
 
-  return days * 40;
+  return days * RENT_COST;
 }
 
 module.exports = calculateRentalCost;
