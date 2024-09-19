aeb4c5f4b9b9c45ed3b02c327557aa9474ce3d14
Tetiana Sokolova <TanitaTei@gmail.com>
2024-09-19 16:25:39 +0300

    calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,15 +6,16 @@
  */
 
 function calculateRentalCost(days) {
-  let price = days * 40;
+  const dailyRate = 40;
+  let totalCost = days * dailyRate;
 
   if (days >= 7) {
-    price -= 50;
+    totalCost -= 50;
   } else if (days >= 3) {
-    price -= 20;
+    totalCost -= 20;
   }
 
-  return price;
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
