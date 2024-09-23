2e19857e39c0cc6d56504bab24cb57858c7957d6
Iryna Mariiko <IrynaMM497@gmail.com>
2024-09-23 18:22:25 +0300

    task done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,13 +6,17 @@
 function calculateRentalCost(days) {
   let carCost = 0;
   const costForDays = days * 40;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const mediumTerm = 3;
+  const mediumTermDiscount = 20;
 
   if (days >= 1 && days <= 2) {
     carCost = costForDays;
-  } else if (days >= 3 && days <= 6) {
-    carCost = costForDays - 20;
-  } else if (days >= 7) {
-    carCost = costForDays - 50;
+  } else if (days >= mediumTerm && days <= 6) {
+    carCost = costForDays - mediumTermDiscount;
+  } else if (days >= longTerm) {
+    carCost = costForDays - longTermDiscount;
   }
 
   return carCost;
