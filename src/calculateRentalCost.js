3be20c68d7a2722fd569d60b16b22ae6bce0ab81
Iryna Mariiko <IrynaMM497@gmail.com>
2024-09-23 16:49:26 +0300

    task done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let carCost = 0;
+  const costForDays = days * 40;
+
+  if (days >= 1 && days <= 2) {
+    carCost = costForDays;
+  } else if (days >= 3 && days <= 6) {
+    carCost = costForDays - 20;
+  } else if (days >= 7) {
+    carCost = costForDays - 50;
+  }
+
+  return carCost;
 }
 
 module.exports = calculateRentalCost;
