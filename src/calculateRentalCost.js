6b2ee38f28264c4e48c55554a780232243ec2992
Egor Kurinnoy <Egor Kurinnoy>
2024-09-23 15:18:55 +0300

    new

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,16 @@
  */
 function calculateRentalCost(days) {
   // write code here
+
+  const sum = 40 * days;
+
+  if (sum < 120) {
+    return sum;
+  } else if (sum >= 280) {
+    return sum - 50;
+  } else if (sum >= 120) {
+    return sum - 20;
+  }
 }
 
 module.exports = calculateRentalCost;
