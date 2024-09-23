31b3269de214091ac3b2cc60088ea7578fc60ff7
Iryna Mariiko <IrynaMM497@gmail.com>
2024-09-23 18:44:35 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,22 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  let carCost = 0;
   const costForDays = days * 40;
   const longTerm = 7;
   const longTermDiscount = 50;
   const mediumTerm = 3;
   const mediumTermDiscount = 20;
 
-  if (days >= 1 && days <= 2) {
-    carCost = costForDays;
-  } else if (days >= mediumTerm && days <= 6) {
-    carCost = costForDays - mediumTermDiscount;
-  } else if (days >= longTerm) {
-    carCost = costForDays - longTermDiscount;
+  if (days >= mediumTerm && days <= 6) {
+    return costForDays - mediumTermDiscount;
   }
 
-  return carCost;
+  if (days >= longTerm) {
+    return costForDays - longTermDiscount;
+  }
+
+  return costForDays;
 }
 
 module.exports = calculateRentalCost;
