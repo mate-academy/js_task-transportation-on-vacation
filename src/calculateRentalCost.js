ecd4d4c5684d880c398f23e7f072e374b1692d86
ab3MN <ab3mn3@gmail.com>
2024-09-25 08:06:00 +0300

    rafactor:replaced magic numbers to constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const price = 40;
-  let total = days * price;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const PRICE_PER_DAY = 40;
 
-  if (days >= 7) {
-    total -= 50;
-  } else if (days >= 3) {
-    total -= 20;
+  let totalPrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    totalPrice -= LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    totalPrice -= SHORT_TERM_DISCOUNT;
   }
 
-  return total;
+  return totalPrice;
 }
 
 module.exports = calculateRentalCost;
