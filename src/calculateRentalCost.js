3b3d188bb328f1ab6a3738d11e3c922bd5d346b0
Aman Mishra <39798790+mishra-aman@users.noreply.github.com>
2024-09-27 17:10:25 +0530

    Solution for calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,19 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const rent = 40;
+  let totalRentAmount = 0;
+
+  if (days >= 7) {
+    totalRentAmount += rent * days - 50;
+  } else if (days >= 3) {
+    totalRentAmount += rent * days - 20;
+  } else if (days < 3) {
+    totalRentAmount += rent * days;
+  }
+
+  return totalRentAmount;
 }
 
 module.exports = calculateRentalCost;
+git a
