43370af458d2cfdde4ca91f068dfbe1b79936348
Dmytro_Krylov <dmutrokrulov@gmail.com>
2024-09-29 12:51:55 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rental = 40;
+  const longTerm = 7;
+  const shortTerm = 3;
+  const totalPrise = days * rental;
+
+  if (days < shortTerm) {
+    return totalPrise;
+  } else if (days >= shortTerm && days < longTerm) {
+    return totalPrise - 20;
+  }
+
+  return totalPrise - 50;
 }
 
 module.exports = calculateRentalCost;
