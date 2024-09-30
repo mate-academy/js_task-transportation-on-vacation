3006dbce43b63e5f09310cb2d4d586af3699521d
Dominik Dudek <dom-dud774@o2.pl>
2024-09-30 16:36:07 +0200

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let rentalCost = 40 * days;
+
+  if (days >= 7) {
+    rentalCost = rentalCost - 50;
+  } else if (days >= 3) {
+    rentalCost = rentalCost - 20;
+  }
+
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
