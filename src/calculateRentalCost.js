3716f74e6ea00e6ea18c9c56c1f30b837ad92d3a
DmytroHoncharuk <dima2005oleksandr@gmail.com>
2024-10-01 23:35:47 +0300

    first version

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,25 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const DAY_PAYMENT = 40;
+  const MIDDLE_TERM = 3;
+  const MIDDLE_TERM_DISCOUNT = 20;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const TOTAL_PRICE = days * DAY_PAYMENT;
+
+  if (days >= LONG_TERM) {
+    return TOTAL_PRICE - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MIDDLE_TERM) {
+    return TOTAL_PRICE - MIDDLE_TERM_DISCOUNT;
+  }
+
+  return TOTAL_PRICE;
 }
 
 module.exports = calculateRentalCost;
