aae03dbbc5c1fe712a20f1b3a0a6c9afd2b496bc
Roman <romanprykhodkouae@mail.com>
2024-10-01 19:48:40 +0400

    task1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,24 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  // write code here
+function calculateRentalCost(Days) {
+  const DAILY_RENTAL_RATE = 40;
+  const LONG_TERM_RENTAL_DAYS = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM_RENTAL_DAYS = 3;
+  const MID_TERM_DISCOUNT = 20;
+
+  const basePrice = Days * DAILY_RENTAL_RATE;
+
+  if (Days >= LONG_TERM_RENTAL_DAYS) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (Days >= MID_TERM_RENTAL_DAYS) {
+    return basePrice - MID_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
