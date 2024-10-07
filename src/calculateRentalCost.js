a4b3e09e06ce691a37205d4614a3342d9af59c22
Alexander Gumeniuk <alexgumeniukv@gmail.com>
2024-10-07 19:12:15 +0300

    added solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const COST_RENT_CAR = 40;
+  const BIG_DISCOUNT = 50;
+  const LESS_DISCOUNT = 20;
 
+  const price = days * COST_RENT_CAR;
+
+  if (days >= 7) {
+    return price - BIG_DISCOUNT;
+  }
+
+  if (days >= 3) {
+    return price - LESS_DISCOUNT;
+  }
+
+  return price;
+}
 module.exports = calculateRentalCost;
