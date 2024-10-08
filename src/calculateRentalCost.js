92f4f5dc87b4e82af4497984f863dd9b50918da7
Matheus Mota <matheusmgf2010@gmail.com>
2024-10-08 19:22:00 -0300

    exercise resolution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,28 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const dailyRate = 40;
+  let totalCost = days * dailyRate;
+
+  if (days >= 7) {
+    totalCost -= 50;
+  } else if (days >= 3) {
+    totalCost -= 20;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
+
+/*
+Every day you rent the car costs $40. If you rent the
+car for 7 or more days, you get $50 off your total.
+Alternatively, if you rent the car for 3 or more days,
+you get $20 off your total.
+
+Implement calculateRentalCost function that returns
+the total amount for different number of days.
+*/
