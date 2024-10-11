31aacbf3c77fb1dcb5df67546b40dd13490f2b9d
Anton Reynov <re1yn9524@gmail.com>
2024-10-11 22:54:30 +0100

    fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,20 +3,18 @@
  *
  * @return {number}
  */
+const RENT_PRICE = 40;
+const DISCOUNT_AFTER_DAY_7 = 50;
+const DISCOUNT_AFTER_DAY_3 = 20;
 function calculateRentalCost(days) {
-  const RENT_PRICE = 40;
-  const DAY_7 = 7;
-  const DAY_3 = 3;
-  const DISCOUNT_AFTER_DAY_7 = 50;
-  const DISCOUNT_AFTER_DAY_3 = 20;
-  let rent = RENT_PRICE * days;
+  const rent = RENT_PRICE * days;
 
-  if (days >= DAY_7) {
-    rent -= DISCOUNT_AFTER_DAY_7;
-  } else if (days >= DAY_3) {
-    rent -= DISCOUNT_AFTER_DAY_3;
+  if (days >= 7) {
+    return rent -DISCOUNT_AFTER_DAY_7;
+  } else if (days >= 3) {
+    return rent - DISCOUNT_AFTER_DAY_3;
   }
   return rent;
+
 }
 module.exports = calculateRentalCost;
- 
