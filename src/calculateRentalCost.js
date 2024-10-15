d919053accd31449c5618f5452db2d712a89aa39
Iryna <irakogut23190@gmail.com>
2024-10-15 14:00:30 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,10 +3,14 @@
  *
  * @return {number}
  */
+
+const seven = 7;
+const three = 3;
+
 function calculateRentalCost(days) {
-  if (days >= 7) {
+  if (days >= seven) {
     return days * 40 - 50;
-  } else if (days >= 3) {
+  } else if (days >= three) {
     return days * 40 - 20;
   } else {
     return 80;
