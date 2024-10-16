66d0d14aebc67cfff84e88e8c167594fd7ab8863
Diana <dianka18.skrupnuk@gmail.com>
2024-10-16 08:12:02 +0300

    Solution2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,12 +5,16 @@
  */
 function calculateRentalCost(days) {
   const everyDayRent = 40;
+  const discountFor7Days = 50;
+  const discountFor3Days = 20;
+  const minDaysFor7DayDiscount = 7;
+  const minDaysFor3DayDiscount = 3;
   let totalSum = everyDayRent * days;
 
-  if (days >= 7) {
-    totalSum = everyDayRent * days - 50;
-  } else if (days >= 3) {
-    totalSum = everyDayRent * days - 20;
+  if (days >= minDaysFor7DayDiscount) {
+    totalSum = everyDayRent * days - discountFor7Days;
+  } else if (days >= minDaysFor3DayDiscount) {
+    totalSum = everyDayRent * days - discountFor3Days;
   }
 
   return totalSum;
