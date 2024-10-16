f084e838c3558e80fefb04f486f307391e71c229
OkMoroz <k.moroz.mail@gmail.com>
2024-10-16 10:10:31 +0300

    fixed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,21 +3,22 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const DAILY_COST = 40;
-  const MANY_DAYS = 7;
-  const BIG_DISCOUNT = 50;
-  const FEW_DAYS = 3;
-  const SMALL_DISCOUNT = 20;
 
-  let totalCost = DAILY_COST * days;
+const DAILY_COST = 40;
+const MANY_DAYS = 7;
+const BIG_DISCOUNT = 50;
+const FEW_DAYS = 3;
+const SMALL_DISCOUNT = 20;
+
+function calculateRentalCost(days) {
+  const totalCost = DAILY_COST * days;
 
   if (days >= MANY_DAYS) {
-    return (totalCost = totalCost - BIG_DISCOUNT);
+    return totalCost - BIG_DISCOUNT;
   }
 
   if (days >= FEW_DAYS) {
-    return (totalCost = totalCost - SMALL_DISCOUNT);
+    return totalCost - SMALL_DISCOUNT;
   }
 
   return totalCost;
