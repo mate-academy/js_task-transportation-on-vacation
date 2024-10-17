36ecb3ba960735d4d59c47ba6e75cbd302f2fa80
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-10-17 17:02:46 +0300

    calculate rent

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const RENT_FOR_DAY = 40;
+  const THREE_DAYS_DISCOUNT = 20;
+  const SEVEN_DAYS_DISCOUNT = 50;
+
+  const THREE_DAYS = 3;
+  const SEVEN_DAYS = 7;
+
+  if (days < THREE_DAYS) {
+    return days * RENT_FOR_DAY;
+  }
+
+  if (days < SEVEN_DAYS) {
+    return days * RENT_FOR_DAY - THREE_DAYS_DISCOUNT;
+  }
+
+  return days * RENT_FOR_DAY - SEVEN_DAYS_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
