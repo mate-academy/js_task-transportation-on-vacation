bf42ca13b41636547a438319eecf30bed7ae641c
Oleksandr Lysunenko <avlisunenko@mail.com>
2024-10-20 14:37:51 +0200

    add const value

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,14 +5,22 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
   const dailyRent = 40;
-  let totalCost = days * dailyRent;
+  const longDays = 7;
+  const longDiscount = 50;
+  const mediumDays = 3;
+  const mediumDiscount = 20;
 
-  if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+  const totalCost = days * dailyRent;
+
+  if (days >= longDays) {
+    return totalCost - longDiscount;
+  }
+
+  if (days >= mediumDays) {
+    return totalCost - mediumDiscount;
   }
 
   return totalCost;
