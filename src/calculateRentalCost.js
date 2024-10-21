c79d494d244908d5c9cac8f4601b257c7d1b4aa0
Max Bolotskyi <max.bolockyi01@gmail.com>
2024-10-21 21:46:23 +0200

    add task solotion

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,25 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const RENTAL_CAR_COST = 40;
+
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const MEDIUM_TERM = 3;
+  const MEDIUM_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    return days * RENTAL_CAR_COST - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MEDIUM_TERM) {
+    return days * RENTAL_CAR_COST - MEDIUM_TERM_DISCOUNT;
+  }
+
+  return days * RENTAL_CAR_COST;
 }
 
 module.exports = calculateRentalCost;
