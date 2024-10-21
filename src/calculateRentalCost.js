e44efb84fd61c7c5c93a03ac4e966e87014a32dd
Alex Shmyhelskyi <shmyhelskyi.o.v@gmail.com>
2024-10-21 15:24:56 +0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyRentalCar = 40;
+  let discount = 0;
+
+  if (days >= 3 && days <= 6) {
+    discount = 20;
+  } else if (days >= 7) {
+    discount = 50;
+  }
+
+  const totalCost = dailyRentalCar * days - discount;
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
