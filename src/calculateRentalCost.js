e88dbdcfd9885eeb208781b4628323a80d5ff5f3
mgucma <marek.gucma@gmail.com>
2024-10-21 04:08:51 +0200

    Solution fix

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -9,7 +9,10 @@ function calculateRentalCost(days) {
 
   if (days >= 7) {
     totalCost -= 50; // Apply $50 discount for 7 or more days
-  } else if (days >= 3) {
+    return totalCost;
+  }
+
+  if (days >= 3) {
     totalCost -= 20; // Apply $20 discount for 3 to 6 days
   }
 
