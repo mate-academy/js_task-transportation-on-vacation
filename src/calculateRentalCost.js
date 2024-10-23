3fcb674944b24d52103d48bb05bc2668ce205665
Edilbek <savchuk8897@gmail.com>
2024-10-23 19:46:39 +0300

    calculateRentalCost solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,25 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  const DAY_PRICE = 40;
+
+  const finalPrice = days * DAY_PRICE;
+
+  if (days >= LONG_TERM) {
+    return finalPrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return finalPrice - SHORT_TERM_DISCOUNT;
+  }
+
+  return finalPrice;
 }
 
 module.exports = calculateRentalCost;
