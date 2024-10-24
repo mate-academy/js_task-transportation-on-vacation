25dfd26e404568f334ea60aca3ff77d3096dcddd
Inga <ingatkachuk8@gmail.com>
2024-10-24 15:53:31 +0500

    Inga Tkachuk add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const carPrice = 40;
+  let result = 0;
+
+  if (days >= 7) {
+    result = days * carPrice - 50;
+
+    return result;
+  }
+
+  if (days >= 3) {
+    result = days * carPrice - 20;
+
+    return result;
+  } else {
+    return 80;
+  }
 }
 
 module.exports = calculateRentalCost;
