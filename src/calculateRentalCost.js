ee0df16ecd21ca4c4125c264bbe42680f85d30c2
Danylo Kachaniuk <logistik2004@ukr.net>
2024-10-25 12:00:05 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const carCost = 40;
+  const totalSpend = carCost * days;
+
+  if (days > 6) {
+    return totalSpend - 50;
+  }
+
+  if (days > 2) {
+    return totalSpend - 20;
+  }
+
+  return totalSpend;
 }
 
 module.exports = calculateRentalCost;
