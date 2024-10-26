c0102b197780c36d9a6961f1058f34846ca22639
Anna Chovnova <annachovnova@ukr.net>
2024-10-26 20:50:22 +0300

    change variable

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,19 +5,21 @@
  */
 function calculateRentalCost(days) {
   const DAY_RENT = 40;
-  const TOTAL_RENT = DAY_RENT * days;
   const LONG_TERM = 7;
+  const BASE_RENT = DAY_RENT * days;
   const LONG_TERM_DISCOUNT = 50;
   const MIDL_TERM = 3;
   const MIDL_TERM_DISCOUNT = 20;
 
   if (days >= LONG_TERM) {
-    return TOTAL_RENT - LONG_TERM_DISCOUNT;
-  } else if (days >= MIDL_TERM) {
-    return TOTAL_RENT - MIDL_TERM_DISCOUNT;
+    return BASE_RENT - LONG_TERM_DISCOUNT;
   }
 
-  return TOTAL_RENT;
+  if (days >= MIDL_TERM) {
+    return BASE_RENT - MIDL_TERM_DISCOUNT;
+  }
+
+  return BASE_RENT;
 }
 
 module.exports = calculateRentalCost;
