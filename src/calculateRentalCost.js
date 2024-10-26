4ce45ed12a87bc64acae84eb648eedb0bc03b389
Petro Boiarenko <vyb1313@gmail.com>
2024-10-26 15:12:12 +0300

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,24 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
 function calculateRentalCost(days) {
-  // write code here
+  const dayCosts = 40;
+  let totalAmount = days * dayCosts;
+  const longTermLoan = 7;
+  const longTermDiscount = 50;
+  const mediumTermLoan = 3;
+  const mediumTermDiscount = 20;
+
+  if (days >= longTermLoan) {
+    totalAmount -= longTermDiscount;
+
+    return totalAmount;
+  }
+
+  if (days >= mediumTermLoan) {
+    totalAmount -= mediumTermDiscount;
+
+    return totalAmount;
+  }
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
