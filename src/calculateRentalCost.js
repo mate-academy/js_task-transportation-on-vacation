c87df46dc70a87940db092c0d206e3ee5846c15b
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2024-10-27 14:08:25 +0100

    fix a typo

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,7 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const RCICE_PER_DAY = 40;
+  const PRICE_PER_DAY = 40;
 
   const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
@@ -13,13 +13,13 @@ function calculateRentalCost(days) {
   const MEDIUM_TERM_DISCOUNT = 20;
 
   if (days >= LONG_TERM) {
-    return days * RCICE_PER_DAY - LONG_TERM_DISCOUNT;
+    return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
   }
 
   if (days >= MEDIUM_TERM) {
-    return days * RCICE_PER_DAY - MEDIUM_TERM_DISCOUNT;
+    return days * PRICE_PER_DAY - MEDIUM_TERM_DISCOUNT;
   }
 
-  return days * RCICE_PER_DAY;
+  return days * PRICE_PER_DAY;
 }
 module.exports = calculateRentalCost;
