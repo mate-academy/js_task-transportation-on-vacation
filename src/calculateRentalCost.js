338b660734d87cae0330e69681edbbf8429acb2f
Ilya Ilchuk <Illya.Illchuk@gmail.com>
2024-10-28 16:00:06 +0200

    update solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,20 +5,21 @@
  */
 function calculateRentalCost(days) {
   const PRICE = 40;
+
+  const LONG_TERM = 7;
   const LONG_TERM_DISCOUNT = 50;
+
+  const SHORT_TERM = 3;
   const SHORT_TERM_DISCOUNT = 20;
-  let bestPrice = 0;
 
-  if (days < 3) {
-    bestPrice = PRICE * days;
+  const bestPrice = days * PRICE;
+
+  if (days >= LONG_TERM) {
+    return bestPrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    bestPrice = PRICE * days - SHORT_TERM_DISCOUNT;
-  }
-
-  if (days >= 7) {
-    bestPrice = PRICE * days - LONG_TERM_DISCOUNT;
+  if (days >= SHORT_TERM) {
+    return bestPrice - SHORT_TERM_DISCOUNT;
   }
 
   return bestPrice;
