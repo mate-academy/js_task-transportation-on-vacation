43022a26f6aade6da0e5eb4beb0610b271b14167
Ilya Ilchuk <Illya.Illchuk@gmail.com>
2024-10-28 15:04:49 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,24 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE = 40;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_DISCOUNT = 20;
+  let bestPrice = 0;
+
+  if (days < 3) {
+    bestPrice = PRICE * days;
+  }
+
+  if (days >= 3) {
+    bestPrice = PRICE * days - SHORT_TERM_DISCOUNT;
+  }
+
+  if (days >= 7) {
+    bestPrice = PRICE * days - LONG_TERM_DISCOUNT;
+  }
+
+  return bestPrice;
 }
 
 module.exports = calculateRentalCost;
