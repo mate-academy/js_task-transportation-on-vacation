d2a47d5b7283c6b8dd9289e0deffa1e45b2dad28
VitaliiShc <vitaliy.shchukin@gmail.com>
2024-10-28 14:13:48 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,25 @@
  *
  * @return {number}
  */
+
+const COST_PER_DAY = 40;
+const MIDDLE_PERIOD = 3;
+const MIDDLE_PERIOD_DISCOUNT = 20;
+const LONG_PERIOD = 7;
+const LONG_PERIOD_DISCOUNT = 50;
+
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCost = days * COST_PER_DAY;
+
+  if (days >= MIDDLE_PERIOD && days < LONG_PERIOD) {
+    return rentalCost - MIDDLE_PERIOD_DISCOUNT;
+  }
+
+  if (days >= LONG_PERIOD) {
+    return rentalCost - LONG_PERIOD_DISCOUNT;
+  }
+
+  return rentalCost;
 }
 
 module.exports = calculateRentalCost;
