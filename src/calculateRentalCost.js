6433065fd1248a8b0d37e4cc66f530781d43cfa0
Vitalii Savenets <vitaliy.savenets@gmail.com>
2024-10-28 16:30:50 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyRentalCost = 40;
+  const discountForLongRental = 50;
+  const discountForMediumRental = 20;
+  const minimumDaysForLongRental = 7;
+  const minimumDaysForMediumRental = 3;
+
+  let cost = dailyRentalCost * days;
+
+  if (days >= minimumDaysForLongRental) {
+    cost -= discountForLongRental;
+  } else if (days >= minimumDaysForMediumRental) {
+    cost -= discountForMediumRental;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
