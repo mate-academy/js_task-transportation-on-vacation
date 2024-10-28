31e124a61da2499892ca2145565322dd4900d632
Bovt Dmytro <mr.bovt2004@gmail.com>
2024-10-28 10:41:56 +0200

    first

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const cost = 40;
+  let discount = 0;
+
+  if (days >= 7) {
+    discount = 50;
+  } else if (days >= 3) {
+    discount = 20;
+  }
+
+  return days * cost - discount;
 }
 
 module.exports = calculateRentalCost;
