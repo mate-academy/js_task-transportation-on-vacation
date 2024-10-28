6ea87b1cf24062225f9e9bb6811b7b9b08787185
Kostiantyn <kostyabondar6@gamil.com>
2024-10-28 17:27:05 +0200

    finish my task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
-}
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const DISCOUNT_LONG_TERM = 50;
+  const SHORT_TERM = 3;
+  const DISCOUNT_SHORT_TERM = 20;
 
+  if (days >= LONG_TERM) {
+    return days * PRICE_PER_DAY - DISCOUNT_LONG_TERM;
+  }
+
+  if (days >= SHORT_TERM) {
+    return days * PRICE_PER_DAY - DISCOUNT_SHORT_TERM;
+  }
+
+  return days * PRICE_PER_DAY;
+}
 module.exports = calculateRentalCost;
