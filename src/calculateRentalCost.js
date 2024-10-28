47cb17a5544b609a48b391f6141f8c47b0117283
Yana <anazigaluk@gmail.com>
2024-10-28 17:52:50 +0200

    calculate rental amount

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const pricePerDay = 40;
+  let totalSum = 0;
+
+  for (let day = 1; day <= days; day++) {
+    totalSum = totalSum + pricePerDay;
+  }
+
+  if (days >= 7) {
+    return totalSum - 50;
+  }
+
+  if (days >= 3) {
+    return totalSum - 20;
+  }
+
+  return totalSum;
 }
 
 module.exports = calculateRentalCost;
