9bbdb6922f0371b9760bc5a965ddae013d27d5ee
VitaliiShc <vitaliy.shchukin@gmail.com>
2024-10-28 14:34:15 +0200

    upd constants name

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,20 +5,20 @@
  */
 
 const COST_PER_DAY = 40;
-const MIDDLE_PERIOD = 3;
-const MIDDLE_PERIOD_DISCOUNT = 20;
+const MEDIUM_PERIOD = 3;
+const MEDIUM_PERIOD_DISCOUNT = 20;
 const LONG_PERIOD = 7;
 const LONG_PERIOD_DISCOUNT = 50;
 
 function calculateRentalCost(days) {
   const rentalCost = days * COST_PER_DAY;
 
-  if (days >= MIDDLE_PERIOD && days < LONG_PERIOD) {
-    return rentalCost - MIDDLE_PERIOD_DISCOUNT;
-  }
-
   if (days >= LONG_PERIOD) {
     return rentalCost - LONG_PERIOD_DISCOUNT;
+  }
+
+  if (days >= MEDIUM_PERIOD) {
+    return rentalCost - MEDIUM_PERIOD_DISCOUNT;
   }
 
   return rentalCost;
