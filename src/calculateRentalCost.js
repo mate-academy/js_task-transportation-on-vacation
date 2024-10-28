216d596b967c552447480723f95b5d25a7d7615c
Bovt Dmytro <mr.bovt2004@gmail.com>
2024-10-28 10:53:52 +0200

    second

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const cost = 40;
+  const COST = 40;
+  const LONG_RENT = 7;
+  const SHORT_RENT = 3;
   let discount = 0;
 
-  if (days >= 7) {
+  if (days >= LONG_RENT) {
     discount = 50;
-  } else if (days >= 3) {
+  } else if (days >= SHORT_RENT) {
     discount = 20;
   }
 
-  return days * cost - discount;
+  return days * COST - discount;
 }
 
 module.exports = calculateRentalCost;
