6ee485f422fa3d30bac1acfe5c998c9d4fa90bfe
Yana <anazigaluk@gmail.com>
2024-10-29 14:04:45 +0200

    improve code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,16 +5,16 @@
  */
 function calculateRentalCost(days) {
   const pricePerDay = 40;
+  const minDiscount = 20;
+  const maxDiscount = 50;
   let totalSum = 0;
 
-  for (let day = 1; day <= days; day++) {
-    totalSum = totalSum + pricePerDay;
-  }
+  totalSum = days * pricePerDay;
 
   if (days >= 7) {
-    return totalSum - 50;
+    return totalSum - maxDiscount;
   } else if (days >= 3) {
-    return totalSum - 20;
+    return totalSum - minDiscount;
   }
 
   return totalSum;
