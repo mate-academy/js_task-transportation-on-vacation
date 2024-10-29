eec1ef34357caaf353dc143fed0cf934da6197b0
natalia646 <75090544+natalia646@users.noreply.github.com>
2024-10-29 13:54:36 +0200

    fix constants

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,23 +3,26 @@
  *
  * @return {number}
  */
-function calculateRentalCost(days) {
-  const RENT_FOR_DAY = 40;
-  const THREE_DAYS_DISCOUNT = 20;
-  const SEVEN_DAYS_DISCOUNT = 50;
 
-  const THREE_DAYS = 3;
-  const SEVEN_DAYS = 7;
+const RENT_PER_DAY = 40;
+const THREE_DAYS_DISCOUNT = 20;
+const SEVEN_DAYS_DISCOUNT = 50;
+
+const THREE_DAYS = 3;
+const SEVEN_DAYS = 7;
+
+function calculateRentalCost(days) {
+  const RENT_PER_ALL_DAYS = days * RENT_PER_DAY;
 
   if (days < THREE_DAYS) {
-    return days * RENT_FOR_DAY;
+    return RENT_PER_ALL_DAYS;
   }
 
   if (days < SEVEN_DAYS) {
-    return days * RENT_FOR_DAY - THREE_DAYS_DISCOUNT;
+    return RENT_PER_ALL_DAYS - THREE_DAYS_DISCOUNT;
   }
 
-  return days * RENT_FOR_DAY - SEVEN_DAYS_DISCOUNT;
+  return RENT_PER_ALL_DAYS - SEVEN_DAYS_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
