440414360d4a43398b9f24c0ba6d1067fbee985b
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2024-10-30 16:20:57 +0200

    final code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const shortTerm = 3;
-  const shortTermDiscount = 20;
-  const longTerm = 7;
-  const longTermDiscount = 50;
-  const price = 40;
+  const SHORT_TURM = 3;
+  const SHORT_TURM_DISCOUNT = 20;
+  const LONG_TURM = 7;
+  const LONG_TURM_DISCOUNT = 50;
+  const PRISE_PER_DAY = 40;
 
-  if (days < shortTerm) {
-    return days * price;
-  } else if (days < longTerm) {
-    return days * price - shortTermDiscount;
+  const basePrise = days * PRISE_PER_DAY;
+
+  if (days >= LONG_TURM) {
+    return basePrise - LONG_TURM_DISCOUNT;
   }
 
-  return days * price - longTermDiscount;
+  if (days >= SHORT_TURM) {
+    return basePrise - SHORT_TURM_DISCOUNT;
+  }
+
+  return basePrise;
 }
 
 module.exports = calculateRentalCost;
