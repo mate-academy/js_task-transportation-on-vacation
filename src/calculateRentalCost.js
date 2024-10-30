798a75d92edc56f791b74709154dc00f6c23198b
Oleksandr Kotlyar <kotlyar7733@gmail.com>
2024-10-30 11:59:38 +0200

    moving constants over function and deleting else

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,18 +3,21 @@
  *
  * @return {number}
  */
+const RENTCOSTFORONEDAY = 40;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM_DISCOUNT = 20;
+const LONG_TERM = 7;
+const SHORT_TERM = 3;
+
 function calculateRentalCost(days) {
-  const rentCostForOneDay = 40;
-  const LONG_TERM_DISCOUNT = 50;
-  const SHORT_TERM_DISCOUNT = 20;
-  const LONG_TERM = 7;
-  const SHORT_TERM = 3;
-  let totalCost = rentCostForOneDay * days;
+  const totalCost = RENTCOSTFORONEDAY * days;
 
   if (days >= LONG_TERM) {
-    totalCost -= LONG_TERM_DISCOUNT;
-  } else if (days >= SHORT_TERM) {
-    totalCost -= SHORT_TERM_DISCOUNT;
+    return totalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return totalCost - SHORT_TERM_DISCOUNT;
   }
 
   return totalCost;
