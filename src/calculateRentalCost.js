5ee26ae3623d5a65ce5321fab9db7698f4570ce6
Oleksii-Ivan Stetsyk <farshaleks@gmail.com>
2024-10-30 15:59:34 +0200

    add first code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const shortTerm = 3;
+  const shortTermDiscount = 20;
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const prise = 40;
+
+  if (days < shortTerm) {
+    return days * prise;
+  } else if (days < longTerm) {
+    return days * prise - shortTermDiscount;
+  }
+
+  return days * prise - longTermDiscount;
 }
 
 module.exports = calculateRentalCost;
