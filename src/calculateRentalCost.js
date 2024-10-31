0c7c81e7ef88711971e0a4ef7bc96e648a9e30ee
Yura <yuradziavoruk5@gmail.com>
2024-10-31 12:47:55 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,13 +5,16 @@
  */
 function calculateRentalCost(days) {
   let totalAmount = 0;
+  const znyzhka = 20;
+  const vznyzhka = 50;
+
 
   if (days < 3) {
     totalAmount = days * 40;
   } else if (days >= 3 && days < 7) {
-    totalAmount = 40 * days - 20;
+    totalAmount = 40 * days - znyzhka;
   } else if (days >= 7) {
-    totalAmount = 40 * days - 50;
+    totalAmount = 40 * days - vznyzhka;
   }
 
   return totalAmount;
