d05def62ccbe3d7c656ac228b66abc95fc265859
Yuliia Zubenko <95251336+yuliiazubenko@users.noreply.github.com>
2024-10-31 07:30:32 +0100

    fix suggested by mentor

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,15 +3,17 @@
  *
  * @return {number}
  */
+const PRICE_PER_DAY = 40;
+
+const LONG_TERM = 7;
+
+const LONG_TERM_DISCOUNT = 50;
+
+const MEDIUM_TERM = 3;
+
+const MEDIUM_TERM_DISCOUNT = 20;
+
 function calculateRentalCost(days) {
-  const PRICE_PER_DAY = 40;
-
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-
-  const MEDIUM_TERM = 3;
-  const MEDIUM_TERM_DISCOUNT = 20;
-
   if (days >= LONG_TERM) {
     return days * PRICE_PER_DAY - LONG_TERM_DISCOUNT;
   }
