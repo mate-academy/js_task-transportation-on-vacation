929b29c1851b600234487d62fe52e082a4e2ba14
Mykhailo Forkosh <forkos277@gmail.com>
2024-11-01 13:41:22 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentalPerDay = 40;
+  const rentalPrice = rentalPerDay * days;
+
+  if (days >= 7) {
+    return rentalPrice - 50;
+  } else if (days >= 3) {
+    return rentalPrice - 20;
+  } else {
+    return rentalPrice;
+  }
 }
 
 module.exports = calculateRentalCost;
