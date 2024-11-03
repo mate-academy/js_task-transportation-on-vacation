f932291b3ae168e3e0da54e2a3aec42f5f1e81fb
Anton <nimachuka@gmail.com>
2024-11-03 17:29:07 +0100

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,12 +1,19 @@
 'use strict';
 
-function calculateRentalCost(days) {
-  let totalCost = days * 40;
+const DAILY_RENTAL_COST = 40;
+const LONG_TERM_DISCOUNT = 50;
+const MID_TERM_DISCOUNT = 20;
 
+function calculateRentalCost(days) {
+  const totalCost = days * DAILY_RENTAL_COST;
+
+  // Apply discounts based on the number of days
   if (days >= 7) {
-    totalCost -= 50;
-  } else if (days >= 3) {
-    totalCost -= 20;
+    return totalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= 3) {
+    return totalCost - MID_TERM_DISCOUNT;
   }
 
   return totalCost;
