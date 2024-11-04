f9f95ffac5936651e625c8d271de150db4eeb400
dimon <80713na@gmail.com>
2024-11-05 00:16:53 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const lease = 40;
+  const price = days * 40;
 
   if (days < 3) {
-    return days * lease;
+    return price;
   }
 
   if (days < 7) {
-    return days * lease - 20;
+    return price - 20;
   }
 
   if (days >= 7) {
-    return days * lease - 50;
+    return price - 50;
   }
 }
 
