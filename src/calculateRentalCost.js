606a266951ec962ff66358493825dff4fcf7639b
Olha Lishenko <lishenko.olha@gmail.com>
2024-11-04 10:35:13 +0200

    task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let totalCost = 0;
+  const minLongTerm = 7;
+  const minLowTerm = 3;
+
+  if (days >= minLongTerm) {
+    totalCost = days * 40 - 50;
+  } else if (days >= minLowTerm) {
+    totalCost = days * 40 - 20;
+  } else {
+    totalCost = days * 40;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
