20536da49d4092bf8b4d42a0aff257e28f3988d4
bombadilx <61424489+bombadilx@users.noreply.github.com>
2024-11-08 22:15:56 +0200

    Fixes for calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,13 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const dailyRate = 40;
-  let totalCost = days * dailyRate;
+  const DAILY_RENTAL_COST = 40;
+  const DISCOUNT_3_OR_MORE_DAYS = 20;
+  const DISCOUNT_7_OR_MORE_DAYS = 50;
+
+  const totalCost = days * DAILY_RENTAL_COST;
 
   if (days >= 7) {
-    totalCost -= 50;
+    return totalCost - DISCOUNT_7_OR_MORE_DAYS;
   } else if (days >= 3) {
-    totalCost -= 20;
+    return totalCost - DISCOUNT_3_OR_MORE_DAYS;
   }
 
   return totalCost;
