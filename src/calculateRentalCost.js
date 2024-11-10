e10053071acd4171092505774aa2e819d00919a8
Andrii Tkachuk <kovklad@gmail.com>
2024-11-10 19:46:07 +0100

    add changed variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  const rentOneDay = 40;
+  const discountMoreFiveDays = 50;
+  const discountMoreThreeDays = 20;
+
   if (days >= 7) {
-    return days * 40 - 50;
+    return days * rentOneDay - discountMoreFiveDays;
   }
 
   if (days >= 3) {
-    return days * 40 - 20;
+    return days * rentOneDay - discountMoreThreeDays;
   }
 
-  return days * 40;
+  return days * rentOneDay;
 }
 
 module.exports = calculateRentalCost;
