c4389b856ea07e9eed69bf446bdda615d380b05e
Artem Petrukhin <85151777+petrukh1n@users.noreply.github.com>
2024-11-10 20:23:25 +0000

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,17 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const carCost = days * 40;
+
+  if (days > 6) {
+    return carCost - 50;
+  }
+
+  if (days > 2) {
+    return carCost - 20;
+  }
+
+  return carCost;
 }
 
 module.exports = calculateRentalCost;
