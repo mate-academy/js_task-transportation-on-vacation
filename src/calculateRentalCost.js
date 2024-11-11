abd47b644309081d089d1cfa8fe3891c66b28956
Kevin Zemko <kevinzemko@protonmail.com>
2024-11-11 22:10:02 +0100

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const cost = 40;
+  const discountAfter7 = 50;
+  const discountAfter3 = 20;
+  let result = 0;
+  if(days < 3){
+    result += days * cost;
+  }
+  if(days < 7){
+    result += (days * cost) - discountAfter7;
+  }
+  if(days >= 7){
+    result += (days * cost) - discountAfter3;
+  }
+  return result
 }
 
 module.exports = calculateRentalCost;
