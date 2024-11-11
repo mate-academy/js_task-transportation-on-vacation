8a1ad4f9879dbaecfd98f9e01c1f21fe7d26d655
QAoct24Olga <olgvvchenko@gmail.com>
2024-11-11 18:04:34 +0200

    Create discount function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_RENTAL_PRICE = 40;
+  const LONG_TERM_RENTAL_DAYS = 7;
+  const LONG_TERM_RENTAL_DISCOUNT = 50;
+  const MINIMUM_TERM_RENTAL_DAYS = 3;
+  const MINIMUM_TERM_RENTAL_DISCOUNT = 20;
+
+  const totalCost = days * DAILY_RENTAL_PRICE;
+
+  if (days >= LONG_TERM_RENTAL_DAYS) {
+    return totalCost - LONG_TERM_RENTAL_DISCOUNT;
+  }
+
+  if (days >= MINIMUM_TERM_RENTAL_DAYS) {
+    return totalCost - MINIMUM_TERM_RENTAL_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
