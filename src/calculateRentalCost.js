73f631a82c67ed5320dcfe961a098fae39bdadc9
Clyacsa <clyacsa@gmail.com>
2024-11-12 22:24:42 +0200

    Updated calculateRental

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const priceADay = 40;
+  const LONG_TERM = 7;
+  const LONG_DISCOUNT = 50;
+  const MIDDLE_TERM = 3;
+  const MIDDLE_DISCOUNT = 20;
+  const TotalPrice = priceADay * days;
+
+  if (days >= MIDDLE_TERM && days < LONG_TERM) {
+    return TotalPrice - MIDDLE_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    return TotalPrice - LONG_DISCOUNT;
+  }
+
+  return TotalPrice;
 }
 
 module.exports = calculateRentalCost;
