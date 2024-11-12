20ca7e3d28685c9a4b18e97afb73e65690971d95
imalexvit <ivityuko@gmail.com>
2024-11-12 18:54:13 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,13 +4,13 @@
  * @return {number}
  */
 
-const DAILY_COST = 40;
-const MIN_DAYS_FOR_SHORT_TERM_DISCOUNT = 3;
-const MIN_DAYS_FOR_LONG_TERM_DISCOUNT = 7;
-const SHORT_TERM_DISCOUNT = 20;
-const LONG_TERM_DISCOUNT = 50;
-
 function calculateRentalCost(days) {
+  const DAILY_COST = 40;
+  const MIN_DAYS_FOR_SHORT_TERM_DISCOUNT = 3;
+  const MIN_DAYS_FOR_LONG_TERM_DISCOUNT = 7;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+
   const totalCost = days * DAILY_COST;
 
   if (days >= MIN_DAYS_FOR_LONG_TERM_DISCOUNT) {
