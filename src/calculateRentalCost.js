5f87b4d37d8e4f3ce58525c917aaa00399c1990b
Diana Khalaim <khalaimdiana@gmail.com>
2024-11-12 14:47:06 +0200

    implement function calculateRentalCost

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const RENT = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    return RENT * days - LONG_TERM_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    return RENT * days - SHORT_TERM_DISCOUNT;
+  }
+
+  return RENT * days;
 }
 
 module.exports = calculateRentalCost;
