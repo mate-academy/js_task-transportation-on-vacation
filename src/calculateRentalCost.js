a1120da25a7007bc4835ccc194164ef30820ec7d
imalexvit <ivityuko@gmail.com>
2024-11-12 18:24:25 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -10,14 +10,14 @@ const MIN_DAYS_FOR_LONG_TERM_DISCOUNT = 7;
 const SHORT_TERM_DISCOUNT = 20;
 const LONG_TERM_DISCOUNT = 50;
 
-function calculateRentalCost(numberOfDays) {
-  const totalCost = numberOfDays * DAILY_COST;
+function calculateRentalCost(days) {
+  const totalCost = days * DAILY_COST;
 
-  if (numberOfDays >= MIN_DAYS_FOR_LONG_TERM_DISCOUNT) {
+  if (days >= MIN_DAYS_FOR_LONG_TERM_DISCOUNT) {
     return totalCost - LONG_TERM_DISCOUNT;
   }
 
-  if (numberOfDays >= MIN_DAYS_FOR_SHORT_TERM_DISCOUNT) {
+  if (days >= MIN_DAYS_FOR_SHORT_TERM_DISCOUNT) {
     return totalCost - SHORT_TERM_DISCOUNT;
   }
 
