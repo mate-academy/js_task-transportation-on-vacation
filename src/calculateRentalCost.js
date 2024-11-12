82604e3b9f205d59b9dcdb7ca0339d89f2e85c5f
Bulavskyi Oleh <bulavskyi.oleh@gmail.com>
2024-11-12 16:00:50 +0200

    fixed if and naming issues

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,19 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const BASE_PRICE = 40;
+  const PRICE_PER_DAY = 40;
   const LONG_TERM = 7;
   const SHORT_TERM = 3;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM_DISCOUNT = 20;
 
+  const basePrice = PRICE_PER_DAY * days;
+
   if (days >= SHORT_TERM && days < LONG_TERM) {
-    return days * BASE_PRICE - SHORT_TERM_DISCOUNT;
-  } else if (days >= LONG_TERM) {
-    return days * BASE_PRICE - LONG_TERM_DISCOUNT;
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
-  return days * BASE_PRICE;
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
