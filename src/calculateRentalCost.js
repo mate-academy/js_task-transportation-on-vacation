70be2d74de4917b475b1644be29bfc7f55f63e26
Nariman Mambetov <narik.mamb1@gmail.com>
2024-11-13 02:10:50 +0200

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_COST = 40;
+  const totalCost = days * DAILY_COST;
+  const MIN_DAYS_FOR_SHORT_TERM_DISCOUNT = 3;
+  const MIN_DAYS_FOR_LONG_TERM_DISCOUNT = 7;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+
+  if (days >= MIN_DAYS_FOR_LONG_TERM_DISCOUNT) {
+    return totalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MIN_DAYS_FOR_SHORT_TERM_DISCOUNT) {
+    return totalCost - SHORT_TERM_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
