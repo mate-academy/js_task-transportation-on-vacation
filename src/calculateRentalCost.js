37e83949a80e4c3f90fbc0d3b9cb665effacf134
Artem Opanasenko <triggerball@ukr.net>
2024-11-13 18:18:49 +0200

    add changes

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,10 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+
   let TAX_PER_DAY = 40;
+  let LONG_TERMIN_DISCOUNT = 50;
+  let MIDDLE_TERMIN_DISCOUNT = 20;
   let LONG_TERMIN = 7;
   let MIDLE_TERMIN = 3;
 
@@ -15,9 +18,9 @@ function calculateRentalCost(days) {
   if (days < MIDLE_TERMIN) {
     return days * TAX_PER_DAY;
   } else if (days >= MIDLE_TERMIN && days < LONG_TERMIN) {
-    return days * TAX_PER_DAY - 20;
+    return days * TAX_PER_DAY - MIDDLE_TERMIN_DISCOUNT;
   } else {
-    return days * TAX_PER_DAY - 50;
+    return days * TAX_PER_DAY - LONG_TERMIN_DISCOUNT;
   }
 }
 
