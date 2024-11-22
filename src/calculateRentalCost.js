113eff0dc82a4177a9782a2036637719f832b17b
Sergiy Lopatkin <lopatkinsergiy@gmail.com>
2024-11-22 17:18:50 +0300

    comm

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const minDiscont=3;
-  const baseDiscont=7;
-  const baseCost=40*days;
-  if (days<minDiscont){
+  const minDiscont = 3;
+  const baseDiscont = 7;
+  const baseCost = 40 * days;
+
+  if (days < minDiscont) {
     return baseCost;
   }
-  if (days>=baseDiscont){
-    return baseCost-50;
+
+  if (days >= baseDiscont) {
+    return baseCost - 50;
   }
-  return baseCost-20;
+
+  return baseCost - 20;
 }
 
 module.exports = calculateRentalCost;
