7aedddec933ae475b0cfb094fe0b1ac70a2fb7a4
Anton Tkachuk <tkacuk2291@gmail.com>
2024-11-24 22:56:22 +0200

    fixed names for viriables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,21 +3,19 @@
  *
  * @return {number}
  */
+const SMALL_TERM_DISCOUNT = 20;
+const LONG_TERM_DISCOUNT = 50;
+
 function calculateRentalCost(days) {
   let total = 40 * days;
 
   if (days >= 7) {
-    total = total - 50;
+    total = total - LONG_TERM_DISCOUNT;
   } else if (days >= 3 && days < 7) {
-    total = total - 20;
+    total = total - SMALL_TERM_DISCOUNT;
   }
+
   return total;
 }
-
-
-
-
-
-
 
 module.exports = calculateRentalCost;
