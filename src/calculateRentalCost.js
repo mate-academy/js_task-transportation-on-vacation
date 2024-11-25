fefd5e5cd37740d82bb3bb661a3399fea2a9a9c8
Alina Yamchuk <alina.utc@gmail.com>
2024-11-25 17:00:42 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyPay = 40;
+
+  if (days <= 2) {
+    return days * dailyPay;
+  }
+
+  let cost = dailyPay * days;
+
+  if (days >= 3 && days <= 6) {
+    cost = cost - 20;
+  }
+
+  if (days >= 7) {
+    cost = cost - 50;
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
