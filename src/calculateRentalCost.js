eea59cd1f1333f8f0af894fc1c475bacb1cf4346
Tania Kolesnik <kolesnik@gmail.com>
2024-11-25 22:33:10 +0000

    implement solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,28 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  let carRent = PRICE_PER_DAY * days;
+
+  if (days >= LONG_TERM) {
+    carRent = carRent - LONG_TERM_DISCOUNT;
+
+    return carRent;
+  }
+
+  if (days >= SHORT_TERM) {
+    carRent = carRent - SHORT_TERM_DISCOUNT;
+
+    return carRent;
+  }
+
+  return carRent;
 }
 
 module.exports = calculateRentalCost;
