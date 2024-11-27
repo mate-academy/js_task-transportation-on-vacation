3b3091d00078915da42cbaddf52b46d4fe5310bc
Lucas Helfensteller Silva <lucashel@gmail.com>
2024-11-27 12:03:29 +0100

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
+
+  const longTerm = 7;
+  const longTermDiscount = 50;
+  const mediumTerm = 3;
+  const mediumTermDiscount = 20;
+  const calc = days * 40;
+
+  if (days >= longTerm) {
+    return calc - longTermDiscount;
+  }
+
+  if (days >= mediumTerm) {
+    return calc - mediumTermDiscount;
+  }
+
+  return calc;
 }
 
 module.exports = calculateRentalCost;
