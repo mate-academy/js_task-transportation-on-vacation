4c21459ec0e44b2a528edd7490cc95a92bc5f660
combine143 <64581819+combine143@users.noreply.github.com>
2024-11-28 13:11:08 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+
+  const MIDDLE_TERM = 3;
+  const MIDDLE_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM) {
+    return days * 40 - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MIDDLE_TERM) {
+    return days * 40 - MIDDLE_TERM_DISCOUNT;
+  } else {
+    return days * 40;
+  }
 }
 
 module.exports = calculateRentalCost;
