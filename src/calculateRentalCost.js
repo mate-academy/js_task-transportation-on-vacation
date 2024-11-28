a76d4cd355f8a79d246c11c505e4375333184c1c
Oskar Koszalka <oskar.koszalka@wp.pl>
2024-11-28 17:00:59 +0100

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,19 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const result = days * 40;
+  const regularDiscount = 20;
+  const longTermDiscount = 50;
+
+  if (days >= 7) {
+    return result - longTermDiscount;
+  }
+
+  if (days >= 3) {
+    return result - regularDiscount;
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
