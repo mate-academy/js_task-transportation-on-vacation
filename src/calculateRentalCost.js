fd6032bcca167e6ba52589cc4b285a24331aaddc
mykhailo-s <mykhailo-s@stellars.tech>
2024-11-30 16:12:29 +0200

    solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const discount = calculateDiscount(days);
+  const finalPrice = days * 40;
+
+  return finalPrice - discount;
+}
+
+function calculateDiscount(days) {
+  if (days >= 7) {
+    return 50;
+  } else if (days >= 3 && days < 7) {
+    return 20;
+  } else {
+    return 0;
+  }
 }
 
 module.exports = calculateRentalCost;
