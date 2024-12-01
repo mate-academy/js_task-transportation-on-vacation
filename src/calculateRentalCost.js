218cf51937024fce7a93c66c53360209fb4fc33d
Vladyslava Korovaina <vladyslava.korovainaya@gmail.com>
2024-12-01 12:00:22 +0000

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,25 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const basePrice = 40;
+
+  if (days === 1) {
+    return basePrice;
+  }
+
+  if (days > 1 && days < 3) {
+    return basePrice * days;
+  }
+
+  if (days >= 3 && days <= 6) {
+    return basePrice * days - 20;
+  }
+
+  if (days > 6) {
+    return basePrice * days - 50;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
