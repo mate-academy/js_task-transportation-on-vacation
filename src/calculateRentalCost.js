862b3ff076cd56b176e31762270c689f1fb76ae2
Flávio Miranda <nascimentoflavio588@hotmail.com>
2024-12-02 14:47:01 -0300

    Add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM = 3;
+  const MID_TERM_DISCOUNT = 20;
+  const PRICE_PER_DAY = 40;
+  const TOTAL_PRICE = days * PRICE_PER_DAY;
+
+  switch (true) {
+    case days >= LONG_TERM:
+      return TOTAL_PRICE - LONG_TERM_DISCOUNT;
+    case days >= MID_TERM:
+      return TOTAL_PRICE - MID_TERM_DISCOUNT;
+    default:
+      return TOTAL_PRICE;
+  }
 }
 
 module.exports = calculateRentalCost;
