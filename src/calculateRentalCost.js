089a1ae09b2976659c950b8bd50987261a11cf19
Александр Ляшенко <sanko.lialko14@gmail.com>
2024-12-02 12:50:41 +0200

    1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,6 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
+  let price = days * 40;
+
+  if (days >= 7) {
+    price -= 50;
+  } else if (days >= 3) {
+    price -= 20;
+  }
+
+  return price;
   // write code here
 }
 
