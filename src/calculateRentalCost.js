51500a65e313d697cebeca4788b9d7dfaa21e28c
Egor Mamtsev <egormamcev@MacBook-Air.local>
2024-12-04 10:10:01 -0600

    done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dayCost = 40;
+  const weekOff = 50;
+  const threeDaysOff = 20;
+  let total;
+
+  total = days * dayCost;
+
+  if (days >= 3 && days <= 6) {
+    total -= threeDaysOff;
+  }
+
+  if (days >= 7) {
+    total -= weekOff;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
