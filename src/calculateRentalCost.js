939d2d86126317033142e6b64c3e5fffd328e8e6
Maksym Korchynskyi <maksym.24.09.2003@gmail.com>
2024-12-08 10:15:01 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const MEDIUM_TERM = 3;
+
+  const LONG_TERM_DISCOUNT = 50;
+  const MEDIUM_TERM_DISCOUNT = 20;
+
+  const price = days * 40;
+
+  if (days >= LONG_TERM) {
+    return price - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MEDIUM_TERM) {
+    return price - MEDIUM_TERM_DISCOUNT;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
