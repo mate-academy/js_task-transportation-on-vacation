40298cea8743e53fffefed4df52a09bae1317c96
Vlada Lavrentieva <vlada.lavr24@gmail.com>
2024-12-09 22:29:55 +0100

    function returns the total amount for different number of days

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,26 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAYS = days;
+  const ONE_DAY_PRISE = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MIDDLE_TERM = 3;
+  const MIDDLE_TERM_DISCOUNT = 20;
+
+  if (DAYS >= LONG_TERM) {
+    const LONG_TERM_TOTAL = DAYS * ONE_DAY_PRISE - LONG_TERM_DISCOUNT;
+
+    return LONG_TERM_TOTAL;
+  }
+
+  if (DAYS >= MIDDLE_TERM) {
+    const MIDDLE_TERM_TOTAL = DAYS * ONE_DAY_PRISE - MIDDLE_TERM_DISCOUNT;
+
+    return MIDDLE_TERM_TOTAL;
+  }
+
+  return DAYS * ONE_DAY_PRISE;
 }
 
 module.exports = calculateRentalCost;
