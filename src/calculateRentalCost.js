7095d0eccfbd59f074d63ed26ff8b406ad06a4cc
Vlada Lavrentieva <vlada.lavr24@gmail.com>
2024-12-09 23:01:32 +0100

    make short code

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,26 +4,15 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const DAYS = days;
-  const ONE_DAY_PRISE = 40;
-  const LONG_TERM = 7;
-  const LONG_TERM_DISCOUNT = 50;
-  const MIDDLE_TERM = 3;
-  const MIDDLE_TERM_DISCOUNT = 20;
+  let PRICE = days * 40;
 
-  if (DAYS >= LONG_TERM) {
-    const LONG_TERM_TOTAL = DAYS * ONE_DAY_PRISE - LONG_TERM_DISCOUNT;
-
-    return LONG_TERM_TOTAL;
+  if (days >= 7) {
+    PRICE -= 50;
+  } else if (days >= 3) {
+    PRICE -= 20;
   }
 
-  if (DAYS >= MIDDLE_TERM) {
-    const MIDDLE_TERM_TOTAL = DAYS * ONE_DAY_PRISE - MIDDLE_TERM_DISCOUNT;
-
-    return MIDDLE_TERM_TOTAL;
-  }
-
-  return DAYS * ONE_DAY_PRISE;
+  return PRICE;
 }
 
 module.exports = calculateRentalCost;
