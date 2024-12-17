4d081272b8aaa454c0b37817b73a751af2dff1e0
Magdalena-Por <anna.meex@gmail.com>
2024-12-17 14:59:06 +0100

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const minRabat = 20;
+  const maxRabat = 50;
+  const dailyCost = 40;
+
+  if (days >= 7) {
+    return days * dailyCost - maxRabat;
+  }
+
+  if (days >= 3) {
+    return days * dailyCost - minRabat;
+  }
+
+  return days * dailyCost;
 }
 
 module.exports = calculateRentalCost;
