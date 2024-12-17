adb280a014519ae7afb1f1b8a7ac897db3e190db
Bohdan Poliienko <mignatov756@gmail.com>
2024-12-17 22:25:49 +0200

    fix variables name

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,12 +7,14 @@ function calculateRentalCost(days) {
   const PRICE = 40;
   const MIDDLE_TERM_DISCOUNT = 20;
   const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3
+  const LONG_TERM = 3
 
-  if (days < 3) {
+  if (days < SHORT_TERM) {
     return days * PRICE;
   }
 
-  if (days >= 3 && days < 7) {
+  if (days >= 3 && days < LONG_TERM) {
     return days * PRICE - MIDDLE_TERM_DISCOUNT;
   }
 
