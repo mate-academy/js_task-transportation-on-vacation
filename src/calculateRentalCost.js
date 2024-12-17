a1a2fe653176795fa7968fa891c6fbfdf0e6feb9
Bohdan Poliienko <mignatov756@gmail.com>
2024-12-17 22:28:48 +0200

    fix variables_v2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,13 +8,13 @@ function calculateRentalCost(days) {
   const MIDDLE_TERM_DISCOUNT = 20;
   const LONG_TERM_DISCOUNT = 50;
   const SHORT_TERM = 3
-  const LONG_TERM = 3
+  const LONG_TERM = 7
 
   if (days < SHORT_TERM) {
     return days * PRICE;
   }
 
-  if (days >= 3 && days < LONG_TERM) {
+  if (days >= SHORT_TERM && days < LONG_TERM) {
     return days * PRICE - MIDDLE_TERM_DISCOUNT;
   }
 
