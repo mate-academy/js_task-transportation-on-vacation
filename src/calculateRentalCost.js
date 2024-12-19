964f135ee4c9266d438c0f5d552f7e2b3d070759
Andrew <andrijvasilik252@gmail.com>
2024-12-19 14:46:48 +0200

    fist commit

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,20 @@
  *
  * @return {number}
  */
+'use strict';
+
+const rentOfDay = 40;
+
 function calculateRentalCost(days) {
-  // write code here
+  if (days >= 7) {
+    return days * rentOfDay - 50;
+  }
+
+  if (days >= 3) {
+    return days * rentOfDay - 20;
+  }
+
+  return days * rentOfDay;
 }
 
 module.exports = calculateRentalCost;
