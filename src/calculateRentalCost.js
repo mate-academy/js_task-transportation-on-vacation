1e514e86a3566e17524bb0fcbd0e31f877dbbc81
Ivan Babiuk <whatifitryfirst@gmail.com>
2024-12-22 18:41:14 +0200

    transportation-on-vacation solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const BASE_COST = 40;
+
+  if (days >= SHORT_TERM && days < LONG_TERM) {
+    return days * BASE_COST - SHORT_TERM_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    return days * BASE_COST - LONG_TERM_DISCOUNT;
+  }
+
+  return days * BASE_COST;
 }
 
 module.exports = calculateRentalCost;
