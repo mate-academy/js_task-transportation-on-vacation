df2c792ec76d94b3f50d3a1bbd204b1564ac4744
Ditiselit <ditiselit@gmail.com>
2024-12-23 14:00:17 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const DAILY_RENTAL_COST = 40;
+  const LONG_TERM_THRESHOLD = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM_THRESHOLD = 3;
+  const MID_TERM_DISCOUNT = 20;
+
+  const totalCost = days * DAILY_RENTAL_COST;
+
+  if (days >= LONG_TERM_THRESHOLD) {
+    return totalCost - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MID_TERM_THRESHOLD) {
+    return totalCost - MID_TERM_DISCOUNT;
+  }
+
+  return totalCost;
 }
 
 module.exports = calculateRentalCost;
