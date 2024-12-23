0fd7d1f18b2feb9ec1f7bb1521b523768c5ff170
Valentyna <adm@adms-MacBook-Pro.local>
2024-12-23 21:55:26 +0200

    add Transportation on Vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let cost = days * 40;
+
+  if (days < 3) {
+    return cost;
+  } else {
+    if (days < 7) {
+      cost = cost - 20;
+    } else {
+      cost = cost - 50;
+    }
+  }
+
+  return cost;
 }
 
 module.exports = calculateRentalCost;
