0dd410db65b6357770005e78e6d33b2de859d794
Wilton Martins <wiltonliramartins@icloud.com>
2024-12-22 22:08:07 -0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,18 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  const baseCost = days * 40;
+  const weekRental = 7;
+  const minDaysDiscount = 3;
+
+  // Aplica desconto baseado nos dias
+  if (days >= weekRental) {
+    return baseCost - 50;
+  } else if (days >= minDaysDiscount) {
+    return baseCost - 20;
+  }
+
+  return baseCost;
 }
 
 module.exports = calculateRentalCost;
