9859523f42bbad5e6bad88304981f39d97e596a1
artur.manuilenko <artur.manuilenko@yalantis.net>
2024-12-26 18:43:34 +0200

    Update rental cost calculation and dependency version

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,20 @@
  */
 function calculateRentalCost(days) {
   // write code here
+
+  let discount = 0;
+  const carCost = 40;
+  const rent = carCost * days;
+
+  if (days >= 3) {
+    discount = 20;
+  }
+
+  if (days >= 7) {
+    discount = 50;
+  }
+
+  return rent - discount;
 }
 
 module.exports = calculateRentalCost;
