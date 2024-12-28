4127dfe7b8ca2dc7bf65713407d512eea085c1f4
PedroWeb10 <pdro_dev@yahoo.com>
2024-12-28 13:37:43 -0300

    solution test js -- transportation on vacation

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,28 @@
+'use strict';
+
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DESCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DESCOUNT = 20;
+
+  const basePrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DESCOUNT;
+  }
+
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DESCOUNT;
+  }
+
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
