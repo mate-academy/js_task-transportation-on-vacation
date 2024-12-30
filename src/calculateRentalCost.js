f7f53944fe04f3c0a01e5f6a2bbf7f05c326dc13
g.palyukh <g.palyukh@gmail.com>
2024-12-31 00:29:49 +0100

    Implement calculateRentalCost function

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCostPerDay = 40;
+  const totalRentalCost = rentalCostPerDay * days;
+
+  if (days >= 7) {
+    return totalRentalCost - 50;
+  }
+
+  if (days >= 3) {
+    return totalRentalCost - 20;
+  }
+
+  return totalRentalCost;
 }
 
 module.exports = calculateRentalCost;
