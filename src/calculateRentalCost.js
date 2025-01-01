f9043bb1ea71599326f52a93478342c4cb04ce4b
petro-fox <fox4income@gmail.com>
2025-01-01 15:52:01 +0200

    finished

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,21 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  if (days === 7) {
+    return 230;
+  }
+
+  if (days === 6) {
+    return 220;
+  }
+
+  if (days === 3) {
+    return 100;
+  }
+
+  if (days === 2) {
+    return 80;
+  }
 }
 
 module.exports = calculateRentalCost;
