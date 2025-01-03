40c4c23de83cc970d3fe830bc0b53cde1e1a331a
g.palyukh <g.palyukh@gmail.com>
2025-01-03 11:12:31 +0100

    Made names descriptive for variables

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,18 +4,23 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const rentalCostPerDay = 40;
-  const totalRentalCost = rentalCostPerDay * days;
+  const PRICE_PER_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
 
-  if (days >= 7) {
-    return totalRentalCost - 50;
+  const basePrice = PRICE_PER_DAY * days;
+
+  if (days >= LONG_TERM) {
+    return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return totalRentalCost - 20;
+  if (days >= SHORT_TERM) {
+    return basePrice - SHORT_TERM_DISCOUNT;
   }
 
-  return totalRentalCost;
+  return basePrice;
 }
 
 module.exports = calculateRentalCost;
