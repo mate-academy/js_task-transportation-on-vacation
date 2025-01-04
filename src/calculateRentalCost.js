b2e2b82ad164150765187971aeb04ab20e7d9321
Andrii Filipenko <andreyfil1337@gmail.com>
2025-01-04 11:49:22 +0200

    add variables for hard-coded literals

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,15 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const cost = 40 * days;
+  const BASE_COST = 40;
+  const SHORT_TERM = 3;
+  const LONG_TERM = 7;
+  const SHORT_TERM_DISCOUNT = 20;
+  const LONG_TERM_DISCOUNT = 50;
+  const totalCost = BASE_COST * days;
 
   switch (true) {
-    case days < 3:
-      return cost;
-    case days < 7:
-      return cost - 20;
-    case days >= 7:
-      return cost - 50;
+    case days < SHORT_TERM:
+      return totalCost;
+    case days < LONG_TERM:
+      return totalCost - SHORT_TERM_DISCOUNT;
+    case days >= LONG_TERM:
+      return totalCost - LONG_TERM_DISCOUNT;
   }
 }
 
