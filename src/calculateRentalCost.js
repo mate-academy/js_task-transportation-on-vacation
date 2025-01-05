e32071c824ca9c407737f2c445b7947923ed8ce2
vibortnik <viktoriiabortnikusa@gmail.com>
2025-01-05 17:25:58 -0600

    Update calculateRentalCost.js

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,19 +1,13 @@
-/**
- * @param {number} days
- *
- * @return {number}
- */
+const PRICE_PER_DAY = 40;
+const LONG_TERM_DISCOUNT = 50;
+const SHORT_TERM_DISCOUNT = 20;
 function calculateRentalCost(days) {
-   if (days === 7) {
-    return 230;
+  if (days < 3) {
+    return PRICE_PER_DAY * days;
+  } else if (days >= 3 && days < 7) {
+    return PRICE_PER_DAY * days - SHORT_TERM_DISCOUNT;
+  } else {
+    return PRICE_PER_DAY * days - LONG_TERM_DISCOUNT;
   }
-  if (days === 6) {
-    return 220;
-  }
-  if (days === 3) {
-    return 100;
-  }
-  return 80; 
 }
-
 module.exports = calculateRentalCost;
