cc7ee19443515fdd8e49ff631092c70763d01766
Yuriy Postrygan <petrikeich@gmail.com>
2025-01-06 20:34:16 +0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,22 @@
  */
 function calculateRentalCost(days) {
   // write code here
+
+  const COST_BY_DAY = 40;
+  const FIRST_DISCOUNT = 20;
+  const SECOND_DISCOUNT = 50;
+  const FIRST_DISCOUNT_TERM = 3;
+  const SECOND_DISCOUNT_TERM = 7;
+
+  if (days < FIRST_DISCOUNT_TERM) {
+    return COST_BY_DAY * days;
+  }
+
+  if (days >= SECOND_DISCOUNT_TERM) {
+    return COST_BY_DAY * days - SECOND_DISCOUNT;
+  }
+
+  return COST_BY_DAY * days - FIRST_DISCOUNT;
 }
 
 module.exports = calculateRentalCost;
