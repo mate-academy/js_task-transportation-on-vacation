dabcc17e9e2466222c9c23e55b80301ee71b921e
Kyrylo Bondarenko <dejsk35@gmail.com>
2025-01-06 11:50:28 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const LONG_TERM = 7;
+  const MID_TERM = 3;
+  const LONG_TERM_DISCOUNT = 50;
+  const MID_TERM_DISCOUNT = 20;
+  const PRICE_PER_DAY = 40;
+  const rentalPrice = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    return rentalPrice - LONG_TERM_DISCOUNT;
+  }
+
+  if (days >= MID_TERM) {
+    return rentalPrice - MID_TERM_DISCOUNT;
+  }
+
+  return rentalPrice;
 }
 
 module.exports = calculateRentalCost;
