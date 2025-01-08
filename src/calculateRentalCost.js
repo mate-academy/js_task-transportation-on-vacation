5e5d4ee0b55708e64f927f67315e197d2a13b0dc
Maksym Maksymov <moamama856@gmail.com>
2025-01-08 16:25:23 +0100

    HomeWork1New

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  if (days === 7) {
-    return 230;
+  if (days < 3) {
+    return 80;
+  }
+
+  if (days === 3) {
+    return 100;
   }
 
   if (days === 6) {
     return 220;
   }
 
-  if (days === 3) {
-    return 100;
+  if (days >= 7) {
+    return 230;
   }
 
   return 80;
