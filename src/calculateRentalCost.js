480c9039ccda1731202c0595aaf13d11cc17042c
DariaSalhalova <salhalovadaria@gmail.com>
2025-01-08 20:36:19 +0200

    first github task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,22 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  let price;
+  const pricePerDay = 40;
+
+  if (days < 3) {
+    price = days * pricePerDay;
+  }
+
+  if (days >= 3 && days < 7) {
+    price = days * pricePerDay - 20;
+  }
+
+  if (days >= 7) {
+    price = days * pricePerDay - 50;
+  }
+
+  return price;
 }
 
 module.exports = calculateRentalCost;
