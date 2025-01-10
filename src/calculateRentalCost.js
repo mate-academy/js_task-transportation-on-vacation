426f3a8b9f12ff65c213242a977e4489f2cbf270
Paolo Mariano <paoloramariano@outlook.com>
2025-01-10 13:55:14 -0300

    transportation-on-vacation-solution-3

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -11,7 +11,7 @@ function calculateRentalCost(days) {
     return basePrice - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= SHORT_TERM || days < LONG_TERM) {
+  if (days >= SHORT_TERM && days < LONG_TERM) {
     return basePrice - SHORT_TERM_DISCOUNT;
   }
 
