65c8c8ec14f6f5da6751f6764493f73e03773a75
Bilokon Yaroslav <uvill.github@gmail.com>
2025-01-15 15:42:55 +0200

    check

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -6,8 +6,8 @@
 function calculateRentalCost(days) {
   const BIG_DISCOUNT = 50;
   const SMALL_DISCOUNT = 20;
-  const LONG_TERM = 7;
   const MEDIUM_TERM = 3;
+  const LONG_TERM = 7;
   const PRICE = 40;
   const cost = days * PRICE;
   let totalCost = 0;
