aa154d24ce9641e194eefc45f5053c7ed732ce4a
Ivan Ilnytskyy <ilniskyi2602@gmail.com>
2025-01-17 12:22:24 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,7 +3,22 @@
  *
  * @return {number}
  */
+const PRICE_PER_DAY = 40;
+const LONG_TERM = 7;
+const LONG_TERM_DOSCOUNT = 50;
+const SHIRT_TERM = 3;
+const SHIRT_TERM_DOSCOUNT = 20;
+
 function calculateRentalCost(days) {
+  let price = days * PRICE_PER_DAY;
+
+  if (days >= LONG_TERM) {
+    price -= LONG_TERM_DOSCOUNT;
+  } else if (days >= SHIRT_TERM) {
+    price -= SHIRT_TERM_DOSCOUNT;
+  }
+
+  return price;
   // write code here
 }
 
