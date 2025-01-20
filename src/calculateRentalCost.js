f5cb18285333e8f95c228c6b4e7d7ac057df18f9
Samarash Eugene <samarasha95@gmail.com>
2025-01-20 21:14:47 +0200

    calculateRentalCost is ready

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  if (days >= 7) {
+    return days * 40 - 50;
+  }
+
+  if (days >= 3) {
+    return days * 40 - 20;
+  }
+
+  if (days > 0) {
+    return days * 40;
+  }
+
+  return days * 40;
 }
 
 module.exports = calculateRentalCost;
