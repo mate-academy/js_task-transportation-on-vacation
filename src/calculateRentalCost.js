b527d03e2271f4b83765afe29145d14de80582e8
VadimKudrenko <48049362+VadimKudrenko@users.noreply.github.com>
2025-01-21 17:28:24 +0200

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,20 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentCost = 40;
+  let totalDiscount = 0;
+
+  if (days >= 3) {
+    totalDiscount = 20;
+  }
+
+  if (days >= 7) {
+    totalDiscount = 50;
+  }
+
+  const totalAmount = rentCost * days - totalDiscount;
+
+  return totalAmount;
 }
 
 module.exports = calculateRentalCost;
