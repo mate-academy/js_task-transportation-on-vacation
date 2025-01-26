c231ac58a3d2aacd11c90e8aafab68fa18e17390
Rodrigo Ferrari <rodrigoferrari94rf@gmail.com>
2025-01-25 22:03:40 -0300

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const baseRentalCost = days * 40;
+  const longTerm = 7;
+  const mediumTerm = 3;
+  const longTermDiscount = 50;
+  const mediumTermDiscount = 20;
+
+  if (days >= mediumTerm) {
+    return days < longTerm
+      ? baseRentalCost - mediumTermDiscount
+      : baseRentalCost - longTermDiscount;
+  }
+
+  return baseRentalCost;
 }
 
 module.exports = calculateRentalCost;
