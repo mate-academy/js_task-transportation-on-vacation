05a45257e6dad70d7f4d5de3d1c668df7e081ae9
Ivan Ryabukha <ryabukhaivan98@gmail.com>
2025-01-27 23:52:41 +0200

    'Solution'

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const BASE_COST = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= SHORT_TERM && days <= 6) {
+    return days * BASE_COST - SHORT_TERM_DISCOUNT;
+  }
+
+  if (days >= LONG_TERM) {
+    return days * BASE_COST - LONG_TERM_DISCOUNT;
+  }
+
+  return BASE_COST * days;
 }
 
 module.exports = calculateRentalCost;
