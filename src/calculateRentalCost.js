7a2de77394ea8e15c64cff9e45cf229e16250317
Roksolana <zvirarch14.08@gmail.com>
2025-01-27 14:14:43 +0200

    implemented task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const priceForDay = 40;
+  const basicDiscount = 20;
+  const extraDiscount = 50;
+
+  if (days >= 3 && days < 7) {
+    return priceForDay * days - basicDiscount;
+  }
+
+  if (days >= 7) {
+    return priceForDay * days - extraDiscount;
+  }
+
+  return priceForDay * days;
 }
 
 module.exports = calculateRentalCost;
