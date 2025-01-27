944c55be129fb62b283e2136cb45d9afc19a674b
Illia <hetsko.ilya@gmail.com>
2025-01-27 21:33:27 +0200

    add task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,15 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  let rentCost = days * 40;
+
+  if (days >= 7) {
+    rentCost -= 50;
+  } else if (days >= 3) {
+    rentCost -= 20;
+  }
+
+  return rentCost;
 }
 
 module.exports = calculateRentalCost;
