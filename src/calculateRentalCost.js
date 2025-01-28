ca413bbfb9596b460d585f044a8d53ef2177d312
Olívio Neves <olivio.servio@yahoo.com.br>
2025-01-28 14:57:35 -0300

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,21 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
-}
+  const pricePerDay = 40;
+  const maiorTempo = 7;
+  const valorMaiorDesconto = 50;
+  const menorTempo = 3;
+  const valorMenorDesconto = 20;
+  const price = days * pricePerDay;
 
+  if (days >= maiorTempo) {
+    return price - valorMaiorDesconto;
+  }
+
+  if (days >= menorTempo) {
+    return price - valorMenorDesconto;
+  } else {
+    return price;
+  }
+}
 module.exports = calculateRentalCost;
