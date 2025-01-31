170533680448bd2990951c1f51c32763b2287f7c
Dmytro Vaskiv <dimavaskiv9@gmail.com>
2025-01-31 18:47:49 +0200

    Solution1

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,13 @@
 function calculateRentalCost(days) {
-  let sum = days * 40;
+  const daysPrice = 40;
+  const discountThreeDays = 20;
+  const discountSevenDays = 50;
+  let sum = days * daysPrice;
 
   if (days >= 7) {
-    sum -= 50;
-  } else if (days >= 3) {
-    sum -= 20;
+    sum -= discountSevenDays;
+  } else if (days >= 3 && days < 7) {
+    sum -= discountThreeDays;
   }
 
   return sum;
