2b45059db63d3a2becf7ec300a86c6abcd62a643
AQingZeo <69696315+AQingZeo@users.noreply.github.com>
2025-01-30 19:54:04 -0800

    Update discount

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,8 +5,8 @@
  */
 function calculateRentalCost(days) {
   let discount = 0;
-	if(days>3) discount = 20;
-	if(days>7) discount = 40;
+	if(days>=3) discount = 20;
+	if(days>=7) discount = 50;
 	return days*40-discount;
 }
 
