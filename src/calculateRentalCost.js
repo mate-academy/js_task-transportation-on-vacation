a5b194229690dbbc6d2e22a12e8e300cb43ec5e1
Anna Poplavska <annapoplavska10@gmail.com>
2025-02-01 10:05:37 +0100

    Task done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,22 @@
  *
  * @return {number}
  */
+const BASE_RENTAL_COST = 40;
+const SHORT_TERM_RENT_DAYS = 3;
+const SHORT_TERM_DISCOUNT = 20;
+const LONG_TERM_RENT_DAYS = 7;
+const LONG_TERM_DISCOUNT = 50;
+
 function calculateRentalCost(days) {
-  // write code here
+  let total = days * BASE_RENTAL_COST;
+
+  if (days >= SHORT_TERM_RENT_DAYS && days < LONG_TERM_RENT_DAYS) {
+    total -= SHORT_TERM_DISCOUNT;
+  } else if (days >= LONG_TERM_RENT_DAYS) {
+    total -= LONG_TERM_DISCOUNT;
+  }
+
+  return total;
 }
 
 module.exports = calculateRentalCost;
