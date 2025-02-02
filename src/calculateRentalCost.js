fa7c221ccef395af4c444b14a0441524a26a1cf7
tiblazy <hauntedapply@gmail.com>
2025-02-02 10:27:06 -0300

    added consts

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,16 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  const dailyCarRent = 40;
-  const totalRent = dailyCarRent * days;
+  const CAR_DAILY_RENT = 40;
+  const LONG_TERM = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+  const PRICE = CAR_DAILY_RENT * days;
 
-  if (days > 6) {
-    return totalRent - 50;
-  } else if (days > 2) {
-    return totalRent - 20;
-  }
-
-  return totalRent;
+  return (
+    (days >= LONG_TERM && PRICE - LONG_TERM_DISCOUNT) ||
+    (days >= SHORT_TERM && PRICE - SHORT_TERM_DISCOUNT) ||
+    PRICE
+  );
 }
 
 module.exports = calculateRentalCost;
