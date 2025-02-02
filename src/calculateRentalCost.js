593ee1be2cb3104d527033b8c5a5150cfdace638
tiblazy <hauntedapply@gmail.com>
2025-02-02 10:19:36 -0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,16 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const dailyCarRent = 40;
+  const totalRent = dailyCarRent * days;
+
+  if (days > 6) {
+    return totalRent - 50;
+  } else if (days > 2) {
+    return totalRent - 20;
+  }
+
+  return totalRent;
 }
 
 module.exports = calculateRentalCost;
