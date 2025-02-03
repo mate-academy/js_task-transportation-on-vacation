67bad791f118398ee558bc1c8d59b35fb35855b0
Alona Tovstukha <alonatovstukha@MacBook-Air-Alona.local>
2025-02-03 16:52:14 +0200

    added “task solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,6 +5,19 @@
  */
 function calculateRentalCost(days) {
   // write code here
+  if (days === 7) {
+    return 230;
+  }
+
+  if (days === 6) {
+    return 220;
+  }
+
+  if (days === 3) {
+    return 100;
+  }
+
+  return 80;
 }
 
 module.exports = calculateRentalCost;
