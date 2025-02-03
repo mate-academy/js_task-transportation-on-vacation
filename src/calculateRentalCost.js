6b61c627daf680d7802ce57b63714b8c1f13655e
Paulohsdev <paulo.ph0dev@gmail.com>
2025-02-03 16:14:59 -0300

    add solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -1,10 +1,25 @@
+/* eslint-disable no-const-assign */
 /**
  * @param {number} days
  *
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const PRICE_DAY = 40;
+  const LONG_TERM = 7;
+  const LONG_DISCOUNT = 50;
+  const SHORT_TERM = 3;
+  const SHORT_DISCOUNT = 20;
+
+  const PRICE = days * PRICE_DAY;
+
+  if (days >= LONG_TERM) {
+    return PRICE - LONG_DISCOUNT;
+  } else if (days >= SHORT_TERM) {
+    return PRICE - SHORT_DISCOUNT;
+  }
+
+  return PRICE;
 }
 
 module.exports = calculateRentalCost;
