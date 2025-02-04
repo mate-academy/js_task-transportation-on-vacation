7fba98b3ed5d2cf88bcd34849fd17d3db52a97b4
Gennadiy99 <login841@gmail.com>
2025-02-04 19:38:23 +0100

    finished task

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -3,8 +3,18 @@
  *
  * @return {number}
  */
+
 function calculateRentalCost(days) {
-  // write code here
+  const rent = 40;
+  let discount = 0;
+
+  if (days >= 7) {
+    discount = 50;
+  } else if (days >= 3) {
+    discount = 20;
+  }
+
+  return days * rent - discount;
 }
 
 module.exports = calculateRentalCost;
