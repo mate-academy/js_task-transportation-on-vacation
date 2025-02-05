3629e56505dfa9bb34e2cdb6ebdfd8dc6913b65c
Taras <taras.holotiuk@gmail.com>
2025-02-05 14:43:19 +0200

    create function "calculateRentalCost"

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,18 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const rentalCost = 40;
+  let result = days * rentalCost;
+
+  if (days >= 7) {
+    return (result -= 50);
+  }
+
+  if (days >= 3) {
+    return (result -= 20);
+  }
+
+  return result;
 }
 
 module.exports = calculateRentalCost;
