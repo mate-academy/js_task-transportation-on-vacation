62b9b0403fd86add06ec0fa9506473e465d136a4
apr1ori <vladgolenok4@gmail.com>
2025-02-06 14:07:05 +0100

    sollution 2

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -8,16 +8,16 @@ function calculateRentalCost(days) {
     return days * 40;
   }
 
-  if (days >= 3) {
-    const totalSum = days * 40;
-
-    return totalSum - 20;
-  }
-
   if (days >= 7) {
     const totalSum = days * 40;
 
     return totalSum - 50;
+  }
+
+  if (days >= 3) {
+    const totalSum = days * 40;
+
+    return totalSum - 20;
   }
 }
 
